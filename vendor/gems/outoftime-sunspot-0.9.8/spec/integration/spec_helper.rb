ecca33350b91ec58c4b3f2fcc618adfa1e68f748
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2009-11-18 17:33:28 +0000

    - updating to the "real" sunspot gems, not the "outoftime" ones - removed gem optiflag
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1085 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- vendor/gems/outoftime-sunspot-0.9.8/spec/integration/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-
-Spec::Runner.configure do |config|
-  config.before(:all) do
-    Sunspot.config.solr.url = 'http://localhost:8983/solr'
-  end
-end
--- vendor/gems/outoftime-sunspot-0.9.8/spec/mocks/photo.rb
+++
@@ -1,8 +0,0 @@
-class Photo < MockRecord
-  attr_accessor :caption
-end
-
-Sunspot.setup(Photo) do
-  text :caption
-  boost 0.75
-end
