e336d37b4c82289eaddb37658cb9b7bf3b7f52f4
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2009-12-04 15:37:14 +0000

    search update behavior depends on context - any index action causes contents to update
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1176 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -9,6 +9,8 @@ class BaseController < ResourceController::Base
 
   before_filter :current_search
 
+  before_filter :set_search_update_mode
+
   private
 
   def set_locale
@@ -21,6 +23,16 @@ class BaseController < ResourceController::Base
     query = params[:search]
     query[:page] = params[:page] unless query.nil? || params[:page].nil?
     @search = Search.from_params(query)
+  end
+
+  # Determine the search update mode: will only the search form be changed,
+  # or will the search results be rendered in the content?
+  def set_search_update_mode
+    if action_name == 'index'
+      @search_update_contents = true
+    else
+      @search_update_contents = false
+    end
   end
   
 end
