b458091b88eac86ac24655b0f7decda00f65a1e9
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2009-12-09 11:59:38 +0000

    splitting of segment transcription strings in a helper method (not used yet)
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1239 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,45 @@ module ApplicationHelper
     end
   end
 
+  # Possibly splits segment transcripts into smaller parts.
+  # This method returns an array of the format [ transcript, translation ], both
+  # parts may be broken down into another 2-element array if the segment is split,
+  # or just contain the original strings when no splitting takes place.
+  def split_segment(segment, limit)
+    original_transcript = segment.transcript || ''
+    original_translation = segment.translation || ''
+    if original_transcript.length > limit || original_translation.length > limit
+
+      scan_regexp = /[^.,;]*[.,;]{1}/
+      transcript_parts = original_transcript.strip.scan(scan_regexp)
+      translation_parts = original_translation.strip.scan(scan_regexp)
+
+      # split by word if there are no
+      if transcript_parts.size < 2 || translation_parts.size < 2
+        transcript_parts = original_transcript.strip.scan(/\S+/)
+        translation_parts = original_translation.strip.scan(/\S+/)
+      end
+      transcript = ''
+      translation = ''
+      length = 0
+
+      while ((length < limit) and (!transcript_parts.empty? && !translation_parts.empty?)) do
+        transcript << (transcript_parts.shift + ' ')
+        translation << (translation_parts.shift + ' ')
+        next_transcript_size = transcript.length + (transcript_parts.first || '').length
+        next_translation_size = translation.length + (translation_parts.first || '').length
+        length = (next_transcript_size > next_translation_size) ? next_transcript_size : next_translation_size
+      end
+      
+      # return an array of transcript / translation parts
+      [ [transcript.strip, transcript_parts.join(' ').strip ], [ translation.strip, translation_parts.join(' ').strip ] ]
+
+    else
+
+      # return the originals
+      [ original_transcript.strip, original_translation.strip ]
+
+    end
+  end
+
 end
