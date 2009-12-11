d3113c6bcbd5db5abed3b6342cb1f6dd166df2ad
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2009-12-11 14:35:12 +0000

    add the tape number to segments
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1262 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- db/migrate/20091211134525_sequence_refactoring_for_segments.rb
+++ db/migrate/20091211134525_sequence_refactoring_for_segments.rb
@@ -4,6 +4,7 @@ class SequenceRefactoringForSegments < ActiveRecord::Migration
 
     change_table :segments do |t|
       t.integer :sequence_number
+      t.integer :tape_number
       t.boolean :speaker_change, :default => false
       t.boolean :chapter_change, :default => false
       t.integer :previous_segment_id
@@ -16,6 +17,7 @@ class SequenceRefactoringForSegments < ActiveRecord::Migration
 
     change_table :segments do |t|
       t.remove :sequence_number
+      t.remove :tape_number
       t.remove :speaker_change
       t.remove :chapter_change
       t.remove :previous_segment_id
