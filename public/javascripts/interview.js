68f7c99849ebb5d02df31084dccf51db055fca58
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-02-09 23:14:13 +0000

    update interface
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1476 34bfa2f4-0c52-0410-afca-732f319d8f5a

---
+++ public/javascripts/interview.js
@@ -0,0 +1,43 @@
+
+
+  // blind effect for accordion-container
+  function toggleContainer(parentId, blindToggle) {
+    var cat_index = -1;
+    var id = parentId.sub('-toggle','');
+    for(var i=0; i< containerElems.length; i++) {
+      if (parentId == containerElems[i].id) {
+          cat_index = i;
+      } else {
+          $(containerElems[i].id.sub('-toggle','')).hide();
+          markClosed(containerElems[i]);
+      }
+    }
+    if(cat_index > -1) {
+      var parent = $(parentId);
+      if(parent.hasClassName('closed')) {
+        // show
+        if(blindToggle) {
+          new Effect.BlindDown(id, { duration: 0.4, transition: Effect.Transitions.sinoidal });
+        } else {
+          Element.show(id);
+        }
+        parent.addClassName('open');
+        parent.removeClassName('closed');
+      } else {
+        // hide
+        if(blindToggle) {
+          new Effect.BlindUp(id, { duration: 0.4, transition: Effect.Transitions.sinoidal, afterFinish: markClosed(parent) });
+        } else {
+          Element.hide(id);
+          markClosed(parent);
+        }
+      }
+    }
+  }
+
+  function markClosed(el) {
+      if(el) {
+          el.addClassName('closed');
+          el.removeClassName('open');
+      }
+  }
---
+++ public/javascripts/search.js
@@ -0,0 +1,91 @@
+
+  var categoryChange = false;
+
+  // clearing the fulltext field before submission or editing
+  function clearFulltextField(id) {
+    var elem = $(id);
+    if(elem) {
+      if(elem.value == '<%= t('search_term') %>') {
+        elem.value = '';
+      }
+    }
+  }
+
+  // blind effect for category facets
+  function toggleCategory(id, blindToggle) {
+    var cat_index = -1;
+    for(var i=0; i< categoryElems.length; i++) {
+      if (id == categoryElems[i].id) {
+          cat_index = i;
+      }
+    }
+    if(cat_index > -1) {
+      var parent = $(id.sub(/_query$/,''));
+      if(parent.hasClassName('closed')) {
+        checkFormSubmission(parent.id);
+        // show
+        if(blindToggle) {
+          Effect.BlindDown(id, { duration: 0.4, transition: Effect.Transitions.sinoidal });
+        } else {
+          Element.show(id);
+        }
+        parent.removeClassName('closed');
+        parent.addClassName('open');
+      } else {
+        // hide
+        if(blindToggle) {
+          Effect.BlindUp(id, { duration: 0.4, transition: Effect.Transitions.sinoidal, afterFinish: markClosed(parent) });
+        } else {
+          Element.hide(id);
+          markClosed(parent);
+        }
+      }
+    }
+  }
+
+  // check for form submission
+  function checkFormSubmission(openCategory) {
+    if(categoryChange) {
+      if(openCategory) {
+        $('open_category').value = openCategory;
+      }
+      submitViaAjax();
+    }
+  }
+
+  // closing happens after the blind effect finishes...
+  function markClosed(elem) {
+    elem.removeClassName('open');
+    elem.addClassName('closed');
+    checkFormSubmission(false);
+  }
+
+  function categoryState(id, state, checkbox) {
+      var input = $(id);
+      if(checkbox) {
+          input.checked = state;
+      } else {
+          input.value = '';
+      }
+  }
+
+  checkedCategories.each(function(id) {
+      categoryState(id, true, true);
+  });
+
+  function submitViaAjax(){
+    beforeSubmit();
+    Form.request('search_facets');
+  }
+
+  function beforeSubmit() {
+    categoryChange = false;
+    clearFulltextField('searchInput');
+    $('search_facets_overlay').show();
+    $('search_header_overlay').show();
+  }
+
+  function afterResponse() {
+    $('search_header_overlay').hide();
+    $('search_facets_overlay').hide();
+  }
