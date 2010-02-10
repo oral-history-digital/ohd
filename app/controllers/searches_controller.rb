8eed9bb1939718228198818bcfe595edc2f9a65d
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-02-10 10:06:34 +0000

    render search results in ANY case
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1512 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -15,9 +15,11 @@ class SearchesController < BaseController
     end
     wants.js do
       puts "\n\n@@@ SEARCH open category: #{@search.open_category}"
+      results_html = render_to_string({ :template => '/interviews/index.html', :layout => false })
       service_html = render_to_string({ :partial => '/searches/search.html', :object => @search })
       search_facets_html = render_to_string({ :partial => '/searches/facets.html', :object => @search })
       render :update do |page|
+        page.replace_html 'innerContent', results_html
         page.replace_html 'baseServices', service_html
         page.replace_html 'baseContainerRight', search_facets_html
       end
