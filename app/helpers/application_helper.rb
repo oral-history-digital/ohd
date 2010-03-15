685bc6e809a48a35a782df13665d1c5c930ce5ac
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-03-15 22:16:52 +0000

    use segment.start_time in segemnt_links
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1736 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,7 +25,7 @@ module ApplicationHelper
   def link_to_segment(segment, match_text='', show_segment_text=false, ajax=false)
     interview = segment.interview
     item = segment.tape.number
-    position = Timecode.new(segment.raw_timecode).time.to_i
+    position = segment.start_time.round
     link_text = show_segment_text ? content_tag(:span, "#{segment.timecode}", :class => :timecode) + truncate(segment_excerpt_for_match(segment, match_text), :length => 300) : "Zum Interview-Ausschnitt"
     if @object.is_a?(Interview) || ajax
       link_to link_text, "javascript:currentPlayer.seek(#{item-1},#{position});"
