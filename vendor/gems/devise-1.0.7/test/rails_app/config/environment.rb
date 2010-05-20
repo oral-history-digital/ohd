e9e3e7f6489828b2849e913a347d7add7f742fcf
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-05-20 17:01:03 +0000

    add devise and warden in rails 2.3.5 specific gem versions
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1902 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- vendor/gems/devise-1.0.7/test/rails_app/config/environment.rb
+++ vendor/gems/devise-1.0.7/test/rails_app/config/environment.rb
@@ -2,6 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+DEVISE_ORM = :active_record unless defined? DEVISE_ORM
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -12,7 +13,7 @@ Rails::Initializer.run do |config|
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.load_paths += [ "#{RAILS_ROOT}/app/#{DEVISE_ORM}/" ]
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
@@ -20,29 +21,13 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'rsolr'
-  config.gem 'rsolr-ext'
-  config.gem 'escape'
-  config.gem 'daemons' # for sunspot
-  config.gem 'sunspot', :lib => 'sunspot', :version => ">=0.10.8"
-  config.gem 'sunspot_rails', :lib => 'sunspot/rails', :version => ">=0.11.5"
-
-  config.gem 'fastercsv'
-
-  config.gem 'smurf'
-
-  config.gem 'unicode'
-
-  config.gem 'warden', :version => '0.10.5'
-  config.gem 'devise', :version => '1.0.7'
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_record ] unless DEVISE_ORM == :active_record
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -53,5 +38,5 @@ Rails::Initializer.run do |config|
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  config.i18n.default_locale = :de
+  # config.i18n.default_locale = :en
 end
