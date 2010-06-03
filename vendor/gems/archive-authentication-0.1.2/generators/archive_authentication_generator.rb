cf0e7de87e8081407ae73d8396e888b2edd3917a
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-06-03 06:51:50 +0000

    more form localizations for the gem that adapts devise
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@1925 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- vendor/gems/archive-authentication-0.1.2/generators/archive_authentication_generator.rb
+++ vendor/gems/archive-authentication-0.1.2/generators/archive_authentication_generator.rb
@@ -15,14 +15,8 @@ class ArchiveAuthenticationGenerator < Rails::Generator::NamedBase
           Dir.glob(File.join(gem_views_dir, '**', '*.erb')).each do |view_file|
             relative_view_path = view_file.sub(gem_views_dir, '')
             puts relative_view_path
-            FileUtils.cp view_file, File.join(app_views_dir, relative_view_path), :preserve => true
-          end
-          puts "Installing tasks:"
-          gem_task_dir = File.join(File.dirname(__FILE__), '../tasks')
-          app_task_dir = File.join(RAILS_ROOT, 'lib/tasks')
-          Dir[File.join(gem_task_dir, '*.rake')].each do |task_file|
-            puts task_file.split('/').last
-            FileUtils.cp task_file, app_task_dir, :preserve => true
+            FileUtils.mkdir_p File.join(app_views_dir, (relative_view_path.split('/') - [ relative_view_path.split('/').last ]).join('/'))
+            FileUtils.cp_r view_file, File.join(app_views_dir, relative_view_path), :preserve => true
           end
           puts "done."
       end
