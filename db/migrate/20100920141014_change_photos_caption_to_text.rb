ec378e2fc12614d174d240c6e3ab642564987b2c
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-09-20 18:13:07 +0000

    photos caption *must* be text, not string, just as in "Redaktionssystem"
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@2251 34bfa2f4-0c52-0410-afca-732f319d8f5a

---
+++ db/migrate/20100920141014_change_photos_caption_to_text.rb
@@ -0,0 +1,25 @@
+class ChangePhotosCaptionToText < ActiveRecord::Migration
+
+  def self.up
+
+    add_column :photos, :caption_string, :string
+
+    Photo.update_all "caption_string = caption"
+
+    remove_column :photos, :caption
+
+    add_column :photos, :caption, :text
+
+    Photo.update_all "caption = caption_string"
+
+    remove_column :photos, :caption_string
+
+  end
+
+  def self.down
+
+    # don't reverse the changes as the text field is backwards-compatible
+
+  end
+
+end
