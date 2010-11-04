04eeba321f9455572e6b141729b47c033081f377
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-11-04 16:57:58 +0000

    clean up migration dependencies - app / archive-authentication gem
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@2350 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- db/migrate/20100623105044_add_admin_comments_to_user_registration.rb
+++ db/migrate/20100623105044_add_admin_comments_to_user_registration.rb
@@ -2,6 +2,9 @@ class AddAdminCommentsToUserRegistration < ActiveRecord::Migration
 
   def self.up
 
+=begin
+    # This was merged into the archive-authentication migration
+    # for user_registrations
     change_table :user_registrations do |t|
       t.string  :admin_comments
       t.integer :user_account_id
@@ -9,6 +12,7 @@ class AddAdminCommentsToUserRegistration < ActiveRecord::Migration
       t.remove :approved_at
       t.datetime  :processed_at
     end
+=end
 
     change_table  :users do |t|
       t.integer :user_account_id
@@ -19,11 +23,13 @@ class AddAdminCommentsToUserRegistration < ActiveRecord::Migration
   end
 
   def self.down
+=begin
     remove_column :user_registrations, :admin_comments
     remove_column :user_registrations, :user_account_id
     remove_column :user_registrations, :login
     remove_column :user_registrations, :processed_at
     add_column  :user_registrations, :approved_at, :datetime
+=end
 
     remove_column :users, :user_account_id
     remove_column :users, :user_registration_id
