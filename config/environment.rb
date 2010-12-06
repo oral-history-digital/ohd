85390823922650c796b76ef0b57e5eed0c34fc54
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2010-12-06 20:28:56 +0000

    complete gem dependencies (archive-authorization)
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@2384 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+AUTHORIZATION_MIXIN = "items and groups"
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -38,6 +40,7 @@ Rails::Initializer.run do |config|
   config.gem 'devise', :version => '1.0.8'
 
   config.gem 'archive-authentication'
+  config.gem 'archive-authorization'
 
   config.gem 'rack-maintenance'
 
