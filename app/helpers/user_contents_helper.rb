f1835a723065df3a1b1c1aa21e02a723a58d5025
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2011-10-19 09:04:23 +0000

    add the submit button for textarea updates
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3031 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/user_contents_helper.rb
+++ app/helpers/user_contents_helper.rb
@@ -52,7 +52,8 @@ module UserContentsHelper
     html << content_tag(:span, options.merge({:id => form_id, :style => 'display: none;'})) do
       form_remote_tag(form_options) do
         if(text_area)
-          text_area_tag(user_content, attribute.to_sym, :id => id, :name => "user_content[#{attribute}]", :onclick => "Event.stop(event);")
+          text_area_tag(user_content, attribute.to_sym, :id => id, :name => "user_content[#{attribute}]", :onclick => "Event.stop(event);") \
+          + submit_tag(t(:update, :scope => 'user_interface.actions'))
         else
           text_field_tag(user_content, attribute.to_sym, :id => id, :name => "user_content[#{attribute}]", :onclick => "Event.stop(event);")
         end
