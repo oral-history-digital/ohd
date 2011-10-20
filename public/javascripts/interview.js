c6ce79decfd19c4978d3c83ccafc22baedb4095f
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2011-10-20 16:01:11 +0000

    replaced filters with working HTML/JS - no functionality yet!
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3037 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- public/javascripts/interview.js
+++ public/javascripts/interview.js
@@ -1,46 +1,4 @@
 
-
-  // blind effect for accordion-container
-  function toggleContainer(parentId, blindToggle) {
-    var cat_index = -1;
-    var id = parentId.sub('-toggle','');
-    for(var i=0; i< containerElems.length; i++) {
-      if (parentId == containerElems[i].id) {
-          cat_index = i;
-      } else {
-          $(containerElems[i].id.sub('-toggle','')).hide();
-          markClosed(containerElems[i]);
-      }
-    }
-    if(cat_index > -1) {
-      var parent = $(parentId);
-      if(parent.hasClassName('closed')) {
-        // show
-        if(blindToggle) {
-          new Effect.BlindDown(id, { duration: 0.8, transition: Effect.Transitions.sinoidal });
-        } else {
-          Element.show(id);
-        }
-        parent.addClassName('open');
-        parent.removeClassName('closed');
-      } else {
-        // hide
-        if(blindToggle) {
-          new Effect.BlindUp(id, { duration: 0.5, transition: Effect.Transitions.sinoidal, afterFinish: markClosed(parent) });
-        } else {
-          Element.hide(id);
-          markClosed(parent);
-        }
-      }
-    }
-  }
-
-  function openContainer(parentId, blindToggle) {
-        if($(parentId).hasClassName('closed')) {
-            toggleContainer(parentId, blindToggle);
-        }
-  }
-
   function markClosed(el) {
       if(el) {
           el.addClassName('closed');
