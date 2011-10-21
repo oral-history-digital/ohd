2627a0ffe0a8328a238ca5b04917b2b0c202858e
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2011-10-21 15:34:07 +0000

    invert filter selections on click on last checkbox
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3044 34bfa2f4-0c52-0410-afca-732f319d8f5a

---
+++ public/javascripts/workbook_filters.js
@@ -0,0 +1,48 @@
+/* JS-API for the workbook filter effects and functionality */
+
+  function invertSelection(elem) {
+    var selection = elem.parentNode.getElementsBySelector('.facet');
+    var changed = false;
+    selection.each(function(facet){
+        var input = facet.down('.checkbox');
+        if(facet.hasClassName('checked')) {
+          if($$('#content_filters .checked').length > 1) {
+              input.checked = false;
+              facet.removeClassName('checked');
+              changed = true;
+          }
+        } else {
+          input.checked = true;
+          facet.addClassName('checked');
+          changed = true;
+        }
+    });
+    return changed;
+  }
+
+  function checkFilter(id, labelElement) {
+      var input = $(id);
+      var item = labelElement.hasClassName('facet') ? labelElement : labelElement.up('.facet');
+      if((input) && (item)) {
+        /*
+          var changed = false;
+          if(item.hasClassName('checked')) {
+              if($$('#content_filters .checked').length > 1) {
+                  input.checked = false;
+                  item.removeClassName('checked');
+                  changed = true;
+              }
+          } else {
+              input.checked = true;
+              item.addClassName('checked');
+              changed = true;
+          }
+        */
+          if(!invertSelection(input)) {
+            // invert all checkboxes
+            invertSelection(item);
+          }
+          input.up('form').request();
+          new Effect.Appear('overlay', { duration: 0.3, to: 0.9, queue: 'front' });
+      }
+  }
