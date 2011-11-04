1cf20f05ae7d41d6e5cfafa5cb4bf40650fcfd03
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2011-11-04 14:26:26 +0000

    apply iconified submit and reset buttons to all inline editing
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3075 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/user_contents_helper.rb
+++ app/helpers/user_contents_helper.rb
@@ -50,7 +50,8 @@ module UserContentsHelper
             :id => form_id,
             :url => user_content_path(user_content),
             :method => :put,
-            :before => "$('#{id + '_interface_status'}').value = ($('user_content_#{user_content.id}').hasClassName('closed') ? 'closed' : '');",
+            :before => "togglingContent = 1; $('#{id + '_interface_status'}').value = ($('user_content_#{user_content.id}').hasClassName('closed') ? 'closed' : '');",
+            :complete => "togglingContent = 0;",
             :html => options.merge({:class => 'inline'})
     })
     js_onclick = "var displayEl = $('#{display_id}'); var fieldWidth = displayEl.offsetWidth - 20; "
@@ -58,16 +59,19 @@ module UserContentsHelper
     js_onclick << "displayEl.hide();$('#{id}').value = displayEl.innerHTML; $('#{form_id}').show(); $('#{id}').setStyle({width: fieldWidth + 'px'"
     js_onclick << ", height: (fieldHeight + 25) + 'px'" if text_area
     js_onclick << "}); Event.stop(event);"
+    js_reset = "$('#{form_id}').hide(); $('#{display_id}').show(); Event.stop(event);"
     html = content_tag(:span, value, options.merge({:id => display_id, :class => "inline-editable", :onclick => js_onclick}))
     html << content_tag((text_area ? :div : :span), options.merge({:id => form_id, :style => 'display: none;'})) do
       form_remote_tag(form_options) do
         form_html = hidden_field_tag :interface_status, 'open', :id => id + '_interface_status'
         form_html += if(text_area)
           text_area_tag(user_content, user_content.send(attribute.to_sym), :id => id, :name => "user_content[#{attribute}]", :onclick => "Event.stop(event);") \
-          + submit_tag(t(:update, :scope => 'user_interface.actions'))
         else
           text_field_tag(user_content, user_content.send(attribute.to_sym), :id => id, :name => "user_content[#{attribute}]", :onclick => "Event.stop(event);")
         end
+        buttons_html = submit_tag(submit_text = t(:update, :scope => 'user_interface.actions'), :title => submit_text,:class => "update", :onclick => "togglingContent = 1;")
+        buttons_html += "<input type='reset' name='#{user_content.id}_#{attribute}_reset' title='#{t(:reset, :scope => 'user_interface.actions')}' onClick=\"#{js_reset}\" class='reset'/>"
+        form_html + buttons_html
       end
     end
     html
