ed76f9c8611f2fa6ae4107f4341c04f051e42e11
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-02-02 17:15:14 +0000

    add import job to cron task
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3537 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- script/cron_daily.rb
+++ script/cron_daily.rb
@@ -1,4 +1,40 @@
 #!/opt/ruby-enterprise-1.8.7-2010.02/bin/ruby
 
-File.open("/tmp/hello.txt", "w") { |fp| fp.puts "Hello, world" }
+require 'rubygems'
+require 'open4'
+
+class SimpleScriptLogger
+
+  LOG_FILE = File.join(File.dirname(__FILE__), '../log', "cron-error.log")
+
+  def initialize
+    File.open(LOG_FILE,"a"){|f|}
+  end
+
+  def log(msg)
+    system "echo '[#{Time.now.strftime('%d.%m.%Y %H:%M')}]#{msg.gsub("'","")}' >> #{LOG_FILE}"
+    puts msg
+  end
+
+end
+
+@logger = SimpleScriptLogger.new
+
+
+def run_as_process(name, call)
+  begin
+    Open4::popen4("cd #{File.join(File.dirname(__FILE__),'..')} && #{call}") do |pid, stdin, stdout, stderr|
+      stdout.each_line {|line| puts line}
+      errors = []
+      stderr.each_line {|line| errors << line unless line.empty?}
+      @logger.log "\n#{name} - FEHLER:\n#{errors.join("")}" unless errors.empty?
+    end
+  rescue Exception => e
+    @logger.log "\n#{name} - ABORTED: #{e.message}\n#{e.respond_to?('backtrace') ? (e.backtrace || '') : ''}"
+    STDOUT.flush
+  end
+end
+
+# Import up to 50 interviews
+run_as_process 'Datenimport', 'rake xml_import:limited number=50 -t'
 
