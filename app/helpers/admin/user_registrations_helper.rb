371d9c94c741e3c02277ea230d294402fbbf491c
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-04-15 14:30:05 +0000

    complete user edit form
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3580 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/admin/user_registrations_helper.rb
+++ app/helpers/admin/user_registrations_helper.rb
@@ -2,7 +2,6 @@ module Admin::UserRegistrationsHelper
 
   def form_edit_field_for(user, attribute, options={})
     value = user.send(attribute)
-    value = '&nbsp;' * 8 if value.blank?
     id = "user_#{user.id}_#{attribute}"
     display_id = id + '_display'
     field_id = id + '_field'
@@ -16,11 +15,26 @@ module Admin::UserRegistrationsHelper
     else
       text_field_tag(user, user.send(attribute.to_sym), :id => id, :name => "user[#{attribute}]", :class => 'editor', :onclick => "Event.stop(event)")
     end
-    html = content_tag(:span, value, options.merge({:id => display_id, :class => "inline-editable", :onclick => "var display = $('#{display_id}'); display.hide(); var field = $('#{field_id}'); var element = field.down('select, input, textarea'); element.disabled = false; element.value = display.innerHTML; field.show(); $('user_registration_data_actions').show(); Event.stop(event);"})) # Event.stop(event)
+    html = content_tag(:span, value, options.merge({:id => display_id, :class => "inline-editable", :onclick => "var display = $('#{display_id}'); display.hide(); var field = $('#{field_id}'); var element = field.down('select, input, textarea'); element.disabled = false; element.value = display.innerHTML; field.show(); $('user_registration_data_actions').show(); element.focus(); Event.stop(event);"})) # Event.stop(event)
     html << content_tag((text_area ? :div : :span), options.merge({:id => field_id, :class => 'inline-editor', :style => 'display: none;'})) do
       form_html
     end
     html
+  end
+
+  def boolean_field_for(user, method)
+    value = user.send(method)
+    checked = case value
+                when true, false
+                  value
+                when String
+                  value.to_i > 0
+                when Numeric
+                  value > 0
+                else
+                  false
+              end
+    "checked = #{value.inspect}" + check_box_tag("user[#{method}]", 1, checked)
   end
 
 end
