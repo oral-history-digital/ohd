78cf72496e19ff19b248c0ded468918407db0d8c
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-05-30 10:37:39 +0000

    refactoring of time formatting, removed possible workbook link in admin area and improved default routes in admin
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3604 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,13 +23,13 @@ class Admin::UsersController < Admin::BaseController
     %w(first_name last_name login).each do |param|
       filter = params[param]
       unless filter.blank?
-        conditionals << "#{param == 'login' ? 'user_accounts.' : ''}#{param} LIKE ?"
+        conditionals << "#{param == 'login' ? 'user_accounts.' : 'users.'}#{param} LIKE ?"
         condition_args << (filter + '%')
       end
     end
     joins = "LEFT JOIN user_accounts ON users.user_account_id = user_accounts.id RIGHT JOIN user_registrations ON users.user_registration_id = user_registrations.id"
     conditions = ["workflow_state = 'registered' AND (" + conditionals.join(' OR ') + ")"] + condition_args
-    @users = User.find(:all, :joins => joins, :conditions => conditions, :order => "admin ASC, last_name ASC")
+    @users = User.find(:all, :joins => joins, :conditions => conditions, :order => "admin ASC, users.last_name ASC")
     respond_to do |format|
       format.html do
       end
