63deeaac9949874809ad3d17513e12153fd4d07d
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-06-17 17:28:29 +0000

    WIP: user_annotations as user_content
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@3650 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,8 +1,14 @@
 class Annotation < ActiveRecord::Base
 
   belongs_to :interview
+  # this is only true for editorial annotations
+  # user annotations are linked to the segment directly, to circumvent the
+  # automatic deletion on an archive import.
 
   named_scope :for_segment, lambda{|segment| { :conditions => ["media_id > ?", Segment.for_media_id(segment.media_id.sub(/\d{4}$/,(segment.media_id[/\d{4}$/].to_i-1).to_s.rjust(4,'0'))).first.media_id], :order => "media_id ASC", :limit => 1 }}
+
+  # displayable user and editorial annotations per interview
+  named_scope :displayable, lambda{|interview| { :conditions => ["media_id LIKE ?", interview.archive_id.upcase.concat('%')]}}
 
   def start_time
     Timecode.new(timecode).time
