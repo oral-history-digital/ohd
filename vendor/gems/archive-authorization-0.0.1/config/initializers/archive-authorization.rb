8d198f9cc11bde31b5340f3227467bc254615f4b
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-10-07 21:17:42 +0000

    removed vendored plugins and gems
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@3915 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- vendor/gems/archive-authorization-0.0.1/config/initializers/archive-authorization.rb
+++
@@ -1,8 +0,0 @@
-# sets the authorization logic library to use.
-
-# 1. 'hardwired': direct association between users and roles
-# 2. 'object roles': directly associated roles or roles associated via an object
-# 3. 'items and groups': roles associated via both group and item
-# 4. 'task and groups': roles associated via a task on an item or a group
-
-AUTHORIZATION_MIXIN = "items and groups"
