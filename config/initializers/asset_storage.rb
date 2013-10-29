d6338a04212bf48de29e85406639a3342cf6fdc3
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-10-29 01:36:16 +0000

    Remove environment-specific storage location.
    NB: Please mount the remote storage to its default location instead.
    
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@3970 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- config/initializers/asset_storage.rb
+++ config/initializers/asset_storage.rb
@@ -1,17 +1,13 @@
+# Please mount the ZWAR storage server locally like this (read-only for normal users!):
+# /etc/fstab:
+#     //eaz-diga.cedis.fu-berlin.de/data /mnt/eaz-diga.cedis.fu-berlin.de/data cifs noauto,sec=ntlmv2,credentials=/etc/cifs.credentials,uid=root,gid=root,nosetuids,file_mode=0644,dir_mode=0755 0 0
+# /etc/cifs.credentials (create the file with permissions 600 and enter your CIFS credentials)
+#     username=...
+#     password=...
+# sudo mkdir -p /mnt/eaz-diga.cedis.fu-berlin.de/data
+
 # This sets the storage server location for asset and media files
 ARCHIVE_MANAGEMENT_DIR = 'redaktionssystem'
-
 REPOSITORY_DIR = 'archiv_dis'
-
-if Rails.env == 'production'
-  ActiveRecord.path_to_storage = '/mnt/eaz-diga.cedis.fu-berlin.de/data/archiv_backup'
-  ActiveRecord.path_to_photo_storage = '/mnt/eaz-diga.cedis.fu-berlin.de/data/archiv_backup/bilder'
-else
-
-
-  # Mac OS X
-  ActiveRecord.path_to_storage = '/home/fgrandel/Development/fub/zwar_daten/archiv_backup' #'/home/jrietema/dev/platform/data/'
-  ActiveRecord.path_to_photo_storage = '/home/fgrandel/Development/fub/zwar_daten/archiv_backup/bilder' #'/share/Z/FOTO_MASTER'
-  # ActiveRecord.path_to_storage = (RUBY_PLATFORM.include?('mswin32') ? 'Z:/archiv_backup/archiv_dis' : '/share/Z/archiv_backup/archiv_dis')
-  # ActiveRecord.path_to_photo_storage = '/Volumes/data/archiv_backup/bilder/FOTOS_SEZ_META_renamed'
-end
+ActiveRecord.path_to_storage = '/mnt/eaz-diga.cedis.fu-berlin.de/data/archiv_backup'
+ActiveRecord.path_to_photo_storage = '/mnt/eaz-diga.cedis.fu-berlin.de/data/archiv_backup/bilder'
