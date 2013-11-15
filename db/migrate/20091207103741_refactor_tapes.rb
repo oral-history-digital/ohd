ed0bf2d776331d58fe6582d7977310da4379a4be
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-11-15 16:24:47 +0000

    move obsolete heading class to migration where it's still used
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@3998 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- db/migrate/20091207103741_refactor_tapes.rb
+++ db/migrate/20091207103741_refactor_tapes.rb
@@ -1,3 +1,17 @@
+class Heading < ActiveRecord::Base
+
+  belongs_to :segment
+  belongs_to :tape
+
+  # only one heading per type per segment
+  validates_uniqueness_of :segment_id, :scope => [ :mainheading ]
+
+  def to_s
+    title
+  end
+
+end
+
 class RefactorTapes < ActiveRecord::Migration
   def self.up
     remove_column(:headings, :timecode)
