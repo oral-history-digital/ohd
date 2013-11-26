cdcb502c3551648ea7db4088c493ca3cbcf3f030
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2013-11-26 03:08:23 +0000

    emphasize speakers in captions
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@4036 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/helpers/interview_helper.rb
+++ app/helpers/interview_helper.rb
@@ -85,7 +85,7 @@ module InterviewHelper
   end
 
   def format_transcript(text)
-    h(text).gsub(/~([^~]*)~/,'<em>\1</em>').gsub(/\s+([.,?;])/,'\1').strip
+    h(text).gsub(/[~*]([^~*]*)[~*]/,'<em>\1</em>').gsub(/\s+([.,?;])/,'\1').strip
   end
 
   def location_to_param(name)
