2d91eb2818c9f21194ba420e5407243682802bab
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2014-01-10 02:44:46 +0000

    upgrade rails from 2.3.17 to 2.3.18
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/trunk@4224 34bfa2f4-0c52-0410-afca-732f319d8f5a

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'fccad7ae7124e81bfbf18487a0dc98e893bb04e81d19e316aff2413c6a4b6265352018d796c3c7e360b4108f8ede7765ced38704292b4e5b221babbfea15caa7';
