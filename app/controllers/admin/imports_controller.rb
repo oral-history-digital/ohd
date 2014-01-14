497876eb76407db64d74d56b9ddec16add537f25
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2014-01-14 18:43:30 +0000

    fix errors in imports list
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@4265 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -24,7 +24,10 @@ class Admin::ImportsController < Admin::BaseController
   private
 
   def collection
-    @interviews = ActiveRecord::Base.connection.select_all("SELECT archive_id, first_name, other_first_names, last_name, name_affix, researched, still_image_file_name, importable_id, time, imports.created_at FROM interviews INNER JOIN interview_translations ON interviews.id = interview_translations.interview_id AND interview_translations.locale = 'de' LEFT JOIN imports ON imports.id = (SELECT imports.id FROM imports WHERE imports.importable_id = interviews.id AND imports.importable_type = 'Interview' ORDER BY time DESC LIMIT 0,1)")
+    @interviews = ActiveRecord::Base.connection.select_all("SELECT archive_id, first_name, last_name, researched, still_image_file_name, importable_id, time, imports.created_at FROM interviews INNER JOIN interview_translations ON interviews.id = interview_translations.interview_id AND interview_translations.locale = 'de' LEFT JOIN imports ON imports.id = (SELECT imports.id FROM imports WHERE imports.importable_id = interviews.id AND imports.importable_type = 'Interview' ORDER BY time DESC LIMIT 0,1)")
+    @interviews.map do |interview|
+      interview['short_title'] = [interview['first_name'], interview['last_name']].join(' ')
+    end
   end
 
 end
