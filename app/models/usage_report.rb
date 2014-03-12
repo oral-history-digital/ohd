b0c0492863b80728032f6020b17a99122a8a974f
jrietema <jrietema@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2014-03-12 01:25:41 +0000

    logins report completed (print data version)
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@4369 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- app/models/usage_report.rb
+++ app/models/usage_report.rb
@@ -2,8 +2,10 @@ class UsageReport < ActiveRecord::Base
 
   belongs_to :user_account
 
+  LOGIN_ACTION = 'SessionsController#create'
+
   TRACKED_ACTIONS = [
-      'SessionsController#create',
+      LOGIN_ACTION,
       'InterviewsController#show',
       'SearchesController#query',
       'LocationReferencesController#map_frame'
@@ -13,9 +15,12 @@ class UsageReport < ActiveRecord::Base
   validates_presence_of :action
   validates_inclusion_of :action, :in => TRACKED_ACTIONS
 
+  named_scope :logged_in_month, lambda{|date| {:conditions => {:logged_at => date.beginning_of_month..date.end_of_month}}}
+  named_scope :logins, {:conditions => {:action => LOGIN_ACTION}}
+
   def validate
     case action
-      when 'SessionsController#create'
+      when LOGIN_ACTION
         validate_user_account
       when 'InterviewsController#show'
         validate_archive_id
@@ -68,6 +73,83 @@ class UsageReport < ActiveRecord::Base
     [ UsageReport.first(:order => "logged_at ASC", :conditions => "logged_at LIKE '%-01 %'").logged_at,
       (UsageReport.last(:order => "logged_at ASC", :conditions => "logged_at LIKE '%-01 %'").logged_at - 1.month)
     ]
+  end
+
+  def self.create_logins_report(date)
+    require 'net/http'
+    require 'uri'
+    geolocation_base = 'http://freegeoip.net/csv'
+    resolved_ips = {}
+    login_reports = UsageReport.logged_in_month(date).logins.scoped({:include => :user_account})
+    timeframes = [:total,0..999,0..1,1..2,2..3,4..6,6..12,12..999]
+    logins = {:total => 0, :users => []}
+    logins_per_country = {nil => {:total => 0, :users => []}}
+    timeframes.each{|t| logins[t] = 0}
+    login_reports.each do |r|
+      logins[:total] += 1
+
+      # check user country or IP geolocation
+      # add to country row and specific_range column
+      # use freegeoip.net/csv/{ip} - "ip","DE","Germany"
+      country = nil
+      if r.user_account && r.user_account.user
+        country = r.user_account.user.country.upcase
+      else
+        if resolved_ips.keys.include?(r.ip)
+          country = resolved_ips[r.ip]
+        else
+          geolocation = URI.parse([geolocation_base, r.ip].join('/'))
+          res = Net::HTTP.get_response(geolocation)
+          if(res.is_a?(Net::HTTPSuccess))
+            response = (res.body || '').split(',')
+            if response.is_a?(Array) && response.first == r.ip
+              country = response[1]
+              resolved_ips[r.ip] = country
+            end
+          end
+        end
+      end
+      logins_per_country[country] ||= {:total => 0, :users => []}
+      logins_per_country[country][:total] += 1
+
+      # data per timeframe columns
+      if r.user_account
+        time = (r.logged_at - (r.user_account.confirmed_at || r.user_account.confirmation_sent_at)) / 1.month
+        timeframes.each do |range|
+          next unless range.is_a?(Range)
+          if time >= range.first && time < range.last
+            logins[range] += 1
+            logins_per_country[country][range] ||= 0
+            logins_per_country[country][range] += 1
+          end
+        end
+        # add unique users
+        unless logins[:users].include?(r.user_account_id)
+          logins[:users] << r.user_account_id
+        end
+        unless logins_per_country[country][:users].include?(r.user_account_id)
+          logins_per_country[country][:users] << r.user_account_id
+        end
+      end
+
+    end
+    sorted_countries = []
+    logins_per_country.keys.compact.each do |country|
+      sorted_countries << [country, logins_per_country[country][:total]]
+    end
+    sorted_countries.sort!{|a,b| b.last <=> a.last }
+
+    puts "Logins per Country (#{resolved_ips.keys.size} geolocation calls)"
+    puts (['Ctry  ', 'unique '.rjust(6)] + timeframes.map{|r| r.to_s.rjust(6,' ')}).join("|")
+    puts (['all   ', logins[:users].size.to_s.rjust(6)] + timeframes.map{|r| logins[r].to_s.rjust(6,' ')}).join("|")
+    ([[nil, 0]] + sorted_countries).each do |entry|
+      country = entry.first
+      puts ([(country || '?').ljust(6,' '), logins_per_country[country][:users].size.to_s.rjust(6)] + timeframes.map{|r|
+        (logins_per_country[country][r] || 0).to_s.rjust(6,' ')
+      }).join("|")
+    end
+
+
   end
 
   protected
