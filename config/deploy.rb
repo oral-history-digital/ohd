0a8fc08302e5c32526d89a3d3644a29e091d39a8
jerico <jerico@34bfa2f4-0c52-0410-afca-732f319d8f5a>
2014-07-01 10:22:57 +0000

    simplify and unify deployment script
    git-svn-id: https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration@4561 34bfa2f4-0c52-0410-afca-732f319d8f5a

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,84 +1,52 @@
 require 'bundler/capistrano'
 
-set :application, "zwar_archiv"
-set :repository,  "https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration"
-set :scm, "subversion"
+set :repository, 'https://dev.cedis.fu-berlin.de/svn/eaz/zwar_archive/branches/integration'
+set :environment, :production
+set :scm, 'subversion'
 set :use_sudo, false
 
-set :deploy_to, "/data/applications/zwar/#{application}"
 set :deploy_via, :remote_cache
-set :copy_exclude, [".git/*", ".svn/*", ".DS_Store", "deploy.rb", "production.rb", "Capfile", "database.yml", "solr.yml", "sunspot.yml"]
-set :bundle_dir, "/data/bundle/01"
+set :copy_exclude, ['.git/*', '.svn/*', '.DS_Store', 'deploy.rb', 'production.rb', 'Capfile', 'database.yml', 'solr.yml', 'sunspot.yml']
+set :bundle_dir, '/data/bundle/01'
 
-set :environment, :undefined
+set :keep_releases, 6
 
-set :keep_releases, 3
-
-desc "prepare to act on the production environment"
+desc 'prepare to act on the production environment'
 task :production do
-  set :environment, :production
-  set :application, 'zwar_archiv'
-  set :keep_releases, 7
+  set :application, :zwar_archiv
+  set :project, :zwar
   set :deploy_to, "/data/applications/#{application}"
-  role :app, "da01.cedis.fu-berlin.de"
-  role :web, "da01.cedis.fu-berlin.de"
-  role :db,  "da01.cedis.fu-berlin.de", :primary => true
+  role :app, 'da01.cedis.fu-berlin.de'
+  role :web, 'da01.cedis.fu-berlin.de'
+  role :db, 'da01.cedis.fu-berlin.de', :primary => true
 end
 
-desc "prepare to act on the demo server"
-task :demo do
-  set :environment, :demo
-  set :application, 'demo'
-  set :keep_releases, 3
-  set :deploy_to, "/data/applications/zwar/#{application}"
-  role :app, "fnf.cedis.fu-berlin.de"
-  role :web, "fnf.cedis.fu-berlin.de"
-  role :db, "fnf.cedis.fu-berlin.de"
-end
-
-desc "prepare to act on the staging environment (ZWAR)"
+desc 'prepare to act on the staging environment (ZWAR)'
 task :zwar_staging do
-  set :environment, :production
-  set :application, 'zwar_archiv'
-  set :keep_releases, 3
+  set :application, :zwar_archiv
+  set :project, :zwar
   set :deploy_to, "/data/applications/staging/#{application}"
-  role :app, "160.45.170.50"
-  role :web, "160.45.170.50"
-  role :db,  "160.45.170.50", :primary => true
+  role :app, '160.45.170.50'
+  role :web, '160.45.170.50'
+  role :db, '160.45.170.50', :primary => true
 end
 
-desc "prepare to act on the staging environment (Hagen)"
+desc 'prepare to act on the staging environment (Hagen)'
 task :hagen_staging do
-  set :environment, :production
-  set :application, 'hagen_archiv'
-  set :keep_releases, 3
+  set :application, :hagen_archiv
+  set :project, :hagen
   set :deploy_to, "/data/applications/staging/#{application}"
-  role :app, "160.45.170.50"
-  role :web, "160.45.170.50"
-  role :db,  "160.45.170.50", :primary => true
-end
-
-desc "prepare to act on the test environment (shared with the bugtracker)"
-task :testing do
-  set :user, 'root'
-  set :environment, :development
-  set :application, 'archive_test'
-  set :deploy_to, "/home/ruby/#{application}"
-  role :app, "160.45.170.231"
-  role :web, "160.45.170.231"
-  role :db,  "160.45.170.231", :primary => true
+  role :app, '160.45.170.50'
+  role :web, '160.45.170.50'
+  role :db, '160.45.170.50', :primary => true
 end
 
 # deploy restart: restart apache
 namespace :deploy do
 
-  # this task checks to see if we know what we're doing
-  task :check_environment do
-    if environment == :undefined
-      puts "\nWARNING: deploy environment is not defined."
-      puts "use 'cap <environment> <task>' to provide an environment."
-      Kernel.exit
-    end
+  desc 'restart the server stack'
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 
   task :symlink_configuration, :roles => :app do
@@ -87,6 +55,10 @@ namespace :deploy do
       run "ln -s #{shared_path}/config/#{config} #{release_path}/config/#{config}"
       run "ln -nfs #{shared_path}/solr/data #{release_path}/solr/data"
     end
+    # project configuration
+    run "rm -f #{release_path}/config/project.yml"
+    run "ln -s #{release_path}/config/projects/#{project}.yml #{release_path}/config/project.yml"
+    # stills
     run "rm -rf #{release_path}/assets/archive_images"
     run "ln -s #{shared_path}/assets/archive_images #{release_path}/assets/archive_images"
     # text_materials
@@ -98,30 +70,11 @@ namespace :deploy do
   end
 
   task :rewrite_stylesheet_urls, :roles => :app do
-    if environment == :production
-      # this places a /archiv before each image url
-      Dir.glob(File.join(File.dirname(__FILE__), '..', 'public', 'stylesheets', '*.css')).each do |file|
-        stylesheet = file.split('/').last
-        run "sed 's/\\/images/\\/archiv\\/images/g' #{current_release}/public/stylesheets/#{stylesheet} > #{current_release}/public/stylesheets/#{stylesheet}.new"
-        run "mv #{current_release}/public/stylesheets/#{stylesheet}.new #{current_release}/public/stylesheets/#{stylesheet}"
-      end
-    end
-    if environment == :demo
-      # this places a /demo before each image url
-      Dir.glob(File.join(current_release, 'public', 'stylesheets', '*.css')).each do |file|
-        stylesheet = file.split('/').last
-        run "sed 's/\\/images/\\/demo\\/images/g' #{current_release}/public/stylesheets/#{stylesheet} > #{current_release}/public/stylesheets/#{stylesheet}.new"
-        run "mv #{current_release}/public/stylesheets/#{stylesheet}.new #{current_release}/public/stylesheets/#{stylesheet}"
-      end
-    end
-  end
-
-  desc "restart the server stack"
-  task :restart, :roles => :app do
-    if environment == :staging
-      run "apache2ctl restart"
-    else
-      run "touch #{current_release}/tmp/restart.txt"
+    # this places a /archiv before each image url
+    Dir.glob(File.join(File.dirname(__FILE__), '..', 'public', 'stylesheets', '*.css')).each do |file|
+      stylesheet = file.split('/').last
+      run "sed 's/\\/images/\\/archiv\\/images/g' #{current_release}/public/stylesheets/#{stylesheet} > #{current_release}/public/stylesheets/#{stylesheet}.new"
+      run "mv #{current_release}/public/stylesheets/#{stylesheet}.new #{current_release}/public/stylesheets/#{stylesheet}"
     end
   end
 
@@ -132,10 +85,10 @@ namespace :deploy do
     on_rollback { rm "#{shared_path}/system/maintenance.html" }
 
     require 'erb'
-    puts "Wartungsarbeiten im Online-Archiv"
+    puts 'Wartungsarbeiten im Online-Archiv'
     set(:reason) { Capistrano::CLI.ui.ask("Die Seite ist zur Zeit nicht erreichbar wegen: ['Wartungsarbeiten'] ")}
-    set(:deadline) { Capistrano::CLI.ui.ask("Voraussichtlich bis: [unabsehbar] ")}
-    maintenance = ERB.new(File.read("./app/views/layouts/maintenance.html.erb"), nil, "%").result(binding)
+    set(:deadline) { Capistrano::CLI.ui.ask('Voraussichtlich bis: [unabsehbar] ')}
+    maintenance = ERB.new(File.read('./app/views/layouts/maintenance.html.erb'), nil, '%').result(binding)
 
     put maintenance, "#{shared_path}/system/maintenance.html", :mode => 0644
   end
@@ -144,8 +97,7 @@ namespace :deploy do
     run "rm #{shared_path}/system/maintenance.html"
   end
 
-
 end
 
-after "deploy:update_code", "deploy:symlink_configuration"
-after "deploy:update_code", "deploy:rewrite_stylesheet_urls"
+after 'deploy:update_code', 'deploy:symlink_configuration'
+after 'deploy:update_code', 'deploy:rewrite_stylesheet_urls'
