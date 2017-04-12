ca171871da7171e04f552a4563c5e5f9e3969de3
cgregor <chrgregor@gmail.com>
2017-04-12 18:45:57 +0200

    extending i18n load-path to better organize translation files
    Former-commit-id: 6ce86b46b49acb5565b3079cbefae5f8f83950b7

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Archive
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :de
     config.x.project = :zwar
   end
