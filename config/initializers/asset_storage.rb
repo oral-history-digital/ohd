94a1de231d25a37f6f6737bf04f95537bc5384e1
cgregor <chrgregor@gmail.com>
2017-04-19 21:45:39 +0200

    comment some CeDiS configs for now
    Former-commit-id: 5944eb0e07907b807ea207da00107247de0ad745

--- config/initializers/asset_storage.rb
+++ config/initializers/asset_storage.rb
@@ -1,7 +1,7 @@
-require 'project_config'
+#require 'project_config'
 
-# This sets the storage server location for asset and media files.
-CeDiS.config.__configure(:storage_dir, File.join(CeDiS.config.cifs_share, 'archiv_backup'))
-CeDiS.config.__configure(:photo_storage_dir, File.join(CeDiS.config.storage_dir, 'bilder'))
-CeDiS.config.__configure(:archive_management_dir, File.join(CeDiS.config.storage_dir, 'redaktionssystem'))
-CeDiS.config.__configure(:repository_dir, File.join(CeDiS.config.storage_dir, 'archiv_dis'))
+## This sets the storage server location for asset and media files.
+#CeDiS.config.__configure(:storage_dir, File.join(CeDiS.config.cifs_share, 'archiv_backup'))
+#CeDiS.config.__configure(:photo_storage_dir, File.join(CeDiS.config.storage_dir, 'bilder'))
+#CeDiS.config.__configure(:archive_management_dir, File.join(CeDiS.config.storage_dir, 'redaktionssystem'))
+#CeDiS.config.__configure(:repository_dir, File.join(CeDiS.config.storage_dir, 'archiv_dis'))
