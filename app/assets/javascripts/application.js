be09e9d50e7003df08d295abbbd1252272454864
cgregor <chrgregor@gmail.com>
2017-04-25 15:55:26 +0200

    require_tree after jquery
    Former-commit-id: 566b11ae06120fb951e6bce360e44e369a2d10c8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
 
 UTF8 = {
 	encode: function(s){
