9bcb23f48b46d6e7a44eb804259e46ea6b51b484
cgregor <chrgregor@gmail.com>
2017-04-25 12:36:50 +0200

    registration and session routes devided
    Former-commit-id: f1863b0a6e5a51a10173230a9b698e32a214bc63

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root to: "home#archive", locale: :de
 
   scope "/:locale", :constraints => {:locale => /[a-z]{2}/} do
-    devise_for :user_accounts
+    #devise_scope :user do
+      #get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+      #get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    #end
+
+    devise_for :user_accounts, skip: [:registrations]
+    resources :user_registrations
 
     localized do
       %w{archive  faq_archive_contents faq_index faq_searching faq_technical map_tutorial}.each do |site|
