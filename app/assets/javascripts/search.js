e421928076c52ffc76a344b9a9209d4e95bb7505
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-05-09 09:11:48 +0200

    removed Ajax from search button made related inline js executable
    Former-commit-id: c0026fd8132f160bfd778444c4b00f9620f5b46d

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -92,8 +92,8 @@
   }
 
   function submitViaAjax() {
-    beforeSubmit();
-    Form.request('search_facets');
+      beforeSubmit();
+      jQuery( "#search_facets" ).submit();
   }
 
   function beforeSubmit() {
