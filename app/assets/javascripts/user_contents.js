a7078794e3f9f23f89e07d01fea393c899915c81
cgregor <chrgregor@gmail.com>
2017-05-12 13:52:39 +0200

    moving modal-window- and user_contents#show-js-functionality to js-files
    Former-commit-id: 48e5b7e57058852faadb2052acb155104399ce9f

--- app/assets/javascripts/user_contents.js
+++ app/assets/javascripts/user_contents.js
@@ -19,6 +19,21 @@ jQuery(function($){
     currentSorting.cancelSorting();
   });
 
+  $(".toggle").click(function(){
+    var elem = $(this).attr('toggle');
+    $(elem).toggle();
+  });
+  
+  $(".open_modal").click(function(){
+    var close = $(this).attr('close');
+    $(close).addClass('closed');
+
+    var url = $(this).attr('url');
+    var classNames = 'topics ' + $(this).attr('class_names');
+
+    openModal(url, {class_names: classNames});
+  });
+
 });
 
 
