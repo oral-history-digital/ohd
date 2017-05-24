5365d67c3d9808d6496f2650acffff86f03837bd
cgregor <chrgregor@gmail.com>
2017-05-24 13:10:46 +0200

    rendering modal content via js.erb
    Former-commit-id: 553a9efe9b4a3cf6644c5629392410b5d6447f4d

--- app/assets/javascripts/modal_window.js
+++ app/assets/javascripts/modal_window.js
@@ -9,11 +9,10 @@ var openModalWindow = function(url, opts) {
       j('#ajax-spinner').show();
     },
     complete: function(data){
-      console.log('data = ' + data);
-      j('#modal_window').html(data);
-      j('#modal_window').addClass('edit ' + opts.class_names || '');
-      j('#modal_window').show();
-      j('#ajax-spinner').hide();
+      //j('#modal_window').html(data);
+      //j('#modal_window').addClass('edit ' + opts.class_names || '');
+      //j('#modal_window').show();
+      //j('#ajax-spinner').hide();
     }
   });
 };
@@ -22,12 +21,12 @@ function closeModalWindow() {
   new Effect.Fade('shades', { from: 0.6, to: 0, duration: 0.4 });
   var win = j('#modal_window');
   win.hide();
-  win.innerHTML = '';
+  win.html('');
 }
 
 jQuery(function($){
 
-  $("#modal_window_close").on('click', function(){
+  $("#modal_window").on('click', "#modal_window_close", function(){
     closeModalWindow(); 
     return false;
   });
