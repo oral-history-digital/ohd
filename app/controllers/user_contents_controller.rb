17a73acc3aba3f2d5ae921f08d891f8789b280f6
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-06-07 14:57:13 +0200

    moved attr_accessible to controler
    Former-commit-id: 809de0bba5987eafd6c852bfcb5811d4641c7968

--- app/controllers/user_contents_controller.rb
+++ app/controllers/user_contents_controller.rb
@@ -1,7 +1,7 @@
 class UserContentsController < BaseController
   include ERB::Util
 
-  layout 'workspace', :except => [ :show, :create ]
+  layout 'workspace', :except => [:show, :create]
 
   before_action :object, except: [:index]
   before_action :collection, only: [:index]
@@ -15,7 +15,7 @@ class UserContentsController < BaseController
 
   skip_before_action :current_search_for_side_panel
 
-  before_action :authorize_owner!, :only => [ :update, :destroy, :publish, :retract ]
+  before_action :authorize_owner!, :only => [:update, :destroy, :publish, :retract]
   rescue_from ActiveRecord::ReadOnlyRecord, :with => :unauthorized_access
 
   def create
@@ -23,13 +23,13 @@ class UserContentsController < BaseController
       format.html do
         render :action => 'show'
       end
-      format.js do 
+      format.js do
         render :partial => 'show', object: @object
       end
     end
   end
 
-  def destroy 
+  def destroy
     respond_to do |format|
       format.html do
         render :action => 'index'
@@ -38,16 +38,16 @@ class UserContentsController < BaseController
     end
   end
 
-  def index 
+  def index
     respond_to do |format|
-      format.html 
-      format.js 
+      format.html
+      format.js
     end
   end
 
   # The show action is used to request info on any existing
   # context-based user_content, or the ability to create a new one (on a 404).
-  def show 
+  def show
     respond_to do |format|
       format.html do
         if @object.nil? || @object.new_record?
@@ -56,7 +56,7 @@ class UserContentsController < BaseController
       end
       format.js do
         if @object.nil? || @object.new_record?
-          render json: { status: 404 }
+          render json: {status: 404}
         else
           render :partial => 'show', :object => @object
         end
@@ -138,7 +138,7 @@ class UserContentsController < BaseController
       format.html do
         render :partial => 'publish_notice'
       end
-      format.js 
+      format.js
     end
   end
 
@@ -182,7 +182,7 @@ class UserContentsController < BaseController
       format.html do
         render :partial => 'topics'
       end
-      format.js 
+      format.js
     end
   end
 
@@ -197,11 +197,11 @@ class UserContentsController < BaseController
         @interview = nil
         if request.referer =~ /interviews/
           @interview = case @object.reference
-            when Interview
-              @object.reference
-            else
-              nil
-          end
+                         when Interview
+                           @object.reference
+                         else
+                           nil
+                       end
         end
         if @interview.nil?
           redirect_to user_contents_path
@@ -216,7 +216,7 @@ class UserContentsController < BaseController
         # didn't get highlighting to work without messing with background images etc.
         render :update do |page|
           page << "$('modal_window').hide();"
-          page.visual_effect(:fade, 'shades', { :from => 0.6 })
+          page.visual_effect(:fade, 'shades', {:from => 0.6})
           page.replace("user_content_#{@object.id}", item_update)
         end
       end
@@ -246,7 +246,13 @@ class UserContentsController < BaseController
   private
 
   def user_content_params
-    params.require(:user_content).permit(:description)
+    params.require(:user_content).permit(:description,
+                                         :user_id,
+                                         :title,
+                                         :interview_references,
+                                         :properties,
+                                         :link_url,
+                                         :persistent)
   end
 
   # make sure the current_user_account is the owner of the resource
@@ -268,7 +274,7 @@ class UserContentsController < BaseController
             klass.new
           else
             id_attr = @id_hash.to_i > 0 ? 'id' : 'id_hash'
-            klass.where(["user_id = ? AND #{id_attr} = ?", current_user_account.user.id, @id_hash ]).first
+            klass.where(["user_id = ? AND #{id_attr} = ?", current_user_account.user.id, @id_hash]).first
           end
         end
       end
@@ -305,7 +311,7 @@ class UserContentsController < BaseController
   def object_params
     @object_params ||= begin
       attributes = {}
-      params[model_name].each_pair do |k,v|
+      params[model_name].each_pair do |k, v|
         # ensure that only real JSON hash is decoded, not plain attributes
         if k == 'title' || v =~ /^[-,();.\/\w\d\s_]+$/
           attributes[k.to_sym] = v
@@ -319,7 +325,7 @@ class UserContentsController < BaseController
         end
       end
       # truncate to length of database columns
-      @@content_column_limits ||= UserContent.content_columns.inject({}){|h, c| h[c.name] = c.limit unless c.limit.blank?; h}
+      @@content_column_limits ||= UserContent.content_columns.inject({}) {|h, c| h[c.name] = c.limit unless c.limit.blank?; h}
       attributes.each do |attr, value|
         if @@content_column_limits.keys.include?(attr.to_s) && value.length > @@content_column_limits[attr.to_s]
           attributes[attr] = @template.truncate(value, :length => @@content_column_limits[attr.to_s])
@@ -339,7 +345,7 @@ class UserContentsController < BaseController
     conds = []
     values = []
     filters = params['content_filters'] || {}
-    types = filters.keys.empty? ? UserContent::CONTENT_TYPES.map{|c| c.to_s.camelize } : []
+    types = filters.keys.empty? ? UserContent::CONTENT_TYPES.map {|c| c.to_s.camelize} : []
     filters.keys.each do |content_type|
       next if content_type =~ /^'?_?all_?'?$/
       filter_value = filters[content_type.to_s.underscore].to_i
@@ -347,7 +353,7 @@ class UserContentsController < BaseController
     end
     return [] if types.size == UserContent::CONTENT_TYPES.size
     conds << "type in ('#{types.join("','")}')"
-    conditions = [ conds.join(' AND ') ]
+    conditions = [conds.join(' AND ')]
     values.each do |value|
       conditions << value
     end
@@ -356,7 +362,7 @@ class UserContentsController < BaseController
 
   def tag_filters
     filters = params['tag_filters'] || {}
-    if((filters.keys.size > 0) && (filters.keys.size < (current_user_account.tags.length+1)))
+    if ((filters.keys.size > 0) && (filters.keys.size < (current_user_account.tags.length+1)))
       return filters.keys
     else
       return []
@@ -366,7 +372,7 @@ class UserContentsController < BaseController
   def collection
     conditions = filter_conditions
     sql_conditions = []
-    sql_conditions << [conditions.shift, 'user_id = ?'].delete_if{|el| el.blank? }.join(' AND ')
+    sql_conditions << [conditions.shift, 'user_id = ?'].delete_if {|el| el.blank?}.join(' AND ')
     sql_conditions += conditions
     sql_conditions << current_user_account.user.id
     includes = []
@@ -374,14 +380,14 @@ class UserContentsController < BaseController
     unless tags.empty?
       includes << :taggings
       includes << :tags
-      sql_conditions = [ sql_conditions.shift + " AND tags.name IN ('#{tags.map{|t| h(t)}.join("','")}')" ] + sql_conditions
+      sql_conditions = [sql_conditions.shift + " AND tags.name IN ('#{tags.map {|t| h(t)}.join("','")}')"] + sql_conditions
     end
     @user_contents = UserContent.where(sql_conditions).includes(includes).order("position ASC, user_contents.created_at DESC")
   end
 
   def tag_list_from_ids(ids)
     cond = ['id IN (']
-    ids.each_with_index do |id,index|
+    ids.each_with_index do |id, index|
       cond.first << ((index > 0) ? ',?' : '?')
       cond << id
     end
@@ -396,7 +402,7 @@ class UserContentsController < BaseController
     current_pos = (0.75 * pos.min).round
     pos_per_step = ((1.25 * pos.max - current_pos) / pos.length).floor + 1
     ids.each do |id|
-      item = current_contents.select{|c| c.id == id.to_i }.first
+      item = current_contents.select {|c| c.id == id.to_i}.first
       unless item.nil?
         UserContent.update_all ['position = ?', current_pos], "id = #{id} AND user_id = #{current_user_account.id}"
         item.position = current_pos
@@ -438,7 +444,7 @@ JS
         item_update = render_to_string(:partial => 'user_content', :object => @object)
         # didn't get highlighting to work without messing with background images etc.
         render :update do |page|
-          page.visual_effect(:fade, 'shades', { :from => 0.6 })
+          page.visual_effect(:fade, 'shades', {:from => 0.6})
           page.replace("user_content_#{@object.id}", item_update)
         end
       end
--- app/models/user_content.rb
+++ app/models/user_content.rb
@@ -20,13 +20,6 @@ class UserContent < ActiveRecord::Base
   #after_validation_on_create :check_persistence, :set_link_url
   after_validation :check_persistence, :set_link_url, :on => :create
 
-  # attr_accessible :user_id,
-  #                 :title,
-  #                 :interview_references,
-  #                 :properties,
-  #                 :description,
-  #                 :link_url,
-  #                 :persistent
 
   validates_presence_of :user_id
 
