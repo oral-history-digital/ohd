f512b6eb1754e3bca567d0aad33946f3c5523a35
cgregor <chrgregor@gmail.com>
2017-06-23 14:09:40 +0200

    some syntax fits
    Former-commit-id: 5bd2f4920e501b8bc2afdb6248d62577617f7f1d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -160,7 +160,7 @@ JS
 
   # provides a date stamp for the registry reference data to circumvent rigid caching
   def map_data_datestamp
-    date = Import.first :select => "time", :conditions => "time < '#{DateTime.now.beginning_of_week.to_s(:db)}'", :order => "time DESC"
+    date = Import.select("time").where("time < '#{DateTime.now.beginning_of_week.to_s(:db)}'").order("time DESC").first
     date = date.nil? ? DateTime.now.beginning_of_week : date.time
     date.strftime('%Y%m%d')
   end
