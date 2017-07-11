a2ff37a95d7e4333b8ab2e8b489442bf6b6fa617
cgregor <chrgregor@gmail.com>
2017-07-11 10:58:52 +0200

    allow nil gender on person
    Former-commit-id: 68eacbd303e9c3b2e6ac750d28dd1def6b4da774

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,9 @@
 class Person < ApplicationRecord
 
+  has_many :contributions
   has_one :history, dependent: :destroy
 
-  validates :gender, inclusion: %w(male female)
+  validates :gender, inclusion: %w(male female), allow_nil: true
 
   translates :first_name, :last_name, :birth_name, :other_first_names, :alias_names
 
