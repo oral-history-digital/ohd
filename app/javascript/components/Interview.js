3b83934be71841f431fb6bab0dd768acafc1549c
cgregor <chrgregor@gmail.com>
2017-07-12 14:52:18 +0200

    cleanup
    Former-commit-id: 75fcc71da9de8acad414a9d9c3b7b48f7f0f7d5e

--- app/javascript/components/Interview.js
+++ app/javascript/components/Interview.js
@@ -1,10 +1,7 @@
 import React from 'react';
-//import { Video } from 'react-videojs';
 import { Player } from 'video-react';
 import "../../../node_modules/video-react/dist/video-react.css"; // import css
 
-
-        //<Video src={this.props.source} />
 export default class Interview extends React.Component {
 
   render () {
@@ -14,7 +11,6 @@ export default class Interview extends React.Component {
             playsInline
             src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
           />
-        <p> Bla </p>
       </div>
     );
   }
