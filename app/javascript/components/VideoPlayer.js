e8cef2a64ce8648c8ae7a2c67b7b5aec2482cd00
cgregor <chrgregor@gmail.com>
2017-07-13 12:57:52 +0200

    cleanup and  prepare VideoPlayer to handle the other events
    Former-commit-id: 0d7f3dfa6beaf35f212b4193edcb5476520eb28d

--- app/javascript/components/VideoPlayer.js
+++ app/javascript/components/VideoPlayer.js
@@ -5,30 +5,32 @@ export default class VideoPlayer extends React.Component {
   componentDidUpdate() {
     if (this.video) {
       this.setVideoTime()
+      this.setVideoVolumne()
       this.setVideoStatus()
     }
   }
 
-  setVideoTime(prevProps) {
+  setVideoTime() {
     if (this.video.duration > 0 && this.props.videoTime > 0) {
       this.video.currentTime = this.props.videoTime * this.video.duration;
     }
   }
 
+  setVideoVolumne() {
+    this.video.volume = this.props.videoVolume;
+  }
+
   setVideoStatus() {
-    //this.video.volume = this.state.videoVolume;
-    //debugger;
-    //this.props.videoStatus === 'play' && !this.video.isPlaying ? this.video.play() : this.video.pause();
     this.props.videoStatus === 'play' ? this.video.play() : this.video.pause();
   }
 
-        //onEnded={(event) => {this.handleVideoEnded(event)}}
-        //onVolumeChange={this.handleVideoVolumeChange}
 
   render () {
     return (
       <video ref={(video) => { this.video = video; }}  
         onTimeUpdate={(event) => {this.props.handleVideoTimeUpdate(event)}}
+        onEnded={(event) => {this.props.handleVideoEnded(event)}}
+        onVolumeChange={this.props.handleVideoVolumeChange}
       >
         <source src={this.props.src}/>
       </video>
@@ -38,4 +40,6 @@ export default class VideoPlayer extends React.Component {
 
 VideoPlayer.propTypes = {
   handleVideoTimeUpdate: React.PropTypes.func,
+  handleVideoEnded: React.PropTypes.func,
+  handleVideoVolumeChange: React.PropTypes.func,
 };
