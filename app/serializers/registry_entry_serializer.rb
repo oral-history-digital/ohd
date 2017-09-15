e2a9e3035cc22d78559cba0114c6249396a4bfa2
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-09-15 14:40:48 +0200

    added validation of longitude and latitude to registryEntrySerializer
    Former-commit-id: 974736087ad9d87d597a91218591a37e76abcd54

--- app/serializers/registry_entry_serializer.rb
+++ app/serializers/registry_entry_serializer.rb
@@ -4,6 +4,15 @@ class RegistryEntrySerializer < ActiveModel::Serializer
              :longitude,
              :descriptor
 
+  def latitude
+    object[:latitude].nil? ? nil : object[:latitude]
+  end
+
+  def longitude
+    object[:longitude].nil? ? nil : object[:longitude]
+  end
+
+
   def descriptor
     object.to_s
   end
