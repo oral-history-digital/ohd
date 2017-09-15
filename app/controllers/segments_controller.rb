8a7dc6b0953604cd8e44f07b719879c0438e41aa
cgregor <chrgregor@gmail.com>
2017-09-15 14:26:32 +0200

    caching segments json
    Former-commit-id: 6625426805b4d6a1228b2addd7fe7c81368d8908

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,17 +1,20 @@
 class SegmentsController < ApplicationController
 
   def index
-    interview = Interview.find_by(archive_id: params[:interview_id])
-    segments = Segment.
-      includes(:translations, :annotations => [:translations], registry_references: {registry_entry: {registry_names: :translations}, registry_reference_type: {} } ).
-      for_interview_id(interview.id)
-    headings = segments.with_heading
+    json = Rails.cache.fetch "segments-headings-for-#{params[:interview_id]}" do
+      interview = Interview.find_by(archive_id: params[:interview_id])
+      segments = Segment.
+        includes(:translations, :annotations => [:translations], registry_references: {registry_entry: {registry_names: :translations}, registry_reference_type: {} } ).
+        for_interview_id(interview.id)
+      headings = segments.with_heading
+      {
+        segments: segments.map{|s| ::SegmentSerializer.new(s)},
+        headings: headings.map{|s| ::SegmentSerializer.new(s)},
+      }
+    end
     respond_to do |format|
       format.json do 
-        render json: {
-          segments: segments.map{|s| ::SegmentSerializer.new(s)},
-          headings: headings.map{|s| ::SegmentSerializer.new(s)},
-        }
+        render json: json
       end
     end
   end
