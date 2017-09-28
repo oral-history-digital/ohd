83dd7c7120d9ad317c448b1331fda97130a6edae
cgregor <chrgregor@gmail.com>
2017-09-28 12:06:03 +0200

    comment some parts of registry... migrations - they are just there hm
    Former-commit-id: e7ae5e71f10a9c3103bd1e922c0ef1d68c6a04b8

--- db/migrate/20140820171517_create_consolidated_registry.rb
+++ db/migrate/20140820171517_create_consolidated_registry.rb
@@ -1,67 +1,67 @@
 class CreateConsolidatedRegistry < ActiveRecord::Migration
   def self.up
     # Start registry migration...
-    create_table :registry_entries do |t|
-      t.string :entry_code
-      t.string :entry_desc
-      t.string :latitude
-      t.string :longitude
-      t.string :workflow_state, :null => false
-      t.boolean :list_priority
-      t.timestamps
-    end
+    #create_table :registry_entries do |t|
+      #t.string :entry_code
+      #t.string :entry_desc
+      #t.string :latitude
+      #t.string :longitude
+      #t.string :workflow_state, :null => false
+      #t.boolean :list_priority
+      #t.timestamps
+    #end
 
-    # NB: The following table supports the acts-as-graph schema.
-    create_table :registry_hierarchies do |t|
-      t.references :ancestor, :null => false
-      t.references :descendant, :null => false
-      t.boolean :direct, :null => false
-      t.integer :count, :null => false
-      t.timestamps
-    end
-    add_index :registry_hierarchies, :ancestor_id
-    add_index :registry_hierarchies, :descendant_id
-    add_index :registry_hierarchies, [:ancestor_id, :descendant_id], :unique => true
+    ## NB: The following table supports the acts-as-graph schema.
+    #create_table :registry_hierarchies do |t|
+      #t.references :ancestor, :null => false
+      #t.references :descendant, :null => false
+      #t.boolean :direct, :null => false
+      #t.integer :count, :null => false
+      #t.timestamps
+    #end
+    #add_index :registry_hierarchies, :ancestor_id
+    #add_index :registry_hierarchies, :descendant_id
+    #add_index :registry_hierarchies, [:ancestor_id, :descendant_id], :unique => true
 
-    create_table :registry_name_types do |t|
-      t.string :code
-      t.string :name
-      t.integer :order_priority
-      t.boolean :allows_multiple
-      t.boolean :mandatory
-    end
+    #create_table :registry_name_types do |t|
+      #t.string :code
+      #t.string :name
+      #t.integer :order_priority
+      #t.boolean :allows_multiple
+      #t.boolean :mandatory
+    #end
 
-    create_table :registry_names do |t|
-      t.references :registry_entry, :null => false
-      t.references :registry_name_type, :null => false
-      t.integer :name_position, :null => false
-      t.timestamps
-    end
-    add_index :registry_names, [:registry_entry_id, :registry_name_type_id, :name_position], :unique => true, :name => 'registry_names_unique_types_and_positions'
+    #create_table :registry_names do |t|
+      #t.references :registry_entry, :null => false
+      #t.references :registry_name_type, :null => false
+      #t.integer :name_position, :null => false
+      #t.timestamps
+    #end
+    #add_index :registry_names, [:registry_entry_id, :registry_name_type_id, :name_position], :unique => true, :name => 'registry_names_unique_types_and_positions'
 
-    # Manually create the registry names translation table
-    # to use UTF8 binary collation.
-    # NB: This makes sure that registry 'das'/'daß' or
-    # 'Andre'/'André' will actually be treated as distinct
-    # entries!
-    create_table :registry_name_translations,
-                 :options => 'COLLATE utf8_bin' do |t|
-      t.references :registry_name, :null => false
-      t.string :locale, :null => false
-      t.text :descriptor
-      t.timestamps
-    end
-    add_index :registry_name_translations, :registry_name_id
-    # Facilitate finding registry entries by name.
-    add_index :registry_name_translations, :descriptor, :length => 255
-    # Make sure that we have unique standard names.
-    add_index :registry_name_translations, [:registry_name_id, :locale], :unique => true
+    ## Manually create the registry names translation table
+    ## to use UTF8 binary collation.
+    ## NB: This makes sure that registry 'das'/'daß' or
+    ## 'Andre'/'André' will actually be treated as distinct
+    ## entries!
+    #create_table :registry_name_translations,
+                 #:options => 'COLLATE utf8_bin' do |t|
+      #t.references :registry_name, :null => false
+      #t.string :locale, :null => false
+      #t.text :descriptor
+      #t.timestamps
+    #end
+    #add_index :registry_name_translations, :registry_name_id
+    ## Facilitate finding registry entries by name.
+    #add_index :registry_name_translations, :descriptor, :length => 255
+    ## Make sure that we have unique standard names.
+    #add_index :registry_name_translations, [:registry_name_id, :locale], :unique => true
 
-    create_table :registry_reference_types do |t|
-      t.references :registry_entry
-      t.string :code
-    end
-    #RegistryReferenceType.create_translation_table! :name => :string
+    #create_table :registry_reference_types do |t|
+      #t.references :registry_entry
+      #t.string :code
+    #end
+    ##RegistryReferenceType.create_translation_table! :name => :string
 
     create_table :registry_references do |t|
       t.references :registry_entry, :null => false
