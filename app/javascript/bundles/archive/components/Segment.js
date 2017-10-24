da3b3f6f74e33cf8060c1de49949afb7af65b5e9
cgregor <chrgregor@gmail.com>
2017-10-24 17:38:17 +0200

    indent Segment
    Former-commit-id: a6094fdce2d1118bd574c4a02578181205c6e21e

--- app/javascript/bundles/archive/components/Segment.js
+++ app/javascript/bundles/archive/components/Segment.js
@@ -26,15 +26,15 @@ export default class Segment extends React.Component {
         }
     }
 
-  css() {
-    //let active = this.props.data.end_time >= this.props.transcriptTime && this.props.data.start_time <= this.props.transcriptTime; 
-    return 'segment ' + (this.state.active ? 'active' : 'inactive');
-  }
+    css() {
+        //let active = this.props.data.end_time >= this.props.transcriptTime && this.props.data.start_time <= this.props.transcriptTime; 
+        return 'segment ' + (this.state.active ? 'active' : 'inactive');
+    }
 
-  transcript() {
-    let locale = this.props.originalLocale ? this.props.interview.lang : this.props.locale
-    return this.props.data.transcripts[locale]
-  }
+    transcript() {
+        let locale = this.props.originalLocale ? this.props.interview.lang : this.props.locale
+        return this.props.data.transcripts[locale]
+    }
 
     userContentForm() {
         return  <UserContentFormContainer 
@@ -42,7 +42,7 @@ export default class Segment extends React.Component {
                     description=''
                     properties={{
                         time: this.props.data.start_time, 
-                        interview_archive_id: this.props.interview.archive_id
+                            interview_archive_id: this.props.interview.archive_id
                     }}
                     reference_id={this.props.data.id}
                     reference_type='Segment'
@@ -51,34 +51,34 @@ export default class Segment extends React.Component {
                 />
     }
 
-  render () {
-    return (
-      <div 
-        onClick={() => this.props.handleSegmentClick(this.props.data.time)} 
-        className={this.css()}
-      >
-        <div className='text'>{this.transcript()}</div>
-        <div 
-            className='annotate' 
-            onClick={() => this.props.openArchivePopup({
-                title: 'Add Annotation', 
-                content: this.userContentForm()
-            })}
-        >
-            Annotate
-        </div>
-        <div className='annotations'>
-          {this.props.data.annotation_texts.map( (annotation, index) => {
-            return <p className='annotation' key={"annotation-" + index} >{annotation}</p>
-          })}
-        </div>
-        <div className='references'>
-          {this.props.data.references.map( (reference, index) => {
-            return <p className='reference' key={"reference-" + index} >{reference.desc[this.props.data.locale]}</p>
-          })}
-        </div>
-      </div>
-    )
-  }
+    render () {
+        return (
+                <div 
+                    onClick={() => this.props.handleSegmentClick(this.props.data.time)} 
+                    className={this.css()}
+                >
+                    <div className='text'>{this.transcript()}</div>
+                    <div 
+                        className='annotate' 
+                        onClick={() => this.props.openArchivePopup({
+                            title: 'Add Annotation', 
+                            content: this.userContentForm()
+                        })}
+                    >
+                        Annotate
+                    </div>
+                    <div className='annotations'>
+                        {this.props.data.annotation_texts.map( (annotation, index) => {
+                            return <p className='annotation' key={"annotation-" + index} >{annotation}</p>
+                        })}
+                    </div>
+                    <div className='references'>
+                        {this.props.data.references.map( (reference, index) => {
+                            return <p className='reference' key={"reference-" + index} >{reference.desc[this.props.data.locale]}</p>
+                        })}
+                    </div>
+                </div>
+        )
+    }
 }
 
