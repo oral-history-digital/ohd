a7fa6128abc05aef1ebc2807e03d67247fbeb2ad
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-10-25 15:45:17 +0200

    link to config/sunspot for deployment
    Former-commit-id: b27e71f07c7653d72cf4ac00e8f5de75da38bd9b

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :branch, :development
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/sunspot.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
