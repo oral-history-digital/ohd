ca70de187ed49fff7f9316bc8cdad5ba9a93af4f
cgregor <chrgregor@gmail.com>
2017-11-13 18:43:07 +0100

    extended reference indexing
    Former-commit-id: cf135b1cadb91dccbd238102018f2f5bbf2a2b6e

--- db/migrate/20171113160053_add_indices_on_registry_references_for_eog.rb
+++ db/migrate/20171113160053_add_indices_on_registry_references_for_eog.rb
@@ -4,6 +4,7 @@ class AddIndicesOnRegistryReferencesForEog < ActiveRecord::Migration[5.0]
       add_index :registry_references, [:ref_object_type, :ref_object_id]
       add_index :registry_references, :ref_object_type
       add_index :registry_references, :ref_object_id
+      add_index :registry_references, :registry_entry_id
     end
   end
 end
