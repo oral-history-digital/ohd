cad38100bfe037c07f86d4209c26708da0c0d0d2
cgregor <chrgregor@gmail.com>
2017-11-13 15:52:15 +0100

    indenting TableOfContents
    Former-commit-id: 14e385e573b37f1f0c52b6cc0ab4a3338ebb7eda

--- app/javascript/bundles/archive/components/TableOfContents.js
+++ app/javascript/bundles/archive/components/TableOfContents.js
@@ -3,51 +3,51 @@ import HeadingContainer from '../containers/HeadingContainer';
 
 export default class TableOfContents extends React.Component {
 
-  prepareHeadings() {
-    let mainIndex = 0;
-    let mainheading = '';
-    let subIndex = 0;
-    let subheading = '';
-    let headings = [];
+    prepareHeadings() {
+        let mainIndex = 0;
+        let mainheading = '';
+        let subIndex = 0;
+        let subheading = '';
+        let headings = [];
 
-    if(this.props.interview && this.props.interview.headings) {
-      this.props.interview.headings.map( (segment, index) => {
-        
-        if (segment.subheading[this.props.locale] === '') {
-          mainIndex += 1;
-          subIndex = 0;
-          mainheading = mainIndex + '. ' + segment.mainheading[this.props.locale]
-          headings.push({main: true, heading: mainheading, time: segment.time, subheadings: []});
+        if(this.props.interview && this.props.interview.headings) {
+            this.props.interview.headings.map( (segment, index) => {
+
+                if (segment.subheading[this.props.locale] === '') {
+                    mainIndex += 1;
+                    subIndex = 0;
+                    mainheading = mainIndex + '. ' + segment.mainheading[this.props.locale]
+                    headings.push({main: true, heading: mainheading, time: segment.time, subheadings: []});
+                }
+                if (segment.subheading[this.props.locale] !== undefined && segment.subheading[this.props.locale] !== '') {
+                    subIndex += 1;
+                    subheading = mainIndex + '.' + subIndex + '. ' + segment.subheading[this.props.locale];
+                    headings[mainIndex - 1].subheadings.push({main: false, heading: subheading, time: segment.time});
+                }
+            })
         }
-        if (segment.subheading[this.props.locale] !== undefined && segment.subheading[this.props.locale] !== '') {
-          subIndex += 1;
-          subheading = mainIndex + '.' + subIndex + '. ' + segment.subheading[this.props.locale];
-          headings[mainIndex - 1].subheadings.push({main: false, heading: subheading, time: segment.time});
-        }
-      })
+
+        return headings;
     }
-
-    return headings;
-  }
 
     emptyHeadingsNote(headings) {
         if(headings.length <= 0)
             return "Keine Überschriften für diese Sprache"
     }
 
-  render () {
-      let headings = this.prepareHeadings();
-    return ( 
-      <div className={'content-index'}>
-            {this.emptyHeadingsNote(headings)}
-            {headings.map( (heading, index) => {
-              return <HeadingContainer 
-                       key={'mainheading-' + index}
-                       data={heading}
-                     />
-            })}
-      </div>
-    );
-  }
+    render () {
+        let headings = this.prepareHeadings();
+        return ( 
+            <div className={'content-index'}>
+                {this.emptyHeadingsNote(headings)}
+                {headings.map( (heading, index) => {
+                    return <HeadingContainer 
+                                key={'mainheading-' + index}
+                                data={heading}
+                            />
+                })}
+            </div>
+        );
+    }
 }
 
