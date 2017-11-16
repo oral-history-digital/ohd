beb89c1c266bd834ff8180f12b8287d6101ba0a6
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-11-16 11:43:11 +0100

    added responsive-archive css popup
    Former-commit-id: 25ab0b6f7c1618324d4a2156a962df0e27450472

--- app/javascript/bundles/archive/components/ArchivePopup.js
+++ app/javascript/bundles/archive/components/ArchivePopup.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 
 export default class ArchivePopup extends React.Component {
 
     css() {
-        let css = 'popup';
+        let css = 'lightbox';
         css += this.props.popup.show ? '' : ' hidden';
         css += ` ${this.props.popup.className || ''}`;
         return css;
     }
 
     buttons() {
-        return <div className='close' onClick={() => this.props.closeArchivePopup()} >
-                    X
-                </div>
+        return <div className='close' onClick={() => this.props.closeArchivePopup()}>
+            X
+        </div>
     }
 
     render() {
         return (
             <div className={this.css()}>
-                <h3>{this.props.popup.title}</h3>
-                <div className='content'> {this.props.popup.content} </div>
-                {this.buttons()}
+                <div className={'popup'}>
+                    <div className={'popup-content-container'}>
+                        <h3 className={'popup-content-title'}>{this.props.popup.title}</h3>
+                        <div className='content'> {this.props.popup.content} </div>
+                    </div>
+                    {this.buttons()}
+                </div>
             </div>
         );
     }
