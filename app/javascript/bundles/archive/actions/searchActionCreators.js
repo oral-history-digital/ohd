32ea835b9f3b3a2f00e4396ff321157ebbc73d5e
cgregor <chrgregor@gmail.com>
2017-11-20 14:41:27 +0100

    cleanup and indention
    Former-commit-id: 8af7a82a26846f87977285ff7fe203cd6d79b0cf

--- app/javascript/bundles/archive/actions/searchActionCreators.js
+++ app/javascript/bundles/archive/actions/searchActionCreators.js
@@ -35,38 +35,36 @@ const requestFacets = () => ({
 });
 
 function receiveFacets(json){
-  return {
-    type: RECEIVE_FACETS,
-    facets: json.facets,
-    receivedAt: Date.now()
-  }
+    return {
+        type: RECEIVE_FACETS,
+        facets: json.facets,
+        receivedAt: Date.now()
+    }
 }
 
 export function loadFacets() {
-  return dispatch => {
-    dispatch(requestFacets())
-    Loader.getJson(FACETS_URL, null, dispatch, receiveFacets);
-  }
+    return dispatch => {
+        dispatch(requestFacets())
+        Loader.getJson(FACETS_URL, null, dispatch, receiveFacets);
+    }
 }
 
 const requestArchiveSearch = (searchQuery) => ({
-  type: REQUEST_ARCHIVE_SEARCH,
-  searchQuery: searchQuery,
+    type: REQUEST_ARCHIVE_SEARCH,
+    searchQuery: searchQuery,
 });
 
 function receiveArchiveSearchResults(json){
-  return {
-    type: RECEIVE_ARCHIVE_SEARCH,
-      allInterviewsCount: json.all_interviews_count,
-      resultPagesCount: json.result_pages_count,
-      resultsCount: json.results_count,
-    foundInterviews: json.interviews,
-    //foundSegmentsForInterviews: json.found_segments_for_interviews,
-    facets: json.facets,
-    //query: json.session_query,
-    //fulltext: json.fulltext,
-    receivedAt: Date.now()
-  }
+    return {
+        type: RECEIVE_ARCHIVE_SEARCH,
+        allInterviewsCount: json.all_interviews_count,
+        resultPagesCount: json.result_pages_count,
+        resultsCount: json.results_count,
+        foundInterviews: json.interviews,
+        //foundSegmentsForInterviews: json.found_segments_for_interviews,
+        facets: json.facets,
+        receivedAt: Date.now()
+    }
 }
 
 export function searchInArchive(searchQuery) {
@@ -75,14 +73,6 @@ export function searchInArchive(searchQuery) {
         Loader.getJson(ARCHIVE_SEARCH_URL, searchQuery, dispatch, receiveArchiveSearchResults);
     }
 }
-
-//export function resetSearchInArchive(searchQuery) {
-    //return dispatch => {
-        //dispatch(requestArchiveSearch(searchQuery))
-        //Loader.getJson(ARCHIVE_RESET_SEARCH_URL, searchQuery, dispatch, receiveArchiveSearchResults);
-    //}
-//}
-
 
 const requestInterviewSearch = (searchQuery) => ({
     type: REQUEST_INTERVIEW_SEARCH,
