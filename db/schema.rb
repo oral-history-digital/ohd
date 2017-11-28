5de3b3b498e095e645b61272c1e40f1ae38722f9
obaidi <obaidi@cedis.fu-berlin.de>
2017-11-28 10:51:15 +0100

    adjusting checkbox classes in flyout
    Former-commit-id: e82cdcef80b4a456a5bfeb9ad954cf19d0128207

--- db/schema.rb
+++ db/schema.rb
@@ -350,7 +350,7 @@ ActiveRecord::Schema.define(version: 20171124144005) do
     t.integer  "speaker_id"
     t.boolean  "speaker_change"
     t.boolean  "chapter_change"
-    t.integer  "note_section_id"
+    t.integer  "notes"
     t.string   "section",              limit: 160
     t.integer  "interview_section_id"
     t.string   "term_id",              limit: 11264
