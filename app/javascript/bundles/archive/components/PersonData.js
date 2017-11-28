73e706fad8f446ab856c61a03ccaf6f1953bc338
ahoch <achim.hoch@cedis.fu-berlin.de>
2017-11-28 15:51:08 +0100

    bugfix pdf export - now with xelatex command
    Former-commit-id: 94f6ceac69a7219b679ee9662761e3d957f4cda3

--- app/javascript/bundles/archive/components/PersonData.js
+++ app/javascript/bundles/archive/components/PersonData.js
@@ -9,9 +9,7 @@ export default class PersonData extends React.Component {
                 <p><span className="flyout-content-label">Name:</span><span className="flyout-content-data">{this.props.name}</span></p>
                 <p><span className="flyout-content-label">Geboren am:</span><span className="flyout-content-data">{this.props.interviewee.date_of_birth}</span></p>
                 <p><span className="flyout-content-label">Erfahrungshintergrund:</span><span className="flyout-content-data">"Noch nicht in DB"</span></p>
-                <p><a href={this.props.archiveId + '.pdf?type=translation'}> <i className="fa fa-download flyout-content-ico"></i><span>Kurzbiografie (pdf)</span> </a></p>
-
-
+                <p><a href={this.props.archiveId + '.pdf?type=translation&kind=history'}> <i className="fa fa-download flyout-content-ico"></i><span>Kurzbiografie (pdf)</span> </a></p>
             </div>
         );
     }
