1fe612546f2d87243a347a069acd776b45c31211
cgregor <chrgregor@gmail.com>
2017-12-01 14:27:44 +0100

    save against not existing person, refactor a little
    Former-commit-id: d1b880021cb41cec83f21e3154d795d242be050f

--- app/javascript/bundles/archive/components/InterviewInfo.js
+++ app/javascript/bundles/archive/components/InterviewInfo.js
@@ -8,18 +8,28 @@ import {
 
 export default class InterviewInfo extends React.Component {
 
-    render() {
-        let cameraName = `${this.props.cinematographer.names[this.props.locale].firstname} ${this.props.cinematographer.names[this.props.locale].lastname}`
-        let interviewerName = `${this.props.interviewer.names[this.props.locale].firstname} ${this.props.interviewer.names[this.props.locale].lastname}`
+    fullName(person) {
+        if(person) {
+          return `${person.names[this.props.locale].firstname} ${person.names[this.props.locale].lastname}`;
+        }
+    }
 
+    content(label, value) {
+        return <p>
+                   <span className="flyout-content-label">{label}:</span>
+                   <span className="flyout-content-data">{value}</span>
+               </p>
+    }
+
+    render() {
         return(
             <div>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'date')}:</span><span className="flyout-content-data">{this.props.interview.created}</span></p>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'duration')}:</span><span className="flyout-content-data">{this.props.interview.formatted_duration}</span></p>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'language')}:</span><span className="flyout-content-data">{ArchiveUtils.translate(this.props, this.props.interview.languages.join(''))}</span></p>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'interview')}:</span><span className="flyout-content-data">{interviewerName}</span></p>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'camera')}:</span><span className="flyout-content-data">{cameraName}</span></p>
-                <p><span className="flyout-content-label">{ArchiveUtils.translate(this.props, 'id')}:</span><span className="flyout-content-data">{this.props.archiveId}</span></p>
+                {this.content(ArchiveUtils.translate(this.props, 'date'), this.props.interview.created)}
+                {this.content(ArchiveUtils.translate(this.props, 'duration'), this.props.interview.formatted_duration)}
+                {this.content(ArchiveUtils.translate(this.props, 'language'), ArchiveUtils.translate(this.props, this.props.interview.languages.join('')))}
+                {this.content(ArchiveUtils.translate(this.props, 'interview'), this.fullName(this.props.interviewer))}
+                {this.content(ArchiveUtils.translate(this.props, 'camera'), this.fullName(this.props.cinematographer))}
+                {this.content(ArchiveUtils.translate(this.props, 'id'), this.props.archiveId)}
                 <p><a href={INTERVIEW_URL +'/' + this.props.archiveId + '.pdf?locale=' + this.props.locale + '&kind=interview'}> <i className="fa fa-download flyout-content-ico"></i><span>{ArchiveUtils.translate(this.props,'transcript')}</span> </a></p>
             </div>
         );
