ca2fa0d38c4ebeafc41bdd65b13d34aa9f1e1a7c
Rico Simke <rico.simke@cedis.fu-berlin.de>
2017-12-08 10:09:18 +0100

    Project.name.to_sym
    Former-commit-id: c3787579aa5cbf66a3fa0285e5b863b79ab7c1f4

--- db/migrate/20171201105841_add_facet_entry_codes_to_registry_entries.rb
+++ db/migrate/20171201105841_add_facet_entry_codes_to_registry_entries.rb
@@ -1,6 +1,6 @@
 class AddFacetEntryCodesToRegistryEntries < ActiveRecord::Migration[5.0]
   def change
-    if Project.name == :zwar
+    if Project.name.to_sym == :zwar
       I18n.locale = :de
       RegistryName.joins(:translations).where(descriptor: "Gruppe").first.registry_entry.update_attribute :entry_code, "forced_labor_groups"
       RegistryName.joins(:translations).where(descriptor: "Einsatzbereich").first.registry_entry.update_attribute :entry_code, "forced_labor_fields"
