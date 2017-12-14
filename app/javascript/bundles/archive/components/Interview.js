55458203cf5686b8630371c70f8d6a842edb6777
cgregor <chrgregor@gmail.com>
2017-12-14 15:28:33 +0100

    fix reload of interview
    Former-commit-id: 3217bfdf516a3bc818278cec23e9f13f8ef71b0f

--- app/javascript/bundles/archive/components/Interview.js
+++ app/javascript/bundles/archive/components/Interview.js
@@ -6,33 +6,41 @@ import InterviewTabsContainer from '../containers/InterviewTabsContainer';
 
 export default class Interview extends React.Component {
   
-  componentDidMount() {
-    if (!this.interviewLoaded()) {
-      this.props.fetchInterview(this.props.match.params.archiveId);
+    componentDidMount() {
+        this.loadInterview();
     }
-  }
 
-  interviewLoaded() {
-    return this.props.data && this.props.data.interview && this.props.archiveId === this.props.match.params.archiveId
-  }
-
-  content() {
-    if (this.interviewLoaded()) {
-      return (
-        <WrapperPageContainer 
-          tabIndex={5}
-        >
-          <VideoPlayerContainer />
-          <InterviewTabsContainer/>
-        </WrapperPageContainer>
-      );
-    } else {
-      return null;
+    componentDidUpdate() {
+        this.loadInterview();
     }
-  }
 
-  render() {
-    return this.content();
-  }
+    loadInterview() {
+        if (!this.interviewLoaded()) {
+            this.props.fetchInterview(this.props.match.params.archiveId);
+        }
+    }
+
+    interviewLoaded() {
+        return this.props.data && this.props.data.interview && this.props.archiveId === this.props.match.params.archiveId
+    }
+
+    content() {
+        if (this.interviewLoaded()) {
+            return (
+                <WrapperPageContainer 
+                tabIndex={5}
+                >
+                <VideoPlayerContainer />
+                <InterviewTabsContainer/>
+                </WrapperPageContainer>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        return this.content();
+    }
 }
 
