bd0b91f28f73bd3d356ff0a23c73f1aeca4619d6
cgregor <chrgregor@gmail.com>
2018-01-12 14:15:32 +0100

    readd publication timestamps to user_contents
    Former-commit-id: c29b0a189b9786727b429f8911332349c27b7da7

--- db/migrate/20130813183809_add_publication_timestamps_to_user_content.rb
+++ db/migrate/20130813183809_add_publication_timestamps_to_user_content.rb
@@ -1,19 +1,15 @@
 class AddPublicationTimestampsToUserContent < ActiveRecord::Migration
 
   def self.up
-  unless Project.name.to_sym == :mog
     UserAnnotation.delete_all
     change_table :user_contents do |t|
       t.datetime :submitted_at
       t.datetime :published_at
     end
   end
-  end
 
   def self.down
-  unless Project.name.to_sym == :mog
     remove_columns :user_contents, :submitted_at, :published_at
-  end
   end
 
 end
