2820f19241ff608509ab5f0da10e84d9e632ac88
cgregor <chrgregor@gmail.com>
2018-01-17 11:38:26 +0100

    cleanup routes, prevent deprecation warning on generalized route
    Former-commit-id: 52f503ec23b5bb07de14bc13c32f5ea0cf68156d

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
         get '/',  to: "home#archive", as: :home
       end
 
-      get 'map', to: 'registry_references#map', :as => :public_map
-      get 'mapframe', to: 'registry_references#map_frame', :as => :map_frame
+      #get 'map', to: 'registry_references#map', :as => :public_map
+      #get 'mapframe', to: 'registry_references#map_frame', :as => :map_frame
       get 'locations', to: 'registry_references#locations', :as => :locations
 
       resources :collections, only: [:show, :index] do
@@ -50,11 +50,6 @@ Rails.application.routes.draw do
         end
       end
 
-      #get 'suche/:suche/:page' => 'searches#query', :as => :search_by_hash_page
-      #get 'suche/:suche' => 'searches#query', :as => :search_by_hash
-      #get 'suchen/neu' => 'searches#new', :as => :new_search
-      #get 'suchen/:page' => 'searches#query', :as => :search_by_page
-      #get 'suchen' => 'searches#query', :as => :search
       resources :searches, only: [:new, :facets, :archive, :interview] do
         collection do
           get :facets
@@ -62,11 +57,8 @@ Rails.application.routes.draw do
           get :interview
         end
       end
-
     end
 
-    #get 'arbeitsmappe' => 'user_contents#index'
-    #get 'workbook' => 'user_contents#index', :locale => :en
     resources :user_contents do
       collection do
         get :segment_annotation
@@ -122,39 +114,6 @@ Rails.application.routes.draw do
     end
   end
 
-
   get 'photos/src/:name' => 'photos#src'
   get 'photos/thumb/:name' => 'photos#thumb'
-
-  #get 'interviews/:id/text_materials/:filename.:extension' => 'interviews#text_materials', :as => :text_materials
-  #get 'interviews/:id/photos/:filename.:extension' => 'interviews#photos', :as => :photos
-  #get 'interviews/stills/:filename.:extension' => 'interviews#stills', :as => :stills
-  #get 'interviews/:id/in/:registry_entry_id' => 'interviews#show', :as => :interview_registry_entries
-
-  get 'webservice/ortssuche' => 'registry_references#index', :as => :public_locations_search, :format => :js
-  get 'webservice/ortssuche.:format' => 'registry_references#index', :as => :public_locations_search_by_format
-  get ':locale/webservice/locations/:date.:format' => 'registry_references#full_index', :as => :public_locations_total_pages
-  get ':locale/webservice/locations/:date/page.:page.:format' => 'registry_references#full_index', :as => :public_locations_by_page
-  #get 'karte' => 'registry_references#map', :as => :public_map
-  #get ':locale/map' => 'registry_references#map', :as => :localized_public_map
-  #get 'kartenframe' => 'registry_references#map_frame', :as => :map_frame, :locale => :de
-  #get ':locale/mapframe' => 'registry_references#map_frame', :as => :localized_map_frame
-
-
-  #get 'login' => 'sessions#new', :as => :anmelden
-  #get 'sign_in' => 'sessions#new', :as => :en_login, :locale => :en
-  #get 'passwort_vergessen' => 'passwords#new', :as => :reset_password
-  #get 'neues_passwort/:reset_password_token' => 'passwords#edit', :as => :change_password
-  #get 'user_accounts' => 'home#index', :as => :devise_for
-  #devise_for :user_account
-  #get 'en/:page_id' => 'home#show', :locale => :en
-  #get 'en/:controller/:action' => '#index', :locale => :en
-  #get 'en/:controller/:action/:id' => '#index', :locale => :en
-  #get 'en/:controller/:action/:id.:format' => '#index', :locale => :en
-  #get 'en' => 'home#show', :locale => :en
-  #get ':page_id' => 'home#show', :as => :home
-  #get ':locale' => 'home#show', :as => :localized_root
-  #get ':locale/:page_id' => 'home#show', :as => :localized_home
-  #get '/' => 'home#show'
-  get '/:controller(/:action(/:id))'
 end
