0f024b3a5dcb0607a40d757ea71c314684229764
Rico Simke <rico.simke@cedis.fu-berlin.de>
2018-01-30 12:32:24 +0100

    gallery img paths for zwar
    Former-commit-id: a47494cb91bc32dc885c4969b728200e80affe0e

--- app/javascript/bundles/archive/components/Gallery.js
+++ app/javascript/bundles/archive/components/Gallery.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import CarouselContainer from '../containers/CarouselContainer';
-
+import { PROJECT } from '../constants/archiveConstants'
 
 export default class Gallery extends React.Component {
+
+    thumbnailSrc(photo) {
+        switch(PROJECT) {
+           case 'mog': {
+               return '/photos/thumb/' + photo.src;
+               break;
+           }
+           case 'zwar': {
+                let p = photo.src.split('.')
+                return 'https://medien.cedis.fu-berlin.de/zwar/gallery/' + p[0] + '_thumb.' + p[1];
+                break;
+           }
+           default: {
+               return null;
+           }
+        }
+    }
 
     renderPhotos() {
 
@@ -17,7 +34,7 @@ export default class Gallery extends React.Component {
                      })}
 
                 >
-                    <img src={'/photos/thumb/'+photo.src}>
+                    <img src={ this.thumbnailSrc(photo) }>
                     </img>
                 </div>
             )
