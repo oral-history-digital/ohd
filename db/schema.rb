590319968a2201977463420949586d5acfa5d2ac
ahoch <achim.hoch@cedis.fu-berlin.de>
2018-02-06 08:54:19 +0100

    new build
    Former-commit-id: c56fe5929e73c638e5895bdc630070057e328c04

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201105841) do
+ActiveRecord::Schema.define(version: 20180205145104) do
 
   create_table "annotation_translations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "annotation_section_id",               null: false
