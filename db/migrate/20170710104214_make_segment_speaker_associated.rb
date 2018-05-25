077402e747ca47191ae786f1bb650b7dc8812428
cgregor <chrgregor@gmail.com>
2018-05-25 13:47:09 +0200

    speed up a little speaker association migration
    Former-commit-id: 2e02a8d5836158fc341e32e1c3ea1e7641b0a705

--- db/migrate/20170710104214_make_segment_speaker_associated.rb
+++ db/migrate/20170710104214_make_segment_speaker_associated.rb
@@ -2,40 +2,42 @@ require 'cyrillizer'
 
 class MakeSegmentSpeakerAssociated < ActiveRecord::Migration[5.0]
   def change
-  unless Project.name.to_sym == :mog
-    add_column :segments, :speaker_id, :integer
+    unless Project.name.to_sym == :mog
+      add_column :segments, :speaker_id, :integer
 
-    Segment.find_each do |segment|
-      #p "*** working: Segment #{segment.id} of Interview #{segment.interview.id}"
-
-      problem = false
-
-      # map speaker ids to abbreviations, and convert them to latin chars (to_lat)
-      #
-      speakers_abbreviations = {}
-      speakers = segment.interview.interviewees + segment.interview.interview_contributors
-      speakers.each do |speaker|
-        speaker_abbreviation = (speaker.first_name[0] + speaker.last_name[0]).to_lat
-        problem = true unless speakers_abbreviations[speaker_abbreviation].blank?
-        speakers_abbreviations[speaker_abbreviation] = speaker.id
-      end
-
-      if problem
-        p "*** Segment #{segment.id} of Interview #{segment.interview.id} seems to have ambigious abbreviations"
-      else
-        # associate speaker to segment
-        speaker_id = speakers_abbreviations[segment.speaker.to_lat]
-        if speaker_id.nil?
-          #binding.pry
-          p "*** Segment #{segment.id} of Interview #{segment.interview.id} has no speaker!!" 
+      Interview.all.each do |interview|
+        speakers_abbreviations = {}
+        contributions = {}
+        speakers = interview.interviewees + interview.interview_contributors
+        speakers.each do |speaker|
+          unless speaker.first_name.blank? || speaker.last_name.blank?
+            # map speaker ids to abbreviations, and convert them to latin chars (to_lat)
+            speakers_abbreviations[(speaker.first_name[0] + speaker.last_name[0]).to_lat] = speaker.id
+          end
         end
-        segment.update_attributes speaker_id: speaker_id
+
+        interview.segments.each do |segment|
+          # associate speaker to segment
+          speaker_id = speakers_abbreviations[segment.speaker.to_lat] 
+          speaker_id = speakers_abbreviations[segment.speaker.reverse.to_lat]  if speaker_id.nil?
+          if speaker_id.nil?
+            interview.contributions.each do |contribution|
+              if segment.speaker.to_lat.downcase.match(/^#{contribution.contribution_type}/)
+                speaker_id = contribution.person_id
+              end
+            end
+          end
+          if speaker_id.nil?
+            binding.pry
+            p "*** Segment #{segment.id} of Interview #{segment.interview.id} has no speaker!!" 
+          end
+          segment.update_attributes speaker_id: speaker_id
+        end
       end
+
+      # interview translations is redundant information by now
+      drop_table :interview_translations
+
     end
-
-    # interview translations is redundant information by now
-    drop_table :interview_translations
-
-  end
   end
 end
