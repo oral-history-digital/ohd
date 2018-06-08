77892ac9bf7afd71ad53f9ea923ac4b3f308edda
cgregor <chrgregor@gmail.com>
2018-06-08 11:39:28 +0200

    prevent some migrations from execution in mog
    Former-commit-id: dca177fa0873885ffc0429344bc05009f10866b9

--- db/migrate/20180514100218_add_column_notes_to_registry_name_translations.rb
+++ db/migrate/20180514100218_add_column_notes_to_registry_name_translations.rb
@@ -1,12 +1,14 @@
 class AddColumnNotesToRegistryNameTranslations < ActiveRecord::Migration[5.0]
-  def change
-    reversible do |dir|
-      dir.up do
-        RegistryName.add_translation_fields! notes: :text
-      end
+  unless Project.name.to_sym == :mog
+    def change
+      reversible do |dir|
+        dir.up do
+          RegistryName.add_translation_fields! notes: :text
+        end
 
-      dir.down do
-        remove_column :registry_name_translations, :notes
+        dir.down do
+          remove_column :registry_name_translations, :notes
+        end
       end
     end
   end
