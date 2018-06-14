a49e7882c9ba12a60e7c3797e1e6643fe40697a0
cgregor <chrgregor@gmail.com>
2018-06-14 10:12:44 +0200

    updated schema
    Former-commit-id: ebfc35d9604046e6e95f458419b822085a115eba

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531100358) do
+ActiveRecord::Schema.define(version: 20180611230746) do
 
   create_table "annotation_translations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "annotation_id"
@@ -72,6 +72,21 @@ ActiveRecord::Schema.define(version: 20180531100358) do
     t.string  "contribution_type"
     t.integer "person_id"
     t.index ["interview_id"], name: "index_contributions_on_interview_id", using: :btree
+  end
+
+  create_table "delayed_jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "priority",                 default: 0, null: false
+    t.integer  "attempts",                 default: 0, null: false
+    t.text     "handler",    limit: 65535,             null: false
+    t.text     "last_error", limit: 65535
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
   end
 
   create_table "histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
