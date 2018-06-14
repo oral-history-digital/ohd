6b4ee00becd151c0d3939faed77ee40f2e1da9ea
cgregor <chrgregor@gmail.com>
2018-06-14 10:22:19 +0200

    adding TODOs to transcript file job
    Former-commit-id: 5255e136810a7de280cfd919f2efef6db6bfd4a1

--- app/jobs/read_transcript_file_job.rb
+++ app/jobs/read_transcript_file_job.rb
@@ -2,8 +2,9 @@ class ReadTranscriptFileJob < ApplicationJob
   queue_as :default
 
   def perform(interview, file_path, tape_id, opts={})
-    #file = File.read file_path
     interview.create_or_update_segments_from_file_for_tape(file_path, tape_id, opts)
     File.delete(file_path) if File.exist?(file_path)
+    # TODO: index and cache interview
+    # TODO: send mail to someone informing about finished interview
   end
 end
