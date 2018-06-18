62ef1054fcc5ece689af535d8b9697cf8b77b30c
cgregor <chrgregor@gmail.com>
2018-06-18 17:58:56 +0200

    new component InterviewForm, intgrated InterviewForm for existing interviews, set values
    Former-commit-id: 11858224ef05e386e8be4e46884d1a4b796c2e28

--- app/javascript/bundles/archive/components/EditInterview.js
+++ app/javascript/bundles/archive/components/EditInterview.js
@@ -1,137 +1,17 @@
 import React from 'react';
 import WrapperPageContainer from '../containers/WrapperPageContainer';
 import AuthShowContainer from '../containers/AuthShowContainer';
-import Form from '../containers/form/Form';
+import InterviewFormContainer from '../containers/InterviewFormContainer';
 import { t } from '../../../lib/utils';
 
 export default class EditInterview extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            hideTapeAndArchiveInputs: false, 
-        };
-
-        this.handleTapeAndArchiveIdFromFileChange = this.handleTapeAndArchiveIdFromFileChange.bind(this);
-        //this.handleFileChange = this.handleFileChange.bind(this);
-    }
-
-    handleTapeAndArchiveIdFromFileChange(name, checked) {
-        if (name === 'tape_and_archive_id_from_file') {
-            // trigger the handleChange and with it the validate function of the inputs archiveId, tapeCount and tapeNumber
-            // in other words: the errors on these inputs would prevent the form from being submitted
-            // some new values together with the prop hidden will remove errors on these inputs
-            //let dummy = checked ? (new Date) : '';
-            this.setState({ 
-                //archiveId: dummy,
-                //tapeCount: dummy,
-                //tapeNumber: dummy,
-                hideTapeAndArchiveInputs: checked
-            })
-        }
-    }
-
-    //handleFileChange(name, file) {
-        //if (name === 'data') {
-            //let fileNameParts = file.name.replace(/\.[^/.]+$/, "").split('_')
-            //this.setState({ 
-                //archiveId: fileNameParts[0],
-                //tapeCount: fileNameParts[1],
-                //tapeNumber: fileNameParts[2],
-            //})
-        //}
-    //}
-
     render() {
         let tabIndex = this.props.locales.length + 4;
-        let _this = this;
         return (
             <WrapperPageContainer tabIndex={tabIndex}>
                 <AuthShowContainer ifLoggedIn={true}>
-                    <Form 
-                        scope='interview'
-                        onSubmit={this.props.submitInterview}
-                        submitText={this.props.archiveId ? 'edit.interview.edit' : 'edit.interview.new'}
-                        elements={[
-                            {
-                                elementType: 'select',
-                                attribute: 'collection_id',
-                                values: this.props.collections,
-                                withEmpty: true,
-                                validate: function(v){return v !== ''},
-                                individualErrorMsg: 'empty'
-                            },
-                            { 
-                                attribute: 'data',
-                                elementType: 'input',
-                                type: 'file',
-                                validate: function(v){return v instanceof File},
-                                //handleChangeCallback: this.handleFileChange
-                            },
-                            {
-                                attribute: 'timecode',
-
-                            },
-                            {
-                                attribute: 'transcript',
-                            },
-                            {
-                                attribute: 'translation',
-                            },
-                            {
-                                attribute: 'annotations',
-                            },
-                            { 
-                                attribute: 'tape_and_archive_id_from_file',
-                                elementType: 'input',
-                                type: 'checkbox',
-                                handleChangeCallback: this.handleTapeAndArchiveIdFromFileChange
-                            },
-                            { 
-                                attribute: 'archive_id',
-                                hidden: this.state.hideTapeAndArchiveInputs,
-                                //value: this.state.dummy,
-                                value: this.state.archiveId,
-                                validate: function(v){return _this.state.hideTapeAndArchiveInputs || /^[A-z]{2}\d{3}$/.test(v)}
-                            },
-                            { 
-                                attribute: 'tape_count',
-                                hidden: this.state.hideTapeAndArchiveInputs,
-                                //value: this.state.dummy,
-                                value: this.state.tapeCount,
-                                validate: function(v){return _this.state.hideTapeAndArchiveInputs || /^\d{1}$/.test(v)}
-                            },
-                            { 
-                                attribute: 'tape_number',
-                                hidden: this.state.hideTapeAndArchiveInputs,
-                                //value: this.state.dummy,
-                                value: this.state.tapeNumber,
-                                validate: function(v){return _this.state.hideTapeAndArchiveInputs || /^\d{1}$/.test(v)}
-                            },
-                            {
-                                elementType: 'select',
-                                attribute: 'language_id',
-                                values: this.props.languages,
-                                withEmpty: true,
-                                validate: function(v){return v !== ''} 
-                            },
-                            { 
-                                attribute: 'interview_date',
-                                elementType: 'input',
-                                type: 'date'
-                            },
-                            { 
-                                attribute: 'video',
-                                elementType: 'input',
-                                type: 'checkbox'
-                            },
-                            { 
-                                attribute: 'translated',
-                                elementType: 'input',
-                                type: 'checkbox'
-                            },
-                        ]}
-                    />
+                    <InterviewFormContainer submitText='edit.interview.new' />
                 </AuthShowContainer>
                 <AuthShowContainer ifLoggedOut={true}>
                     {t(this.props, 'devise.failure.unauthenticated')}
--- app/javascript/bundles/archive/containers/EditInterviewContainer.js
+++ app/javascript/bundles/archive/containers/EditInterviewContainer.js
@@ -1,26 +1,16 @@
 import { connect } from 'react-redux';
 
 import EditInterview from '../components/EditInterview';
-import { submitInterview } from '../actions/interviewActionCreators';
 
-//import ArchiveUtils from '../../../lib/utils';
-
-// Which part of the Redux global state does our component want to receive as props?
 const mapStateToProps = (state) => {
     return { 
-        //data: ArchiveUtils.getInterview(state),
         locale: state.archive.locale,
         locales: state.archive.locales,
-        archiveId: state.archive.archiveId,
         translations: state.archive.translations,
-        collections: state.archive.collections,
-        languages: state.archive.languages,
-        account: state.account,
     }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    submitInterview: (params) => dispatch(submitInterview(params))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditInterview);
