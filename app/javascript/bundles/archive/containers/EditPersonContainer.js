c07912ec018a492e22c133e13933731fe569af6c
cgregor <chrgregor@gmail.com>
2018-06-19 10:15:24 +0200

    some preparations on EditPerson component
    Former-commit-id: 284d33759539305367db39c3b412cfac35bc1e1a

--- app/javascript/bundles/archive/containers/EditPersonContainer.js
+++ app/javascript/bundles/archive/containers/EditPersonContainer.js
@@ -3,19 +3,10 @@ import { connect } from 'react-redux';
 import EditPerson from '../components/EditPerson';
 import { submitPerson } from '../actions/interviewActionCreators';
 
-//import ArchiveUtils from '../../../lib/utils';
-
-// Which part of the Redux global state does our component want to receive as props?
 const mapStateToProps = (state) => {
     return { 
-        //data: ArchiveUtils.getPerson(state),
         locale: state.archive.locale,
-        locales: state.archive.locales,
-        archiveId: state.archive.archiveId,
         translations: state.archive.translations,
-        collections: state.archive.collections,
-        languages: state.archive.languages,
-        account: state.account,
     }
 }
 
