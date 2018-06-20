844718740702d98b72d5e6e9ffc1d6162bfaeeb8
cgregor <chrgregor@gmail.com>
2018-06-20 12:23:20 +0200

    better error messaging and indention on loader
    Former-commit-id: 9fb8c1e4d5f5c52b1d762029fac22b0cebd30ba5

--- app/javascript/lib/loader.js
+++ app/javascript/lib/loader.js
@@ -2,65 +2,74 @@ import request from 'superagent';
 import noCache from 'superagent-no-cache';
 
 //if (typeof Loader == "undefined") {
-  var Loader = {
+var Loader = {
     getJson: function(url, queryParams, dispatch, callback) {
-      request.get(url)
-        .use(noCache)
-        .set('Accept', 'application/json')
-        .query(queryParams)
-        .end( (error, res) => {
-          if (res) {
-            if (res.error) {
-              console.log("loading json from " + url + " failed: " + error);
-            } else {
-              if (typeof callback === "function") {
-                dispatch(callback(JSON.parse(res.text)));
-              }
-            }
-          }
-        });
+        request.get(url)
+            .use(noCache)
+            .set('Accept', 'application/json')
+            .query(queryParams)
+            .end( (error, res) => {
+                if (error) {
+                    console.log("loading json from " + url + " failed: " + error);
+                    console.log("original error: " + error.original);
+                } else if (res) {
+                    if (res.error) {
+                        console.log("loading json from " + url + " failed: " + res.error);
+                    } else {
+                        if (typeof callback === "function") {
+                            dispatch(callback(JSON.parse(res.text)));
+                        }
+                    }
+                }
+            });
     },
 
     delete: function(url, dispatch, callback) {
-      request.delete(url)
-        .set('Accept', 'application/json')
-        .end( (error, res) => {
-          if (res) {
-            if (res.error) {
-              console.log("deleting from " + url + " failed: " + error);
-            } else {
-              if (typeof callback === "function") {
-                dispatch(callback(JSON.parse(res.text)));
-              }
-            }
-          }
-        });
+        request.delete(url)
+            .set('Accept', 'application/json')
+            .end( (error, res) => {
+                if (error) {
+                    console.log("loading json from " + url + " failed: " + error);
+                    console.log("original error: " + error.original);
+                } else if (res) {
+                    if (res.error) {
+                        console.log("deleting from " + url + " failed: " + error);
+                    } else {
+                        if (typeof callback === "function") {
+                            dispatch(callback(JSON.parse(res.text)));
+                        }
+                    }
+                }
+            });
     },
 
     put: function(url, params, dispatch, successCallback, errorCallback) {
-      request.put(url)
-        .send(params)
-        .set('Accept', 'application/json')
-        .end( (error, res) => {
-          if (res) {
-            let json = JSON.parse(res.text);
-            if (res.error) {
-              if (typeof errorCallback === "function") {
-                dispatch(errorCallback(json));
-              } else {
-                console.log("loading json from " + url + " failed: " + error);
-              }
-            } else if (json.error) {
-              if (typeof errorCallback === "function") {
-                dispatch(errorCallback(json));
-              } 
-            } else {
-              if (typeof successCallback === "function") {
-                dispatch(successCallback(JSON.parse(res.text)));
-              }
-            }
-          }
-        });
+        request.put(url)
+            .send(params)
+            .set('Accept', 'application/json')
+            .end( (error, res) => {
+                if (error) {
+                    console.log("loading json from " + url + " failed: " + error);
+                    console.log("original error: " + error.original);
+                } else if (res) {
+                    let json = JSON.parse(res.text);
+                    if (res.error) {
+                        if (typeof errorCallback === "function") {
+                            dispatch(errorCallback(json));
+                        } else {
+                            console.log("loading json from " + url + " failed: " + error);
+                        }
+                    } else if (json.error) {
+                        if (typeof errorCallback === "function") {
+                            dispatch(errorCallback(json));
+                        } 
+                    } else {
+                        if (typeof successCallback === "function") {
+                            dispatch(successCallback(JSON.parse(res.text)));
+                        }
+                    }
+                }
+            });
     },
 
     post: function(url, params, dispatch, successCallback, errorCallback) {
@@ -78,27 +87,30 @@ import noCache from 'superagent-no-cache';
         })
         req.set('Accept', 'application/json')
         req.end( (error, res) => {
-          if (res) {
-            let json = JSON.parse(res.text);
-            if (res.error) {
-              if (typeof errorCallback === "function") {
-                dispatch(errorCallback(json));
-              } else {
+            if (error) {
                 console.log("loading json from " + url + " failed: " + error);
-              }
-            } else if (json.error) {
-              if (typeof errorCallback === "function") {
-                dispatch(errorCallback(json));
-              } 
-            } else {
-              if (typeof successCallback === "function") {
-                dispatch(successCallback(JSON.parse(res.text)));
-              }
+                console.log("original error: " + error.original);
+            } else if (res) {
+                let json = JSON.parse(res.text);
+                if (res.error) {
+                    if (typeof errorCallback === "function") {
+                        dispatch(errorCallback(json));
+                    } else {
+                        console.log("loading json from " + url + " failed: " + error);
+                    }
+                } else if (json.error) {
+                    if (typeof errorCallback === "function") {
+                        dispatch(errorCallback(json));
+                    } 
+                } else {
+                    if (typeof successCallback === "function") {
+                        dispatch(successCallback(JSON.parse(res.text)));
+                    }
+                }
             }
-          }
         });
     }
-  };
+};
 //};
 
 export default Loader;
