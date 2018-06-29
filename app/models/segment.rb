7c9f92f9cb423ae288b8de031981f5dec5b3f409
Rico Simke <rico.simke@cedis.fu-berlin.de>
2018-06-29 13:38:09 +0200

    make subtitles work
    Former-commit-id: 68d383654f8f1f8e5138179d7eda36dc7e1a0662

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,6 +1,7 @@
 require 'globalize'
 
 class Segment < ActiveRecord::Base
+  include IsoHelpers
 
   belongs_to :interview, inverse_of: :segments
   belongs_to :speaking_person, 
@@ -214,7 +215,7 @@ class Segment < ActiveRecord::Base
   end
 
   def as_vtt_subtitles(lang)
-    raw_segment_text = text(lang)
+    raw_segment_text = text(projectified(lang))
     segment_text = speaker_changed(raw_segment_text) ? raw_segment_text.sub(/:/,"").strip() :  raw_segment_text
     "#{Time.at(start_time).utc.strftime('%H:%M:%S.%3N')} --> #{Time.at(end_time).utc.strftime('%H:%M:%S.%3N')}\n#{segment_text}"
   end
@@ -225,8 +226,8 @@ class Segment < ActiveRecord::Base
     #Annotation.for_segment(self)
   #end
 
-  def speaker_changed
-    speaker_change || translations.first.text && translations.first.text[1] == ":"
+  def speaker_changed(raw_segment_text = false)
+    raw_segment_text && raw_segment_text[1] == ":"
   end
 
   # returns the segment that leads the chapter
