c6aa001cf2deec9ddb160229c2e6d2afd612e8ac
Rico Simke <rico.simke@cedis.fu-berlin.de>
2018-07-03 15:53:57 +0200

    fixed language in interview info
    Former-commit-id: 8eb094752a2fcf01360a193d2100a8c52f482da8

--- app/javascript/bundles/archive/actions/searchActionCreators.js
+++ app/javascript/bundles/archive/actions/searchActionCreators.js
@@ -60,7 +60,6 @@ function receiveArchiveSearchResults(json){
         resultPagesCount: json.result_pages_count,
         resultsCount: json.results_count,
         foundInterviews: json.interviews,
-        // foundSegmentsForInterviews: json.found_segments_for_interviews,
         facets: json.facets,
         receivedAt: Date.now()
     }
