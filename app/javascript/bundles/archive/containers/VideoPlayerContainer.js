2c585537b558e8abc6bc8654cfa9b2b58313f2d6
cgregor <chrgregor@gmail.com>
2018-07-06 13:09:32 +0200

    fit some containers to new generalized dataActions
    Former-commit-id: 4c6c264f3e29765f8b8c87be5400b4865168a5a2

--- app/javascript/bundles/archive/containers/VideoPlayerContainer.js
+++ app/javascript/bundles/archive/containers/VideoPlayerContainer.js
@@ -5,23 +5,20 @@ import {handleVideoTimeChange, handleVideoEnded, setNextTape} from '../actions/v
 import {handleTranscriptScroll} from '../actions/interviewActionCreators';
 import {openArchivePopup} from '../actions/archivePopupActionCreators';
 
-import ArchiveUtils from '../../../lib/utils';
+import { getInterview } from '../../../lib/utils';
 
-// Which part of the Redux global state does our component want to receive as props?
 const mapStateToProps = (state) => {
-    let data = ArchiveUtils.getInterview(state);
     return {
         project: state.archive.project,
         archiveId: state.archive.archiveId,
-        interview: data && data.interview,
-        segments: data && data.segments,
-        interviewee: data && data.interview.interviewees[0],
-        tape: state.archive.tape,
-        videoTime: state.archive.videoTime,
-        videoStatus: state.archive.videoStatus,
         locale: state.archive.locale,
         locales: state.archive.locales,
         translations: state.archive.translations,
+        interview: getInterview(state),
+        tape: state.interview.tape,
+        videoTime: state.interview.videoTime,
+        videoStatus: state.interview.videoStatus,
+        actualSegment: state.interview.actualSegment,
     }
 }
 
@@ -33,7 +30,4 @@ const mapDispatchToProps = (dispatch) => ({
     openArchivePopup: (params) => dispatch(openArchivePopup(params))
 })
 
-// Don't forget to actually use connect!
-// Note that we don't export Interview, but the redux "connected" version of it.
-// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
 export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
