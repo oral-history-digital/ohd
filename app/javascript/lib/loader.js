4a459c03ba9a061283f099cd5ff444c83f0d2823
cgregor <chrgregor@gmail.com>
2018-07-11 12:33:18 +0200

    fit loader to filter undefined values, fix error messaging
    Former-commit-id: e742716688f6342ae763321b91255c1b1c899a88

--- app/javascript/lib/loader.js
+++ app/javascript/lib/loader.js
@@ -47,25 +47,30 @@ var Loader = {
 
     put: function(url, params, dispatch, successCallback, errorCallback) {
         let req = request.put(url);
-        Loader.submit(req, params, dispatch, successCallback, errorCallback);
+        Loader.submit(req, url, params, dispatch, successCallback, errorCallback);
     },
 
     post: function(url, params, dispatch, successCallback, errorCallback) {
         let req = request.post(url);
-        Loader.submit(req, params, dispatch, successCallback, errorCallback);
+        Loader.submit(req, url, params, dispatch, successCallback, errorCallback);
     },
 
-    submit: function(req, params, dispatch, successCallback, errorCallback) {
+    submit: function(req, url, params, dispatch, successCallback, errorCallback) {
         let scope = Object.keys(params)[0];
         Object.keys(params[scope]).map((param, index) => {
-            if (param === 'data') {
-                // like this it is possible to upload one file through a file-input called data.
-                // you need more file-inputs? change the implementation here!
-                req.attach(`${scope}[${param}]`, params[scope][param]);
+            if (params[scope][param]) {
+                if (param === 'data') {
+                    // like this it is possible to upload one file through a file-input called data.
+                    // you need more file-inputs? change the implementation here!
+                    req.attach(`${scope}[${param}]`, params[scope][param]);
+                } else {
+                    req.field(`${scope}[${param}]`, 
+                        typeof(params[scope][param]) === 'object' ? JSON.stringify(params[scope][param]) : params[scope][param]
+                    );
+                }
             } else {
-                req.field(`${scope}[${param}]`, 
-                    typeof(params[scope][param]) === 'object' ? JSON.stringify(params[scope][param]) : params[scope][param]
-                );
+                // clean params from undefined values
+                delete params[scope][param];
             }
         })
         req.set('Accept', 'application/json')
