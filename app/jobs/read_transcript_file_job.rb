629603c439601c05324abe02b9c441103e1c434f
cgregor <chrgregor@gmail.com>
2018-09-24 15:08:06 +0200

    some improvements on read_transcript_file_job
    Former-commit-id: 51f50f3f566dae61b942fa9f23e8657ef10aa638

--- app/jobs/read_transcript_file_job.rb
+++ app/jobs/read_transcript_file_job.rb
@@ -4,10 +4,13 @@ class ReadTranscriptFileJob < ApplicationJob
   def perform(interview, file_path, tape_id, file_column_names, file_column_languages)
     interview.create_or_update_segments_from_file(file_path, tape_id, file_column_names, file_column_languages)
     File.delete(file_path) if File.exist?(file_path)
-  end
-
-  def after(job)
+    logger.info "*** created  segments for #{interview.archive_id}"
+    interview.update_attributes duration: interview.segments.last.start_time + 7
+    Sunspot.index interview.segments
+    Sunspot.index interview
+    Sunspot.commit
     clear_cache interview
     # TODO: send mail to someone informing about finished interview
   end
+
 end
