f69986da5ec9448b2adebb6216cadfdcfa439f12
Joerg-Michael Baur <michael.baur@cedis.fu-berlin.de>
2018-10-10 13:32:57 +0200

    capistrano: link webpacker.yml, as well
    Former-commit-id: f1bb472445bdfd6fbe068af6811550561c4feb73

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :repo_url, "git@gitlab.cedis.fu-berlin.de:dis/zwar-archive.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/sunspot.yml", "config/project.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/sunspot.yml", "config/project.yml", "config/webpacker.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
