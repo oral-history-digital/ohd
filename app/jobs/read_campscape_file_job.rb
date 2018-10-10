a8b1cc0dec28f4d8ce38ca7b4a258ebbc5a830ad
cgregor <chrgregor@gmail.com>
2018-10-10 21:07:54 +0200

    read biography and birth_location on interviews batch import
    Former-commit-id: 6e7e20296a23e7fcf0933dd9a93ad31e8134911a

--- app/jobs/read_campscape_file_job.rb
+++ app/jobs/read_campscape_file_job.rb
@@ -11,6 +11,7 @@ class ReadCampscapeFileJob < ApplicationJob
   end
 
   def read_file(file_path)
+    I18n.locale = :en
     report << ""
     report << "Import Report for file #{file_path.split('/').last}:"
 
@@ -29,6 +30,10 @@ class ReadCampscapeFileJob < ApplicationJob
           interviewee = Person.create first_name: data[1][0..200], birth_name: data[2][0..200], last_name: data[3][0..200], other_first_names: data[4][0..200], gender: gender(data[5]), date_of_birth: data[6] || data[7]
           report << "    Created Person #{interviewee.id}, #{interviewee.name[:en]} as interviewee."
         end
+
+        short_bio = interviewee.biographical_entries.where(text: data[11]).first
+        interviewee.biographical_entries << BiographicalEntry.create(text: data[11]) unless short_bio || data[11].blank?
+
         interview_date = Date.parse(data[17]) rescue nil
         if interview_date
           interview_date = interview_date.strftime("%d.%m.%Y")
@@ -56,8 +61,12 @@ class ReadCampscapeFileJob < ApplicationJob
 
         Contribution.find_or_create_by person_id: interviewee.id, interview_id: interview.id, contribution_type: Project.contribution_types['interviewee']
 
-        group_id = find_or_create_group(data[8]).id
-        create_reference(group_id, interview) if group_id
+        group = find_or_create_group(data[8])
+        create_reference(group.id, interview) if group
+
+        birth_location_type = RegistryReferenceType.find_by_code('birth_location')
+        place = find_or_create_place(data[9], data[10])
+        create_reference(place.id, interview, birth_location_type.id) if place
       end
     end
   end
@@ -73,25 +82,6 @@ class ReadCampscapeFileJob < ApplicationJob
   def archive_id
     number = Interview.last ? (Interview.last.id + 1) : 1
     "new#{format("%04d", number)}"
-  end
-
-  def find_or_create_group(name)
-    group = nil
-    forced_labor_groups = RegistryEntry.find_by_entry_code('forced_labor_groups')
-    forced_labor_groups = RegistryEntry.create_with_parent_and_name(RegistryEntry.root_node.id, 'forced_labor_groups') unless forced_labor_groups
-    
-    # the following group_names contain only the first registry_name of a registry_entry with all it s translations!!
-    forced_labor_groups.children.each do |c| 
-      group = c if c.registry_names.first.translations.map(&:descriptor).include?(name)
-    end
-
-    if group
-      report << "    Added to group #{group.localized_hash[:en]}."
-    else
-      group = RegistryEntry.create_with_parent_and_name(forced_labor_groups.id, name[0..200]) 
-      report << "    Created group #{group.localized_hash[:en]} and added interview to it."
-    end
-    group
   end
 
   def find_or_create_collection(name)
@@ -113,8 +103,8 @@ class ReadCampscapeFileJob < ApplicationJob
     lang = ISO_639.find(name)
     lang = ISO_639.find_by_english_name(name) unless lang
     if lang
-      language = Language.find_by_code(lang.alpha3_terminologic) 
-      language = Language.create(code: lang.alpha3_terminologic) unless language
+      language = Language.find_by_code(lang.alpha3) 
+      language = Language.create(code: lang.alpha3, name: lang.english_name) unless language
     end
     if language
       report << "    Used #{language.code}." 
@@ -124,8 +114,58 @@ class ReadCampscapeFileJob < ApplicationJob
     language
   end
 
-  def create_reference(registry_entry_id, interview)
-    RegistryReference.create registry_entry_id: registry_entry_id, ref_object_id: interview.id, ref_object_type: "Interview", registry_reference_type_id: nil, ref_position: 0, original_descriptor: "", ref_details: "", ref_comments: "", ref_info: "", workflow_state: "checked", interview_id: interview.id
+  def find_or_create_group(name)
+    group = nil
+    forced_labor_groups = RegistryEntry.find_by_entry_code('forced_labor_groups')
+    
+    # the following group_names contain only the first registry_name of a registry_entry with all it s translations!!
+    forced_labor_groups.children.each do |c| 
+      group = c if c.registry_names.first.translations.map(&:descriptor).include?(name)
+    end
+
+    if group
+      report << "    Added to group #{group.localized_hash[:en]}."
+    else
+      group = RegistryEntry.create_with_parent_and_name(forced_labor_groups.id, name[0..200]) 
+      report << "    Created group #{group.localized_hash[:en]} and added interview to it."
+    end
+    group
+  end
+
+  def find_or_create_place(name, country_name)
+    place = nil
+    country = nil
+
+    places = RegistryEntry.find_by_entry_code('places')
+    
+    # find or create country registry_entry as a child of places
+    places.children.each do |c| 
+      country = c if c.registry_names.first.translations.map(&:descriptor).include?(country_name)
+    end
+
+    if country
+      report << "    Added to country #{country.localized_hash[:en]}."
+    else
+      country = RegistryEntry.create_with_parent_and_name(places.id, country_name[0..200]) 
+      report << "    Created country #{country.localized_hash[:en]} and added interview to it."
+    end
+
+    # find or create place registry_entry as a child of country
+    country.descendants.each do |c| 
+      place = c if c.registry_names.first.translations.map(&:descriptor).include?(name)
+    end
+
+    if place
+      report << "    Added to place #{place.localized_hash[:en]}."
+    else
+      place = RegistryEntry.create_with_parent_and_name(country.id, name[0..200]) 
+      report << "    Created place #{place.localized_hash[:en]} and added interview to it."
+    end
+    place
+  end
+
+  def create_reference(registry_entry_id, interview, type_id=nil)
+    RegistryReference.create registry_entry_id: registry_entry_id, ref_object_id: interview.id, ref_object_type: "Interview", registry_reference_type_id: type_id, ref_position: 0, original_descriptor: "", ref_details: "", ref_comments: "", ref_info: "", workflow_state: "checked", interview_id: interview.id
   end
 
 end
