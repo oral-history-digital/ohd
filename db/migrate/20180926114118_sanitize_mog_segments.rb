f6805ebd6dd2ed5482dccf1dbd2e8b84d401c2fa
cgregor <chrgregor@gmail.com>
2018-10-12 11:17:07 +0200

    improved sanitize mog migration
    Former-commit-id: 11f73ecca4aae5cf0e507cd2c3acaba424f23232

--- db/migrate/20180926114118_sanitize_mog_segments.rb
+++ db/migrate/20180926114118_sanitize_mog_segments.rb
@@ -2,9 +2,8 @@ class SanitizeMogSegments < ActiveRecord::Migration[5.0]
   def change
     if Project.name.to_s == :mog
       Segment.find_each do |segment|
-        speaker_changed = segment.text =~ /^\S*: / ? true : false
         segment.translations.each do |t|
-          t.update_attributes text: Nokogiri::HTML.parse(t.text).text.sub(/^\S*: /, "")
+          t.update_attributes text: Nokogiri::HTML.parse(t.text).text.sub(/^:[\S ]/, "")
         end
       end
     end
