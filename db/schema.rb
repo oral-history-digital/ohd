c1ede75199650d1d49545758e26c1f477c0f0db6
cgregor <chrgregor@gmail.com>
2018-10-24 14:13:18 +0200

    updated schema
    Former-commit-id: fe46a70052ec5390fd66d4b819e05340c46468c1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_143618) do
+ActiveRecord::Schema.define(version: 2018_10_24_112716) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_10_12_143618) do
     t.integer "person_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "workflow_state", default: "unshared"
   end
 
   create_table "biographical_entry_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
