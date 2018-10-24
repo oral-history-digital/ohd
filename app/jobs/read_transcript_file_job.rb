687775aa388c9a122f823f07069a06c9dbd9023c
cgregor <chrgregor@gmail.com>
2018-10-24 20:25:31 +0200

    delete redundant indexing; auto-commiting enabled now
    Former-commit-id: 42b857c98d74e36184b06422baf14997d81a6a26

--- app/jobs/read_transcript_file_job.rb
+++ app/jobs/read_transcript_file_job.rb
@@ -6,9 +6,6 @@ class ReadTranscriptFileJob < ApplicationJob
     File.delete(file_path) if File.exist?(file_path)
     logger.info "*** created  segments for #{interview.archive_id}"
     interview.update_attributes duration: interview.segments.last.start_time + 7
-    Sunspot.index interview.segments
-    Sunspot.index interview
-    Sunspot.commit
     clear_cache interview
     # TODO: send mail to someone informing about finished interview
   end
