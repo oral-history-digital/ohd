eab16bef69617afbdff9ab75da34546ab8c88b51
cgregor <chrgregor@gmail.com>
2018-10-25 11:53:59 +0200

    updated schema
    Former-commit-id: 3c9aa828f3d34c854ccc3976b2407134c7280f3a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_112716) do
+ActiveRecord::Schema.define(version: 2018_10_24_163513) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
