f61a07fa49338af44f2fa1798348447235b87153
cgregor <chrgregor@gmail.com>
2018-11-06 10:33:20 +0100

    skip authorization on sessions_controller
    Former-commit-id: fb70b660f384057dbd99bd955ce049c90c92c7b9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < Devise::SessionsController  
 
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
   #clear_respond_to 
   respond_to :json, :html
 
