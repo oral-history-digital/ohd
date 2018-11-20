e5342e7f6cf8bde6f2113dda799b5ae4f032c8f5
Joerg-Michael Baur <michael.baur@cedis.fu-berlin.de>
2018-11-20 09:59:19 +0100

    capistrano: configure stage zwar_archive
    Former-commit-id: db0051d05bc7abefdc117535a73158a50bf4efef

---
+++ config/deploy/zwar_archive.rb
@@ -0,0 +1,15 @@
+server "deploy_da03", roles: %w{app db web}
+
+set :application, "zwar_archive"
+set :branch, :development
+set :stage, :production
+set :deploy_to, "/data/applications/#{fetch :application}"
+set :bundle_path, "/data/bundle/01"
+
+#set :default_env, {
+#      'project_name' => fetch(:application)
+#    }
+
+set :rbenv_type, :system
+set :rbenv_ruby, '2.5.1'
+set :rbenv_custom_path, '/opt/rbenv'
