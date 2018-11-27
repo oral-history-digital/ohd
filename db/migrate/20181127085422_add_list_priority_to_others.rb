2f30f65455e79d7752c9aa64b8be641528b243fc
Rico Simke <rico.simke@cedis.fu-berlin.de>
2018-11-27 10:17:00 +0100

    shortened migration
    Former-commit-id: 296ade4460724f8b0140c42484d58d14009d3647

--- db/migrate/20181127085422_add_list_priority_to_others.rb
+++ db/migrate/20181127085422_add_list_priority_to_others.rb
@@ -1,11 +1,9 @@
 class AddListPriorityToOthers < ActiveRecord::Migration[5.2]
   def change
     other = RegistryEntry.find(28214)
-    other.list_priority = true
-    other.save
+    other.update_attribute(:list_priority, true)
 
     other = RegistryEntry.find(28177)
-    other.list_priority = true
-    other.save
+    other.update_attribute(:list_priority, true)
   end
 end
