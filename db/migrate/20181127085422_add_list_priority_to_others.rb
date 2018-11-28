d6a1e7a9b926dd808a1dc324b355525bfc5a778e
Rico Simke <rico.simke@cedis.fu-berlin.de>
2018-11-28 09:38:28 +0100

    latest migration only for zwar
    Former-commit-id: b91c376704b3c5633acf72e037fcc249042e6b77

--- db/migrate/20181127085422_add_list_priority_to_others.rb
+++ db/migrate/20181127085422_add_list_priority_to_others.rb
@@ -1,9 +1,11 @@
 class AddListPriorityToOthers < ActiveRecord::Migration[5.2]
   def change
-    other = RegistryEntry.find(28214)
-    other.update_attribute(:list_priority, true)
+    if Project.name.to_sym == :zwar
+      other = RegistryEntry.find(28214)
+      other.update_attribute(:list_priority, true)
 
-    other = RegistryEntry.find(28177)
-    other.update_attribute(:list_priority, true)
+      other = RegistryEntry.find(28177)
+      other.update_attribute(:list_priority, true)
+    end
   end
 end
