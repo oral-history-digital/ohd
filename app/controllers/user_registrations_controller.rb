58396530f8a26e1f4fbe85cd66ff878079921438
cgregor <chrgregor@gmail.com>
2018-12-18 12:58:16 +0100

    fit mailing to splitted mailer
    Former-commit-id: 4dc79b0370a70485865ae80552dd0a4fd508fe9e

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -16,7 +16,7 @@ class UserRegistrationsController < ApplicationController
   def create
     @user_registration = UserRegistration.new(user_registration_params)
     if @user_registration.save
-      UserAccountMailer.new_registration_info(@user_registration).deliver
+      AdminMailer.new_registration_info(@user_registration).deliver
       render json: {registration_status: render_to_string("submitted.#{params[:locale]}.html", layout: false)}
     elsif !@user_registration.errors[:email].nil? && @user_registration.email =~ /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
       @user_registration = UserRegistration.where(email: @user_registration.email).first
