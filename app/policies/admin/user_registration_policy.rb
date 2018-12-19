7b7b6bfd230d59c73acfb9a315946a4debf31e03
cgregor <chrgregor@gmail.com>
2018-12-19 10:47:27 +0100

    improve admin-user_registration_policy
    Former-commit-id: f2efc65c9506da1ba8920e3ed19b5585af313f6e

--- app/policies/admin/user_registration_policy.rb
+++ app/policies/admin/user_registration_policy.rb
@@ -1,7 +1,16 @@
 class Admin::UserRegistrationPolicy < ApplicationPolicy
+
+  def show?
+    user.admin?
+  end
+
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin?
+        scope.all
+      else
+        []
+      end
     end
   end
 end
