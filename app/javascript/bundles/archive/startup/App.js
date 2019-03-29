b8227cb0ac88c5ba7e379f6c4664ccb5fcf6bed1
cgregor <chrgregor@gmail.com>
2019-03-29 13:49:36 +0100

    some indention
    Former-commit-id: e57234be7038114718423da66b296ce39253ac36

--- app/javascript/bundles/archive/startup/App.js
+++ app/javascript/bundles/archive/startup/App.js
@@ -24,28 +24,28 @@ import OrderNewPasswordContainer from '../containers/OrderNewPasswordContainer';
 import HomeContainer from '../containers/HomeContainer';
 
 const App = (props) => (
-  <Provider store={archiveStore(props)}>
-    <BrowserRouter>
-      <div>
-          <Route exact path="/:locale" component={HomeContainer} />
-          <Route path="/:locale/accounts/current" component={WrappedAccountContainer} />
-          <Route exact path="/:locale/interviews/new" component={EditInterviewContainer} />
-          <Route exact path="/:locale/interviews/:archiveId" component={InterviewContainer} />
-          <Route path="/:locale/transcripts/new" component={UploadTranscriptContainer} />
-          <Route path="/:locale/uploads/new" component={UploadsContainer} />
-          <Route exact path="/:locale/people/new" component={EditPersonContainer} />
-          <Route path="/:locale/registry_entries" component={RegistryEntriesTreeContainer} />
-          <Route path="/:locale/searches/archive" component={ArchiveSearchContainer} />
-          <Route path="/:locale/roles" component={WrappedRolesContainer} />
-          <Route path="/:locale/permissions" component={WrappedPermissionsContainer} />
-          <Route path="/:locale/user_accounts/password/new" component={OrderNewPasswordContainer} />
-          <Route path="/:locale/user_accounts/password/edit" component={ActivateAccountContainer} />
-          <Route exact path="/:locale/user_registrations/:resetPasswordToken/activate" component={ActivateAccountContainer} />
-          <Route exact path="/:locale/user_registrations/new" component={RegisterContainer} />
-          <Route exact path="/:locale/user_registrations" component={UserRegistrationsContainer} />
-      </div>
-    </BrowserRouter>
-  </Provider>
+    <Provider store={archiveStore(props)}>
+        <BrowserRouter>
+            <div>
+                <Route exact path="/:locale" component={HomeContainer} />
+                <Route path="/:locale/accounts/current" component={WrappedAccountContainer} />
+                <Route exact path="/:locale/interviews/new" component={EditInterviewContainer} />
+                <Route exact path="/:locale/interviews/:archiveId" component={InterviewContainer} />
+                <Route path="/:locale/transcripts/new" component={UploadTranscriptContainer} />
+                <Route path="/:locale/uploads/new" component={UploadsContainer} />
+                <Route exact path="/:locale/people/new" component={EditPersonContainer} />
+                <Route path="/:locale/registry_entries" component={RegistryEntriesTreeContainer} />
+                <Route path="/:locale/searches/archive" component={ArchiveSearchContainer} />
+                <Route path="/:locale/roles" component={WrappedRolesContainer} />
+                <Route path="/:locale/permissions" component={WrappedPermissionsContainer} />
+                <Route path="/:locale/user_accounts/password/new" component={OrderNewPasswordContainer} />
+                <Route path="/:locale/user_accounts/password/edit" component={ActivateAccountContainer} />
+                <Route exact path="/:locale/user_registrations/:resetPasswordToken/activate" component={ActivateAccountContainer} />
+                <Route exact path="/:locale/user_registrations/new" component={RegisterContainer} />
+                <Route exact path="/:locale/user_registrations" component={UserRegistrationsContainer} />
+            </div>
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
