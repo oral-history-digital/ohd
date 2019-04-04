04274acb38a11cd3d6896eb36ee33e803c519431
cgregor <chrgregor@gmail.com>
2019-04-04 13:37:46 +0200

    working on vtt-/srt-import
    Former-commit-id: 82217fb02f0f1ef829eef8a07eec0cd3df4b9021

--- app/jobs/read_transcript_file_job.rb
+++ app/jobs/read_transcript_file_job.rb
@@ -2,7 +2,14 @@ class ReadTranscriptFileJob < ApplicationJob
   queue_as :default
 
   def perform(interview, file_path, tape_id, file_column_names, file_column_languages, receiver)
-    interview.create_or_update_segments_from_file(file_path, tape_id, file_column_names, file_column_languages)
+    extension = File.extname(file_path).strip.downcase[1..-1]
+    case extension
+    # only ods is tested
+    when 'ods', 'xlsx', 'xlsm','xls', 'xlm', 'csv'
+      interview.create_or_update_segments_from_spreadsheet(file_path, tape_id, file_column_names, file_column_languages)
+    when 'vtt', 'srt'
+      interview.create_or_update_segments_from_vtt(file_path, tape_id)
+    end
     File.delete(file_path) if File.exist?(file_path)
     logger.info "*** created  segments for #{interview.archive_id}"
     interview.update_attributes duration: interview.segments.last.time + 7
