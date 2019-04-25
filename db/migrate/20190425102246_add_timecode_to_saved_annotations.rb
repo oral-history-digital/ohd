6646a6f84358368b1b507930ba1ac8f20f875168
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-04-25 12:40:04 +0200

    make old user annotations reusable
    Former-commit-id: 4ab681bbdfe853f44eed0230115a912648024142

---
+++ db/migrate/20190425102246_add_timecode_to_saved_annotations.rb
@@ -0,0 +1,13 @@
+class AddTimecodeToSavedAnnotations < ActiveRecord::Migration[5.2]
+  def change
+    UserContent.where(type: "UserAnnotation").each do |uc|
+      if uc.properties["time"] == nil
+        uc.properties["time"] = uc.reference_id.yield_self{|id| Segment.find(id)}.time
+        uc.properties["tape_nbr"]= uc.media_id.split("_").last(2).first.to_i.to_s
+        uc.properties["segmentIndex"]= uc.reference_id
+        uc.properties["interview_archive_id"]= uc.media_id.split("_").first.downcase
+        uc.save
+      end
+    end
+  end
+end
