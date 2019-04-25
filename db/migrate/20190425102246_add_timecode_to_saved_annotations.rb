6a44cd4d86cbcf549f2c297f257ab4e0defaa700
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-04-25 13:38:02 +0200

    ignore user annotations if segment cannot be found
    Former-commit-id: edaf8f561fb11dd607530d5688309b4e596cc294

--- db/migrate/20190425102246_add_timecode_to_saved_annotations.rb
+++ db/migrate/20190425102246_add_timecode_to_saved_annotations.rb
@@ -1,7 +1,7 @@
 class AddTimecodeToSavedAnnotations < ActiveRecord::Migration[5.2]
   def change
     UserContent.where(type: "UserAnnotation").each do |uc|
-      if uc.properties["time"] == nil
+      if uc.properties["time"] == nil && Segment.find_by(id: uc.reference_id)
         uc.properties["time"] = uc.reference_id.yield_self{|id| Segment.find(id)}.time
         uc.properties["tape_nbr"]= uc.media_id.split("_").last(2).first.to_i.to_s
         uc.properties["segmentIndex"]= uc.reference_id
