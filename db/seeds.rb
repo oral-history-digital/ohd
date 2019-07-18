f1aea545d0a906623c720004f6a121b4e8c3b674
cgregor <chrgregor@gmail.com>
2019-07-18 12:11:17 +0200

    updating seeds to new synthax, adding accessibility-registry_entry
    Former-commit-id: 249edd4a1d3fa01bb5017041d0832760efc29424

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ RegistryNameType.create code: "spelling", name: "Bezeichner", order_priority: 3,
 name = RegistryName.create registry_entry_id: root.id, name_position: 0, descriptor: 'root', registry_name_type_id: 1
 
 # create places registry_entry as a child of root
-places = RegistryEntry.create_with_parent_and_name(root.id, 'places') 
+places = RegistryEntry.create_with_parent_and_names(root.id, 'en::places') 
 
 # create location registry_reference_types
 [["home_location", "Place of Residence"], ["deportation_location", "Place of Deportation"], ["forced_labor_location", "Place of Forced Labor"], ["return_location", "Residence since 1945"], ["birth_location", "Place of Birth"], ["interview_location", "Place of Interview"], ["birth_place", "Place of Birth"], ["place_of_death", "Place of Death"]].each do |code, name|
@@ -19,12 +19,15 @@ places = RegistryEntry.create_with_parent_and_name(root.id, 'places')
 end
 
 # create forced_labor_groups registry_entry as a child of root
-forced_labor_groups = RegistryEntry.create_with_parent_and_name(root.id, 'forced_labor_groups')
-groups = RegistryEntry.create_with_parent_and_name(root.id, 'Group', 'groups')
-group_details = RegistryEntry.create_with_parent_and_name(root.id, 'Group Details', 'group_details')
+forced_labor_groups = RegistryEntry.create_with_parent_and_names(root.id, 'forced_labor_groups')
+groups = RegistryEntry.create_with_parent_and_names(root.id, 'en::Group', 'groups')
+group_details = RegistryEntry.create_with_parent_and_names(root.id, 'en::Group Details', 'group_details')
 
 # create camps registry_entry as a child of root
-camps = RegistryEntry.create_with_parent_and_name(root.id, 'Camps and detention facilities', 'camps') 
+camps = RegistryEntry.create_with_parent_and_names(root.id, 'en::Camps and detention facilities', 'camps') 
+
+# create camps registry_entry as a child of root
+accessibility = RegistryEntry.create_with_parent_and_names(root.id, 'en::Accessibility', 'accessibility') 
 
 # create default collection
 Collection.create name: 'default'
