1239b96b44d497035879e8944194d7d0daa31427
cgregor <chrgregor@gmail.com>
2019-08-12 15:32:07 +0200

    fixing  INTARCH-472 (adding registry_references to  segmments)
    Former-commit-id: 8345c374abf4eff5d10d87277587471a27486152

--- app/javascript/bundles/archive/containers/SegmentRegistryReferencesContainer.js
+++ app/javascript/bundles/archive/containers/SegmentRegistryReferencesContainer.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 import SegmentRegistryReferences from '../components/SegmentRegistryReferences';
 import { openArchivePopup, closeArchivePopup } from '../actions/archivePopupActionCreators';
 import { fetchData } from '../actions/dataActionCreators';
+import { getCookie } from '../../../lib/utils';
 
 const mapStateToProps = (state) => {
     return { 
         translations: state.archive.translations,
         registryEntries: state.data.registry_entries,
         registryEntriesStatus: state.data.statuses.registry_entries,
+        account: state.data.accounts.current,
+        editView: getCookie('editView')
     }
 }
 
