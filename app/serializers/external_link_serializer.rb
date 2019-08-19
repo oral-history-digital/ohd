1b04410f4188d80131c6b7d3e2517c4c05b90d3e
cgregor <chrgregor@gmail.com>
2019-08-19 13:31:16 +0200

    adding translations to external_link-serializer
    Former-commit-id: fa622def7831696da93e38cde7cc289a75c15c04

--- app/serializers/external_link_serializer.rb
+++ app/serializers/external_link_serializer.rb
@@ -1,7 +1,8 @@
 class ExternalLinkSerializer < ApplicationSerializer
   attributes :id, 
     :name, 
-    :url 
+    :url,
+    :translations
 
   def url
     I18n.available_locales.inject({}) do |mem, locale|
@@ -10,4 +11,17 @@ class ExternalLinkSerializer < ApplicationSerializer
     end
   end
 
+  # serialized translations are needed to construct 'translations_attributes' e.g. in MultiLocaleInput
+  # containing the id of a translation
+  #
+  # without the id a translation would not be updated but newly created!!
+  #
+  def translations
+    I18n.available_locales.inject({}) do |mem, locale|
+      translation = object.translations.where(locale: locale).first
+      ExternalLink.last.translations.where(locale: :de).first.attributes.reject{|k,v| !ExternalLink.translated_attribute_names.include?(k.to_sym)}
+      mem[locale] = translation.attributes.reject{|k,v| !(ExternalLink.translated_attribute_names + [:id]).include?(k.to_sym)}
+      mem
+    end
+  end
 end
