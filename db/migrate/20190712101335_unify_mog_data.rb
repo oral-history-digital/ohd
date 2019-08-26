f52256bf9d3a3145d914e6537dd17c7da3f2e7de
cgregor <chrgregor@gmail.com>
2019-08-26 11:56:52 +0200

    extending unification migration
    Former-commit-id: 6858f0960a03a9675ec4df7d89c26e54aa1c129c

--- db/migrate/20190712101335_unify_mog_data.rb
+++ db/migrate/20190712101335_unify_mog_data.rb
@@ -7,27 +7,46 @@ class UnifyMogData < ActiveRecord::Migration[5.2]
       Contribution.where(contribution_type: 'Translated').update_all contribution_type: 'translator'
       Contribution.where(contribution_type: 'Indexed').update_all contribution_type: 'segmentator'
 
-      connection.execute(<<-EOQ)
-        UPDATE segment_translations SET locale = 'el' WHERE locale='ell';
-        UPDATE segment_translations SET locale = 'de' WHERE locale='deu';
-        UPDATE segment_translations SET locale = 'en' WHERE locale='eng';
 
-        UPDATE annotation_translations SET locale = 'el' WHERE locale='ell';
-        UPDATE annotation_translations SET locale = 'de' WHERE locale='deu';
-        UPDATE annotation_translations SET locale = 'en' WHERE locale='eng';
+      execute "UPDATE collection_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE collection_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE collection_translations SET locale = 'en' WHERE locale='eng';"
 
-        UPDATE biographical_entry_translations SET locale = 'el' WHERE locale='ell';
-        UPDATE biographical_entry_translations SET locale = 'de' WHERE locale='deu';
-        UPDATE biographical_entry_translations SET locale = 'en' WHERE locale='eng';
+      execute "UPDATE language_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE language_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE language_translations SET locale = 'en' WHERE locale='eng';"
 
-        UPDATE registry_reference_type_translations SET locale = 'el' WHERE locale='ell';
-        UPDATE registry_reference_type_translations SET locale = 'de' WHERE locale='deu';
-        UPDATE registry_reference_type_translations SET locale = 'en' WHERE locale='eng';
+      execute "UPDATE photo_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE photo_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE photo_translations SET locale = 'en' WHERE locale='eng';"
 
-        UPDATE text_materials SET locale = 'el' WHERE locale='ell';
-        UPDATE text_materials SET locale = 'de' WHERE locale='deu';
-        UPDATE text_materials SET locale = 'en' WHERE locale='eng';
-      EOQ
+      execute "UPDATE interview_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE interview_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE interview_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE registry_name_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE registry_name_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE registry_name_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE person_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE person_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE person_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE segment_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE segment_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE segment_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE annotation_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE annotation_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE annotation_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE biographical_entry_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE biographical_entry_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE biographical_entry_translations SET locale = 'en' WHERE locale='eng';"
+
+      execute "UPDATE registry_reference_type_translations SET locale = 'el' WHERE locale='ell';"
+      execute "UPDATE registry_reference_type_translations SET locale = 'de' WHERE locale='deu';"
+      execute "UPDATE registry_reference_type_translations SET locale = 'en' WHERE locale='eng';"
     end
 
     if Project.name.to_sym == :zwar
