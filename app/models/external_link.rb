3e02db58a287a3105bfee48543448a1e70fa49f0
cgregor <chrgregor@gmail.com>
2019-08-27 10:36:30 +0200

    rm unfunctional validation for external_link_translation
    Former-commit-id: c54d40b971d126e96d6e9ee826cdaafc039f42a0

--- app/models/external_link.rb
+++ app/models/external_link.rb
@@ -3,9 +3,4 @@ class ExternalLink < ApplicationRecord
   belongs_to :project
   translates :url
   accepts_nested_attributes_for :translations
-
-  class Translation
-    validates :url, presence: true
-    validates :external_link_id, uniqueness: { scope: :locale, case_sensitive: false }
-  end
 end
