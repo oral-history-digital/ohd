78928c10c24779cfd2bcbe6960e8abc249838773
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-08-30 11:56:08 +0200

    Project -> Project.current
    Former-commit-id: 0567f9bf7b07f443e6f66f3cda376a3906e265c5

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -7,17 +7,16 @@ class PersonSerializer < ApplicationSerializer
                :text,
                :typology,
              # :histories
-             ] | 
-             MetadataField.where(ref_object_type: 'Person', source: 'registry_reference_type').inject([]) { |mem, i| mem << i.name } |
-             MetadataField.where(source: 'Person').inject([]) { |mem, i| mem << i.name } 
-
+             ] |
+             MetadataField.where(ref_object_type: "Person", source: "registry_reference_type").inject([]) { |mem, i| mem << i.name } |
+             MetadataField.where(source: "Person").inject([]) { |mem, i| mem << i.name }
 
   def names
     object.translations.each_with_object({}) { |i, hsh|
       alpha2_locale = ISO_639.find(i.locale.to_s).alpha2
       hsh[alpha2_locale] = { firstname: i.first_name,
                             lastname: i.last_name,
-                            birthname: i.birth_name } 
+                            birthname: i.birth_name }
     }
   end
 
@@ -55,7 +54,7 @@ class PersonSerializer < ApplicationSerializer
   end
 
   def biographical_entries
-    object.biographical_entries.inject({}) { |mem, c| mem[c.id] = Rails.cache.fetch("#{Project.cache_key_prefix}-biographical_entry-#{c.id}-#{c.updated_at}") { BiographicalEntrySerializer.new(c) }; mem }
+    object.biographical_entries.inject({}) { |mem, c| mem[c.id] = Rails.cache.fetch("#{Project.current.cache_key_prefix}-biographical_entry-#{c.id}-#{c.updated_at}") { BiographicalEntrySerializer.new(c) }; mem }
   end
 
   Project.current.registry_reference_type_metadata_fields.select { |f| f["ref_object_type"] == "person" }.each do |f|
