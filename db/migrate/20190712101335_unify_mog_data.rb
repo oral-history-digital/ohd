497a33ff8f84382a162b840acd9a6f1d700b0561
cgregor <chrgregor@gmail.com>
2019-09-09 20:40:46 +0200

    changed  condition for migration
    Former-commit-id: d42237801b2ce612e83033bfdd3e07d13e277c3e

--- db/migrate/20190712101335_unify_mog_data.rb
+++ db/migrate/20190712101335_unify_mog_data.rb
@@ -1,6 +1,6 @@
 class UnifyMogData < ActiveRecord::Migration[5.2]
   def change
-    if Project.name.to_sym == :mog
+    if Project.identifier.to_sym == :mog
       Contribution.where(contribution_type: 'Informants').update_all contribution_type: 'interviewee'
       Contribution.where(contribution_type: 'Interviewer').update_all contribution_type: 'interviewer'
       Contribution.where(contribution_type: 'Transcribed / described').update_all contribution_type: 'transcriptor'
