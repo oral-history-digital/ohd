ef5226e8713849f06587328540025cc5273690ad
cgregor <chrgregor@gmail.com>
2019-09-11 13:00:55 +0200

    replace Project.current with current_project in controllers
    Former-commit-id: b506c04e81e898609c206117f3f123ea80041c8a

--- app/controllers/registry_references_controller.rb
+++ app/controllers/registry_references_controller.rb
@@ -10,7 +10,7 @@ class RegistryReferencesController < ApplicationController
     @registry_reference = RegistryReference.create(registry_reference_params)
 
     clear_cache(@registry_reference.ref_object)
-    Rails.cache.delete "#{Project.current.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
+    Rails.cache.delete "#{current_project.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
 
     respond_to do |format|
       format.json do
@@ -44,7 +44,7 @@ class RegistryReferencesController < ApplicationController
     @registry_reference.update_attributes registry_reference_params
 
     clear_cache(@registry_reference.ref_object)
-    Rails.cache.delete "#{Project.current.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
+    Rails.cache.delete "#{current_project.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
 
     respond_to do |format|
       format.json do
@@ -66,7 +66,7 @@ class RegistryReferencesController < ApplicationController
     @registry_reference.destroy
 
     clear_cache ref_object
-    Rails.cache.delete "#{Project.current.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
+    Rails.cache.delete "#{current_project.cache_key_prefix}-interview-segments-#{@registry_reference.ref_object.interview.id}-#{@registry_reference.ref_object.interview.segments.maximum(:updated_at)}" if @registry_reference.ref_object_type == 'Segment'
 
     respond_to do |format|
       format.html do
@@ -103,7 +103,7 @@ class RegistryReferencesController < ApplicationController
       format.json do
         interview = Interview.find_by(archive_id: params[:archive_id])
 
-        json = Rails.cache.fetch "#{Project.current.cache_key_prefix}-interview-locations-#{interview.id}-#{interview.updated_at}" do
+        json = Rails.cache.fetch "#{current_project.cache_key_prefix}-interview-locations-#{interview.id}-#{interview.updated_at}" do
           segment_ref_locations = RegistryReference.segments_for_interview(interview.id).with_locations.first(100)
           ref_locations = RegistryReference.for_interview(interview.id).with_locations
           {
@@ -133,7 +133,7 @@ class RegistryReferencesController < ApplicationController
       format.html { render 'react/app' }
       format.json do
         json = {
-          data: @registry_references.inject({}){|mem, s| mem[s.id] = Rails.cache.fetch("#{Project.current.cache_key_prefix}-registry_reference-#{s.id}-#{s.updated_at}"){::RegistryReferenceSerializer.new(s).as_json}; mem},
+          data: @registry_references.inject({}){|mem, s| mem[s.id] = Rails.cache.fetch("#{current_project.cache_key_prefix}-registry_reference-#{s.id}-#{s.updated_at}"){::RegistryReferenceSerializer.new(s).as_json}; mem},
           data_type: 'registry_references',
           extra_params: extra_params
         }.to_json
