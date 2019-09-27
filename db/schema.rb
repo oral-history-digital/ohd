bcaa71f6f404a2258ebe7f4c99ab632e3014edad
cgregor <chrgregor@gmail.com>
2019-09-27 18:51:13 +0200

    change column collections.project_id to integer
    Former-commit-id: 38afa7a30c15dcabb3bc68c0e8f320a8c01b1b5f

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_142046) do
+ActiveRecord::Schema.define(version: 2019_09_27_164046) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -102,7 +102,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_142046) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "homepage"
-    t.string "project_id"
+    t.integer "project_id"
   end
 
   create_table "contributions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
