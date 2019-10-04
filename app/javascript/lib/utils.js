8ba1b7f917a73ee5e2504d3d9ea027b77f34ebcf
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-10-04 16:02:44 +0200

    avoid error 'props.facets[k.replace(...)].subfacets[element] is undefined'
    Former-commit-id: d884a2c072260ee94567fc3a1bdead57290e9af2

--- app/javascript/lib/utils.js
+++ app/javascript/lib/utils.js
@@ -226,7 +226,8 @@ export function queryToText(query, props) {
                 queryText = queryText + nextElement + key + ": ";
                 if (Array.isArray(value)) {
                     value.forEach(function (element, index) {
-                        let val = props.facets[k.replace('[]','')]['subfacets'][element]['name'][props.locale]
+                        let el = props.facets[k.replace('[]','')]['subfacets'][element]
+                        let val = el ? el['name'][props.locale] : ''
                         // let locale_element = (element + '').toLowerCase().split().join('_')
                         // val = val || t(props, 'search_facets')[locale_element];
                         // val = val || locale_element
