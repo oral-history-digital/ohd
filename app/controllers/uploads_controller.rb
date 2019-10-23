e2ec94deac82e69b1b00e0201defa000d7b52a05
cgregor <chrgregor@gmail.com>
2019-10-23 17:08:38 +0200

    missing in last commit
    Former-commit-id: 07f2523d612a4ee3fc14c88c88507a948b5105d7

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -18,7 +18,8 @@ class UploadsController < ApplicationController
     file_path = File.join(Rails.root, 'tmp', file.original_filename)
     File.open(file_path, 'wb') {|f| f.write(file.read) }
 
-    "read_#{upload_params[:type]}_file_job".classify.constantize.perform_later(file_path, current_user_account, current_project)
+    current_locale ||= (params[:locale] || current_project.default_locale).to_sym
+    "read_#{upload_params[:type]}_file_job".classify.constantize.perform_later(file_path, current_user_account, current_project, current_locale)
 
     respond_to do |format|
       format.html { render 'react/app' }
