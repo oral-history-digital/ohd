83ec74460654bfb6011c67d5a16bc9260c33d579
cgregor <chrgregor@gmail.com>
2019-11-05 17:25:24 +0100

    fixing RegisterForm
    Former-commit-id: 854af0d9ba886f4c261ee935daf619d1ab79a959

--- app/javascript/bundles/archive/containers/RegisterFormContainer.js
+++ app/javascript/bundles/archive/containers/RegisterFormContainer.js
@@ -2,14 +2,16 @@ import { connect } from 'react-redux';
 
 import RegisterForm from '../components/RegisterForm';
 import { submitRegister } from '../actions/accountActionCreators';
+import { getProject } from '../../../lib/utils';
 
 const mapStateToProps = (state) => {
+    let project = getProject(state);
     return { 
         projectId: state.archive.projectId,
         locale: state.archive.locale,
         translations: state.archive.translations,
-        country_keys: state.archive.country_keys,
-        externalLinks: state.archive.externalLinks
+        countryKeys: state.archive.countryKeys,
+        externalLinks: project && project.external_links,
     }
 }
 
