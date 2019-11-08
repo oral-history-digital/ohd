c4ebb1ed1e417d63820246f900d775e6296ccb62
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-11-08 15:12:57 +0100

    simpler change of workflow state
    Former-commit-id: f728ebe7d779459fe8a6a4b4a4779664fd18aac7

--- app/javascript/bundles/archive/components/InterviewForm.js
+++ app/javascript/bundles/archive/components/InterviewForm.js
@@ -86,10 +86,11 @@ export default class InterviewForm extends React.Component {
                     elementType: 'select',
                     attribute: 'workflow_state',
                     values: this.props.interview && Object.values(this.props.interview.transitions_to),
+                    value: this.props.interview.workflow_state,
                     optionsScope: 'workflow_states',
-                    help: (
-                        <span>{'=: ' + t(this.props, `workflow_states.${this.props.interview.workflow_state}`)}</span>
-                    )
+                    // help: (
+                    //     <span>{'=: ' + t(this.props, `workflow_states.${this.props.interview.workflow_state}`)}</span>
+                    // )
                 }
             )
         }
