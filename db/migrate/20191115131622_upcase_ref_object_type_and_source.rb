10ac7ff29240d71a9091b90a63559e077d402d16
cgregor <chrgregor@gmail.com>
2019-11-15 14:51:38 +0100

    classify underscore metadata_field-sources as well
    Former-commit-id: 53b57577d04cfa9df581ee64a4f2f0a264aa6b5a

--- db/migrate/20191115131622_upcase_ref_object_type_and_source.rb
+++ db/migrate/20191115131622_upcase_ref_object_type_and_source.rb
@@ -1,6 +1,12 @@
 class UpcaseRefObjectTypeAndSource < ActiveRecord::Migration[5.2]
-  def change
+  def up
     execute "UPDATE metadata_fields SET source = CONCAT(UCASE(LEFT(source, 1)), SUBSTRING(source, 2));"
     execute "UPDATE metadata_fields SET ref_object_type = CONCAT(UCASE(LEFT(ref_object_type, 1)), SUBSTRING(ref_object_type, 2));"
+
+    execute "UPDATE metadata_fields SET source = 'RegistryEntry' WHERE source = 'Registry_entry';"
+    execute "UPDATE metadata_fields SET source = 'RegistryReferenceType' WHERE source = 'Registry_reference_type';"
+  end
+
+  def down
   end
 end
