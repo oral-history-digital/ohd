2acb4b2a62ca09830b3ba9d6f003c467dbca198a
cgregor <chrgregor@gmail.com>
2019-11-26 23:03:30 +0100

    fix migration
    Former-commit-id: 53916b626e6f061ee45ea093f128cc56347f01bc

--- db/migrate/20191126143949_convert_registry_entry_metadata_fields_to_registry_reference_type_metadata_fields.rb
+++ db/migrate/20191126143949_convert_registry_entry_metadata_fields_to_registry_reference_type_metadata_fields.rb
@@ -7,26 +7,29 @@ class ConvertRegistryEntryMetadataFieldsToRegistryReferenceTypeMetadataFields < 
       original_name = metadata_field.name
       singular_name = metadata_field.name.gsub(/s$/, '')
 
-      rr_type = RegistryReferenceType.create(
-          registry_entry_id: RegistryEntry.find_by_code(original_name).id,
-          code: singular_name,
-      )
+      registry_entry = RegistryEntry.find_by_code(original_name)
+      if registry_entry
+        rr_type = RegistryReferenceType.create(
+          registry_entry_id: registry_entry.id,
+            code: singular_name,
+        )
 
-      metadata_field.update_attributes({
-          name: singular_name, 
-          source: "RegistryReferenceType",
-          registry_reference_type_id: rr_type.id,
-          ref_object_type: "Person",
-      })
+        metadata_field.update_attributes({
+            name: singular_name, 
+            source: "RegistryReferenceType",
+            registry_reference_type_id: rr_type.id,
+            ref_object_type: "Person",
+        })
 
-      # after creation of new RegistryReferenceType and renaming of metadata field code into singularis:
+        # after creation of new RegistryReferenceType and renaming of metadata field code into singularis:
 
-      RegistryEntry.find_by_code(original_name).children.map(&:id).map{|id|RegistryReference.where(registry_entry_id: id)}.flatten.each do |rr|
-          rr.update_attributes({
-              registry_reference_type_id: rr_type.id,
-              ref_object_type: "Person",
-              ref_object_id: Interview.find(rr.ref_object_id).interviewees.first.id,
-          })
+        registry_entry.children.map(&:id).map{|id|RegistryReference.where(registry_entry_id: id)}.flatten.each do |rr|
+            rr.update_attributes({
+                registry_reference_type_id: rr_type.id,
+                ref_object_type: "Person",
+                ref_object_id: Interview.find(rr.ref_object_id).interviewees.first.id,
+            })
+        end
       end
     end
   end
