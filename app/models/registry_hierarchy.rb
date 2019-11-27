8b52ea8ba960c4d0df4e10f068c09d1fbb8579be
Rico Simke <rico.simke@cedis.fu-berlin.de>
2019-11-27 12:46:10 +0100

    touch descendants and ancestors after_create and before_update of registry_hierarchy
    Former-commit-id: cedf0da6bfb88d353eca32d52f581a77bdc5bff4

--- app/models/registry_hierarchy.rb
+++ app/models/registry_hierarchy.rb
@@ -3,6 +3,9 @@ require 'acts-as-dag'
 class RegistryHierarchy < ActiveRecord::Base
   acts_as_dag_links :node_class_name => 'RegistryEntry'
 
+  after_create :touch_objects
+  before_destroy :touch_objects
+
   def destroy_or_make_indirect
     if destroyable?
       destroy
@@ -11,5 +14,11 @@ class RegistryHierarchy < ActiveRecord::Base
       save
     end
   end
+
+  def touch_objects
+    ancestor && ancestor.touch
+    descendant && descendant.touch
+  end
+
 
 end
