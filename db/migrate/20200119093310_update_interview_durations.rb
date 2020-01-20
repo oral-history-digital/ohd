b3e3769623852a0126c9c98c6314ecd7c1fa7a7c
cgregor <chrgregor@gmail.com>
2020-01-20 09:40:14 +0100

    improve recalculation of durations
    Former-commit-id: 9987660d2431d642527e916fac584de0c4a4c4c8

--- db/migrate/20200119093310_update_interview_durations.rb
+++ db/migrate/20200119093310_update_interview_durations.rb
@@ -1,6 +1,10 @@
 class UpdateInterviewDurations < ActiveRecord::Migration[5.2]
   def up
-    Interview.where(duration: [nil, 0]).each{|i| i.recalculate_duration! }
+    Interview.where(duration: [nil, 0]).each do |i| 
+      i.recalculate_duration! 
+    rescue
+      p "*** could not recalculate duration for #{i.archive_id}"
+    end
   end
   def down
   end
