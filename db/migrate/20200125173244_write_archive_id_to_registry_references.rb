15c1146432991a4d2be34071231b14e0a0d337e4
cgregor <chrgregor@gmail.com>
2020-01-27 18:51:18 +0100

    improve 20200125173244_write_archive_id_to_registry_references migration
    Former-commit-id: 17bc61ad1ce851d9ffd16052689f9b8ceff8816b

--- db/migrate/20200125173244_write_archive_id_to_registry_references.rb
+++ db/migrate/20200125173244_write_archive_id_to_registry_references.rb
@@ -1,7 +1,7 @@
 class WriteArchiveIdToRegistryReferences < ActiveRecord::Migration[5.2]
   def change
     add_column :registry_references, :archive_id, :string
-    RegistryReference.find_each do |rr|
+    RegistryReference.where(archive_id: nil).find_each do |rr|
       if rr.ref_object_type == "Interview"
         rr.update_attributes archive_id: Interview.find(rr.ref_object_id).archive_id
       elsif rr.ref_object_type == "Person"
