19abeb6e607b1c4a0203f12e8b8cb93f6536cc22
cgregor <chrgregor@gmail.com>
2020-02-03 18:27:49 +0100

    INTARCH-628: set children_only-attribute in registry_reference_types to true (default)
    Former-commit-id: 077bf5a0a82ab41975e6ae920f659c655dcf471d

--- db/migrate/20191127152639_add_children_only_to_registry_reference_types.rb
+++ db/migrate/20191127152639_add_children_only_to_registry_reference_types.rb
@@ -1,5 +1,6 @@
 class AddChildrenOnlyToRegistryReferenceTypes < ActiveRecord::Migration[5.2]
   def change
-    add_column :registry_reference_types, :children_only, :boolean, default: false
+    add_column :registry_reference_types, :children_only, :boolean, default: true
+    RegistryReferenceType.update_all children_only: true
   end
 end
