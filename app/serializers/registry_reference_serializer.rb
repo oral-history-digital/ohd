fb2d2a80fc50e309311803240e371d8b55fc7136
cgregor <chrgregor@gmail.com>
2020-02-25 17:48:19 +0100

    put ref_info and ref_details into registry_reference_serializer as well
    Former-commit-id: 6d42ebff5f3d0ddc1bc37e15d2f81c9c7768beb7

--- app/serializers/registry_reference_serializer.rb
+++ app/serializers/registry_reference_serializer.rb
@@ -4,6 +4,8 @@ class RegistryReferenceSerializer < ApplicationSerializer
              :ref_object_type,
              :registry_entry_id,
              :registry_reference_type_id,
-             :archive_id
+             :archive_id,
+             :ref_info,
+             :ref_details
 
 end
