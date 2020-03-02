e0eff0f0142d84c65afb5176bcb27bb44cee49c5
cgregor <chrgregor@gmail.com>
2020-03-02 18:10:02 +0100

    improve tape_number on segments migration
    Former-commit-id: f1c5fedcfd2d89346873048b0353dc9e19f96a76

--- db/migrate/20200106172235_write_tape_number_to_segments.rb
+++ db/migrate/20200106172235_write_tape_number_to_segments.rb
@@ -1,7 +1,7 @@
 class WriteTapeNumberToSegments < ActiveRecord::Migration[5.2]
   def change
     Tape.find_each(batch_size: 200) do |tape|
-      tape.segments.update_all(tape_number: tape.number)
+      tape.segments.where(tape_number: nil).update_all(tape_number: tape.number)
     end
   end
 end
