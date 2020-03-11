7d5b43c189cbe4d2a54aa1de52cfe7ccf56be182
cgregor <chrgregor@gmail.com>
2020-03-11 22:26:39 +0100

    Revert "comment initializer only once for the first deploy to production"
    This reverts commit c87d72448a205444182b5ddc441b5f51233a3e69 [formerly aab29cbd3baed22a1a0e49fdbdd4e2b2e854327d].
    
    
    Former-commit-id: ce2fc3968dea1e1bdc2639220968c859c0b27515

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-#Rails.application.config.assets.precompile += %w( cluster.js responsive.js ) + Project.all.map{|i| "responsive-#{i.shortname.downcase}.css"} 
+Rails.application.config.assets.precompile += %w( cluster.js responsive.js ) + Project.all.map{|i| "responsive-#{i.shortname.downcase}.css"} 
