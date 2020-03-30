bdd9d425a2c4094c94f2197969c37637e7824aea
cgregor <chrgregor@gmail.com>
2020-03-30 17:40:16 +0200

    refer to RegistryReferenceType instead of RegistryEntry on facets etc
    Former-commit-id: 5fbeb60c42eaa399349a92408581185035c68dba

--- db/migrate/20200330062445_associate_registry_stuff_to_metadata_fields.rb
+++ db/migrate/20200330062445_associate_registry_stuff_to_metadata_fields.rb
@@ -1,11 +1,13 @@
 class AssociateRegistryStuffToMetadataFields < ActiveRecord::Migration[5.2]
   def change
-    Project.first.registry_entry_search_facets.each do |facet| 
-      facet.registry_entry = RegistryEntry.find_by_code(facet.name)
-      facet.save
-    end
-    Project.first.registry_reference_type_search_facets.each do |facet| 
-      facet.registry_reference_type = RegistryReferenceType.find_by_code(facet.name)
+    Project.first.registry_reference_type_search_facets.each do |facet|
+      rrt  = RegistryReferenceType.find_by_code(facet.name)
+      unless rrt
+        re = RegistryEntry.find_by_code(facet.name)
+        rrt = RegistryReferenceType.create registry_entry_id: re.id, code: facet.name, children_only: true
+      end
+      facet.registry_entry = re
+      facet.registry_reference_type = rrt
       facet.save
     end
   end
