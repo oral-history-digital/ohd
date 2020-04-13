438720cd9ca1c0ab990f75f6627b5dc24ed5201c
cgregor <chrgregor@gmail.com>
2020-04-13 19:29:10 +0200

    set all contributions and references to public or checked
    Former-commit-id: 851c3d0e30892e46b62c32799ebe5fe527f29298

--- db/migrate/20200411143647_add_workflow_state_to_contributions.rb
+++ db/migrate/20200411143647_add_workflow_state_to_contributions.rb
@@ -1,5 +1,7 @@
 class AddWorkflowStateToContributions < ActiveRecord::Migration[5.2]
   def change
     add_column :contributions, :workflow_state, :string, default: 'unshared'
+    Contribution.update_all workflow_state: 'public'
+    RegistryReference.update_all workflow_state: 'checked'
   end
 end
