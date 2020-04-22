82885eda03c035568b3b71b304b78548523b1433
cgregor <chrgregor@gmail.com>
2020-04-22 11:31:11 +0200

    rm features tests
    Former-commit-id: 47b8bced25f90f98b179a6f9d41a0c31c416ebc3

--- features/support/test_pages/registration_confirmation_page.rb
+++
@@ -1,9 +0,0 @@
-class TestPages::RegistrationConfirmationPage < TestPages::ApplicationPage
-  def path
-    '/anmeldung/user_registrations'
-  end
-
-  def a_success_message_element
-    'h1:contains("Ihre Registrierung ist eingegangen")'
-  end
-end
