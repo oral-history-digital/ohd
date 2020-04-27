808551da1a9b5ade385965a428a53b3b46a7fe91
cgregor <chrgregor@gmail.com>
2020-04-27 14:43:14 +0200

    missing dir.up and down added to transaltion
    Former-commit-id: f850873887efb7000ad8a0c507c8cb6baabd95bf

--- db/migrate/20200425122722_rename_project_rgb_color_columns.rb
+++ db/migrate/20200425122722_rename_project_rgb_color_columns.rb
@@ -6,25 +6,29 @@ class RenameProjectRgbColorColumns < ActiveRecord::Migration[5.2]
 
     reversible do |dir|
 
-      primary_colors = {
-        mog: '#006fb7',
-        zwar: '#8f201c',
-        cdoh: '#006169',
-        campscapes: '#730033',
-        dg: '#006fb7'
-      }
+      dir.up do
+        primary_colors = {
+          mog: '#006fb7',
+          zwar: '#8f201c',
+          cdoh: '#006169',
+          campscapes: '#730033',
+          dg: '#006fb7'
+        }
 
-      secondary_colors = {
-        mog: '#d6c537',
-        zwar: '#8f201c',
-        cdoh: '#efae0f',
-        campscapes: '#021E62',
-        dg: '#006fb7'
-      }
+        secondary_colors = {
+          mog: '#d6c537',
+          zwar: '#8f201c',
+          cdoh: '#efae0f',
+          campscapes: '#021E62',
+          dg: '#006fb7'
+        }
 
-      %w(mog zwar cdoh campscapes dg).each do |shortname|
-        project = Project.where("lower(shortname) = ?", shortname).first
-        project && project.update_attributes(primary_color: primary_colors[shortname.to_sym], secondary_color: secondary_colors[shortname.to_sym])
+        %w(mog zwar cdoh campscapes dg).each do |shortname|
+          project = Project.where("lower(shortname) = ?", shortname).first
+          project && project.update_attributes(primary_color: primary_colors[shortname.to_sym], secondary_color: secondary_colors[shortname.to_sym])
+        end
+      end
+      dir.down do
       end
     end
   end
