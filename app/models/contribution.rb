be6aef9556c8024b990c39f361e761a0a1a5dcb3
Doris Maassen <doris.maassen@cedis.fu-berlin.de>
2020-05-11 19:32:57 +0200

    fix labels and config for contributors
    Former-commit-id: 30e0082039e1b9fd706d6f35dc55d4b7b3b844f8

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,12 +4,12 @@ class Contribution < ApplicationRecord
   # a contribution to the Archive in a specific area (contribution_type)
 
   belongs_to :interview
-  belongs_to :person, 
+  belongs_to :person,
     -> { includes(:translations) }
 
   validates_associated :interview, :person
   validates_presence_of :contribution_type
-  validates :contribution_type, inclusion: %w(interviewee interviewer transcriptor translator segmentator cinematographer quality_manager proofreader)
+  validates :contribution_type, inclusion: %w(interviewee interviewer transcriptor translator segmentator cinematographer quality_manager_interviewing proofreader)
   validates_uniqueness_of :person_id, :scope => [ :interview_id, :contribution_type ]
 
   after_save :touch_interview
