43a8d33ed65d54c79cb5cbe7bf98474fc8e575e9
cgregor <chrgregor@gmail.com>
2020-05-19 20:06:33 +0200

    fixing typology-migration
    Former-commit-id: 2b8d363fe17d090e2389169fa7ff247b3d07234e

--- db/migrate/20200505181812_transform_typologies_to_registry_entries.rb
+++ db/migrate/20200505181812_transform_typologies_to_registry_entries.rb
@@ -3,10 +3,10 @@ class TransformTypologiesToRegistryEntries < ActiveRecord::Migration[5.2]
     if Project.current.identifier.to_sym == :mog
       typology_rrt = RegistryReferenceType.create name: 'Typology', code: 'typology'
 
-      typology_re = RegistryEntry.create_with_parent_and_names(1, "de::Typologie##el::Εμπειρία", 'typology')
+      typology_re = RegistryEntry.create_with_parent_and_names(1, "de::Typologie#el::Εμπειρία", 'typology')
 
       %w(resistance occupation flight concentration_camp persecution_of_jews retaliation collaboration).each do |typology|
-        RegistryEntry.create_with_parent_and_names(typology_re.id, "de::#{I18n.t(typology, :de)}##el::#{I18n.t(typology, :el)}", typology)
+        RegistryEntry.create_with_parent_and_names(typology_re.id, "de::#{I18n.t(typology, :de, scope: 'search_facets')}#el::#{I18n.t(typology, :el, scope: 'search_facets')}", typology)
       end
 
       Person.all.each do |person|
