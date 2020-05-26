3055a0388219d0ee54d662e15b707d84ab939585
cgregor <chrgregor@gmail.com>
2020-05-26 13:56:31 +0200

    set missing speaker_id s on mog segments
    Former-commit-id: ede0fd50acbe49f4b0f19fe54f4972a3a787f22b

---
+++ db/migrate/20200526114905_set_speaker_ids_on_mog_segments.rb
@@ -0,0 +1,15 @@
+class SetSpeakerIdsOnMogSegments < ActiveRecord::Migration[5.2]
+  def change
+    if Project.first.initials == 'mog'
+      Interview.all.each do |i| 
+        i.tapes.each do |t| 
+          t.segments.each do |s|
+            if s.speaker_id.nil?
+              s.update_attributes(speaker_id: s.prev.speaker_id) if s.prev.speaker_id
+            end
+          end
+        end
+      end
+    end
+  end
+end
