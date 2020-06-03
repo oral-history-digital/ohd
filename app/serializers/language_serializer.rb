b513aec739314067338769b3cfd384cd3e16cb7e
cgregor <chrgregor@gmail.com>
2020-06-03 17:00:03 +0200

    add language code to serialization
    Former-commit-id: f11489ee3d0651944653e3da1a9285b15c1d69f2

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -2,7 +2,8 @@ class LanguageSerializer < ApplicationSerializer
   #languages: Language.all.map { |c| { value: c.id.to_s, name: c.localized_hash, locale: ISO_639.find(c.code.split(/[\/-]/)[0]).alpha2 } },
   attributes :id,
     :name,
-    :locale
+    :locale,
+    :code
 
   def name
     object.localized_hash(:name)
