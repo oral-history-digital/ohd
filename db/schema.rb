64499d53931a1b67256a34bb277c0c11a2997a74
cgregor <chrgregor@gmail.com>
2020-06-05 11:34:40 +0200

    updated schema
    Former-commit-id: 9aed893a14f5ba607965ae6c5c49a97e94ffdf99

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_114905) do
+ActiveRecord::Schema.define(version: 2020_06_04_140041) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", limit: 255, null: false
