5595a092c1095c7bd6d6e8d420e2f0facf03641e
cgregor <chrgregor@gmail.com>
2020-06-17 15:04:22 +0200

    finish cleanup: mv properties from interview to interviewee
    Former-commit-id: 6f8c17dec79ad92e2f763ac35189e9822dc1a2c9

--- app/javascript/bundles/archive/components/InterviewPreview.js
+++ app/javascript/bundles/archive/components/InterviewPreview.js
@@ -102,15 +102,17 @@ export default class InterviewPreview extends React.Component {
     interviewDetails() {
         let interviewee = getInterviewee(this.props);
         let yearOfBirth = interviewee && interviewee.year_of_birth;
+        let forcedLaborGroup = interviewee && interviewee.forced_labor_group[this.props.locale];
+        let forcedLaborField = interviewee && interviewee.forced_labor_field[this.props.locale];
 
         if (this.props.projectId === 'zwar') {
             return (
                 <div className={'search-result-data'} lang={this.props.locale}>
                     <span>{t(this.props, `search_facets.${this.props.interview.media_type}`)}</span> <span>{this.props.interview.duration_human}</span><br/>
                     <span>{this.props.interview.language[this.props.locale]}</span>
-                    <small className={this.facetToClass("forced-labor-group")}><br/>{this.props.interview.forced_labor_group[this.props.locale]}</small>
+                    <small className={this.facetToClass("forced-labor-group")}><br/>{forcedLaborGroup}</small>
                     <small className={this.facetToClass("year-of-birth")}><br/>{t(this.props, 'year_of_birth')} {yearOfBirth}</small>
-                    <small className={this.facetToClass("forced-labor-field")}><br/>{this.props.interview.forced_labor_field[this.props.locale]}</small>
+                    <small className={this.facetToClass("forced-labor-field")}><br/>{forcedLaborField}</small>
                 </div>
             );
         }
