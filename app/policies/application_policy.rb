80a374381be08fc4172f4f975b26b464b6cd7f57
cgregor <chrgregor@gmail.com>
2020-06-17 13:15:34 +0200

    fit policies to class and instance
    Former-commit-id: 682da904de03f2688afec61b3080707538629d9e

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -15,23 +15,23 @@ class ApplicationPolicy
   end
 
   def create?
-    user.admin? || user.permissions?(record.class_name, :create)
+    user.admin? || user.permissions?(resolve_class_name(record), :create)
   end
 
   def new?
-    user.admin? || user.permissions?(record.class_name, :new)
+    user.admin? || user.permissions?(resolve_class_name(record), :new)
   end
 
   def update?
-    user.admin? || user.permissions?(record.class_name, :update) || user.tasks?(record) 
+    user.admin? || user.permissions?(resolve_class_name(record), :update) || user.tasks?(record) 
   end
 
   def edit?
-    user.admin? || user.permissions?(record.class_name, :edit) || user.tasks?(record) 
+    user.admin? || user.permissions?(resolve_class_name(record), :edit) || user.tasks?(record) 
   end
 
   def destroy?
-    user.admin? || user.permissions?(record.class_name, :destroy)
+    user.admin? || user.permissions?(resolve_class_name(record), :destroy)
   end
 
   class Scope
@@ -46,4 +46,9 @@ class ApplicationPolicy
       scope.all
     end
   end
+
+  private
+    def resolve_class_name(record)
+      record.respond_to?(:class_name) ? record.class_name : record.class.name
+    end
 end
