aa6e01a3f9e57e2526af8c37f1fb5818fd223a27
cgregor <chrgregor@gmail.com>
2020-06-20 18:25:47 +0200

    bulk import for registry entries
    Former-commit-id: 6d2c7f8913dae98ae0ddb0aa2e96a921bc154998

---
+++ app/jobs/read_bulk_registry_entries_file_job.rb
@@ -0,0 +1,94 @@
+class ReadBulkRegistryEntriesFileJob < ApplicationJob
+  queue_as :default
+
+  def perform(file_path, receiver, project, locale)
+    read_file(file_path, project, locale)
+
+    #WebNotificationsChannel.broadcast_to(
+      #receiver,
+      #title: 'edit.upload_bulk_metadata.processed',
+      #file: File.basename(file_path),
+    #)
+
+    AdminMailer.with(receiver: receiver, type: 'read_campscape', file: file_path).finished_job.deliver_now
+    #File.delete(file_path) if File.exist?(file_path)
+  end
+
+  def read_file(file_path, project, locale)
+    I18n.locale = locale
+
+    csv_options = { col_sep: ";", row_sep: :auto, quote_char: "\x00", encoding: "windows-1251:utf-8" }
+    csv = Roo::CSV.new(file_path, csv_options: csv_options)
+    if csv.first.length == 1
+      csv_options.update(col_sep: "\t")
+      csv = Roo::CSV.new(file_path, csv_options: csv_options)
+    end
+
+    csv.each_with_index do |data, index|
+      unless index == 0
+
+        parent_name = data[0]
+        parent_id   = data[1]
+        name        = data[2]
+        id          = data[3]
+        description = data[4]
+        latitude    = data[5]
+        longitude   = data[6]
+
+        begin
+          unless name.blank? && id.blank?
+            entry = nil
+            entry_attributes = {
+              descriptor: name, 
+              code: name && name.downcase.gsub(/\s+/, "_"),
+              latitude: latitude,
+              longitude: longitude,
+              workflow_state: 'public',
+              list_priority: false
+            }
+            parent = nil
+
+            #
+            # if id is given the entry will be updated,
+            # otherwise it will be created
+            #
+            if id
+              entry = RegistryEntry.find(id)
+              entry.update_attributes(entry_attributes)
+            else
+              entry = RegistryEntry.create(entry_attributes)
+              RegistryName.create registry_entry_id: entry.id, registry_name_type_id: 1, name_position: 0, descriptor: name, locale: locale
+            end
+
+            #
+            # Parent`s attributes won`t update!
+            # A parent is searched for by id if given.
+            # Otherwise a parent is searched by name.
+            #
+            # To update a parent`s attributes, the parent has to be written in a row with it`s 
+            # id and attributes like a normal entry.
+            #
+            if parent_id
+              parent = RegistryEntry.find(parent_id)
+            elsif !parent_id && parent_name
+              parent_name = RegistryName.where(descriptor: parent_name).first
+              parent = parent_name && parent_name.registry_entry
+            end
+
+            RegistryHierarchy.find_or_create_by(ancestor_id: parent.id, descendant_id: entry.id) if parent
+          end
+        rescue StandardError => e
+          log("#{e.message}: #{e.backtrace}")
+        end
+      end
+    end
+  end
+
+  def log(text, error=true)
+    File.open(File.join(Rails.root, 'log', 'registry_entries_import.log'), 'a') do |f|
+      f.puts "* #{DateTime.now} - #{error ? 'ERROR' : 'INFO'}: #{text}"
+    end
+  end
+
+end
+
