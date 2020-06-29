2339d43766ba4f00d39db71b8fe813aa36580ea7
Doris Maassen <doris.maassen@cedis.fu-berlin.de>
2020-06-29 15:31:53 +0200

    remove devise plugin which was used to use user as a proxy for user_account
    Former-commit-id: e523b59a1dfd937d8dc8fb4319c30362cba99507

--- vendor/plugins/cedis_archive/lib/authentication_extension.rb
+++
@@ -1,3 +0,0 @@
-require 'devise'
-
-ActionController::Base.send :include, Devise::Controllers::ArchiveAuthenticationHelpers
--- vendor/plugins/cedis_archive/lib/cedis_archive.rb
+++ vendor/plugins/cedis_archive/lib/cedis_archive.rb
@@ -1,6 +1,2 @@
 # CeDiS Archive
 require 'sunspot/media_id_adapter'
-
-require 'tag_extension'
-
-require 'authentication_extension'
