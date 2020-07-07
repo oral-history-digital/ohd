dcd9759753b2b13a55e45b542848d0c9b657ed54
cgregor <chrgregor@gmail.com>
2020-07-07 13:21:49 +0200

    limit index size
    Former-commit-id: 7251dcf88616407bcb8c5195f42a1bc0604a90a7

--- db/migrate/20200628131445_add_index_on_registry_codes.rb
+++ db/migrate/20200628131445_add_index_on_registry_codes.rb
@@ -1,6 +1,6 @@
 class AddIndexOnRegistryCodes < ActiveRecord::Migration[5.2]
   def change
-    add_index  :registry_entries, :code
-    add_index  :registry_reference_types, :code
+    add_index  :registry_entries, :code, length: 50
+    add_index  :registry_reference_types, :code, length: 50
   end
 end
