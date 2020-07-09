fef8265a9e6d46b2838f01b269f2241e248a2244
cgregor <chrgregor@gmail.com>
2020-07-09 14:30:04 +0200

    fix user_registration.register: delete some more user_registration-attributes before saving them  to  user_account - thus preventing rollback - thus deliver updated json
    Former-commit-id: 2708b46cba303eb68d85ecccbf428cf75002d0ee

--- app/models/user_registration.rb
+++ app/models/user_registration.rb
@@ -207,6 +207,8 @@ EVAL
   def save_registration_data_and_user_data_to_user_account
     reg_attrs = self.attributes
     reg_attrs.delete('id')
+    reg_attrs.delete('workflow_state')
+    reg_attrs.delete('user_account_id')
     reg_attrs.delete('email') # user account already contains the email
     reg_attrs.delete('application_info') # we do not need the YAML field
     self.user_account.update_attributes(reg_attrs)
