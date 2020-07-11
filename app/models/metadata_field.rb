3a5d0e911b0f82f68c1f04e987f24bf225c341c6
cgregor <chrgregor@gmail.com>
2020-07-11 14:08:44 +0200

    allow several metadata_fields with same name per project if source is different
    Former-commit-id: 81d69fea4779f2b1bba4f21ec4e993d893a3fe63

--- app/models/metadata_field.rb
+++ app/models/metadata_field.rb
@@ -8,6 +8,6 @@ class MetadataField < ApplicationRecord
   translates :label, fallbacks_for_empty_translations: true, touch: true
   accepts_nested_attributes_for :translations
   serialize :values
-  validates_uniqueness_of :name, :scope => [:project_id]
+  validates_uniqueness_of :name, :scope => [:project_id, :source]
 
 end
