1a0869f5e3162fd370493d5b629c6f825e0c8227
cgregor <chrgregor@gmail.com>
2020-07-12 16:04:39 +0200

    indention fix
    Former-commit-id: 85d1b3aa2dfe3170e7e1ec6815099501cdf179c4

--- app/javascript/bundles/archive/components/EditPerson.js
+++ app/javascript/bundles/archive/components/EditPerson.js
@@ -52,7 +52,7 @@ export default class EditPerson extends React.Component {
         return (
             <WrapperPageContainer tabIndex={tabIndex}>
                 <AuthShowContainer ifLoggedIn={true}>
-		  <h1 className='registry-entries-title'>{t(this.props, `edit.person.new`)}</h1>
+                    <h1 className='registry-entries-title'>{t(this.props, `edit.person.new`)}</h1>
                     {this.content()}
                 </AuthShowContainer>
                 <AuthShowContainer ifLoggedOut={true}>
