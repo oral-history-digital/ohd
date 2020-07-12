b0fec3ea0410b0fe13c36bd2017c3eebe9827fc5
cgregor <chrgregor@gmail.com>
2020-07-12 14:18:55 +0200

    fixing account is undefined
    Former-commit-id: 8be15ff087f1715369ba14777b8cd0cfbf066d52

--- app/javascript/bundles/archive/components/AuthShow.js
+++ app/javascript/bundles/archive/components/AuthShow.js
@@ -8,16 +8,15 @@ export default class AuthShow extends React.Component {
             // admin
             return this.props.children;
         } else if (
-            (this.props.isLoggedIn && this.props.ifLoggedIn && this.props.account.project_ids.indexOf(this.props.projectId) > -1 ) ||
+            (this.props.isLoggedIn && this.props.ifLoggedIn && this.props.account && this.props.account.project_ids.indexOf(this.props.projectId) > -1 ) ||
             (this.props.project.isCatalog && this.props.ifCatalog)
-            )
-        {
+        ) {
             // logged in and registered for the current project
             return this.props.children;
         } else if (
             (
                 !this.props.isLoggedIn ||
-                (this.props.isLoggedIn && this.props.account.project_ids.indexOf(this.props.projectId) === -1)
+                (this.props.isLoggedIn && this.props.account && this.props.account.project_ids.indexOf(this.props.projectId) === -1)
             ) &&
             this.props.ifLoggedOut
         ) {
