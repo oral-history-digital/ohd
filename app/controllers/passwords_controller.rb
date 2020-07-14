6c57a8a35f4cd126a440ae62a2bd5c712e03ff2d
Doris Maassen <doris.maassen@cedis.fu-berlin.de>
2020-07-14 15:04:54 +0200

    remove unused devise unlocking templates
    Former-commit-id: 4066545564b4930a09624733b911fd3860d328bd

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,4 @@
-class PasswordsController < Devise::PasswordsController  
+class PasswordsController < Devise::PasswordsController
 
   skip_before_action :require_no_authentication
   skip_after_action :verify_authorized
@@ -13,25 +13,24 @@ class PasswordsController < Devise::PasswordsController
     yield resource if block_given?
 
     if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
       if Devise.sign_in_after_reset_password
         flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
         set_flash_message!(:notice, flash_message)
         sign_in(resource_name, resource)
       else
         set_flash_message!(:notice, :updated_not_active)
-      end 
+      end
       #respond_with resource, location: after_resetting_password_path_for(resource)
       render json: resource
     else
       set_minimum_password_length
       #respond_with resource
       render json: {active: true, error: 'devise.sessions.invalid_token'}
-    end 
-  end 
+    end
+  end
 
   #def resource_params
     #params.require(resource_name).permit(:password, :password_confirmation, :reset_password_token)
-  #end 
+  #end
 
 end
