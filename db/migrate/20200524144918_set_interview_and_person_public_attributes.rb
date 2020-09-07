c7640f1b2995b90d058d5bf9d2aec766e394bebb
cgregor <chrgregor@gmail.com>
2020-09-07 13:36:33 +0200

    updating migration to set public attributes
    Former-commit-id: 745bd9e44dc857f98dc7022809481f795b38fb53

--- db/migrate/20200524144918_set_interview_and_person_public_attributes.rb
+++ db/migrate/20200524144918_set_interview_and_person_public_attributes.rb
@@ -2,7 +2,11 @@ class SetInterviewAndPersonPublicAttributes < ActiveRecord::Migration[5.2]
   def change
     interview_attributes = %w(archive_id media_type interview_date duration tape_count language_id collection_id observations)
     person_attributes = %w(first_name last_name alias_names other_first_names gender date_of_birth)
-    Interview.update_all(properties: {public_attributes: interview_attributes.inject({}){|mem, att| mem[att] = true; mem}})
-    Person.update_all(properties: {public_attributes: person_attributes.inject({}){|mem, att| mem[att] = true; mem}})
+    Interview.all.each do |interview|
+      interview.update_attributes properties: interview.properties.update(public_attributes: interview_attributes.inject({}){|mem, att| mem[att] = true; mem})
+    end
+    Person.all.each do |person| 
+      person.update_attributes properties: person.properties.update(public_attributes: person_attributes.inject({}){|mem, att| mem[att] = true; mem})
+    end
   end
 end
