6d616d0f52dbc6c2b704aca6dd53776feefbc88c
cgregor <chrgregor@gmail.com>
2020-09-21 11:46:46 +0200

    fix
    Former-commit-id: c910755bf1ff359aae8a78df4de64f25bb088f78

--- app/javascript/bundles/archive/components/AdminActions.js
+++ app/javascript/bundles/archive/components/AdminActions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { t } from '../../../lib/utils';
+import { t, admin } from '../../../lib/utils';
 
 export default class AdminActions extends React.Component {
 
