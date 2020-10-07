250826dbb8bf8e9e1d5eab5fae27c1447730f5c2
cgregor <chrgregor@gmail.com>
2020-10-07 18:26:03 +0200

    change back to development branch zwar-live
    Former-commit-id: 207f0abc4c1888356da031a87cb4672fcb0ed732

--- config/deploy/zwar_archive.rb
+++ config/deploy/zwar_archive.rb
@@ -1,8 +1,7 @@
 server "deploy_da03", roles: %w{app db web}
 
 set :application, "zwar_archive"
-#set :branch, :development
-set :branch, 'deploy/prepare_zwar_live'
+set :branch, :development
 set :stage, :production
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
