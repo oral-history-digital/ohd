b9dc2d7d7ccab2b9a9a05361efe7c062a990e8bc
cgregor <chrgregor@gmail.com>
2020-10-20 16:52:19 +0200

    cache biographical entries pdf
    Former-commit-id: 206cfd499434f457560b9addabe056c984b7766e

--- app/controllers/biographical_entries_controller.rb
+++ app/controllers/biographical_entries_controller.rb
@@ -24,7 +24,9 @@ class BiographicalEntriesController < ApplicationController
     @biography = true
     respond_to do |format|
       format.pdf do
-        pdf = render_to_string(:template => '/latex/biographical_entries.pdf.erb', :layout => 'latex.pdf.erbtex')
+        pdf = Rails.cache.fetch("#{current_project.cache_key_prefix}-biographical-entries-#{@interview.archive_id}-#{@interview.interviewees.first.biographical_entries.maximum(:updated_at)}") do
+          render_to_string(:template => '/latex/biographical_entries.pdf.erb', :layout => 'latex.pdf.erbtex')
+        end
         send_data pdf, filename: "#{@interview.archive_id}_biography_#{params[:lang]}.pdf", :type => "application/pdf", :disposition => "attachment"
       end
     end
