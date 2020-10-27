b564ce1ddd6b3f14d914f0acab75dbc2efd44cde
cgregor <chrgregor@gmail.com>
2020-10-27 14:54:59 +0100

    fix extend  projects migration
    Former-commit-id: 39434655a550c06c54c9d5ffb367070297bad4da

--- db/migrate/20201026132241_extend_projects.rb
+++ db/migrate/20201026132241_extend_projects.rb
@@ -1,25 +1,33 @@
 class ExtendProjects < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :projects, :has_map, :boolean
     add_column :projects, :aspect_x, :integer
     add_column :projects, :aspect_y, :integer
     add_column :projects, :archive_id_number_length, :integer
+
+    attributes = {
+      aspect_x: 4, 
+      aspect_y: 3,
+      archive_id_number_length: 3
+    }
+
+    if [:cdoh, :mog].include?(Project.current.identifier.to_sym)
+      attributes[:aspect_x] = 16
+      attributes[:aspect_y] = 9
+    end
+
+    if Project.current.identifier.to_sym == :zwar
+      attributes[:has_map] = true
+    end
+
+    Project.reset_column_information
+    Project.current.update_attributes(attributes)
   end
 
-  attributes = {
-    aspect_x: 4, 
-    aspect_y: 3,
-    archive_id_number_length: 3
-  }
-
-  if [:cdoh, :mog].includes?(Project.current.identifier.to_sym)
-    attributes[:aspect_x] = 16
-    attributes[:aspect_x] = 9
+  def down
+    remove_column :projects, :has_map
+    remove_column :projects, :aspect_x
+    remove_column :projects, :aspect_y
+    remove_column :projects, :archive_id_number_length
   end
-
-  if Project.current.identifier.to_sym == :zwar
-    attributes[:has_map] = true
-  end
-
-  Project.current.update_attributes(attributes)
 end
