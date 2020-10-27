9f384da7c27657e269430ece4b95ce96c75fbc8c
cgregor <chrgregor@gmail.com>
2020-10-27 13:30:09 +0100

    set obligatory project-attributes in migration
    Former-commit-id: e952077a362ef11384d9d39da3d4c25ae362a13c

--- db/migrate/20201026132241_extend_projects.rb
+++ db/migrate/20201026132241_extend_projects.rb
@@ -5,4 +5,21 @@ class ExtendProjects < ActiveRecord::Migration[5.2]
     add_column :projects, :aspect_y, :integer
     add_column :projects, :archive_id_number_length, :integer
   end
+
+  attributes = {
+    aspect_x: 4, 
+    aspect_y: 3,
+    archive_id_number_length: 3
+  }
+
+  if [:cdoh, :mog].includes?(Project.current.identifier.to_sym)
+    attributes[:aspect_x] = 16
+    attributes[:aspect_x] = 9
+  end
+
+  if Project.current.identifier.to_sym == :zwar
+    attributes[:has_map] = true
+  end
+
+  Project.current.update_attributes(attributes)
 end
