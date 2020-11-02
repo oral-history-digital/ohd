ae00e93adffd91ee332dc5881253760ae7d554b5
Marc Altmann <marcaltmann@posteo.de>
2020-11-02 21:11:06 +0100

    Do not hard-code breakpoints
    Former-commit-id: 06fc320f851c3cb696dc92a110db467a94b7c66c

--- app/javascript/lib/media-queries.js
+++ app/javascript/lib/media-queries.js
@@ -6,9 +6,12 @@ export const SCREEN_WIDTH_NARROW = 0;
 export const SCREEN_WIDTH_MEDIUM = 1;
 export const SCREEN_WIDTH_WIDE   = 2;
 
+const BREAKPOINT_MEDIUM = 768;
+const BREAKPOINT_WIDE = 1200;
+
 export function currentScreenWidth() {
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    if (window.matchMedia('(min-width: 1200px)').matches) {
+  if (window.matchMedia(`(min-width: ${BREAKPOINT_MEDIUM}px)`).matches) {
+    if (window.matchMedia(`(min-width: ${BREAKPOINT_WIDE}px)`).matches) {
       return SCREEN_WIDTH_WIDE;
     } else {
       return SCREEN_WIDTH_MEDIUM;
