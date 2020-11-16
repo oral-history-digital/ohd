e0dbb4b38a4df1b6a53e2f2d746a753cac478585
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2020-11-16 10:32:48 +0100

    Refactor PopupMenu with hooks
    Former-commit-id: aa4723ce2ec5ee5ad4084a4f4f09d71dacf9ef76

--- app/javascript/bundles/archive/components/PopupMenu.js
+++ app/javascript/bundles/archive/components/PopupMenu.js
@@ -1,60 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-
 import { t } from '../../../lib/utils';
 
-class PopupMenu extends Component {
-    constructor(props) {
-        super(props);
+function PopupMenu(props) {
+    const [isOpen, setIsOpen] = useState(false);
 
-        this.state = { isOpen: false };
-        this.open = this.open.bind(this);
-        this.close = this.close.bind(this);
-    }
+    const open = () => setIsOpen(true);
+    const close = () => setIsOpen(false);
 
-    open() {
-        this.setState({ isOpen: true });
-    }
-
-    close() {
-        this.setState({ isOpen: false });
-    }
-
-    render() {
-        const { children } = this.props;
-        const { isOpen } = this.state;
-
-        return isOpen ?
-            (<OutsideClickHandler onOutsideClick={this.close}>
-                <div
-                    className="flyout-sub-tabs-content-ico-link"
-                    title={t(this.props, 'more')}
-                    onClick={this.close}
-                >
-                    <i className="fa fa-ellipsis-v" />
-                </div>
-                <div className={classNames('popup-menu', {
-                    'popup-menu--invisible': !isOpen})}>
-                    <i
-                        className='popup-menu__close fa-times fa'
-                        onClick={this.close}
-                    />
-                    <ul className="popup-menu__list">
-                        {children}
-                    </ul>
-                </div>
-            </OutsideClickHandler>) : (
-                <div
-                    className="flyout-sub-tabs-content-ico-link"
-                    title={t(this.props, 'more')}
-                    onClick={this.open}
-                >
-                    <i className="fa fa-ellipsis-v" />
-                </div>
-            );
-    }
+    return isOpen ?
+        (<OutsideClickHandler onOutsideClick={close} display="inline-block">
+            <div
+                className="flyout-sub-tabs-content-ico-link"
+                title={t(props, 'more')}
+                onClick={close}
+            >
+                <i className="fa fa-ellipsis-v" />
+            </div>
+            <div className={classNames('popup-menu', {
+                'popup-menu--invisible': !isOpen})}>
+                <i
+                    className="popup-menu__close fa-times fa"
+                    onClick={close}
+                />
+                <ul className="popup-menu__list">
+                    {props.children}
+                </ul>
+            </div>
+        </OutsideClickHandler>) : (
+            <div
+                className="flyout-sub-tabs-content-ico-link"
+                title={t(props, 'more')}
+                onClick={open}
+            >
+                <i className="fa fa-ellipsis-v" />
+            </div>
+        );
 }
 
 function PopupMenuItem({ children }) {
