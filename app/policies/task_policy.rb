a41fea7477d38961f0561115c367741e119d0bcf
cgregor <chrgregor@gmail.com>
2020-11-17 13:27:42 +0100

    touch interview on task, change task-policy
    Former-commit-id: 343955b803b1256e233e4c99766b0717e9559ba1

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,4 +1,25 @@
 class TaskPolicy < ApplicationPolicy
+
+  def create?
+    user.admin?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.user_account == user || record.supervisor == user.permissions || user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    create?
+  end
+
   class Scope < Scope
     def resolve
       #scope.where(user: user).or.where(supervisor: user)
