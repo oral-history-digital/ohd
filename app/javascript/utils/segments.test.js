fd486a91a13945a4f891dafd6fba1d64f8d98bfd
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2020-11-18 15:11:49 +0100

    Proof of concept utility functions
    Former-commit-id: b888a8fffa678ad6f8c8d482bb836b82fe9a2d80

---
+++ app/javascript/utils/segments.test.js
@@ -0,0 +1,25 @@
+import { segmentsForTape } from './segments';
+
+describe('segmentsForTape', () => {
+    const interview = {
+        segments: {
+            1: {
+                39147: { id: 39147, type: 'Segment' },
+            },
+        },
+    };
+
+    it('returns segments for a given tape', () => {
+        const actual = segmentsForTape(interview, 1);
+        const expected = {
+            39147: { id: 39147, type: 'Segment' },
+        };
+        expect(actual).toEqual(expected);
+    });
+
+    it('returns an empty object if tape is not found', () => {
+        const actual = segmentsForTape(interview, 5);
+        const expected = {};
+        expect(actual).toEqual(expected);
+    });
+});
