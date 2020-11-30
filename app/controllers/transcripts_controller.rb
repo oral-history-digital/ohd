ea55745e053731ae7a58b015f9b2b1d818f04ca2
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2020-11-30 15:18:27 +0100

    Update contribution speaker designations when uploading transcripts
    Former-commit-id: 1831cf939fd12ef16bccf3fec0090c81823ed82d

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -9,10 +9,12 @@ class TranscriptsController < ApplicationController
 
   def create
     authorize :upload, :create?
+
     file = params[:transcript].delete(:data)
     file_path = create_tmp_file(file)
 
     interview = Interview.find_by_archive_id(transcript_params[:archive_id])
+
     tape = transcript_params[:tape_number] ? interview.tapes.find_by_media_id(
       "#{interview.archive_id}_#{format('%02d', interview.tapes.count)}_#{format('%02d', transcript_params[:tape_number])}"
     ) : interview.tapes.first
@@ -20,7 +22,11 @@ class TranscriptsController < ApplicationController
     update_tape_durations_and_time_shifts(interview) if transcript_params[:tape_durations]
     tape.segments.destroy_all if transcript_params[:delete_existing]
 
+    update_contributions(interview)
+
     locale = ISO_639.find(Language.find(transcript_params[:transcript_language_id]).code.split(/[\/-]/)[0]).send("alpha2")
+
+
 
     ReadTranscriptFileJob.perform_later(interview, file_path, tape.id, locale, current_user_account)
 
@@ -46,7 +52,7 @@ class TranscriptsController < ApplicationController
         :tape_durations,
         :time_shifts,
         :delete_existing,
-        contributions_attributes: [:person_id, :contribution_type, :speaker_designation]
+        contributions_attributes: [:id, :person_id, :contribution_type, :speaker_designation]
     )
   end
 
@@ -62,4 +68,12 @@ class TranscriptsController < ApplicationController
     end
   end
 
+  def update_contributions(interview)
+    contribution_attributes = transcript_params[:contributions_attributes]
+
+    contribution_attributes.each do |attributes|
+      contribution = Contribution.find(attributes[:id])
+      contribution.update_attributes(speaker_designation: attributes[:speaker_designation])
+    end
+  end
 end
