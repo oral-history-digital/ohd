b29bcbaf85657889adc96d2a809d4668e7c39153
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2020-11-30 14:08:57 +0100

    INTARCH-1046 Make main headings go to respective video position
    Former-commit-id: 642e9271333fb536637cd1235cddd45cfe9a0a61

--- app/javascript/bundles/archive/components/Heading.js
+++ app/javascript/bundles/archive/components/Heading.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import SubHeadingContainer from '../containers/SubHeadingContainer';
 import SegmentHeadingFormContainer from '../containers/SegmentHeadingFormContainer';
-import { t, admin } from '../../../lib/utils';
+import AuthorizedContent from './AuthorizedContent';
 
 export default class Heading extends React.Component {
+    constructor(props) {
+        super(props);
 
-    constructor(props, context) {
-        super(props, context);
         this.state = {
             expanded: false,
             active: false
         }
+
         this.toggle = this.toggle.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
 
-
-    componentWillReceiveProps(nextProps) {
+    UNSAFE_componentWillReceiveProps(nextProps) {
         let endTime = (this.props.nextHeading) ? this.props.nextHeading.time : this.props.data.duration;
         let active = this.props.tape === this.props.data.tape_nbr && endTime > nextProps.transcriptTime && this.props.data.time <= nextProps.transcriptTime;
         if (active !== this.state.active) {
@@ -34,26 +36,24 @@ export default class Heading extends React.Component {
         }
     }
 
-    handleClick(tape, time) {
-        if (this.props.data.subheadings.length > 0) {
-            this.toggle();
-        } else {
-            let tabIndex = (this.props.interview.lang === this.props.locale) ? 0 : 1;
-            this.props.handleSegmentClick(tape, time, false);
-        }
+    handleClick() {
+        const { data, handleSegmentClick } = this.props;
+
+        handleSegmentClick(data.tape_nbr, data.time, false);
     }
 
     subHeadings() {
         if (this.props.data.main) {
             return <div className={this.state.expanded ? 'expanded' : 'collapsed'}>
-                {this.props.data.subheadings.map((heading, index) => {
-                    return <div><SubHeadingContainer
-                        key={'heading-' + index}
-                        data={heading}
-                        nextSubHeading={this.props.data.subheadings[index+1] || this.props.nextHeading}
-                    />{this.editHeading()}
+                {this.props.data.subheadings.map((heading, index) => (
+                    <div key={index}>
+                        <SubHeadingContainer
+                            data={heading}
+                            nextSubHeading={this.props.data.subheadings[index+1] || this.props.nextHeading}
+                        />
+                        {this.editHeading()}
                     </div>
-                })}
+                ))}
             </div>;
         }
     }
@@ -64,13 +64,13 @@ export default class Heading extends React.Component {
         } else {
             let icoClass = this.state.expanded ? 'heading-ico active' : 'heading-ico';
             return <div className={icoClass}
-                        onClick={() => this.toggle()}/>
+                        onClick={this.toggle}/>
         }
     }
 
     editHeading() {
-        if (admin(this.props, this.props.data.segment)) {
-            return (
+        return (
+            <AuthorizedContent object={this.props.data.segment}>
                 <span
                     className='flyout-sub-tabs-content-ico-link'
                     onClick={() => this.props.openArchivePopup({
@@ -81,21 +81,23 @@ export default class Heading extends React.Component {
                 >
                         <i className="fa fa-pencil"/>
                 </span>
-            )
-        }
+            </AuthorizedContent>
+        );
     }
 
     render() {
-        let css = 'mainheading ' + (this.state.active ? 'active' : 'inactive');
         return (
             <div className='heading'>
                 {this.expandable()}
-                <div
-                    className={css}
-                    onClick={() => this.handleClick(this.props.data.tape_nbr, this.props.data.time)}
-                >
-                    <span className='chapter-number'>{this.props.data.chapter}</span>
-                    <span className='chapter-text' dangerouslySetInnerHTML={{__html: this.props.data.heading}} />
+                <div className={classNames('mainheading', this.state.active ? 'active' : 'inactive')}>
+                    <span className='chapter-number' onClick={this.handleClick}>
+                        {this.props.data.chapter}
+                    </span>
+                    <span
+                        className='chapter-text'
+                        onClick={this.handleClick}
+                        dangerouslySetInnerHTML={{__html: this.props.data.heading}}
+                    />
                     {this.editHeading()}
                 </div>
                 {this.subHeadings()}
@@ -103,3 +105,13 @@ export default class Heading extends React.Component {
         )
     }
 }
+
+Heading.propTypes = {
+    data: PropTypes.object.isRequired,
+    nextHeading: PropTypes.object.isRequired,
+    locale: PropTypes.string.isRequired,
+    tape: PropTypes.number.isRequired,
+    transcriptTime: PropTypes.number.isRequired,
+    handleSegmentClick: PropTypes.func.isRequired,
+    openArchivePopup: PropTypes.func.isRequired,
+};
--- app/javascript/bundles/archive/containers/HeadingContainer.js
+++ app/javascript/bundles/archive/containers/HeadingContainer.js
@@ -3,25 +3,16 @@ import { connect } from 'react-redux';
 import Heading from '../components/Heading';
 import { handleSegmentClick } from '../actions/interviewActionCreators';
 import { openArchivePopup } from '../actions/archivePopupActionCreators';
-import {getInterview } from '../../../lib/utils'
 
-const mapStateToProps = (state) => {
-    return {
-        locale: state.archive.locale,
-        transcriptTime: state.interview.transcriptTime,
-        tape: state.interview.tape,
-        interview: getInterview(state),
-        account: state.data.accounts.current,
-        editView: state.archive.editView,
-    }
-}
+const mapStateToProps = state => ({
+    locale: state.archive.locale,
+    transcriptTime: state.interview.transcriptTime,
+    tape: state.interview.tape,
+});
 
 const mapDispatchToProps = (dispatch) => ({
     handleSegmentClick: (tape, time, tabIndex) => dispatch(handleSegmentClick(tape, time, tabIndex)),
     openArchivePopup: (params) => dispatch(openArchivePopup(params)),
 })
 
-// Don't forget to actually use connect!
-// Note that we don't export Interview, but the redux "connected" version of it.
-// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
 export default connect(mapStateToProps, mapDispatchToProps)(Heading);
