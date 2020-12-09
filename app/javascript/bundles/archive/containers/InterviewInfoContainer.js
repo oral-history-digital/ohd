93faea88337071774eb1cce183ddc1d75d97bef3
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2020-12-09 14:15:29 +0100

    INTARCH-1101 Filter contribution list in interview tab
    Former-commit-id: 3a63e2272bccea32328d6f917310996191247d1f

--- app/javascript/bundles/archive/containers/InterviewInfoContainer.js
+++ app/javascript/bundles/archive/containers/InterviewInfoContainer.js
@@ -1,37 +1,18 @@
 import { connect } from 'react-redux';
+
 import InterviewInfo from '../components/InterviewInfo';
-import { openArchivePopup, closeArchivePopup } from '../actions/archivePopupActionCreators';
-import { fetchData } from '../actions/dataActionCreators';
+import { getInterview, getProject } from 'lib/utils';
 
-import { getInterview, getProject, getCookie } from '../../../lib/utils';
+const mapStateToProps = state => ({
+    locale: state.archive.locale,
+    translations: state.archive.translations,
+    project: getProject(state),
+    collections: state.data.collections,
+    interview: getInterview(state),
+    collections: state.data.collections,
+    languages: state.data.languages,
+    // the following is just a trick to force rerender after deletion
+    contributionsLastModified: state.data.statuses.contributions.lastModified,
+});
 
-const mapStateToProps = (state) => {
-    let project = getProject(state);
-    return {
-        locale: state.archive.locale,
-        translations: state.archive.translations,
-        archiveId: state.archive.archiveId,
-        projectId: state.archive.projectId,
-        project: project,
-        collections: state.data.collections,
-        interview: getInterview(state),
-        people: state.data.people,
-        peopleStatus: state.data.statuses.people,
-        collections: state.data.collections,
-        // the following is just a trick to force rerender after deletion
-        contributionsLastModified: state.data.statuses.contributions.lastModified,
-        contributionTypes: state.archive.contributionTypes,
-        languages: state.data.languages,
-        editView: state.archive.editView,
-        account: state.data.accounts.current,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-    openArchivePopup: (params) => dispatch(openArchivePopup(params)),
-    closeArchivePopup: () => dispatch(closeArchivePopup()),
-    fetchData: (props, dataType, archiveId, nestedDataType, extraParams) => dispatch(fetchData(props, dataType, archiveId, nestedDataType, extraParams)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(InterviewInfo);
-
+export default connect(mapStateToProps)(InterviewInfo);
