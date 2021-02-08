31e7cb421585c394cae28046c76382b4d0905e5b
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-02-08 14:08:00 +0100

    Move validators to forms module
    Former-commit-id: bdadfc3abaeadfa799a8e234e9f105d4acc7bbfa

--- app/javascript/modules/forms/index.js
+++ app/javascript/modules/forms/index.js
@@ -2,3 +2,4 @@ export { default as Form } from './Form';
 export { default as SubmitOnBlurForm } from './SubmitOnBlurForm';
 export { default as FormElement } from './Element';
 export { default as InputContainer } from './InputContainer';
+export { validateTapeNumber } from './validators';
