af8a1badd6a6cfa05dae6d74f5d622b5a651fdaf
cgregor <chrgregor@gmail.com>
2021-02-16 13:21:23 +0100

    fix subheading-editing in TOC
    Former-commit-id: 81130a3f8512338db6af30ae5054fcab3f7d8880

--- app/javascript/bundles/archive/components/Heading.js
+++ app/javascript/bundles/archive/components/Heading.js
@@ -51,7 +51,7 @@ export default class Heading extends React.Component {
                             data={heading}
                             nextSubHeading={this.props.data.subheadings[index+1] || this.props.nextHeading}
                         />
-                        {this.editHeading()}
+                        {this.editHeading(heading.segment)}
                     </div>
                 ))}
             </div>;
@@ -68,13 +68,13 @@ export default class Heading extends React.Component {
         }
     }
 
-    editHeading() {
+    editHeading(segment) {
         return (
-            <AuthorizedContent object={this.props.data.segment}>
+            <AuthorizedContent object={segment}>
                 <span
                     className='flyout-sub-tabs-content-ico-link'
                     onClick={() => this.props.openArchivePopup({
-                        content: <SegmentHeadingFormContainer segment={this.props.data.segment} />
+                        content: <SegmentHeadingFormContainer segment={segment} />
                     })}
                 >
                         <i className="fa fa-pencil"/>
@@ -96,7 +96,7 @@ export default class Heading extends React.Component {
                         onClick={this.handleClick}
                         dangerouslySetInnerHTML={{__html: this.props.data.heading}}
                     />
-                    {this.editHeading()}
+                    {this.editHeading(this.props.data.segment)}
                 </div>
                 {this.subHeadings()}
             </div>
