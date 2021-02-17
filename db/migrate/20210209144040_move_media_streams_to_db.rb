2be43bd6b93ca01366d89d98fb592bcf31473710
cgregor <chrgregor@gmail.com>
2021-02-17 20:42:01 +0100

    fix mediastreams migration
    Former-commit-id: ee535b65a941d6e8dba9a34bf276eb10b9000547

--- db/migrate/20210209144040_move_media_streams_to_db.rb
+++ db/migrate/20210209144040_move_media_streams_to_db.rb
@@ -9,7 +9,7 @@ class MoveMediaStreamsToDb < ActiveRecord::Migration[5.2]
     end
 
     %w(video audio).each do |media_type|
-      Project.media_streams[media_type].each do |resolution, path|
+      Project.media_streams[media_type] && Project.media_streams[media_type].each do |resolution, path|
         MediaStream.create project_id: Project.first.id, resolution: resolution, path: path, media_type: media_type
       end
     end
