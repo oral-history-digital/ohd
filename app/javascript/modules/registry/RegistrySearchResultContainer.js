96f65c806363056615c9f0d76a10d3ff094cc9ba
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-02-23 14:53:42 +0100

    Clean up RegistrySearchResult
    Former-commit-id: 653ecaa8e77175b83275ec9a84f9d598d5d63f91

--- app/javascript/modules/registry/RegistrySearchResultContainer.js
+++ app/javascript/modules/registry/RegistrySearchResultContainer.js
@@ -1,30 +1,10 @@
 import { connect } from 'react-redux';
 
-import { openArchivePopup, closeArchivePopup } from 'modules/ui';
-import { fetchData, deleteData, getProjects } from 'modules/data';
-import { addRemoveRegistryEntryId, getLocale, getProjectId } from 'modules/archive';
+import { getLocale } from 'modules/archive';
 import RegistrySearchResult from './RegistrySearchResult';
 
-const mapStateToProps = (state) => {
-    return {
-        archiveId: state.archive.archiveId,
-        locale: getLocale(state),
-        projectId: getProjectId(state),
-        projects: getProjects(state),
-        translations: state.archive.translations,
-        registryEntriesStatus: state.data.statuses.registry_entries,
-        account: state.data.accounts.current,
-        editView: state.archive.editView,
-        selectedRegistryEntryIds: state.archive.selectedRegistryEntryIds,
-    }
-}
+const mapStateToProps = state => ({
+    locale: getLocale(state),
+});
 
-const mapDispatchToProps = (dispatch) => ({
-    fetchData: (props, dataType, archiveId, nestedDataType, extraParams) => dispatch(fetchData(props, dataType, archiveId, nestedDataType, extraParams)),
-    deleteData: (props, dataType, id, nestedDataType, nestedId, skipRemove) => dispatch(deleteData(props, dataType, id, nestedDataType, nestedId, skipRemove)),
-    addRemoveRegistryEntryId: (id) => dispatch(addRemoveRegistryEntryId(id)),
-    openArchivePopup: (params) => dispatch(openArchivePopup(params)),
-    closeArchivePopup: () => dispatch(closeArchivePopup())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(RegistrySearchResult);
+export default connect(mapStateToProps)(RegistrySearchResult);
