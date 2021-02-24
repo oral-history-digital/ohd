893f75a9b73b76c1aad167fa3e4ecabe74529b8f
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-02-24 16:31:33 +0100

    Add system initializer service
    Former-commit-id: 4036ec204285dc87a02b2d465c494fc3e05b1105

---
+++ app/services/system_initializer.rb
@@ -0,0 +1,30 @@
+class SystemInitializer
+  class << self
+    def create
+      create_contribution_types
+    end
+
+    def create_contribution_types
+      %w(
+        interviewee
+        interviewer
+        cinematographer
+        sound
+        producer
+        other_attender
+        quality_manager_interviewing
+        transcriptor
+        segmentator
+        translator
+        proofreader
+        research
+      ).each do |code|
+        ContributionType.create code: code, label: I18n.t("contributions.#{code}", locale: :de), locale: :de
+      end
+    end
+
+    def create_languages
+      # ...
+    end
+  end
+end
