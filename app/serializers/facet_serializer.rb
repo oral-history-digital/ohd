454836025b3142155019ffc7eed1da2c03790d49
cgregor <chrgregor@gmail.com>
2021-03-03 18:19:08 +0100

    do not crash if RegistryReferenceType has no RegistryEntry
    Former-commit-id: 7c8f789ff6c4438cdedc903568aeb32210a83fe5

--- app/serializers/facet_serializer.rb
+++ app/serializers/facet_serializer.rb
@@ -10,12 +10,16 @@ class FacetSerializer < ApplicationSerializer
   def subfacets
     case object.class.name
     when "RegistryReferenceType"
-      object.registry_entry.children.includes(registry_names: :translations).inject({}) do |mem, child|
-        mem[child.id.to_s] = {
-          name: child.localized_hash(:descriptor),
-          count: 0,
-        }
-        mem
+      if object.registry_entry
+        object.registry_entry.children.includes(registry_names: :translations).inject({}) do |mem, child|
+          mem[child.id.to_s] = {
+            name: child.localized_hash(:descriptor),
+            count: 0,
+          }
+          mem
+        end
+      else
+        {}
       end
     when "Person"
     end
