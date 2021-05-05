49e78bb052a1ca35dff7200eec3c32e39d349fde
cgregor <chrgregor@gmail.com>
2021-05-05 17:54:27 +0200

    rm implicit action from serialized objects
    Former-commit-id: 2860ec1988ec16eb1fd93c971f507a7fc4aa28d8

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,20 +1,8 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :type, :action, :translations
+  attributes :id, :type, :translations
 
   def type 
     object.class.name
-  end
-
-  # this is to calculate  permissions on the serialized object
-  #
-  # so a user needs the permission with attributes type == object.class.name and action_name == update
-  # or a task with authorized_type == object.class.name and authorized_id == object.id
-  # to see all necessary buttons
-  #
-  # see the admin-function in app/javascript/lib/utils.js
-  #
-  def action
-    :update
   end
 
   # serialized translations are needed to construct 'translations_attributes' e.g. in MultiLocaleInput
