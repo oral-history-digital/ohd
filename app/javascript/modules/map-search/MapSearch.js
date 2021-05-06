8772696aee6a56ecfd919f9eaac35d75253048e0
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-05-06 20:22:05 +0200

    Refactor locations map
    Former-commit-id: 98b9d73387463623ef9aeb7f9a4ab4425d1f2fd0

--- app/javascript/modules/map-search/MapSearch.js
+++ app/javascript/modules/map-search/MapSearch.js
@@ -1,64 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import 'leaflet';  // Needed to provide global L for leaflet-extra-markers import.
-import 'leaflet-extra-markers'
-import 'leaflet.markercluster'
-import './leaflet.markercluster-regionbound/leaflet.markercluster-regionbound.min.js'
-import './leaflet.cedis.regioncluster/leaflet.cedis.regioncluster.js'
-
-import './leaflet.markercluster-regionbound/MarkerCluster.Aggregations.css'
-import './leaflet.markercluster-regionbound/MarkerCluster.css'
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import 'leaflet-extra-markers/dist/css/leaflet.extra-markers.min.css';
-import './leaflet.cedis.regioncluster/leaflet.cedis.regioncluster.css'
-
 import { pathBase } from 'modules/routes';
-import { INDEX_MAP } from 'modules/flyout-tabs';
 import { Spinner } from 'modules/spinners';
 import { ScrollToTop } from 'modules/user-agent';
 
 export default class MapSearch extends React.Component {
     componentDidMount() {
-        this.props.setFlyoutTabsIndex(INDEX_MAP);
-
         if (this.props.markersFetched) {
             this.initMap();
         } else {
             this.search();
         }
-    }
-
-    initMap() {
-        window.SucheKarte.init('map', this.props.foundMarkers,  {
-            useClustering: true,
-            controlsCollapsed: false,
-            showControls: true,
-            fitBounds: true,
-            allowClustersOfOne: false,
-            showBorders: false,
-        });
-    }
-
-    removeMap() {
-        window.SucheKarte.map?.remove();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (!prevProps.markersFetched && this.props.markersFetched) {
-            this.initMap()
-        } else if (Object.keys(prevProps.foundMarkers).length !== Object.keys(this.props.foundMarkers).length) {
-            this.removeMap();
-            this.initMap();
-        }
-        if (!prevProps.isLoggedIn && this.props.isLoggedIn) {
-            this.search(this.props.query);
-        }
-    }
-
-    componentWillUnmount() {
-        this.removeMap();
     }
 
     search(query={}) {
@@ -70,12 +23,7 @@ export default class MapSearch extends React.Component {
         return (
             <ScrollToTop>
                 <div className='wrapper-content map'>
-                    {
-                        this.props.isMapSearching ?
-                            <Spinner /> :
-                            null
-                    }
-                    <div id='map' />
+                    <Spinner />
                 </div>
             </ScrollToTop>
         )
