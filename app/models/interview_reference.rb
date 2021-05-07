369575119772aff57fed9c331f546acdd3e8ece4
cgregor <chrgregor@gmail.com>
2021-05-07 17:01:15 +0200

    cleanup
    Former-commit-id: 655fd28635a33e717f702e9c3c96f63b607ea7bd

--- app/models/interview_reference.rb
+++ app/models/interview_reference.rb
@@ -7,28 +7,6 @@ class InterviewReference < UserContent
     title_tokens.join(' ')
   end
 
-  # provides user_content attributes for new user_content
-  # except the link_url, which is generated in the view
-  def user_content_attributes
-    attr = {}
-    attr[:properties] = {
-            :citation => '',
-            :video => reference.video?,
-            :language => reference.language,
-            :translated => reference.translated,
-            :duration => reference.duration.timecode,
-            :segmented => reference.segmented,
-            :table_of_contents => reference.researched
-    }
-    attr
-  end
-
-  # path to show the resource
-  # TODO: cleanup: delete this method!
-  #def get_content_path
-    #interview_path(:id => reference.archive_id)
-  #end
-
   def self.for_interview(interview)
     interview = case interview
       when Interview
