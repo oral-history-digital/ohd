3fb389831c55402e6a9ddcdb7d405cd1412c7557
cgregor <chrgregor@gmail.com>
2021-05-14 19:39:31 +0200

    no project_id in permission_serializer
    Former-commit-id: 86fab11856e5fe7e62411a1744c47312ec79d17d

--- app/serializers/permission_serializer.rb
+++ app/serializers/permission_serializer.rb
@@ -10,8 +10,4 @@ class PermissionSerializer < ApplicationSerializer
     object.created_at && object.created_at.strftime('%d.%m.%Y %M:%H Uhr')
   end
 
-  def project_id
-    object.role && object.role.project_id
-  end
-
 end
