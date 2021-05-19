0868c28ae109fdad8c85032c83cfaec3834d1ca8
Marc Altmann <marcaltmann@posteo.de>
2021-05-19 16:06:53 +0200

    Add metadata labels
    Former-commit-id: 3f798800eb8989f939351a9ef38244cb5e2296b8

---
+++ app/javascript/modules/gallery/PhotoMetadatum.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { useI18n } from 'modules/i18n';
+
+export default function PhotoMetadatum({
+    className,
+    label,
+    value,
+}) {
+    const { t } = useI18n();
+
+    return (
+        <p className={className}>
+            {t(`modules.gallery.metadatum.${label}`)}: {value}
+        </p>
+    );
+}
+
+PhotoMetadatum.propTypes = {
+    className: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+};
