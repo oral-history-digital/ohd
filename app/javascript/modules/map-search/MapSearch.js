c77a0276b126ca77bba8a4d893ab0feb1a951831
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-05-25 13:53:01 +0200

    Revert "Try out different map style"
    This reverts commit b1e0dc4794b5693603ef5c80d4bf42123deb09ea [formerly 60376c8ac78cabc0c25e978882a3f134700b1435].
    
    
    Former-commit-id: e771cebcbc52d1f070992779a2dd0167bf63201e

--- app/javascript/modules/map-search/MapSearch.js
+++ app/javascript/modules/map-search/MapSearch.js
@@ -64,8 +64,8 @@ export default function MapSearch({
                         isMapSearching && <MapOverlay />
                     }
                     <TileLayer
-                        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-                        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
                     {
                         mapReferenceTypes && mapMarkers?.map(marker => {
