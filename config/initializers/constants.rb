461f38d8765b5142c463ac4c6fbe0d7a200a2b86
cgregor <chrgregor@gmail.com>
2021-06-07 18:17:37 +0200

    use OHD_DOMAIN from constants in routes
    Former-commit-id: 0cab031285f839edff8136afd1d375f3ab4b571f

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,1 @@
-OHD_DOMAIN = Rails.env == 'development' ? 'http://localhost:3000' : 'http://da03.cedis.fu-berlin.de:100' # will be 'http://portal.oral-history.digital'
+OHD_DOMAIN = Rails.env == 'development' ? 'http://localhost:3000' : 'http://portal.oral-history.digital'
