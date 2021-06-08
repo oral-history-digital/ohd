59853cd3934eb2c6fefd010722058b730e05f993
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-06-08 17:34:57 +0200

    Fix migration
    Former-commit-id: 88e52d9a1d284a2a130e60dfb8b1bb2bbab1b4a0

--- db/migrate/20210529103017_mv_admin_comments_to_user_registration_projects.rb
+++ db/migrate/20210529103017_mv_admin_comments_to_user_registration_projects.rb
@@ -1,15 +1,15 @@
 class MvAdminCommentsToUserRegistrationProjects < ActiveRecord::Migration[5.2]
   def up
     add_column :user_registration_projects, :admin_comments, :string
-    UserRegistration.all.each do |ur| 
+    UserRegistration.all.each do |ur|
       ur.user_registration_projects.update_all admin_comments: ur.admin_comments
     end
     remove_column :user_registrations, :admin_comments
   end
 
   def down
-    add_column :user_registration, :admin_comments, :string
-    UserRegistration.all.each do |ur| 
+    add_column :user_registrations, :admin_comments, :string
+    UserRegistration.all.each do |ur|
       ur.update_attributes admin_comments: ur.user_registration_projects.first.admin_comments
     end
     remove_column :user_registration_projects, :admin_comments
