5c0edc8d9004f36d58aff298fd98f78624945b71
cgregor <chrgregor@gmail.com>
2021-06-08 12:25:41 +0200

    activate user_registration
    Former-commit-id: 72d4c3081ff0ad2e260440cd50213c08bbce8fff

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -119,6 +119,7 @@ class UserAccount < ApplicationRecord
       self.deactivated_at = nil
       # theoretically we do not need this check, but unfortunately we have some legacy accounts without UserRegistration
       unless self.user_registration.nil?
+        self.user_registration.update_attributes activated_at: Time.now
         self.user_registration.projects.each do |project|
           AdminMailer.with(registration: self, project: project).new_registration_info.deliver
         end
