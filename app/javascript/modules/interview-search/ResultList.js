6cefd6088d6c117e4862de9fb90875de808ff54e
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-06-15 15:55:38 +0200

    Add PhotoResult component
    Former-commit-id: ff2dfbcb22c5467559fd252e7f57746e2628ceaf

--- app/javascript/modules/interview-search/ResultList.js
+++ app/javascript/modules/interview-search/ResultList.js
@@ -4,51 +4,31 @@ import PropTypes from 'prop-types';
 import { FoundSegmentContainer } from 'modules/transcript';
 import { PersonContainer } from 'modules/people';
 import { BiographicalEntryContainer } from 'modules/interviewee-metadata';
-import { PhotoContainer } from 'modules/gallery';
 import { RegistryEntryContainer } from 'modules/registry';
+import PhotoResult from './PhotoResult';
 
 const modelToComponent = {
     Segment: FoundSegmentContainer,
     Person: PersonContainer,
     BiographicalEntry: BiographicalEntryContainer,
-    Photo: PhotoContainer,
+    Photo: PhotoResult,
     RegistryEntry: RegistryEntryContainer,
 };
 
 export default function ResultList({
     model,
-    interview,
     searchResults,
-    mediaTime,
 }) {
-    let active = false;
-    return searchResults.map(data => {
-        if (model === 'Segment') {
-            if (data.time <= mediaTime + 10 && data.time >= mediaTime - 5) {
-                active = true;
-            }
+    return searchResults.map(data => createElement(
+        modelToComponent[model],
+        {
+            key: data.id,
+            data: data,
         }
-
-        const result = createElement(modelToComponent[model],
-            {
-                key: data.id,
-                data: data,
-                tape_count: interview.tape_count,
-                active: false,
-                // TODO: reintegrate counter with different models
-                //index: index+1,
-                //foundSegmentsAmount: searchResults.foundSegments.length
-            }
-        )
-
-        return result;
-    });
+    ));
 }
 
 ResultList.propTypes = {
     model: PropTypes.string.isRequired,
-    interview: PropTypes.object.isRequired,
     searchResults: PropTypes.object.isRequired,
-    mediaTime: PropTypes.number.isRequired,
-    setArchiveId: PropTypes.func.isRequired,
 };
