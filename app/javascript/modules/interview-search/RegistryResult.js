16d9bcf21199cdd8fd2b6312110679c23ae8e447
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-06-16 19:07:21 +0200

    Check if flattenedRefTree is present
    Former-commit-id: 35ac9f15896220e4fca3765f824a5217b7f0ae26

--- app/javascript/modules/interview-search/RegistryResult.js
+++ app/javascript/modules/interview-search/RegistryResult.js
@@ -4,12 +4,17 @@ import { useSelector } from 'react-redux';
 import { getFlattenedRefTree } from 'modules/data';
 import { useI18n } from 'modules/i18n';
 import { FoundSegmentContainer } from 'modules/transcript';
+import { Spinner } from 'modules/spinners';
 
 export default function RegistryResult({
     data,
 }) {
     const { locale } = useI18n();
     const flattenedRefTree = useSelector(getFlattenedRefTree);
+
+    if (!flattenedRefTree) {
+        return <Spinner small />;
+    }
 
     return (
         <div className="SearchResult">
