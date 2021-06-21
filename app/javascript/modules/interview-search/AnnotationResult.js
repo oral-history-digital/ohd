9ba6ed2456c707ecc14142f1e4bfe21d34885591
Marc Altmann <marcaltmann@posteo.de>
2021-06-21 16:00:44 +0200

    Display segment with annotation
    Former-commit-id: 2a4036c3f248efe688bc50e09dff3f00ad46b73f

--- app/javascript/modules/interview-search/AnnotationResult.js
+++ app/javascript/modules/interview-search/AnnotationResult.js
@@ -1,28 +1,62 @@
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import { useI18n } from 'modules/i18n';
+import { getCurrentInterview } from 'modules/data';
+import TranscriptResult from './TranscriptResult';
+
+function getSegmentById(segments, segmentId) {
+    // Preconditions
+    if (typeof segments !== 'object') {
+        throw new TypeError('segments argument must be object');
+    }
+    if (typeof segmentId !== 'number') {
+        throw new TypeError('segmentId argument must be number');
+    }
+    if (typeof segmentId < 0) {
+        throw new TypeError('segmentId must not be negative');
+    }
+
+    const segmentArray = Object.values(segments);
+
+    const obj = Object.assign.apply(null, [{}].concat(segmentArray));
+
+    const segment = obj[segmentId];
+
+    if (typeof segment === 'undefined') {
+        throw new ReferenceError('Segment cannot be found');
+    }
+
+    return segment;
+}
 
 export default function AnnotationResult({
     data,
 }) {
+    const interview = useSelector(getCurrentInterview);
+
+    let segment = null;
+    const { segments } = interview;
+
+    if (segments) {
+        segment = getSegmentById(segments, data.segment_id);
+    }
+
     const { locale } = useI18n();
 
     const annotationText = data.text[locale];
 
-    console.log(data);
-
     return (
-        <button
+        <div
             type="button"
             className="SearchResult"
         >
-            <p className="SearchResult-meta">
-            </p>
             <p
                 className="SearchResult-text"
                 dangerouslySetInnerHTML={{__html: annotationText}}
             />
-        </button>
+            {segment && <TranscriptResult data={segment} />}
+        </div>
     );
 }
 
