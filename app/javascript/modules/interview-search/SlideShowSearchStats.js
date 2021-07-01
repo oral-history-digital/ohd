5990ca2cf6bae5b58285682357ea87c37b0de9b2
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-07-01 14:26:49 +0200

    Update translations, styling
    Former-commit-id: 341655de27cf721a26b610a46c1079e095702324

--- app/javascript/modules/interview-search/SlideShowSearchStats.js
+++ app/javascript/modules/interview-search/SlideShowSearchStats.js
@@ -23,53 +23,56 @@ export default function SlideShowSearchStats({
 
     return (
         <div className="SearchStats">
+            <h3 className="SearchStats-heading">
+                {t('modules.interview_search.search_results.heading')}
+            </h3>
             <ul className="SearchStats-list">
                 {
                     filteredSegments.length > 0 && (
                         <li className="SearchStats-item">
-                            {filteredSegments.length} {t('segment_results')}
+                            {filteredSegments.length} {t('modules.interview_search.search_results.in_transcript')}
                         </li>
                     )
                 }
                 {
                     toc.length > 0 && (
                         <li className="SearchStats-item">
-                            {toc.length} {t('heading_results')}
+                            {toc.length} {t('modules.interview_search.search_results.in_toc')}
                         </li>
                     )
                 }
                 {
                     annotations.length > 0 && (
                         <li className="SearchStats-item">
-                            {annotations.length} {t('annotation_results')}
+                            {annotations.length} {t('modules.interview_search.search_results.in_annotations')}
                         </li>
                     )
                 }
                 {
                     registryEntries.length > 0 && (
                         <li className="SearchStats-item">
-                            {registryEntries.length} {t('registryentry_results')}
+                            {registryEntries.length} {t('modules.interview_search.search_results.in_registry')}
                         </li>
                     )
                 }
                 {
                     biographicalEntries.length > 0 && (
                         <li className="SearchStats-item">
-                            {biographicalEntries.length} {t('biographicalentry_results')}
+                            {biographicalEntries.length} {t('modules.interview_search.search_results.in_biography')}
                         </li>
                     )
                 }
                 {
                     photos.length > 0 && (
                         <li className="SearchStats-item">
-                            {photos.length} {t('photo_results')}
+                            {photos.length} {t('modules.interview_search.search_results.in_photo_captions')}
                         </li>
                     )
                 }
                 {
                     observations.length > 0 && (
                         <li className="SearchStats-item">
-                            {observations.length} {t('observation_results')}
+                            {observations.length} {t('modules.interview_search.search_results.in_observations')}
                         </li>
                     )
                 }
