45aad1f470656f4980f0d1d36f02053f3eef33dc
cgregor <chrgregor@gmail.com>
2021-07-20 16:47:00 +0200

    INTARCH-1160: join registry_names of same type without comma
    Former-commit-id: ecf73f652b568dd004d66815c58875c69838d182

--- app/models/registry_entry.rb
+++ app/models/registry_entry.rb
@@ -386,9 +386,16 @@ class RegistryEntry < ApplicationRecord
       includes(:translations, :registry_name_type).
       #where("registry_name_translations.locale": locale).
       order('registry_name_types.order_priority').
-      map do |rn|
-        rn.descriptor(locale)
-      end.flatten.uniq.join(', ')
+      group_by{|rn| rn.registry_name_type_id}.
+      map do |tid, rns| 
+        birth_name = rns.first.registry_name_type && rns.first.registry_name_type.code == 'birth_name'
+        names = rns.map{|rn| rn.descriptor(locale)}.join(" ")
+        birth_name ? "#{I18n.backend.translate(locale, 'search_facets.born')} #{names}" : names
+      end.
+      join(", ")
+      #group_by{|rn| rn.registry_name_type_id}.
+      #map{|tid, rns| rns.map(&:descriptor).join(" ")}.
+      #join(", ")
   end
 
 end
