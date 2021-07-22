0f4ab6129b08edc0019928e25aa7ca0dbde0be6d
cgregor <chrgregor@gmail.com>
2021-07-22 13:36:58 +0200

    fix transcript download-path
    Former-commit-id: aaa9d30dda7b7b21967f70c5f707cb5b3f3bc21b

--- app/javascript/modules/interview-metadata/InterviewTextMaterialsContainer.js
+++ app/javascript/modules/interview-metadata/InterviewTextMaterialsContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 
-import { getCurrentAccount, getCurrentInterview, getCurrentProject } from 'modules/data';
+import { getCurrentAccount, getCurrentInterview, getCurrentProject, getProjects } from 'modules/data';
 import { getLocale, getProjectId, getTranslations } from 'modules/archive';
 import InterviewTextMaterials from './InterviewTextMaterials';
 
@@ -9,6 +9,7 @@ const mapStateToProps = (state) => {
         locale: getLocale(state),
         translations: getTranslations(state),
         projectId: getProjectId(state),
+        projects: getProjects(state),
         interview: getCurrentInterview(state),
         project: getCurrentProject(state),
         // the following is just a trick to force rerender after deletion
