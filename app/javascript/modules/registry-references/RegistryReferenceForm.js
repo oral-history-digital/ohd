61fff99b0fd0d7f99215d3a48321d2bb2eaaaf66
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-07-22 12:19:08 +0200

    Refactor RegistryReferenceForm component
    Former-commit-id: e0c42690985da70d7764a456cbbb4502a92cc3e2

--- app/javascript/modules/registry-references/RegistryReferenceForm.js
+++ app/javascript/modules/registry-references/RegistryReferenceForm.js
@@ -1,97 +1,108 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Form } from 'modules/forms';
 
-export default class RegistryReferenceForm extends Component {
-    componentDidMount() {
-        this.loadRegistryReferenceTypes();
-    }
-
-    componentDidUpdate() {
-        this.loadRegistryReferenceTypes();
-    }
-
-    loadRegistryReferenceTypes() {
-        if (!this.props.registryReferenceTypesStatus[`for_projects_${this.props.project?.id}`]) {
-            this.props.fetchData(this.props, 'registry_reference_types', null, null, `for_projects=${this.props.project?.id}`);
+export default function RegistryReferenceForm({
+    registryReference,
+    registryReferenceTypes,
+    registryReferenceTypesStatus,
+    refObject,
+    project,
+    interview,
+    projectId,
+    projects,
+    locale,
+    registryReferenceTypeId,
+    lowestAllowedRegistryEntryId,
+    inTranscript,
+    fetchData,
+    submitData,
+    closeArchivePopup,
+    onSubmit,
+}) {
+    useEffect(() => {
+        if (!registryReferenceTypesStatus[`for_projects_${project?.id}`]) {
+            fetchData({ locale, projectId, projects }, 'registry_reference_types', null, null, `for_projects=${project?.id}`);
         }
-    }
+    })
 
-    elements() {
+    function buildElements() {
         let elements = [
             {
                 elementType: 'registryEntryTreeSelect',
                 attribute: 'registry_entry_id',
-                lowestAllowedRegistryEntryId: this.props.lowestAllowedRegistryEntryId,
-                inTranscript: this.props.inTranscript,
+                lowestAllowedRegistryEntryId,
+                inTranscript,
                 goDeeper: true
             },
         ]
-        if (!this.props.inTranscript) {
+        if (!inTranscript) {
             elements.push(
                 {
                     elementType: 'select',
                     attribute: 'workflow_state',
                     values: ['preliminary', 'checked', 'rejected'],
-                    value: this.props.registryReference && this.props.registryReference.workflow_state,
+                    value: registryReference?.workflow_state,
                     optionsScope: 'workflow_states',
                 }
             )
         }
-        if (!this.props.registryReferenceTypeId) {
+        if (!registryReferenceTypeId) {
             elements.push(
                 {
                     elementType: 'select',
                     attribute: 'registry_reference_type_id',
-                    values: this.registryReferenceTypes(),
-                    value: this.props.registryReference && this.props.registryReference.registry_reference_type_id,
+                    values: buildRegistryReferenceTypes(),
+                    value: registryReference?.registry_reference_type_id,
                     withEmpty: true,
                 }
             )
         }
+
         return elements;
     }
 
-    registryReferenceTypes() {
-        if (this.props.registryReferenceTypes) {
-            return this.props.inTranscript ?
-                Object.values(this.props.registryReferenceTypes).filter(r => r.use_in_transcript) :
-                Object.values(this.props.registryReferenceTypes);
+    function buildRegistryReferenceTypes() {
+        if (registryReferenceTypes) {
+            return inTranscript ?
+                Object.values(registryReferenceTypes).filter(r => r.use_in_transcript) :
+                Object.values(registryReferenceTypes);
         }
     }
 
-    render() {
-        return (
-            <div>
-                <Form
-                    scope='registry_reference'
-                    data={this.props.registryReference}
-                    values={!this.props.registryReference && {
-                        ref_object_id: this.props.refObject.id,
-                        ref_object_type: this.props.refObject.type,
-                        interview_id: this.props.interview.id,
-                        registry_reference_type_id: this.props.registryReferenceTypeId || null,
-                        // TODO: change following dummy-values with meaningfull ones
-                        ref_position: 1,
-                        workflow_state: 'preliminary'
-                    }}
-                    onSubmit={params => {
-                        this.props.submitData(this.props, params);
-                        this.props.closeArchivePopup();
-                        this.props?.onSubmit();
-                    }}
-                    elements={this.elements()}
-                />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Form
+                scope='registry_reference'
+                data={registryReference}
+                values={!registryReference && {
+                    ref_object_id: refObject.id,
+                    ref_object_type: refObject.type,
+                    interview_id: interview.id,
+                    registry_reference_type_id: registryReferenceTypeId || null,
+                    // TODO: change following dummy-values with meaningful ones
+                    ref_position: 1,
+                    workflow_state: 'preliminary'
+                }}
+                onSubmit={params => {
+                    submitData({ locale, projectId, projects }, params);
+                    closeArchivePopup();
+                    if (typeof onSubmit === 'function') {
+                        onSubmit();
+                    }
+                }}
+                elements={buildElements()}
+            />
+        </div>
+    );
 }
 
 RegistryReferenceForm.propTypes = {
     locale: PropTypes.string.isRequired,
     projectId: PropTypes.string.isRequired,
     projects: PropTypes.object.isRequired,
+    project: PropTypes.object.isRequired,
     registryReference: PropTypes.object,
     refObject: PropTypes.object,
     inTranscript: PropTypes.bool,
