2930257219a56cee91e03f5fc3d38a13b2434af5
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-08-17 15:43:09 +0200

    Reduce dependencies
    Former-commit-id: ad57051eb75ef04247bdfbadc01de1581f0da0b9

--- app/javascript/modules/edit-table/index.js
+++ app/javascript/modules/edit-table/index.js
@@ -3,4 +3,3 @@ export { NAME as EDIT_TABLE_NAME } from './constants';
 export { default as editTableReducer } from './reducer';
 
 export { default as EditTableContainer } from './components/EditTableContainer';
-export { default as EditTableButtonsContainer } from './components/EditTableButtonsContainer';
--- app/javascript/modules/media-player/components/MediaPlayerButtonsContainer.js
+++ app/javascript/modules/media-player/components/MediaPlayerButtonsContainer.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { changeToInterviewEditView, getInterviewEditView } from 'modules/archive';
 import { enableAutoScroll, disableAutoScroll, getAutoScroll } from 'modules/interview';
 import MediaPlayerButtons from './MediaPlayerButtons';
 
 const mapStateToProps = (state) => ({
     autoScroll: getAutoScroll(state),
+    editViewEnabled: !!getInterviewEditView(state),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     enableAutoScroll,
     disableAutoScroll,
+    changeToInterviewEditView,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(MediaPlayerButtons);
