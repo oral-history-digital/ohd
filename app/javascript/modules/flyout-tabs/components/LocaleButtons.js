90b5bfe744fb419516cad9b584e4d41aa18ec078
cgregor <chrgregor@gmail.com>
2021-08-20 12:07:43 +0200

    fix locale replacement in url
    Former-commit-id: 2e71e6f1fcc71c4cc41f24543f5fb1dfd0614e57

--- app/javascript/modules/flyout-tabs/components/LocaleButtons.js
+++ app/javascript/modules/flyout-tabs/components/LocaleButtons.js
@@ -28,7 +28,7 @@ class LocaleButtons extends Component {
         if (/^\/[a-z]{2}\/{0,1}$/.test(location.pathname)) {
             newPath = `/${locale}/`;
         } else {
-            newPath = location.pathname.replace(/^\/[a-z]{2,4}\/[a-z]{2}\//, pathBase({projectId, locale, projects}) + '/');
+            newPath = location.pathname.replace(/^(\/[a-z]{2,4}){0,1}\/([a-z]{2})\//, '/' + '$1' + locale + '/');
         }
 
         history.push(newPath);
