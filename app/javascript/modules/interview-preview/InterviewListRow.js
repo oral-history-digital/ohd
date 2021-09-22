4137e333749e8d277837ba95bddcfe7fd59351e2
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-09-22 21:45:56 +0200

    Refactor InterviewListRow component
    Former-commit-id: c8b402a4cf44304d540a31520960f0cf5b2e1c70

--- app/javascript/modules/interview-preview/InterviewListRow.js
+++ app/javascript/modules/interview-preview/InterviewListRow.js
@@ -1,119 +1,131 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaEyeSlash } from 'react-icons/fa';
 
-import { pathBase } from 'modules/routes';
-import { t } from 'modules/i18n';
+import { usePathBase } from 'modules/routes';
 import { humanReadable } from 'modules/data';
-import { AuthShowContainer, admin } from 'modules/auth';
+import { AuthShowContainer, useAuthorization } from 'modules/auth';
 import loadIntervieweeWithAssociations from './loadIntervieweeWithAssociations';
 import searchResultCount from './searchResultCount';
 
-export default class InterviewListRow extends Component {
-    componentDidMount() {
-        const { fulltext, interview, searchInInterview } = this.props;
+export default function InterviewListRow({
+    project,
+    projectId,
+    projects,
+    interview,
+    interviewee,
+    fulltext,
+    interviewSearchResults,
+    selectedArchiveIds,
+    locale,
+    translations,
+    peopleStatus,
+    setArchiveId,
+    searchInInterview,
+    addRemoveArchiveId,
+    fetchData,
+}) {
+    const { isAuthorized } = useAuthorization();
+    const pathBase = usePathBase();
 
-        loadIntervieweeWithAssociations(this.props);
+    useEffect(() => {
+        loadIntervieweeWithAssociations({ projectId, projects, locale, peopleStatus, interviewee, fetchData });
+
         if (fulltext) {
-            searchInInterview(`${pathBase(this.props)}/searches/interview`, { fulltext, id: interview.archive_id });
+            searchInInterview(`${pathBase}/searches/interview`, { fulltext, id: interview.archive_id });
         }
-    }
+    }, []);
 
-    componentDidUpdate() {
-        loadIntervieweeWithAssociations(this.props);
-    }
+    const searchResults = interviewSearchResults[interview.archive_id];
+    const resultCount = searchResultCount(searchResults);
 
-    render() {
-        const { project, interview, interviewee, fulltext, interviewSearchResults,
-            selectedArchiveIds, locale, setArchiveId, searchInInterview,
-            addRemoveArchiveId } = this.props;
+    return (
+        <tr>
+            {
+                isAuthorized(interview, 'show') && isAuthorized({ type: 'General' }, 'edit') && (
+                    <td>
+                        <input
+                            type="checkbox"
+                            className="export-checkbox"
+                            checked={selectedArchiveIds.indexOf(interview.archive_id) > 0}
+                            onChange={() => addRemoveArchiveId(interview.archive_id)}
+                        />
+                    </td>
+                )
+            }
+            <td>
+                <Link className="search-result-link"
+                    onClick={() => {
+                        setArchiveId(interview.archive_id);
+                        searchInInterview(`${pathBase}/searches/interview`, { fulltext, id: interview.archive_id });
+                    }}
+                    to={`${pathBase}/interviews/${interview.archive_id}`}
+                >
+                    {
+                        project.is_catalog ? (
+                            interview.title?.[locale]
+                        ) : (
+                            <div>
+                                <AuthShowContainer ifLoggedIn>
+                                    {interview.short_title && interview.short_title[locale]}
+                                    {
+                                        interview.workflow_state === 'unshared' && (
+                                            <FaEyeSlash />
+                                        )
+                                    }
+                                </AuthShowContainer>
+                                <AuthShowContainer ifLoggedOut ifNoProject>
+                                    {interview.anonymous_title[locale]}
+                                </AuthShowContainer>
+                            </div>
+                        )
+                    }
+                </Link>
+            </td>
+            {
+                project.list_columns.map(column => {
+                    let obj = (column.ref_object_type === 'Interview' || column.source === 'Interview') ?
+                        interview :
+                        interviewee;
 
-        const searchResults = interviewSearchResults[interview.archive_id];
-        const resultCount = searchResultCount(searchResults);
-
-        return (
-            <tr>
-                {
-                    admin(this.props, interview, 'show') && admin(this.props, {type: 'General'}, 'edit') && (
-                        <td>
-                            <input
-                                type='checkbox'
-                                className='export-checkbox'
-                                checked={selectedArchiveIds.indexOf(interview.archive_id) > 0}
-                                onChange={() => {addRemoveArchiveId(interview.archive_id)}}
-                            />
+                    return (
+                        <td key={column.name}>
+                            {obj && humanReadable(obj, column.name, { locale, translations }, {})}
                         </td>
-                    )
-                }
-                <td>
-                    <Link className="search-result-link"
-                        onClick={() => {
-                            setArchiveId(interview.archive_id);
-                            searchInInterview(`${pathBase(this.props)}/searches/interview`, { fulltext, id: interview.archive_id });
-                        }}
-                        to={pathBase(this.props) + '/interviews/' + interview.archive_id}
-                    >
-                        {
-                            project.is_catalog ? (
-                                interview.title && interview.title[locale]
-                            ) : (
-                                <div>
-                                    <AuthShowContainer ifLoggedIn>
-                                        {interview.short_title && interview.short_title[locale]}
-                                        {
-                                            interview.workflow_state === 'unshared' && (
-                                                <FaEyeSlash />
-                                            )
-                                        }
-                                    </AuthShowContainer>
-                                    <AuthShowContainer ifLoggedOut ifNoProject>
-                                        {interview.anonymous_title[locale]}
-                                    </AuthShowContainer>
-                                </div>
-                            )
-                        }
-                    </Link>
-                </td>
-                {
-                    project.list_columns.map(column => {
-                        let obj = (column.ref_object_type === 'Interview' || column.source === 'Interview') ?
-                            interview :
-                            interviewee;
-
-                        return (
-                            <td key={column.name}>
-                                {obj && humanReadable(obj, column.name, this.props, {})}
-                            </td>
-                        );
-                    })
-                }
-                {
-                    fulltext && resultCount > 0 && (
-                        <td>
-                            <Link className="search-result-link"
-                                onClick={() => setArchiveId(interview.archive_id)}
-                                to={pathBase(this.props) + '/interviews/' + interview.archive_id}
-                            >
-                                {resultCount}
-                            </Link>
-                        </td>
-                    )
-                }
-            </tr>
-        );
-    }
+                    );
+                })
+            }
+            {
+                fulltext && resultCount > 0 && (
+                    <td>
+                        <Link className="search-result-link"
+                            onClick={() => setArchiveId(interview.archive_id)}
+                            to={`${pathBase}/interviews/${interview.archive_id}`}
+                        >
+                            {resultCount}
+                        </Link>
+                    </td>
+                )
+            }
+        </tr>
+    );
 }
 
 InterviewListRow.propTypes = {
+    projectId: PropTypes.string.isRequired,
+    projects: PropTypes.object.isRequired,
     fulltext: PropTypes.string,
     locale: PropTypes.string.isRequired,
+    translations: PropTypes.object.isRequired,
     interview: PropTypes.object,
     interviewee: PropTypes.object.isRequired,
     interviewSearchResults: PropTypes.object,
     project: PropTypes.object.isRequired,
+    peopleStatus: PropTypes.object.isRequired,
     selectedArchiveIds: PropTypes.array.isRequired,
     setArchiveId: PropTypes.func.isRequired,
     searchInInterview: PropTypes.func.isRequired,
     addRemoveArchiveId: PropTypes.func.isRequired,
+    fetchData: PropTypes.func.isRequired,
 };
