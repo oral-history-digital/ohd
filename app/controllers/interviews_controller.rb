29fbf696c710c1bd5d812094efa32680aa373194
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-10-14 11:03:14 +0200

    INTARCH-1462: Fix crashing pdfs for some languages
    Former-commit-id: 1a7f353fec191aea2a3993d3f1be1817089b4a40

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -136,7 +136,8 @@ class InterviewsController < ApplicationController
   def show
     @interview = Interview.find_by_archive_id(params[:id])
     interview_locale = @interview.alpha3_transcript_locales.first && ISO_639.find(@interview.alpha3_transcript_locales.first).alpha2.to_sym
-    @locale = params[:lang] # change this to params[:locale] if  you want e.g. header and footer in locale
+
+    @locale = params[:locale]
 
     respond_to do |format|
       format.json do
@@ -150,7 +151,6 @@ class InterviewsController < ApplicationController
       end
       format.pdf do
         @lang = "#{params[:lang]}-public"
-        #@locale = params[:lang] # change this to params[:locale] if  you want e.g. header and footer in locale
         @lang_human = I18n.t(params[:lang], locale: @locale)
         @orig_lang = "#{interview_locale}-public"
         first_segment_with_heading = @interview.segments.with_heading.first
