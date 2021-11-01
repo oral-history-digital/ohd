01abb42588d53275f9c18fc354ff5985d1f8a6cc
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-11-01 19:29:10 +0100

    Refactor InterviewTabPanel component
    Former-commit-id: fd4924b90ef70b31728d2ef41ffe53c23048082f

--- app/javascript/modules/flyout-tabs/components/InterviewTabPanelContainer.js
+++ app/javascript/modules/flyout-tabs/components/InterviewTabPanelContainer.js
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getCurrentInterview, getCurrentInterviewee, getCurrentProject, getProjects, getCurrentAccount } from 'modules/data';
+import { getCurrentInterview, getCurrentInterviewee, getCurrentProject } from 'modules/data';
 import { searchInArchive } from 'modules/search';
-import { setViewMode, getLocale, getArchiveId, getProjectId, getTranslations, getEditView } from 'modules/archive';
+import { setViewMode, getArchiveId, getProjectId } from 'modules/archive';
 import InterviewTabPanel from './InterviewTabPanel';
 import { hideFlyoutTabs } from '../actions';
 
@@ -12,14 +12,9 @@ const mapStateToProps = (state) => {
     return {
         archiveId: getArchiveId(state),
         projectId: getProjectId(state),
-        projects: getProjects(state),
         interview: getCurrentInterview(state),
         interviewee: getCurrentInterviewee(state),
         hasMap: project && project.has_map === 1,
-        locale: getLocale(state),
-        translations: getTranslations(state),
-        account: getCurrentAccount(state),
-        editView: getEditView(state),
     };
 };
 
