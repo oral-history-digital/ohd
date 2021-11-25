1b2ac27be3c32334cf67960725731f6d00b62c37
cgregor <chrgregor@gmail.com>
2021-11-25 12:47:54 +0100

    mv symlinking logos to model, fix transcript pdf
    Former-commit-id: 54d447cd94990e89aa95c32f454903819db8d148

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,4 +1,13 @@
 class UploadedFile < ApplicationRecord
   belongs_to :ref, polymorphic: true, touch: true
   has_one_attached :file
+
+  after_save :symlink
+  def symlink
+    symlink_path = File.join(Rails.root, 'tmp', 'files', file.blob.filename.to_s)
+    File.delete(symlink_path) if File.file?(symlink_path)
+    blob_path = ActiveStorage::Blob.service.send(:path_for, file.key)
+    File.symlink(blob_path, symlink_path)
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,93 @@
+class User < ApplicationRecord
+
+  require 'user_account'
+  #require 'archive-authorization'
+
+  belongs_to :user_account
+  belongs_to :user_registration
+
+  #has_many :user_contents
+
+  #has_many :searches
+
+  ##acts_as_authorized_user
+
+  #has_many :user_roles
+  #has_many :roles, through: :user_roles
+  #has_many :permissions, through: :roles
+
+  #has_many :tasks
+  #has_many :supervised_tasks,
+           #class_name: 'Task',
+           #foreign_key: :supervisor_id
+
+  #delegate :email, :login,
+           #:to => :user_account
+
+  #def tasks?(record)
+    #!tasks.where(authorized: record).where.not(workflow_state: 'finished').blank? #||
+    ##!supervised_tasks.where(authorized: record).blank?
+  #end
+
+  #def permissions?(klass, action_name)
+    #!permissions.where(klass: klass, action_name: action_name).blank?
+  #end
+
+  #def roles?(klass, action_name)
+    #!roles.joins(:permissions).where("permissions.klass": klass, "permissions.action_name": action_name).blank?
+  #end
+
+  #def to_s
+    #[ first_name, last_name ].compact.join(' ')
+  #end
+
+  #def zipcity
+    #"#{zipcode} #{city}"
+  #end
+
+  #def zipcity=(str)
+    #self.zipcode = (str[/^\s*\d+/] || '').strip
+    #self.city = str.sub(zipcode, '').strip
+  #end
+
+  #def email=(address)
+    #user_account.update_attribute :email, address
+    #user_account.reload
+    #user_registration.update_attribute :email, address
+    #user_registration.reload
+  #end
+
+  #def admin?
+    #read_attribute(:admin) == true
+  #end
+
+  #def tags
+    #Tag.for_user(self) unless Tag.nil?
+  #end
+
+  ## Authenticate a user based on configured attribute keys. Returns the
+  ## authenticated user if it's valid or nil.
+  #def authenticate(attributes={})
+    #return unless Devise.authentication_keys.all? { |k| attributes[k].present? }
+    #conditions = attributes.slice(*Devise.authentication_keys)
+    #auth_proxy = find_for_authentication(conditions)
+    #auth_proxy.user if auth_proxy.try(:valid_for_authentication?, attributes)
+  #end
+
+  #private
+
+  ## Find first record based on conditions given (ie by the sign in form).
+  ## Overwrite to add customized conditions, create a join, or maybe use a
+  ## namedscope to filter records while authenticating.
+  ## Example:
+  ##
+  ##   def self.find_for_authentication(conditions={})
+  ##     conditions[:active] = true
+  ##     find(:first, :conditions => conditions)
+  ##   end
+  ##
+  #def find_for_authentication(conditions)
+    #UserAccount.where(conditions).first
+  #end
+
+end
