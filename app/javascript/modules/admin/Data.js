2ffd5b78e470826cdf6484882d648d105f140b87
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2021-12-14 19:52:28 +0100

    Refactor Data component
    Former-commit-id: 99f0f2ce4143f5d2aafd5a9696a68d98b29bb84a

--- app/javascript/modules/admin/Data.js
+++ app/javascript/modules/admin/Data.js
@@ -1,164 +1,153 @@
-import { createElement, Component } from 'react';
-import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { AuthorizedContent } from 'modules/auth';
 import { ArchivePopupButton, PopupMenu } from 'modules/ui';
-import { humanReadable } from 'modules/data';
-import { pluralize, camelCase } from 'modules/strings';
-import { t } from 'modules/i18n';
+import { pluralize } from 'modules/strings';
+import { useI18n } from 'modules/i18n';
+import BaseData from './BaseData';
+import JoinedData from './JoinedData';
+import DataDetailsContainer from './DataDetailsContainer';
 
-export default class Data extends Component {
+export default function Data({
+    data,
+    joinedData,
+    locale,
+    projectId,
+    projects,
+    task,
+    form,
+    scope,
+    outerScope,
+    outerScopeId,
+    showComponent,
+    hideShow,
+    hideEdit,
+    hideDelete,
+    optionsScope,
+    detailsAttributes,
+    closeArchivePopup,
+    deleteData,
+}) {
+    const { t } = useI18n();
 
-    baseData() {
-        if (this.props.showComponent) {
-            return createElement(this.props.showComponent, {data: this.props.data, scope: this.props.scope });
-        } else {
-            return (
-                <div className='base-data box'>
-                    <p className='name'>{this.name()}</p>
-                </div>
-            )
-        }
-    }
-
-    name() {
-        const { data, locale } = this.props;
-
-        return data.title || (data.name?.hasOwnProperty(locale) ? data.name[locale] : data.name);
-    }
-
-    details() {
-        return (
-            <div className='details'>
-                {
-                    this.props.detailsAttributes.map((attribute, index) => {
-                        if (attribute === 'src') {
-                            return <img src={ this.props.data.src } />
-                        } else {
-                            return (
-                                <p className='detail' key={attribute}>
-                                    <span className='name'>{t(this.props, `activerecord.attributes.${this.props.scope}.${attribute}`) + ': '}</span>
-                                    <span className='content'>{humanReadable(this.props.data, attribute, this.props, {})}</span>
-                                </p>
-                            )
-                        }
-                    })
-                }
-            </div>
-        )
-    }
-
-    show() {
-        return (
-            <ArchivePopupButton
-                title={this.name()}
-                type="show"
-            >
-                {this.details()}
-            </ArchivePopupButton>
-        )
-    }
-
-    edit() {
-        return (
-            <ArchivePopupButton
-                title={`${this.name()} ${t(this.props, `edit.${this.props.scope}.edit`)}`}
-                type="edit"
-            >
-                <>
-                    {this.props.hideShow && this.details()}
-                    {this.props.form(this.props.data, this.props.closeArchivePopup)}
-                </>
-            </ArchivePopupButton>
-        )
-    }
-
-    destroy() {
+    function destroy() {
         // skip remove from state, only remove server-side
-        this.props.deleteData(this.props, pluralize(this.props.scope), this.props.data.id, null, null, true);
+        deleteData({ locale, projectId, projects }, pluralize(scope), data.id,
+            null, null, true);
         // only remove from state
-        this.props.deleteData(
-            this.props,
-            this.props.outerScope ? pluralize(this.props.outerScope) : pluralize(this.props.scope),
-            this.props.outerScopeId || this.props.data.id,
-            this.props.outerScope ? pluralize(this.props.scope) : null,
-            this.props.outerScope ? this.props.data.id : null,
+        deleteData(
+            { locale, projectId, projects },
+            outerScope ? pluralize(outerScope) : pluralize(scope),
+            outerScopeId || data.id,
+            outerScope ? pluralize(scope) : null,
+            outerScope ? data.id : null,
             null,
             true
         );
-        this.props.closeArchivePopup();
+        closeArchivePopup();
     }
 
-    delete() {
-        return (
-            <ArchivePopupButton
-                title={t(this.props, 'delete')}
-                type="delete"
-            >
-                <>
-                    <p>{this.name()}</p>
-                    <div className='any-button' onClick={() => this.destroy()}>
-                        {t(this.props, 'delete')}
-                    </div>
-                </>
-            </ArchivePopupButton>
-        )
+    if (!data) {
+        return null;
     }
 
-    joinedData() {
-        if (this.props.joinedData) {
-            return Object.keys(this.props.joinedData).map((joined_model_name_underscore, index) => {
-                let props = {
-                    data: this.props.data[pluralize(joined_model_name_underscore)],
-                    task: this.props.data.type === 'Task' && this.props.data,
-                    initialFormValues: {
-                        [`${this.props.scope}_id`]: this.props.data.id,
-                        //
-                        // the following could be generalized better
-                        // at the moment it is ment to get the polymorphic association 'ref'
-                        // and multiple possible types of uploaded_file into the form
-                        //
-                        ref_id: this.props.data.id,
-                        ref_type: this.props.data.type,
-                        type: camelCase(joined_model_name_underscore)
-                    }
-                }
+    const name = data.title ||
+        (data.name?.hasOwnProperty(locale) ? data.name[locale] : data.name);
 
-                return (
-                    <div className={`${pluralize(joined_model_name_underscore)} box`} key={`${joined_model_name_underscore}-box`}>
-                        <h4 className='title'>{t(this.props, `activerecord.models.${joined_model_name_underscore}.other`)}</h4>
-                        {createElement(this.props.joinedData[joined_model_name_underscore], props)}
-                    </div>
-                );
-            });
-        } else {
-            return null;
-        }
-    }
+    return (
+        <div className="data boxes">
+            <BaseData
+                name={name}
+                data={data}
+                scope={scope}
+                showComponent={showComponent}
+            />
 
-    buttons() {
-        return (
-            <AuthorizedContent object={[this.props.data, this.props.task]} action='update'>
+            <AuthorizedContent object={[data, task]} action='update'>
                 <PopupMenu>
-                    <PopupMenu.Item>{!this.props.hideShow && this.show()}</PopupMenu.Item>
-                    <PopupMenu.Item>{!this.props.hideEdit && this.edit()}</PopupMenu.Item>
-                    <PopupMenu.Item>{!this.props.hideDelete && this.delete()}</PopupMenu.Item>
+                    <PopupMenu.Item>
+                        {!hideShow && (
+                            <ArchivePopupButton
+                                title={name}
+                                type="show"
+                            >
+                                <DataDetailsContainer
+                                    detailsAttributes={detailsAttributes}
+                                    data={data}
+                                    scope={scope}
+                                    optionsScope={optionsScope}
+                                />
+                            </ArchivePopupButton>
+                        )}
+                    </PopupMenu.Item>
+                    <PopupMenu.Item>
+                        {!hideEdit && (
+                            <ArchivePopupButton
+                                title={`${name} ${t(`edit.${scope}.edit`)}`}
+                                type="edit"
+                            >
+                                <>
+                                    {hideShow && (
+                                        <DataDetailsContainer
+                                            detailsAttributes={detailsAttributes}
+                                            data={data}
+                                            scope={scope}
+                                            optionsScope={optionsScope}
+                                        />
+                                    )}
+                                    {form(data, closeArchivePopup)}
+                                </>
+                            </ArchivePopupButton>
+                        )}
+                    </PopupMenu.Item>
+                    <PopupMenu.Item>
+                        {!hideDelete && (
+                            <ArchivePopupButton
+                                title={t('delete')}
+                                type="delete"
+                            >
+                                <p>{name}</p>
+                                <button
+                                    type="button"
+                                    className="Button any-button"
+                                    onClick={destroy}
+                                >
+                                    {t('delete')}
+                                </button>
+                            </ArchivePopupButton>
+                        )}
+                    </PopupMenu.Item>
                 </PopupMenu>
             </AuthorizedContent>
-        );
-    }
 
-    render() {
-        if (this.props.data) {
-            return (
-                <div className='data boxes'>
-                    {this.baseData()}
-                    {this.buttons()}
-                    {this.joinedData()}
-                </div>
-            )
-        } else {
-            return null;
-        }
-    }
+            {joinedData && (
+                <JoinedData
+                    joinedData={joinedData}
+                    data={data}
+                    scope={scope}
+                />
+            )}
+        </div>
+    );
 }
+
+Data.propTypes = {
+    locale: PropTypes.string.isRequired,
+    projectId: PropTypes.string.isRequired,
+    projects: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
+    joinedData: PropTypes.object,
+    task: PropTypes.object,
+    form: PropTypes.func.isRequired,
+    scope: PropTypes.string.isRequired,
+    optionsScope: PropTypes.string.isRequired,
+    outerScope: PropTypes.string,
+    outerScopeId: PropTypes.string,
+    showComponent: PropTypes.element,
+    hideShow: PropTypes.bool,
+    hideEdit: PropTypes.bool,
+    hideDelete: PropTypes.bool,
+    detailsAttributes: PropTypes.array,
+    closeArchivePopup: PropTypes.func.isRequired,
+    deleteData: PropTypes.func.isRequired,
+};
---
+++ app/javascript/modules/admin/DataDetails.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+
+import { humanReadable } from 'modules/data';
+import { useI18n } from 'modules/i18n';
+
+export default function DataDetails({
+    detailsAttributes,
+    data,
+    scope,
+    optionsScope,
+    locale,
+    translations,
+}) {
+    const { t } = useI18n();
+
+    return (
+        <div className="details">
+            {
+                detailsAttributes.map(attribute => {
+                    if (attribute === 'src') {
+                        return (
+                            <img
+                                key={attribute}
+                                src={data.src}
+                                alt=""
+                            />
+                        );
+                    } else {
+                        return (
+                            <p
+                                key={attribute}
+                                className="detail"
+                            >
+                                <span className='name'>
+                                    {t(`activerecord.attributes.${scope}.${attribute}`) + ': '}
+                                </span>
+                                <span className='content'>
+                                    {humanReadable(data, attribute, { locale, translations, optionsScope }, {})}
+                                </span>
+                            </p>
+                        )
+                    }
+                })
+            }
+        </div>
+    );
+}
+
+DataDetails.propTypes = {
+    detailsAttributes: PropTypes.array.isRequired,
+    data: PropTypes.object.isRequired,
+    scope: PropTypes.string.isRequired,
+    optionsScope: PropTypes.string.isRequired,
+    locale: PropTypes.string.isRequired,
+    translations: PropTypes.object.isRequired,
+};
