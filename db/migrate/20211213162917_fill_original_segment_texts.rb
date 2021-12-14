aeb782a89bca9054ae6c3046311900a8cb111987
cgregor <chrgregor@gmail.com>
2021-12-14 19:57:24 +0100

    INTARCH-1621: fill segment-translations
    Former-commit-id: c3eb2f9b08da5d31d034ff0b6970d904dba17ac0

---
+++ db/migrate/20211213162917_fill_original_segment_texts.rb
@@ -0,0 +1,14 @@
+class FillOriginalSegmentTexts < ActiveRecord::Migration[5.2]
+  def change
+    execute "DELETE FROM segment_translations WHERE text IS NULL OR text = ''"
+    Segment.joins(:translations).group(:segment_id).count.each do |segment_id, translations_count|
+      if [1, 2].include?(translations_count)
+        Segment::Translation.where(segment_id: segment_id).each do |translation|
+          locale = translation.locale.to_s.sub('-public', '')
+          text = translation.text
+          Segment::Translation.create(segment_id: segment_id, locale: locale, text: text)
+        end
+      end
+    end
+  end
+end
