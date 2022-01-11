35557ab6c3ab5da019b3e0967d16912581214866
cgregor <chrgregor@gmail.com>
2022-01-11 10:35:26 +0100

    set cookies SameSite=Lax and expire session cookie after 2 days
    Former-commit-id: 26ed041f1962774fb945cbdcecec6c1be477a28b

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_archive_session', same_site: :strict
+Rails.application.config.session_store :cookie_store, key: '_archive_session', same_site: :lax, expires: 2.days.from_now 
 
 
