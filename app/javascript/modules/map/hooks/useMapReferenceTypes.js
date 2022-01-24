a5aa38238df4e3c80c670bc6b13b6572db2e4051
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-01-24 13:00:00 +0100

    Translate a string
    Former-commit-id: 34735d85d3abab1282d7c6194b39108c25f70dc4

--- app/javascript/modules/map/hooks/useMapReferenceTypes.js
+++ app/javascript/modules/map/hooks/useMapReferenceTypes.js
@@ -1,10 +1,12 @@
 import useSWRImmutable from 'swr/immutable';
 
+import { useI18n } from 'modules/i18n';
 import { fetcher } from 'modules/api';
 import { usePathBase } from 'modules/routes';
 import { MARKER_COLOR_SEGMENT_TYPE } from '../constants';
 
 export default function useMapReferenceTypes() {
+    const { t } = useI18n();
     const pathBase = usePathBase();
 
     const path = `${pathBase}/searches/map_reference_types`;
@@ -16,7 +18,7 @@ export default function useMapReferenceTypes() {
     if (data) {
         combinedTypes = data.concat({
             id: 'S',
-            name: 'Erwähnungen',
+            name: t('modules.map.mentions'),
             color: MARKER_COLOR_SEGMENT_TYPE,
         })
     }
