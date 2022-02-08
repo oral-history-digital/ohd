bb7846e10186ba7a0f098e46be6541850d9a9888
Marc Altmann <marcaltmann@posteo.de>
2022-02-08 14:37:49 +0100

    INTARCH-831: Fix biography pdf line break handling
    Former-commit-id: aae1af56c903edea01d37f8e866ca4dac3dc7a5e

--- app/models/biographical_entry.rb
+++ app/models/biographical_entry.rb
@@ -1,7 +1,7 @@
 class BiographicalEntry < ApplicationRecord
 
   belongs_to :person, touch: true
-  
+
   translates :text, :start_date, :end_date, fallbacks_for_empty_translations: true, touch: true
 
   scope :with_public_state, ->{ where(workflow_state: 'public') }
@@ -21,5 +21,12 @@ class BiographicalEntry < ApplicationRecord
 
   def interview_id
     person.interviews.first && person.interviews.first.id
+  end
+
+  def for_latex(locale)
+    sanitized_text = ActionView::Base.full_sanitizer.sanitize(text(locale))
+      .gsub("&amp;", "&")
+    escaped_text = LatexToPdf.escape_latex(sanitized_text)
+    escaped_text.gsub(/\r?\n/, '~\newline~')
   end
 end
