f025c016df4b7382a5e9f61b4c461b145c3e01f7
cgregor <chrgregor@gmail.com>
2022-02-18 13:00:53 +0100

    fix constants
    Former-commit-id: d6f3492afeeeac20a01b814367de3351ebf5ae54

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
-OHD_DOMAIN = OHD_DOMAINS[Rails.env]
 OHD_DOMAINS = {
   'development' => 'http://portal.oral-history.localhost:3000',
   'production' => 'https://portal.oral-history.digital',
   'staging' => 'https://test.oral-history.digital'
 }
+OHD_DOMAIN = OHD_DOMAINS[Rails.env]
 DEFAULT_PRIMARY_COLOR = '#e01217'
