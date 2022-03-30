b84dc243a77258dc3849c91a49a003437fd241eb
cgregor <chrgregor@gmail.com>
2022-03-30 16:15:29 +0200

    update archive_ids of tsa and bms
    Former-commit-id: 5cc7862cd5fccfc7c145ea01056f75ab533e3ccb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_115326) do
+ActiveRecord::Schema.define(version: 2022_03_30_141154) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 255, null: false
