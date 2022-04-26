8efec1f7856bb644ae6631d9ff78aae6c1cb7e03
cgregor <chrgregor@gmail.com>
2022-04-26 16:20:01 +0200

    refactor SingleTextInputForm and RegistryNameForm to function-components
    Former-commit-id: 82d5720e273767060e27065350eb94ddbcf2f14d

--- app/javascript/modules/registry/RegistryNameForm.js
+++ app/javascript/modules/registry/RegistryNameForm.js
@@ -1,44 +1,54 @@
-import { Component } from 'react';
-
 import { Form } from 'modules/forms';
 
-export default class RegistryNameForm extends Component {
+export default function RegistryNameForm({
+    index,
+    submitData,
+    onSubmitCallback,
+    onCancel,
+    formClasses,
+    data,
+    nested,
+    registryEntryId,
+    registryNameTypes,
+    projectId,
+    projects,
+    locale,
+}) {
+    const defaultNameType = Object.values(registryNameTypes).find(r => r.code === 'spelling')
 
-    render() {
-        let defaultNameType = Object.values(this.props.registryNameTypes).find(r => r.code === 'spelling')
-        let _this = this;
-        return (
-            <Form
-                scope='registry_name'
-                onSubmit={function(params){_this.props.submitData(_this.props, params, _this.props.index);}}
-                onSubmitCallback={_this.props.onSubmitCallback}
-                formClasses={_this.props.formClasses}
-                data={this.props.data}
-                values={{
-                    registry_entry_id: (_this.props.data && _this.props.data.registry_entry_id) || _this.props.registryEntryId,
-                    registry_name_type_id: (_this.props.data && _this.props.data.registry_name_type_id) || defaultNameType.id,
-                    name_position: 1,
-                }}
-                submitText='submit'
-                elements={[
-                    {
-                        attribute: 'descriptor',
-                        multiLocale: true
-                    },
-                    {
-                        elementType: 'textarea',
-                        multiLocale: true,
-                        attribute: 'notes',
-                    },
-                    {
-                        elementType: 'select',
-                        attribute: 'registry_name_type_id',
-                        value: (_this.props.data && _this.props.data.registry_name_type_id) || defaultNameType.id,
-                        values: this.props.registryNameTypes && Object.values(this.props.registryNameTypes),
-                        validate: function(v){return /^\d+$/.test(v)}
-                    },
-                ]}
-            />
-        );
-    }
+    return (
+        <Form
+            scope='registry_name'
+            onSubmit={function(params){submitData({projectId, projects, locale}, params, index);}}
+            onSubmitCallback={onSubmitCallback}
+            onCancel={onCancel}
+            formClasses={formClasses}
+            data={data}
+            nested={nested}
+            values={{
+                registry_entry_id: (data?.registry_entry_id) || registryEntryId,
+                registry_name_type_id: (data?.registry_name_type_id) || defaultNameType.id,
+                name_position: 1,
+            }}
+            submitText='submit'
+            elements={[
+                {
+                    attribute: 'descriptor',
+                    multiLocale: true
+                },
+                {
+                    elementType: 'textarea',
+                    multiLocale: true,
+                    attribute: 'notes',
+                },
+                {
+                    elementType: 'select',
+                    attribute: 'registry_name_type_id',
+                    value: (data?.registry_name_type_id) || defaultNameType.id,
+                    values: registryNameTypes && Object.values(registryNameTypes),
+                    validate: function(v){return /^\d+$/.test(v)}
+                },
+            ]}
+        />
+    );
 }
