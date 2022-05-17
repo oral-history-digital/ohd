0ecd0da8830e3a1750ea580ef6b7c913526dad52
cgregor <chrgregor@gmail.com>
2022-05-17 18:37:51 +0200

    fix norm_data-registry_name_type migration
    Former-commit-id: 22e81f43b180a00e6a952a7a6844485e171f300a

--- db/migrate/20220517134918_rm_norm_data_registry_name_type.rb
+++ db/migrate/20220517134918_rm_norm_data_registry_name_type.rb
@@ -2,7 +2,8 @@ class RmNormDataRegistryNameType < ActiveRecord::Migration[5.2]
   def change
     Project.all.each do |project|
       spelling = project.registry_name_types.where(code: 'spelling').first
-      project.registry_name_types.where(code: 'norm_data').first.registry_names.update_all(registry_name_type_id: spelling.id)
+      norm_data = project.registry_name_types.where(code: 'norm_data').first
+      norm_data.registry_names.update_all(registry_name_type_id: spelling.id) if norm_data
     end
 
     RegistryNameType.where(code: 'norm_data').destroy_all
