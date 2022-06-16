4757b96be8d4575a7b13b42db32dab8da1bffaf8
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-06-16 12:18:17 +0200

    INTARCH-1854: Remove export search results button
    Former-commit-id: f88b7d5c537bdad9c8394142c68e68aae98dd5b9

--- app/javascript/modules/search/components/SearchActions.js
+++ app/javascript/modules/search/components/SearchActions.js
@@ -1,28 +1,21 @@
 import PropTypes from 'prop-types';
-import { FaStar, FaDownload } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 import moment from 'moment';
-import queryString from 'query-string';
-import range from 'lodash.range';
 
-import { PROJECT_DG } from 'modules/constants';
 import { useI18n } from 'modules/i18n';
 import { Modal } from 'modules/ui';
-import { usePathBase } from 'modules/routes';
 import { UserContentFormContainer } from 'modules/workbook';
 import { useSearchParams } from 'modules/query-string';
 import queryToText from '../queryToText';
 import useFacets from '../useFacets';
 
 export default function SearchActions({
-    projectId,
     locale,
     translations,
 }) {
     const { t } = useI18n();
-    const pathBase = usePathBase();
     const { facets } = useFacets();
-    const { allParams, fulltext, facets: urlFacets, yearOfBirthMin,
-        yearOfBirthMax } = useSearchParams();
+    const { allParams } = useSearchParams();
 
     function saveSearchForm(closeModal) {
         moment.locale(locale);
@@ -41,42 +34,17 @@ export default function SearchActions({
         />
     }
 
-    const showExportSearchLink = Object.keys(allParams).length > 0 && projectId !== PROJECT_DG;
-
-    const params = {
-        fulltext,
-        ...urlFacets,
-        year_of_birth: range(yearOfBirthMin, yearOfBirthMax + 1),
-    };
-    const paramStr = queryString.stringify(params, { arrayFormat: 'bracket' });
-    const exportUrl = `${pathBase}/searches/archive.csv?${paramStr}`;
-
     return (
-        <>
-            <Modal
-                title={t('save_search')}
-                trigger={<><FaStar className="Icon Icon--text" /> {t('save_search')}</>}
-            >
-                {saveSearchForm}
-            </Modal>
-            {
-                showExportSearchLink && (
-                    <Modal
-                        title={t('export_search_results')}
-                        trigger={<><FaDownload className="Icon Icon--text" /> {t('export_search_results')}</>}
-                    >
-                        <a href={exportUrl} download>
-                            CSV
-                        </a>
-                    </Modal>
-                )
-            }
-        </>
+        <Modal
+            title={t('save_search')}
+            trigger={<><FaStar className="Icon Icon--text" /> {t('save_search')}</>}
+        >
+            {saveSearchForm}
+        </Modal>
     );
 }
 
 SearchActions.propTypes = {
-    projectId: PropTypes.string.isRequired,
     locale: PropTypes.string.isRequired,
     translations: PropTypes.object.isRequired,
 };
