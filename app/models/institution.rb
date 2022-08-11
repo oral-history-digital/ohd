82c1ea6fe81b5c1853cdb075ca934932ffec841a
cgregor <chrgregor@gmail.com>
2022-08-11 14:34:56 +0200

    dependent destroy institution_project
    Former-commit-id: 883c9950bbc90c530965357e49fb9a11363f91f4

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,7 +1,7 @@
 class Institution < ApplicationRecord
   has_many :logos, as: :ref, dependent: :destroy
   belongs_to :parent, class_name: 'Institution'
-  has_many :institution_projects
+  has_many :institution_projects, dependent: :destroy
   has_many :projects, through: :institution_projects
   has_many :collections
 
