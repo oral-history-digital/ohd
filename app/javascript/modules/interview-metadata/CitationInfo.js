9e01ed4b8c1a66a838436d708b1c964286ece43e
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-09-14 12:54:39 +0200

    INTARCH-1953: Fix url for CitationInfo
    Former-commit-id: 5a1fb8f1bf3348b955187c1dedfd7b49bb665953

--- app/javascript/modules/interview-metadata/CitationInfo.js
+++ app/javascript/modules/interview-metadata/CitationInfo.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import { useI18n } from 'modules/i18n';
 import { usePathBase } from 'modules/routes';
-import { Fetch, getCollectionsForCurrentProjectFetched } from 'modules/data';
+import { OHD_LOCATION } from 'modules/constants';
 
 export default function CitationInfo({
     interview,
@@ -12,16 +12,13 @@ export default function CitationInfo({
     projectDoi,
     projectId,
     projectName,
-    archiveDomain,
 }) {
     const { t } = useI18n();
     const pathBase = usePathBase();
     const collection = collections?.[interview.collection_id];
 
-    let selfLink;
-    if (archiveDomain) {
-        selfLink = `${archiveDomain}${pathBase}/interviews/${interview.archive_id}`;
-    }
+    const domain = project.archive_domain || OHD_LOCATION;
+    const selfLink = `${domain}${pathBase}/interviews/${interview.archive_id}`;
 
     let doiLink;
     if (interview.doi_status === 'created') {
@@ -41,38 +38,34 @@ export default function CitationInfo({
                 <span className="flyout-content-label">
                     {t('citation')}:
                 </span>
-                <Fetch
-                    fetchParams={['collections', null, null, `for_projects=${project?.id}`]}
-                    testSelector={getCollectionsForCurrentProjectFetched}
-                >
-                    <span className="flyout-content-data">
-                        {interview.anonymous_title && `${interview.anonymous_title?.[locale]}, `}
-                        {t('interview')}
-                        {' '}
-                        {`${interview.archive_id}, `}
-                        {`${interview.interview_date}, `}
-                        {projectName && `${projectName[locale]}, `}
-                        {collectionName}
-                        {selfLink && <a href={selfLink}>{selfLink}</a>}
-                        {
-                            doiLink && (<>
-                                {', '}
-                                {t('doi.name')}:
-                                {' '}
-                                <a href={doiLink}>{doiLink}</a>
-                                {' '}
-                                {`(${t('called')}: ${currentDateStr})`}
-                            </>)
-                        }
-                    </span>
-                </Fetch>
+                <span className="flyout-content-data">
+                    {interview.anonymous_title && `${interview.anonymous_title?.[locale]}, `}
+                    {t('interview')}
+                    {' '}
+                    {`${interview.archive_id}, `}
+                    {`${interview.interview_date}, `}
+                    {projectName && `${projectName[locale]}, `}
+                    {collectionName}
+                    {selfLink && <a href={selfLink}>{selfLink}</a>}
+                    {
+                        doiLink && (<>
+                            {', '}
+                            {t('doi.name')}:
+                            {' '}
+                            <a href={doiLink}>{doiLink}</a>
+                            {' '}
+                            {`(${t('called')}: ${currentDateStr})`}
+                        </>)
+                    }
+                </span>
             </p>
         </div>
     );
 }
 
 CitationInfo.propTypes = {
-    archiveDomain: PropTypes.string.isRequired,
+    collections: PropTypes.object.isRequired,
+    locale: PropTypes.string.isRequired,
     projectDoi: PropTypes.string.isRequired,
     projectId: PropTypes.string.isRequired,
     projectName: PropTypes.object.isRequired,
