db557cdca3ed60f846f4d66db380499cbfc0794f
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-09-28 13:53:30 +0200

    INTARCH-1967: Add bookmark button to InterviewDetailsLeftSide
    Former-commit-id: 4d0bab797f1620034a044197c4cf24ea6157f008

--- app/javascript/modules/interview/components/InterviewDetailsLeftSide.js
+++ app/javascript/modules/interview/components/InterviewDetailsLeftSide.js
@@ -1,8 +1,10 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaStar } from 'react-icons/fa';
 
+import { WorkbookItemFormContainer } from 'modules/workbook';
+import { Modal } from 'modules/ui';
 import { InterviewInfoContainer, InterviewContributorsContainer } from 'modules/interview-metadata';
 import { SelectedRegistryReferencesContainer } from 'modules/registry-references';
 import { ContentField } from 'modules/forms';
@@ -29,62 +31,84 @@ export default function InterviewDetailsLeftSide({
     }
 
     return (
-        <div className={classNames('flyout-folder')} >
-            <div style={{ padding: "5%" }} >
-                <h3>{t('person_info')}</h3>
-                <div>
-                    <PersonDataContainer />
-                    {interviewee && (
-                        <SelectedRegistryReferencesContainer refObject={interviewee} />
+        <div className="wrapper-content interviews flyout-folder">
+            <div className="u-align-right">
+                <Modal
+                    title={t('save_interview_reference_tooltip')}
+                    trigger={(<>
+                        <FaStar className="Icon Icon--text" /> <span>{t('save_interview_reference')}</span>
+                    </>)}
+                    triggerClassName="Button Button--transparent"
+                >
+                    {closeModal => (
+                        <WorkbookItemFormContainer
+                            description=""
+                            properties={{title: interview.title}}
+                            reference_id={interview.id}
+                            reference_type='Interview'
+                            media_id={interview.archive_id}
+                            type='InterviewReference'
+                            submitLabel={t('notice')}
+                            onSubmit={closeModal}
+                            onCancel={closeModal}
+                        />
                     )}
-                </div>
-                <h3>{t('interview_info')}</h3>
-                <InterviewInfoContainer />
-                <InterviewContributorsContainer/>
-                { interview?.properties?.subcollection &&
-                    <ContentField
-                        label={t('subcollection')}
-                        value={interview.properties.subcollection}
-                    />
-                }
-                {interview?.properties?.link && (
-                    <ContentField
-                        label="Link"
-                        value={(
-                            <a
-                                href={interview.properties.link}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                {interview.properties.link}
-                            </a>
-                        )}
-                    />)
-                }
-                {projectId === 'campscapes' && (
-                    <div className="footer-navigation">
-                        <Link
-                            className={classNames('search-result-link', {
-                                'hidden': !prevArchiveId,
-                            })}
-                            to={`${pathBase}/interviews/${prevArchiveId}`}
-                        >
-                            <FaChevronLeft className="Icon Icon--text" />
-                            {prevArchiveId}
-                        </Link>
-                        {' '}
-                        <Link
-                            className={classNames('search-result-link', {
-                                'hidden': !nextArchiveId,
-                            })}
-                            to={`${pathBase}/interviews/${nextArchiveId}`}
-                        >
-                            {nextArchiveId}
-                            <FaChevronRight className="Icon Icon--text" />
-                        </Link>
-                    </div>
+                </Modal>
+            </div>
+
+            <h3>{t('person_info')}</h3>
+            <div>
+                <PersonDataContainer />
+                {interviewee && (
+                    <SelectedRegistryReferencesContainer refObject={interviewee} />
                 )}
             </div>
+            <h3>{t('interview_info')}</h3>
+            <InterviewInfoContainer />
+            <InterviewContributorsContainer/>
+            { interview?.properties?.subcollection &&
+                <ContentField
+                    label={t('subcollection')}
+                    value={interview.properties.subcollection}
+                />
+            }
+            {interview?.properties?.link && (
+                <ContentField
+                    label="Link"
+                    value={(
+                        <a
+                            href={interview.properties.link}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            {interview.properties.link}
+                        </a>
+                    )}
+                />)
+            }
+            {projectId === 'campscapes' && (
+                <div className="footer-navigation">
+                    <Link
+                        className={classNames('search-result-link', {
+                            'hidden': !prevArchiveId,
+                        })}
+                        to={`${pathBase}/interviews/${prevArchiveId}`}
+                    >
+                        <FaChevronLeft className="Icon Icon--text" />
+                        {prevArchiveId}
+                    </Link>
+                    {' '}
+                    <Link
+                        className={classNames('search-result-link', {
+                            'hidden': !nextArchiveId,
+                        })}
+                        to={`${pathBase}/interviews/${nextArchiveId}`}
+                    >
+                        {nextArchiveId}
+                        <FaChevronRight className="Icon Icon--text" />
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
