b5aeab585fa56e319eaa158bb26c4539f38a1bb3
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-10-06 19:37:47 +0200

    Add some methods to Event model
    Former-commit-id: ac8ec22bf4f7370d364d561953aa1ef4c573c939

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,26 @@ class Event < ApplicationRecord
   validates :display_date, length: { maximum: 100 }, allow_blank: true
   validates :person, presence: true
   validates :event_type, presence: true
+  validate :end_date_after_start_date
+
+  def single_day?
+    start_date == end_date
+  end
+
+  def period?
+    !single_day?
+  end
+
+  def duration
+    difference = (end_date - start_date).to_i
+    difference + 1
+  end
+
+  private
+
+  def end_date_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, 'must be at or after start date')
+    end
+  end
 end
