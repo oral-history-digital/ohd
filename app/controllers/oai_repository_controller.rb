68cd45d79e2a7e2c0f524497e197017f904987ab
cgregor <chrgregor@gmail.com>
2022-11-21 21:09:01 +0100

    rm oai_repository_controller
    Former-commit-id: 7df0c6f530a8aff5e99cb0d3cfa74dd594f4e6d9

--- app/controllers/oai_repository_controller.rb
+++
@@ -1,8 +0,0 @@
-class OaiRegositoryController < OaiRepository::ServicesController
-
-  skip_before_action :authenticate_user_account!
-  skip_after_action :verify_authorized
-  skip_after_action :verify_policy_scoped
-
-end
-
