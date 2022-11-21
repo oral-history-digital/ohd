e8761a69e6576686aaffa45a31f5d3cec65f88dd
cgregor <chrgregor@gmail.com>
2022-11-21 15:14:26 +0100

    fit oai
    Former-commit-id: 229a2ba37abbcd6675cbeed6aac80c08abd035e0

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,7 +2,8 @@ require 'globalize'
 require 'webvtt'
 
 class Interview < ApplicationRecord
-  include OaiRepository::Set
+  include OAI
+  #include OaiRepository::Set
   include ActiveModel::Validations
   include Export
 
@@ -381,11 +382,6 @@ class Interview < ApplicationRecord
           contributions.where(contribution_type_id: contribution_type.id).map(&:person)
         end
       end
-
-      OaiRepository.repository_name = project.name[project.default_locale]
-      OaiRepository.repository_url = project.archive_domain
-      OaiRepository.record_prefix = project.archive_domain
-      OaiRepository.admin_email = project.contact_email
     end
   end
 
@@ -703,78 +699,6 @@ class Interview < ApplicationRecord
       changed
     end
   end
-
-  def oai_dc_identifier
-    archive_id
-    "oai:#{project.identifier}:#{archive_id}"
-  end
-
-  def oai_dc_creator
-    anonymous_title
-  end
-
-  def oai_dc_subject
-    project.registry_reference_type_metadata_fields.where(use_in_results_table: true, ref_object_type: 'Interview').each do |field|
-      "#{field.label(project.default_locale)}: #{self.send(field.name).map{|f| RegistryEntry.find(f).to_s(project.default_locale)}.join(';')}"
-    end
-  end
-
-  def oai_dc_description
-    "Lebensgeschichtliches #{self.video}-Interview in #{self.language.name.downcase}er Sprache mit Transkription, deutscher Übersetzung, Erschließung, Kurzbiografie und Fotografien"
-  end
-
-  def oai_dc_publisher
-    "Interview-Archiv \"#{project.name('de')}\""
-  end
-
-  def oai_dc_contributor
-    oai_contributors = [
-      %w(interviewers Interviewführung),
-      %w(cinematographers Kamera),
-      %w(transcriptors Transkripteur),
-      %w(translators Übersetzer),
-      %w(segmentators Erschließer)
-    ].inject([]) do |mem, (contributors, contribution)|
-      if self.send(contributors).length > 0
-        "#{contribution}: " + self.send(contributors).map{|contributor| "#{contributor.last_name(project.default_locale)}, #{contributor.first_name(project.default_locale)}"}.join('; ')
-      end
-      mem
-    end
-    if !project.cooperation_partner.blank?
-      oai_contributors << "Kooperationspartner: #{project.cooperation_partner}"
-    end
-    oai_contributors << "Projektleiter: #{project.leader}"
-    oai_contributors << "Projektmanager: #{project.manager}"
-    oai_contributors << "Hosting Institution: #{project.institutions.map(&:name).join(', ')}"
-    oai_contributors.join('. ')
-  end
-
-  def oai_dc_date
-    interview_date && Date.parse(interview_date).strftime("%d.%m.%Y") rescue interview_date
-  end
-
-  #def oai_dc_type
-  #end
-
-  def oai_dc_format
-    video
-  end
-
-  #def oai_dc_source
-  #end
-
-  def oai_dc_language
-    language && language.name
-  end
-
-  #def oai_dc_relation
-  #end
-
-  #def oai_dc_coverage
-  #end
-
-  #def oai_dc_rights
-  #end
 
   def type
     'Interview'
