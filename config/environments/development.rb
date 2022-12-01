3afb31fe575da4a71db08537b2fb2eb33c158b3b
cgregor <chrgregor@gmail.com>
2022-12-01 17:01:27 +0100

    whitelist hosts
    Former-commit-id: 7a717635cfa7c504d8ae5e64b6cb74f32374b753

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "portal.oral-history.localhost"
+  config.hosts = ["localhost", "www.example.com", "portal.oral-history.localhost"]
 end
