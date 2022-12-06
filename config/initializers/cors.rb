b25e04f22c012a77b601e9360500c2ec52f645cc
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-12-06 16:17:52 +0100

    Try again
    Former-commit-id: 47ebfdae3d47a38d21fe4f2db083132786340eef

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'archiv.eiserner-vorhang.de'
+    origins 'https://archiv.eiserner-vorhang.de'
 
-    resource '/de/hls.key', headers: :any, methods: :get
+    resource *, headers: :any, methods: :get
   end
 end
