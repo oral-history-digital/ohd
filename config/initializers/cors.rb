81bd524d5e0422c1c3bac11e549addcb08ec1251
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-12-06 16:20:56 +0100

    Fix syntax error
    Former-commit-id: 71465ea9176f6c0ca4263d16a0627ddb74b10bad

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://archiv.eiserner-vorhang.de'
 
-    resource *, headers: :any, methods: :get
+    resource '*', headers: :any, methods: :get
   end
 end
