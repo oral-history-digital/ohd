0245f912fa8b277ba09807ab9d88c61647d849f5
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-12-12 11:56:58 +0100

    Fix displaying events in thumbnails
    Former-commit-id: 16b4501d732db60c24b2e84be13d13a7f2e697c0

--- app/javascript/modules/events/index.js
+++ app/javascript/modules/events/index.js
@@ -1,5 +1,5 @@
 export { default as usePersonEvents } from './usePersonEvents';
 export { default as Event } from './Event';
-export { default as EventAlt } from './EventAlt';
 export { default as EventContentField } from './EventContentField';
 export { default as EventForm } from './EventForm';
+export { default as formatEventShort } from './formatEventShort';
