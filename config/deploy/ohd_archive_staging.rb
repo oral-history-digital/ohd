6f229718124398c38050110f8a4d09c3ed616582
cgregor <chrgregor@gmail.com>
2022-12-15 22:04:54 +0100

    deploy sso-branch to ohd-staging
    Former-commit-id: f6193028de83ae173d9599265f214e1bbd8cf2ac

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -2,6 +2,7 @@ server "deploy_da03", roles: %w{app db web}
 
 set :application, "ohd_archive_staging"
 set :stage, :staging
+set :branch, 'sso'
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
