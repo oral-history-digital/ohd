f4530f024b9e35381520669aacf5c023fba645e5
cgregor <chrgregor@gmail.com>
2022-12-19 16:51:42 +0100

    Revert "merged sso into development"
    This reverts commit e6258a5b346281852c995cf62c5c2db1a11c52ca [formerly 07a683adc68e573efcea94a8d489b4a251acbade], reversing
    changes made to 84477cf570a02cf916a8ff9afc4c3e14054330a5 [formerly dc82e7a83c01fdf85bb31aacc1d938c22b6ff1eb].
    
    
    Former-commit-id: cabf062e2e3d9c865c468e8d7dde4ac78a0225d1

--- app/models/contribution_type.rb
+++ app/models/contribution_type.rb
@@ -4,11 +4,4 @@ class ContributionType < ApplicationRecord
 
   translates :label, fallbacks_for_empty_translations: true, touch: true
   accepts_nested_attributes_for :translations
-
-  after_update :touch_contributions
-  def touch_contributions
-    contributions.update_all updated_at: Time.now
-    project.interviews.update_all updated_at: Time.now
-  end
-
 end
