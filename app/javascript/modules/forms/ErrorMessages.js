6521d751e928d3b2ec15dd6272cb76031c5a3afe
cgregor <chrgregor@gmail.com>
2023-01-13 12:20:46 +0100

    add error messages on submit
    Former-commit-id: c47481ae81cee6c76cd2a8b5578eebe9544be93d

---
+++ app/javascript/modules/forms/ErrorMessages.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { useI18n } from 'modules/i18n';
+
+export default function ErrorMessages({
+    elements,
+    errors,
+    scope,
+}) {
+
+    const { t } = useI18n();
+    const atLeastOneError = Object.values(errors).find( e => e);
+
+    if (!atLeastOneError) return null;
+
+    return (
+        <p className='notifications'>
+            <ul>
+                {elements.map((element) => {
+                    if (errors[element.attribute]) {
+                        const key = element.labelKey || `activerecord.attributes.${scope}.${element.attribute}`;
+                        const elementType = element.elementType === 'input' ? `${element.type}_input` : element.elementType
+
+                        return (
+                            <li>
+                                <b>{element.label ? element.label : t(key)}: </b>
+                                {
+                                    element.individualErrorMsg ?
+                                        t(`activerecord.errors.models.${scope}.attributes.${element.attribute}.${element.individualErrorMsg}`) :
+                                        t(`activerecord.errors.default.${elementType}`)
+                                }
+                            </li>
+                        )
+                    }
+                })}
+            </ul>
+        </p>
+    )
+}
+
