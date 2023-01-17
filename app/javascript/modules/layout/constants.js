c0fa4225397190ed6b2d386baed7f024ac0338f3
cgregor <chrgregor@gmail.com>
2023-01-17 12:26:07 +0100

    updating staging domains
    Former-commit-id: b7ce8027e9bad8cdd91cd360162820de4133e28c

--- app/javascript/modules/layout/constants.js
+++ app/javascript/modules/layout/constants.js
@@ -1,5 +1,5 @@
 export const OHD_DOMAINS = {
     development: 'http://portal.oral-history.localhost:3000',
-    staging: 'https://test.oral-history.digital',
+    staging: 'https://staging.oral-history.digital',
     production: 'https://portal.oral-history.digital',
 }
