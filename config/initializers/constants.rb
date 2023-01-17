6ca25197801e460df94638345852c407953aee44
cgregor <chrgregor@gmail.com>
2023-01-17 12:26:07 +0100

    updating staging domains
    Former-commit-id: b7ce8027e9bad8cdd91cd360162820de4133e28c

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 OHD_DOMAINS = {
   'development' => 'http://portal.oral-history.localhost:3000',
   'production' => 'https://portal.oral-history.digital',
-  'staging' => 'https://test.oral-history.digital'
+  'staging' => 'https://staging.oral-history.digital'
 }
 OHD_DOMAIN = OHD_DOMAINS[Rails.env]
 DEFAULT_PRIMARY_COLOR = '#e01217'
