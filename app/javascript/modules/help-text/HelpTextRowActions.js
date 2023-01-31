46f5d2b135ad1f7fe2d9c964c7975b55fa2d7eb4
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-01-31 17:37:25 +0100

    INTARCH-2084: Use admin table for person administration
    Former-commit-id: 1e3a461926b414747b6d98ab949ffd9bf6af22df

--- app/javascript/modules/help-text/HelpTextRowActions.js
+++ app/javascript/modules/help-text/HelpTextRowActions.js
@@ -11,8 +11,7 @@ export default function HelpTextRowActions({
         <RowActions
             row={row}
             displayComponent={HelpTextDisplay}
-            formComponent={HelpTextForm}
-            showDelete={false}
+            editComponent={HelpTextForm}
         />
     );
 }
