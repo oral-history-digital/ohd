2e31b2ac067b7b88f71ffb2ae9664bf3a65190f0
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-02-02 18:15:19 +0100

    Show pseudonym in person admin table
    Former-commit-id: 9b39dda9154bfa181fb09858bd05e861ae6c5846

---
+++ app/javascript/modules/tables/BooleanCell.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { FaCheck, FaTimes } from 'react-icons/fa';
+
+import { useI18n } from 'modules/i18n';
+
+export default function BooleanCell({
+    getValue
+}) {
+    const { locale } = useI18n();
+
+    return getValue() ?
+      <FaCheck /> :
+      null;
+}
+
+BooleanCell.propTypes = {
+    getValue: PropTypes.func.isRequired,
+};
