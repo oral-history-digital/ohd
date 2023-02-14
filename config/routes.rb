05945d343b8c8c35cf2c1a87044d6483851c7c86
cgregor <chrgregor@gmail.com>
2023-02-14 17:08:34 +0100

    fix routes
    Former-commit-id: 35affd6e342f7213b1b8f9e10223f68583d1852e

--- config/routes.rb
+++ config/routes.rb
@@ -234,7 +234,7 @@ Rails.application.routes.draw do
   #
   constraints(lambda { |request| ohd = URI.parse(OHD_DOMAIN); [ohd.host].include?(request.host) }) do
     scope "/:locale" do
-      get "/", to: redirect {|params, request| "/#{params[:locale]}/projects"}
+      get "/", to: "projects#show"
       resources :projects, only: [:create, :update, :destroy, :index]
       resources :institutions
       resources :help_texts, only: [:index, :update]
@@ -276,7 +276,7 @@ Rails.application.routes.draw do
   get "/de/hls.key" => "media_streams#hls"
 
   mount OaiRepository::Engine => "/de/oai_repository"
-  root to: redirect("#{OHD_DOMAIN}/de/projects")
+  root to: redirect("#{OHD_DOMAIN}/de")
 
   devise_for :user_accounts,
     controllers: { sessions: "sessions", passwords: "passwords" },
