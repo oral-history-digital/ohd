883ac09a88a40d81bbd47018e453e02985f79231
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-02-23 12:10:39 +0100

    Better error handling with SWR
    Former-commit-id: 27acbda10871f714dc1daf62ffc1b3f5b36f07a0

--- app/javascript/modules/api/fetcher.js
+++ app/javascript/modules/api/fetcher.js
@@ -1,4 +1,19 @@
-export default function fetcher(url) {
-    return fetch(url, { headers: { 'Accept': 'application/json' } })
-        .then(res => res.json());
+export default async function fetcher(url) {
+    const res = await fetch(url, { headers: { 'Accept': 'application/json' } });
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+        const error = new Error('An error occurred while fetching the data.');
+        // Attach extra info to the error object.
+        try {
+            error.info = await res.json();
+        } catch (e) {
+            console.log(`Fetcher error: ${e.message}`);
+        }
+        error.status = res.status;
+        throw error;
+    }
+
+    return res.json();
 }
