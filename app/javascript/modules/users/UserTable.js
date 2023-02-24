990eda15e6e866df8a46eeef10fe066d56262d21
cgregor <chrgregor@gmail.com>
2023-02-24 17:02:12 +0100

    differentiate between ohd and other projects in user-adminstration-table
    Former-commit-id: 930a3e6424ff0064cbc905a151d964d7fc128355

--- app/javascript/modules/users/UserTable.js
+++ app/javascript/modules/users/UserTable.js
@@ -1,6 +1,10 @@
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
 import { useI18n } from 'modules/i18n';
+import { getProjectId } from 'modules/archive';
+import { getCurrentProject } from 'modules/data';
+
 import { TableWithPagination, DateCell } from 'modules/tables';
 import useUsers from './useUsers';
 import UserRowActions from './UserRowActions';
@@ -9,8 +13,14 @@ import ProjectShortnamesCell from './ProjectShortnamesCell';
 export default function UserTable() {
     const { t, locale } = useI18n();
     const { data, isLoading } = useUsers();
+    const projectId = useSelector(getProjectId);
+    const project = useSelector(getCurrentProject);
 
-    const columns = useMemo(() => ([
+    const getCurrentUserRegistrationProject = (row, project) => {
+        return Object.values(row.user_registration_projects).find(p => p.project_id === project.id)
+    };
+
+    const baseColumns = useMemo(() => ([
         {
             id: 'first_name',
             accessorFn: row => row.first_name,
@@ -26,6 +36,9 @@ export default function UserTable() {
             accessorFn: row => row.email,
             header: t('activerecord.attributes.user_registration.email'),
         },
+    ]), [locale]);
+
+    const ohdColumns = useMemo(() => ([
         {
             accessorKey: 'activated_at',
             header: t('activerecord.attributes.user_registration.activated_at'),
@@ -36,12 +49,36 @@ export default function UserTable() {
             header: t('activerecord.models.project.other'),
             cell: ProjectShortnamesCell,
         },
+    ]), [locale]);
+
+    const projectColumns = useMemo(() => ([
+        {
+            id: 'workflow_state',
+            header: t('activerecord.attributes.default.workflow_state'),
+            accessorFn: row => {
+                const workflowState = getCurrentUserRegistrationProject(row, project).workflow_state;
+                return t(`user_registration_projects.workflow_states.${workflowState}`);
+            },
+        },
+        {
+            accessorKey: 'updated_at',
+            header: t('activerecord.attributes.default.updated_at'),
+            accessorFn: row => {
+                return getCurrentUserRegistrationProject(row, project).updated_at;
+            },
+            cell: DateCell,
+        },
+    ]), [locale]);
+
+    const actionColumns = useMemo(() => ([
         {
             id: 'actions',
             header: t('modules.tables.actions'),
             cell: UserRowActions
         }
     ]), [locale]);
+
+    const columns = baseColumns.concat(projectId !== 'ohd' ? projectColumns : ohdColumns).concat(actionColumns);
 
     return (
         <TableWithPagination
