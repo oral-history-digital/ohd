7cc1453b122dcf04a7e38a7236fba4881e951c9c
cgregor <chrgregor@gmail.com>
2023-02-24 17:02:12 +0100

    differentiate between ohd and other projects in user-adminstration-table
    Former-commit-id: 930a3e6424ff0064cbc905a151d964d7fc128355

--- app/serializers/user_registration_project_serializer.rb
+++ app/serializers/user_registration_project_serializer.rb
@@ -5,7 +5,9 @@ class UserRegistrationProjectSerializer < ApplicationSerializer
     :workflow_state,
     :workflow_states,
     :admin_comments,
-    :activated_at
+    :activated_at,
+    :updated_at,
+    :created_at
 
   def activated_at
     object.activated_at && object.activated_at.strftime("%d.%m.%Y %H:%M")
