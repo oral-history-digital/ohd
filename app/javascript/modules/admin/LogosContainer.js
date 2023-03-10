868e23ad3c327b0cf99b193b4754262546b69155
cgregor <chrgregor@gmail.com>
2023-03-11 00:03:50 +0100

    fix logos
    Former-commit-id: 3ae8a9ad4bbd87e75fe65a1a5ac0aadfb4181aae

--- app/javascript/modules/admin/LogosContainer.js
+++ app/javascript/modules/admin/LogosContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getCurrentProject, fetchData, deleteData, submitData, getProjects,
+import { getCurrentProject, fetchData, deleteData, submitData,
     getCurrentAccount, getProjectLocales } from 'modules/data';
 import { getLocale, getProjectId, getTranslations } from 'modules/archive';
 import DataList from './DataList';
@@ -11,7 +11,7 @@ const mapStateToProps = state => {
     return {
         locale: getLocale(state),
         projectId: getProjectId(state),
-        projects: getProjects(state),
+        project: getCurrentProject(state),
         translations: getTranslations(state),
         account: getCurrentAccount(state),
         editView: true,
--- app/javascript/modules/admin/SponsorLogosContainer.js
+++ app/javascript/modules/admin/SponsorLogosContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getCurrentProject, getProjectLocales, fetchData, deleteData, submitData, getProjects,
+import { getCurrentProject, getProjectLocales, fetchData, deleteData, submitData,
     getCurrentAccount } from 'modules/data';
 import { getLocale, getProjectId, getTranslations } from 'modules/archive';
 import DataList from './DataList';
@@ -11,7 +11,7 @@ const mapStateToProps = state => {
     return {
         locale: getLocale(state),
         projectId: getProjectId(state),
-        projects: getProjects(state),
+        project: getCurrentProject(state),
         translations: getTranslations(state),
         account: getCurrentAccount(state),
         editView: true,
