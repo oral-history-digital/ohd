9b55b9c0c28bf359250c41d26bf31c99a6474a8f
cgregor <chrgregor@gmail.com>
2023-03-10 22:57:30 +0100

    cleanup
    Former-commit-id: 830d13d31f74bb71f55ebd647a5d7335ad8a3cfc

--- app/javascript/modules/routes/RouteDivider.js
+++ app/javascript/modules/routes/RouteDivider.js
@@ -3,12 +3,10 @@ import { useSelector } from 'react-redux';
 import { LayoutContainer, useScrollBelowThreshold } from 'modules/layout';
 import { getCurrentProject, getProjects } from 'modules/data';
 import { MemoizedRoutesWithoutProjectId, MemoizedRoutesWithProjectId } from './Routes';
-//import projectByDomain from './projectByDomain';
 
 function RouteDivider() {
     const project = useSelector(getCurrentProject);
     const projects = useSelector(getProjects);
-    //const projectFromDomain = projectByDomain(projects);
     const isScrollBelowThreshold = useScrollBelowThreshold();
 
     return (
