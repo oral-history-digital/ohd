22fcadf67951fd51ee70286116beafd1c0d495e7
cgregor <chrgregor@gmail.com>
2023-03-17 10:19:03 +0100

    trying to reduce delayed-job attempts
    Former-commit-id: 0f8a5116efa46f3c54c350fa39d548737753319f

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,8 @@ class ApplicationJob < ActiveJob::Base
     @@jobs_logger ||= Logger.new("#{Rails.root}/log/jobs.log")
   end
 
+  # overrides the Delayed::Worker.max_attempts
+  def max_attempts
+    1
+  end
 end
