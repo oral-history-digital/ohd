3a329a441d780f523da4978dce25a8ab3e3960f6
cgregor <chrgregor@gmail.com>
2023-03-22 14:44:12 +0100

    remove (de-)activated_at columns as well
    Former-commit-id: f2d4cae40c807c2c3f5cc9a5fc570e6bc155abe8

--- db/migrate/20230321101543_add_workflow_to_users.rb
+++ db/migrate/20230321101543_add_workflow_to_users.rb
@@ -1,5 +1,8 @@
 class AddWorkflowToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :workflow_state, :string, default: 'created'
+    User.where.not(confirmed_at: nil).update_all(workflow_state: 'confirmed')
+    remove_column :users, :activated_at
+    remove_column :users, :deactivated_at
   end
 end
