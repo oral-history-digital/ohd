edb4d6bd8c049594280dbeb745fb277347970f16
cgregor <chrgregor@gmail.com>
2023-04-03 20:05:16 +0200

    sign in after confirmation
    Former-commit-id: c2e38dad85c7788ba66bec9f3b5e418b692c5d09

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,14 @@ class ConfirmationsController < Devise::ConfirmationsController
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
 
+  def show
+    super do |resource|
+      resource.confirm! # workflow
+      Doorkeeper::AccessToken.create!(resource_owner_id: resource.id)
+      sign_in(resource) if resource.errors.empty?
+    end
+  end
+
   protected
 
   def after_confirmation_path_for(resource_name, resource)
