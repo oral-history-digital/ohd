0c048ffee840680d0efc5242d916c97780800aa5
cgregor <chrgregor@gmail.com>
2023-04-20 14:20:45 +0200

    fit user-policy to projects without user_projects
    Former-commit-id: 47754b9bb1d4f9f87d1f90f9c669cff16c64a085

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,16 +8,16 @@ class UserPolicy < ApplicationPolicy
     show?
   end
 
-  #def confirm_new_email?
-    #show?
-  #end
-
   class Scope < Scope
     def resolve
       if user && (user.admin? || user.permissions.map(&:klass).include?(scope.to_s))
-        scope.joins(:user_projects).
-          where("user_projects.project_id = ?", project.id).
-          where.not(confirmed_at: nil)
+        if project.is_ohd?
+          scope.where.not(confirmed_at: nil)
+        else
+          scope.joins(:user_projects).
+            where("user_projects.project_id = ?", project.id).
+            where.not(confirmed_at: nil)
+        end
       else
         scope.none
       end
