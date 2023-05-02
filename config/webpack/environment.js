4f2247a710d893cc078a33e5dfb69af30f04a147
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-05-02 11:50:47 +0200

    Enable Webpack code splitting
    Former-commit-id: 21364c5151e18978525a9927ef9983c998927bec

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,5 @@
 const { environment } = require('@rails/webpacker');
 
-const nodeModulesLoader = environment.loaders.get('nodeModules');
-
-if (!Array.isArray(nodeModulesLoader.exclude)) {
-  nodeModulesLoader.exclude = nodeModulesLoader.exclude === null ? [] : [nodeModulesLoader.exclude];
-}
-
-nodeModulesLoader.exclude.push(/react-table/);
+environment.splitChunks();
 
 module.exports = environment;
