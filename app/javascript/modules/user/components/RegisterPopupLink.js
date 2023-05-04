1387a231b40f64cc2cecb119ac29b2e8494cffb7
cgregor <chrgregor@gmail.com>
2023-05-04 14:00:45 +0200

    fixes on registration
    Former-commit-id: 4d0f0b4454bf56356fbb76c56a8f76048019a810

--- app/javascript/modules/user/components/RegisterPopupLink.js
+++ app/javascript/modules/user/components/RegisterPopupLink.js
@@ -3,15 +3,18 @@ import { useSelector } from 'react-redux';
 import { Modal } from 'modules/ui';
 import { useI18n } from 'modules/i18n';
 import RegisterFormContainer from './RegisterFormContainer';
+import { getCurrentProject } from 'modules/data';
 
 export default function RegisterPopupLink ({
 }) {
     const { t } = useI18n();
+    const currentProject = useSelector(getCurrentProject);
+    const showStepOne = !currentProject.is_ohd;
 
     return (
         <>
             <Modal
-                title={t('user.registration')}
+                title={(showStepOne ? t('modules.project_access.request_step_one') : '') + t('modules.registration.title')}
                 triggerClassName="Button Button--transparent Button--withoutPadding Button--primaryColor"
                 trigger={t('user.registration')}
             >
