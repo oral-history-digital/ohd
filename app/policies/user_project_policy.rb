e4fb42f43bb96683792c22e0fc8412eae16be242
cgregor <chrgregor@gmail.com>
2023-05-09 16:01:21 +0200

    extend  messaging to user
    Former-commit-id: da49af32046d24458c5131b97857235e11063f1f

--- app/policies/user_project_policy.rb
+++ app/policies/user_project_policy.rb
@@ -2,4 +2,10 @@ class UserProjectPolicy < ApplicationPolicy
   def create?
     user
   end
+
+  def update?
+    user && (user.user_projects.include?(record) || user.admin? ||
+             user.roles?(project, resolve_class_name(record), m))
+  end
+
 end
