d8b28d0c29c8195b5ad203faf2fe067b90ac2a2f
cgregor <chrgregor@gmail.com>
2023-05-15 15:02:20 +0200

    add confirm-popup on user remove
    Former-commit-id: 82702b55803e8720b9b35c9e4f7624953b727ff3

--- app/javascript/modules/users/UserForm.js
+++ app/javascript/modules/users/UserForm.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Dialog } from '@reach/dialog';
 
+import { useI18n } from 'modules/i18n';
 import { Form } from 'modules/forms';
 import { submitDataWithFetch } from 'modules/api';
 import { useMutateData, useMutateDatum } from 'modules/data';
@@ -17,6 +20,9 @@ export default function UserForm({
     const mutateData = useMutateData('users', dataPath);
     const mutateDatum = useMutateDatum();
     const pathBase = usePathBase();
+    const { t } = useI18n();
+
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
     const formElements = [
         {
@@ -25,7 +31,10 @@ export default function UserForm({
             values: data && Object.values(data.workflow_states.filter((ws) => ws !== 'correct_project_access_data')),
             value: data?.workflow_state,
             optionsScope: `workflow_states.user${project.is_ohd ? '' : '_project'}s`,
-            withEmpty: true
+            withEmpty: true,
+            handlechangecallback: (name, value) => {
+                if (value === 'remove') setShowConfirmDialog(true);
+            },
         },
     ];
 
@@ -38,40 +47,59 @@ export default function UserForm({
     }
 
     return (
-        <Form
-            scope={scope}
-            onSubmit={ async (params) => {
-                mutateData( async data => {
-                    const result = await submitDataWithFetch(pathBase, params);
-                    const updatedDatum = result.data;
+        <>
+            <Dialog isOpen={showConfirmDialog} onDismiss={close} className={'Modal-dialog'} >
+                <h2>{t('Soll dieser Nutzer*innen-Account wirklich endgültig aus der Datenbank gelöscht werden?')}</h2>
+                <div className="Form-footer u-mt">
+                    <input
+                        type="button"
+                        className="Button Button--secondaryAction"
+                        value={t('cancel')}
+                        onClick={() => setShowConfirmDialog(false)}
+                    />
+                    <input
+                        type="button"
+                        className="Button Button--primaryAction"
+                        value={'OK'}
+                        onClick={() => setShowConfirmDialog(false)}
+                    />
+                </div>
+            </Dialog>
+            <Form
+                scope={scope}
+                onSubmit={ async (params) => {
+                    mutateData( async data => {
+                        const result = await submitDataWithFetch(pathBase, params);
+                        const updatedDatum = result.data;
 
-                    if (userId) {
-                        mutateDatum(userId, 'users');
-                    }
-
-                    if (typeof onSubmit === 'function') {
-                        onSubmit();
-                    }
-
-                    if (updatedDatum.workflow_state !== 'removed') {
-                        return {
-                            ...data,
-                            data: {
-                                ...data?.data,
-                                [updatedDatum.id]: updatedDatum
-                            }
+                        if (userId) {
+                            mutateDatum(userId, 'users');
                         }
-                    } else {
-                        const { [updatedDatum.id]: {}, ...rest } = data?.data;
-                        return {data: rest};
-                    }
-                });
-            }}
-            data={data}
-            values={{ default_locale: locale }}
-            submitText='submit'
-            elements={formElements}
-        />
+
+                        if (typeof onSubmit === 'function') {
+                            onSubmit();
+                        }
+
+                        if (updatedDatum.workflow_state !== 'removed') {
+                            return {
+                                ...data,
+                                data: {
+                                    ...data?.data,
+                                    [updatedDatum.id]: updatedDatum
+                                }
+                            }
+                        } else {
+                            const { [updatedDatum.id]: {}, ...rest } = data?.data;
+                            return {data: rest};
+                        }
+                    });
+                }}
+                data={data}
+                values={{ default_locale: locale }}
+                submitText='submit'
+                elements={formElements}
+            />
+        </>
     );
 }
 
