2a083e0a813bcc969ea5b70444bfb846721c73a7
cgregor <chrgregor@gmail.com>
2023-05-16 15:46:11 +0200

    added project-overview
    Former-commit-id: 84509154313aa9ba3b3d23b397f5d2afa8299df2

---
+++ app/javascript/modules/users/ProjectsOverview.js
@@ -0,0 +1,55 @@
+import { useState} from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { getProjects } from 'modules/data';
+import { useI18n } from 'modules/i18n';
+
+export default function ProjectsOverview({
+    user,
+}) {
+    const { t } = useI18n();
+    const projects = useSelector(getProjects);
+    const userProjects = Object.values(user.user_projects);
+
+    const [showProject, setShowProject] = useState('');
+
+    const projectDisplay = userProject => {
+        const workflowState = t(`workflow_states.user_projects.${userProject.workflow_state}`);
+        const project = projects[userProject.project_id];
+
+        if (project.is_ohd) {
+            return null;
+        }
+
+        return (
+            <li
+                key={userProject.id}
+                className="DetailList-item"
+            >
+                <h4 onClick={() => setShowProject(showProject === project.shortname ? '' : project.shortname)}>
+                    { project.shortname }
+                </h4>
+                <div className={showProject === project.shortname ? '' : 'hidden'} >
+                    <p>{ t('modules.project_access.actual_workflow_state') + ': ' + workflowState }</p>
+                    <p>{ t('modules.project_access.at') + ': ' + (userProject.processed_at || userProject.activated_at) }</p>
+                </div>
+            </li>
+        )
+    }
+
+    return (
+        <>
+            <h3>{ t('modules.project_access.many') }</h3>
+            <ul className="DetailList">
+                {userProjects.map(urp => (
+                    projectDisplay(urp)
+                ))}
+            </ul>
+        </>
+    );
+}
+
+ProjectsOverview.propTypes = {
+    row: PropTypes.object.isRequired,
+};
