5172caed5b3964fad87afc3384f7a5681640a56a
cgregor <chrgregor@gmail.com>
2023-05-22 17:25:16 +0200

    project access block revoked process improved
    Former-commit-id: e397e5d7a6dda15327828842c74d56126e4bf15e

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -41,6 +41,15 @@ class CustomDeviseMailer < Devise::Mailer
     devise_mail(record, :project_access_blocked, opts)
   end
 
+  def project_access_block_revoked(record, opts={})
+    @project = opts[:project]
+    @user_project = opts[:user_project]
+    @conditions_link = @project.external_links.where(internal_name: 'conditions').first
+    opts[:from] = @project.contact_email
+    opts[:reply_to] = @project.contact_email
+    devise_mail(record, :project_access_block_revoked, opts)
+  end
+
   def account_removed(record, opts={})
     opts[:from] = 'mail@oral-history.digital'
     opts[:reply_to] = 'mail@oral-history.digital' 
