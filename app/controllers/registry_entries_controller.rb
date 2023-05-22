b89ba19178dc932b3876420235746f674fa8c001
cgregor <chrgregor@gmail.com>
2023-05-22 10:26:05 +0200

    adding status column to registry-entries-export
    Former-commit-id: 3f34cc6bd9298db57481f95718bb9cfbab7f60de

--- app/controllers/registry_entries_controller.rb
+++ app/controllers/registry_entries_controller.rb
@@ -122,7 +122,7 @@ class RegistryEntriesController < ApplicationController
           root = params[:root_id] ? RegistryEntry.find(params[:root_id]) : current_project.root_registry_entry
           csv = Rails.cache.fetch "#{current_project.cache_key_prefix}-registry-entries-csv-#{root.id}-#{params[:lang]}-#{cache_key_date}" do
             CSV.generate(col_sep: "\t", quote_char: "\x00") do |row|
-              row << ['parent_name', 'parent_id', 'name', 'id', 'description', 'latitude', 'longitude', 'GND ID', 'OSM ID', 'Verknüpfte Interviews'] 
+              row << ['parent_name', 'parent_id', 'name', 'id', 'description', 'latitude', 'longitude', 'GND ID', 'OSM ID', 'Verknüpfte Interviews', 'Status'] 
               root.on_all_descendants do |entry|
                 entry.parents.each do |parent|
                   row << [
@@ -135,7 +135,8 @@ class RegistryEntriesController < ApplicationController
                     entry.longitude,
                     entry.gnd_id && entry.gnd_id.gsub(/[\r\n\t]/, ''),
                     entry.osm_id && entry.osm_id.gsub(/[\r\n\t]/, ''),
-                    entry.registry_references.map(&:archive_id).compact.uniq.join('#')
+                    entry.registry_references.map(&:archive_id).compact.uniq.join('#'),
+                    entry.workflow_state,
                   ]
                 end
               end
