428abf4a9656fc56e4cc2eab51729f8ca56f7c1c
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-05-31 18:04:17 +0200

    INTARCH-2181: Implement archive tile locks
    Former-commit-id: 08eb83df0629fd07f155fcd3caa5b686b511b602

--- app/javascript/modules/auth/index.js
+++ app/javascript/modules/auth/index.js
@@ -3,3 +3,4 @@ export { useProjectAccessStatus } from './projectAccessStatus-hook';
 export { default as admin } from './admin';
 export { default as AuthorizedContent } from './AuthorizedContent';
 export { default as AuthShowContainer } from './AuthShowContainer';
+export { PROJECT_ACCESS_GRANTED, PROJECT_ACCESS_REQUESTED } from './constants';
