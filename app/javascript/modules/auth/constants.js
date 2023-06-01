4c8fdfcd08a5bdf1fe5400765cadbf64bd9fb853
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-06-01 15:52:38 +0200

    INTARCH-2183: Implement interview tile locks
    Former-commit-id: 87c5cbd820f72ae9b68287d0e45034aa9d3c1179

--- app/javascript/modules/auth/constants.js
+++ app/javascript/modules/auth/constants.js
@@ -1,2 +1,3 @@
 export const PROJECT_ACCESS_GRANTED = 'project_access_granted';
 export const PROJECT_ACCESS_REQUESTED = 'project_access_requested';
+export const PROJECT_ACCESS_REJECTED = 'project_access_rejected';
