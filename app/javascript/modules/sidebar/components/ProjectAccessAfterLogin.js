c1dbcc89b04b1a61186ec6aef40660b9d8366d7d
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-06-05 17:54:56 +0200

    INTARCH-2182: Update project access status in sidebar
    Former-commit-id: 13a9fc6960148d3a705d4a1139201b49b76908f9

--- app/javascript/modules/sidebar/components/ProjectAccessAfterLogin.js
+++ app/javascript/modules/sidebar/components/ProjectAccessAfterLogin.js
@@ -1,19 +1,23 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 
 import { Modal } from 'modules/ui';
-import RequestProjectAccessFormContainer from './RequestProjectAccessFormContainer';
 import { useI18n } from 'modules/i18n';
 import { getCurrentProject, getCurrentUser } from 'modules/data';
+import { RequestProjectAccessFormContainer } from 'modules/user';
+import { LockRegular, LockSolid } from 'modules/icon';
 
-export default function ProjectAccessAlert ({
+export default function ProjectAccessAfterLogin({
+    className,
 }) {
-
     const user = useSelector(getCurrentUser);
     const project = useSelector(getCurrentProject);
     const { t } = useI18n();
 
-    if (project.grant_access_without_login) return null;
-    if (!!user && project?.grant_project_access_instantly) return null;
+    if (project?.grant_project_access_instantly || user.admin) {
+        return null;
+    }
 
     const currentUserProject = user?.user_projects &&
         Object.values(user.user_projects).find( up => {
@@ -24,26 +28,28 @@ export default function ProjectAccessAlert ({
         currentUserProject?.workflow_state === 'project_access_requested' ||
         currentUserProject?.workflow_state === 'project_access_data_corrected'
     ) {
-        return <div className='error'>
-                <p>{`${t('modules.project_access.request_in_process_text1')}`}</p>
-                <p>{`${t('modules.project_access.request_in_process_text2')}`}</p>
-            </div>
+        return (<div>
+            <p>
+                <LockRegular className="Icon Icon--inlineSvg" />
+                {' '}
+                {`${t('modules.project_access.request_in_process_text1')}`}
+            </p>
+            <p>{`${t('modules.project_access.request_in_process_text2')}`}</p>
+        </div>);
     } else if (currentUserProject?.workflow_state === 'project_access_rejected') {
         return <div className='error'>{`${t('modules.project_access.rejected_text')}`}</div>
     } else if (user.workflow_state === 'blocked' || currentUserProject?.workflow_state === 'project_access_blocked') {
         return <div className='error'>{`${t('modules.project_access.blocked_text')}`}</div>
     } else {
         return (
-            <>
-                <p className='error'>
-                    {t('modules.project_access.request_access_explanation1')}
-                </p>
-                <p className='error'>
-                    {t('modules.project_access.request_access_explanation2')}
+            <div className={classNames(className)}>
+                <p>
+                    <LockSolid className="Icon Icon--inlineSvg" />
+                    {' '}
+                    {t('modules.project_access.request_access_explanation')}
                 </p>
                 <Modal
                     key='request-project-access-popup'
-                    title={t('modules.project_access.request_access_link')}
                     triggerClassName='Button Button--fullWidth Button--secondaryAction u-mt-small u-mb-small'
                     trigger={t('modules.project_access.request_access_link')}
                 >
@@ -55,7 +61,11 @@ export default function ProjectAccessAlert ({
                         />
                     )}
                 </Modal>
-            </>
+            </div>
         )
     }
 }
+
+ProjectAccessAfterLogin.propTypes = {
+    className: PropTypes.string,
+};
