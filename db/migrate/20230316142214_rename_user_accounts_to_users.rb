f9a6dddb0f5578e94042859cedb3712a15fbfd92
cgregor <chrgregor@gmail.com>
2023-06-05 20:02:42 +0200

    fix migration
    Former-commit-id: 23e2c8bea4e1d44a0c406bc2282cdffc830044f0

--- db/migrate/20230316142214_rename_user_accounts_to_users.rb
+++ db/migrate/20230316142214_rename_user_accounts_to_users.rb
@@ -1,7 +1,7 @@
 class RenameUserAccountsToUsers < ActiveRecord::Migration[7.0]
   def change
     #UserRegistration.where(user_account_id: nil).each do |user_registration|
-    UserRegistration.each do |user_registration|
+    UserRegistration.all.each do |user_registration|
       if user_registration.user_account_id.nil?
         user_registration.create_account
         user_registration.save_registration_data_and_user_data_to_user_account
