3fd2cf6630c2f887477bdf9aca29aceaa726add1
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-06-19 11:09:27 +0200

    INTARCH-2196: Refactor WorkbookItemForm
    Former-commit-id: 974e0e38edddd6a3894249ff6a49a77dba3e0f17

--- app/javascript/modules/workbook/components/WorkbookItemFormContainer.js
+++ app/javascript/modules/workbook/components/WorkbookItemFormContainer.js
@@ -1,21 +1,12 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getCurrentProject, getCurrentInterview, getProjects } from 'modules/data';
-import { getLocale, getArchiveId, getProjectId, getTranslations } from 'modules/archive';
-import { getCurrentTape } from 'modules/media-player';
+import { getCurrentInterview } from 'modules/data';
 import { createWorkbook, updateWorkbook } from '../actions';
 import WorkbookItemForm from './WorkbookItemForm';
 
 const mapStateToProps = state => ({
-    archiveId: getArchiveId(state),
-    projectId: getProjectId(state),
-    projects: getProjects(state),
-    project: getCurrentProject(state),
     interview: getCurrentInterview(state),
-    tape: getCurrentTape(state),
-    locale: getLocale(state),
-    translations: getTranslations(state),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
