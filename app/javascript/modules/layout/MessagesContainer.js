d58900d168758f99cb3c0793d3cf04b79c8514d2
cgregor <chrgregor@gmail.com>
2023-06-28 16:08:06 +0200

    do not show messages after confirm
    Former-commit-id: f07afe116abf222d874a90e64d5b655884e530be

--- app/javascript/modules/layout/MessagesContainer.js
+++ app/javascript/modules/layout/MessagesContainer.js
@@ -1,13 +1,11 @@
 import { connect } from 'react-redux';
 
 import { getLocale, getTranslations } from 'modules/archive';
-import { getIsRegistered } from 'modules/user';
 import Messages from './Messages';
 
 const mapStateToProps = (state) => ({
     locale: getLocale(state),
     translations: getTranslations(state),
-    isRegistered: getIsRegistered(state),
 });
 
 export default connect(mapStateToProps)(Messages);
