b4d2cb381d1aa0219682aba0a6ae40a47a2a6f8d
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-07-03 10:58:57 +0200

    INTARCH-2195: Refactor DataSearchForm component
    Former-commit-id: cf60de25b86be5a35ebf3a23f5343fb6fefa5742

--- app/javascript/modules/admin/ContributionTypesSearchFormContainer.js
+++ app/javascript/modules/admin/ContributionTypesSearchFormContainer.js
@@ -1,19 +1,13 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getLocale, getTranslations, getProjectId } from 'modules/archive';
 import { resetQuery, setQueryParams, getContributionTypesQuery } from 'modules/search';
-import { fetchData, getCurrentProject, getContributionTypesStatus } from 'modules/data';
+import { fetchData } from 'modules/data';
 import { hideSidebar } from 'modules/sidebar';
 import DataSearchForm from './DataSearchForm';
 
 const mapStateToProps = (state) => ({
-    translations: getTranslations(state),
-    locale: getLocale(state),
-    projectId: getProjectId(state),
-    project: getCurrentProject(state),
     query: getContributionTypesQuery(state),
-    dataStatus: getContributionTypesStatus(state),
     scope: 'contribution_type',
     searchableAttributes: [
         {attributeName: 'label'},
--- app/javascript/modules/admin/RegistryReferenceTypesSearchFormContainer.js
+++ app/javascript/modules/admin/RegistryReferenceTypesSearchFormContainer.js
@@ -1,19 +1,13 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getLocale, getTranslations, getProjectId } from 'modules/archive';
 import { resetQuery, setQueryParams, getRegistryReferenceTypesQuery } from 'modules/search';
-import { fetchData, getCurrentProject, getRegistryReferenceTypesStatus } from 'modules/data';
+import { fetchData } from 'modules/data';
 import { hideSidebar } from 'modules/sidebar';
 import DataSearchForm from './DataSearchForm';
 
 const mapStateToProps = (state) => ({
-    translations: getTranslations(state),
-    locale: getLocale(state),
-    projectId: getProjectId(state),
-    project: getCurrentProject(state),
     query: getRegistryReferenceTypesQuery(state),
-    dataStatus: getRegistryReferenceTypesStatus(state),
     scope: 'registry_reference_type',
     searchableAttributes: [
         {attributeName: 'name'},
