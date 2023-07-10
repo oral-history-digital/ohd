fc89de78a633ac9f8098c0d6caeffc16a50ff0e4
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-07-10 17:48:14 +0200

    INTARCH-2227: Fix back-button bugs
    Former-commit-id: 6eeb7eb1ac2a530e2658195b356ec8daaf444b5e

--- app/javascript/modules/interview/components/InterviewTabsContainer.js
+++ app/javascript/modules/interview/components/InterviewTabsContainer.js
@@ -1,13 +1,10 @@
 import {connect} from 'react-redux';
 
 import { getCurrentInterview } from 'modules/data';
-import { getLocale, getProjectId } from 'modules/archive';
 import InterviewTabs from './InterviewTabs';
 
 const mapStateToProps = state => ({
     interview: getCurrentInterview(state),
-    locale: getLocale(state),
-    projectId: getProjectId(state),
 });
 
 export default connect(mapStateToProps)(InterviewTabs);
--- app/javascript/modules/startpage/HomeContainer.js
+++ app/javascript/modules/startpage/HomeContainer.js
@@ -1,15 +1,10 @@
 import { connect } from 'react-redux';
 
 import Home from './Home';
-import { getProjectTranslation, getShowFeaturedInterviews,
-    getShowStartPageVideo, getInstitutions, getIsCampscapesProject } from 'modules/data';
+import { getInstitutions } from 'modules/data';
 
 const mapStateToProps = state => ({
-    projectTranslation: getProjectTranslation(state),
-    showStartPageVideo: getShowStartPageVideo(state),
-    showFeaturedInterviews: getShowFeaturedInterviews(state),
     institutions: getInstitutions(state),
-    isCampscapesProject: getIsCampscapesProject(state),
 });
 
 export default connect(mapStateToProps)(Home);
