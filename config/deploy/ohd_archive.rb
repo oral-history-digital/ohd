11b24cdc5e00cc3f5fd672cd55faae6c3883c56d
Marc Altmann <marcaltmann@posteo.de>
2023-08-03 12:15:06 +0200

    Adapt deploy config for ohd_archive

--- config/deploy/ohd_archive.rb
+++ config/deploy/ohd_archive.rb
@@ -5,7 +5,7 @@ set :stage, :production
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive.yml"
-set :branch, 'ohd-user-administration'
+set :branch, ENV.fetch('BRANCH', 'develop')
 
 #set :default_env, {
 #      'project_name' => fetch(:application)
