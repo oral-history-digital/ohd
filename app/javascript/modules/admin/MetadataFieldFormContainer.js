58bb8db566a8811f3191b941663e878526f7f0b9
Marc Altmann <marcaltmann@posteo.de>
2023-08-10 16:15:18 +0200

    INTARCH-2250: Remove GlobalRegistryReferenceTypes

--- app/javascript/modules/admin/MetadataFieldFormContainer.js
+++ app/javascript/modules/admin/MetadataFieldFormContainer.js
@@ -3,14 +3,9 @@ import { bindActionCreators } from 'redux';
 
 import MetadataFieldForm from './MetadataFieldForm';
 import { submitData } from 'modules/data';
-import { getRegistryReferenceTypesForCurrentProject } from 'modules/data';
-
-const mapStateToProps = state => ({
-    registryReferenceTypes: getRegistryReferenceTypesForCurrentProject(state),
-});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     submitData,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(MetadataFieldForm);
+export default connect(null, mapDispatchToProps)(MetadataFieldForm);
