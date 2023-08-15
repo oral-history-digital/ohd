03c52f268c635ce71e1e18f194c70caca81b8602
cgregor <chrgregor@gmail.com>
2023-08-15 10:54:19 +0200

    cleanup, rm mog-columns

--- app/models/history.rb
+++
@@ -1,7 +0,0 @@
-class History < ApplicationRecord
-
-  belongs_to :person
-  
-  translates :forced_labor_details, :return_date, :deportation_date, :punishment, :liberation_date, fallbacks_for_empty_translations: true, touch: true
-
-end
