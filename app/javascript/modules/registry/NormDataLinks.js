ecc44b76a8a12a07d3bfa9d608c9d5702d9e5ae5
Marc Altmann <marcaltmann@posteo.de>
2023-08-22 18:16:51 +0200

    Extract NormDataLinks component

---
+++ app/javascript/modules/registry/NormDataLinks.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+import { useI18n } from 'modules/i18n';
+
+export default function NormDataLinks({
+    registryEntry,
+}) {
+    const { t } = useI18n();
+
+    return registryEntry.norm_data.map((normDatum) => (
+        <a
+            key={normDatum.nid}
+            href={`${normDatum.norm_data_provider.url_without_id}${normDatum.nid}`}
+            target="_blank"
+            rel="noreferrer"
+            className="Link flyout-sub-tabs-content-ico-link"
+            title={t('norm_data.link_hover')}
+        >
+            &nbsp;{normDatum.norm_data_provider.name}&nbsp;
+        </a>
+    ));
+}
+
+NormDataLinks.propTypes = {
+    registryEntry: PropTypes.object.isRequired,
+};
