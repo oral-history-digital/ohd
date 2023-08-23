b7e74a0f96bc450d8affa3fc6f4c73ded23c8b69
Marc Altmann <marcaltmann@posteo.de>
2023-08-23 09:38:05 +0200

    Finish refactoring RegistryEntry component

--- app/javascript/modules/registry/RegistryEntry.js
+++ app/javascript/modules/registry/RegistryEntry.js
@@ -1,9 +1,10 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import { AuthorizedContent, admin } from 'modules/auth';
-import { t } from 'modules/i18n';
+import { AuthorizedContent, useAuthorization } from 'modules/auth';
+import { useI18n } from 'modules/i18n';
+import { useProject } from 'modules/routes';
 import { Modal, Checkbox } from 'modules/ui';
 import RegistryEntryShowContainer from './RegistryEntryShowContainer';
 import RegistryEntries from './RegistryEntries';
@@ -13,147 +14,140 @@ import NormDataLinks from './NormDataLinks';
 import RegistryEntryToggleChildren from './RegistryEntryToggleChildren';
 import RegistryEntryLabel from './RegistryEntryLabel';
 
-export default class RegistryEntry extends Component {
-    constructor(props) {
-        super(props);
+export default function RegistryEntry({
+    addRemoveRegistryEntryId,
+    children,
+    className,
+    data,
+    fetchData,
+    hideCheckbox,
+    hideEditButtons,
+    registryEntries,
+    registryEntriesStatus,
+    registryEntryParent,
+    selectedRegistryEntryIds,
+}) {
+    const { t, locale } = useI18n();
+    const { project, projectId } = useProject();
+    const { isAuthorized } = useAuthorization();
 
-        this.state = {
-            childrenVisible: false,
-        };
+    const [childrenVisible, setChildrenVisible] = useState(false);
 
-        this.showChildren = this.showChildren.bind(this);
-    }
+    useEffect(() => {
+        reloadRegistryEntry();
+    });
 
-    componentDidMount() {
-        this.reloadRegistryEntry();
-    }
-
-    componentDidUpdate() {
-        this.reloadRegistryEntry();
-    }
-
-    reloadRegistryEntry() {
-        const { registryEntries, registryEntriesStatus, data, fetchData } = this.props;
-
+    function reloadRegistryEntry() {
         if (
             (!registryEntries[data.id] && !registryEntriesStatus[data.id]) ||
             /^reload/.test(registryEntriesStatus[data.id])
         ) {
-            fetchData(this.props, 'registry_entries', data.id);
+            fetchData({ project, projectId, locale }, 'registry_entries', data.id);
         }
     }
 
-    showChildren() {
-        this.setState(prevState => ({ childrenVisible: !prevState.childrenVisible }));
+    function showChildren() {
+        setChildrenVisible((prev) => !prev);
     }
 
-    hasReferences() {
-        return admin(this.props, {type: 'General'}, 'edit')
-            ? this.props.data.registry_references_count > 0
-            : this.props.data.public_registry_references_count > 0;
+    function hasReferences() {
+        return isAuthorized({ type: 'General' }, 'edit')
+            ? data.registry_references_count > 0
+            : data.public_registry_references_count > 0;
     }
 
-    render() {
-        const { className, selectedRegistryEntryIds, data, addRemoveRegistryEntryId,
-            registryEntryParent, children, hideEditButtons,
-            hideCheckbox } = this.props;
-        const { childrenVisible } = this.state;
+    const showOpenStreetMapLink = data.latitude + data.longitude !== 0;
+    const showEditButtons = !hideEditButtons;
 
-        const showOpenStreetMapLink = data.latitude + data.longitude !== 0;
-        const showEditButtons = !hideEditButtons;
+    return (
+        <li
+            id={`entry_${data.id}`}
+            className={classNames('RegistryEntry', className)}
+        >
+            <div className="RegistryEntry-content">
+                {!hideCheckbox && (
+                    <AuthorizedContent object={{type: 'RegistryEntry'}} action='update'>
+                        <Checkbox
+                            className='select-checkbox'
+                            checked={selectedRegistryEntryIds.includes(data.id)}
+                            onChange={() => addRemoveRegistryEntryId(data.id)}
+                        />
+                    </AuthorizedContent>
+                )}
 
-        return (
-            <li
-                id={`entry_${data.id}`}
-                className={classNames('RegistryEntry', className)}
-            >
-                <div className="RegistryEntry-content">
-                    {!hideCheckbox && (
-                        <AuthorizedContent object={{type: 'RegistryEntry'}} action='update'>
-                            <Checkbox
-                                className='select-checkbox'
-                                checked={selectedRegistryEntryIds.includes(data.id)}
-                                onChange={() => addRemoveRegistryEntryId(data.id)}
+                {showEditButtons && (
+                    <RegistryEntryToggleChildren
+                        count={data.children_count}
+                        isOpen={childrenVisible}
+                        onToggle={showChildren}
+                    />
+                )}
+
+                {hasReferences() ? (
+                    <Modal
+                        title={t('activerecord.models.registry_entry.actions.show')}
+                        triggerClassName="Button Button--transparent Button--withoutPadding RegistryEntry-label is-clickable"
+                        trigger={<RegistryEntryLabel registryEntry={data} />}
+                    >
+                        {close => (
+                            <RegistryEntryShowContainer
+                                registryEntryId={data.id}
+                                registryEntryParent={registryEntryParent}
+                                onSubmit={close}
+                                normDataLinks={<NormDataLinks registryEntry={data} />}
                             />
-                        </AuthorizedContent>
-                    )}
+                        )}
+                    </Modal>
+                ) : <RegistryEntryLabel registryEntry={data} />
+                }
 
-                    {showEditButtons && (
-                        <RegistryEntryToggleChildren
-                            count={data.children_count}
-                            isOpen={childrenVisible}
-                            onToggle={this.showChildren}
+                <div>
+                    <AuthorizedContent object={data} action="update">
+                        <NormDataLinks registryEntry={data} />
+                    </AuthorizedContent>
+
+                    {showOpenStreetMapLink && (
+                        <OpenStreetMapLink
+                            lat={data.latitude}
+                            lng={data.longitude}
                         />
                     )}
 
-                    {this.hasReferences() ? (
-                        <Modal
-                            title={t(this.props, 'activerecord.models.registry_entry.actions.show')}
-                            triggerClassName="Button Button--transparent Button--withoutPadding RegistryEntry-label is-clickable"
-                            trigger={<RegistryEntryLabel registryEntry={data} />}
-                        >
-                            {close => (
-                                <RegistryEntryShowContainer
-                                    registryEntryId={data.id}
-                                    registryEntryParent={registryEntryParent}
-                                    onSubmit={close}
-                                    normDataLinks={<NormDataLinks registryEntry={data} />}
-                                />
-                            )}
-                        </Modal>
-                    ) : <RegistryEntryLabel registryEntry={data} />
-                    }
-
-                    <div>
+                    {showEditButtons && (
                         <AuthorizedContent object={data} action="update">
-                            <NormDataLinks registryEntry={data} />
-                        </AuthorizedContent>
-
-                        {showOpenStreetMapLink && (
-                            <OpenStreetMapLink
-                                lat={data.latitude}
-                                lng={data.longitude}
+                            <RegistryEntryEditButtons
+                                registryEntry={data}
+                                parentRegistryEntry={registryEntryParent}
                             />
-                        )}
-
-                        {showEditButtons && (
-                            <AuthorizedContent object={data} action="update">
-                                <RegistryEntryEditButtons
-                                    registryEntry={data}
-                                    parentRegistryEntry={registryEntryParent}
-                                />
-                            </AuthorizedContent>
-                        )}
-                    </div>
+                        </AuthorizedContent>
+                    )}
                 </div>
+            </div>
 
-                {children}
+            {children}
 
-                {childrenVisible && (
-                    <RegistryEntries
-                        className="RegistryEntry-children"
-                        registryEntryParent={data}
-                    />
-                )}
-            </li>
-        )
-    }
+            {childrenVisible && (
+                <RegistryEntries
+                    className="RegistryEntry-children"
+                    registryEntryParent={data}
+                />
+            )}
+        </li>
+    );
 }
 
 RegistryEntry.propTypes = {
     data: PropTypes.object.isRequired,
     className: PropTypes.string,
-    locale: PropTypes.string.isRequired,
     hideEditButtons: PropTypes.bool,
     hideCheckbox: PropTypes.bool,
-    translations: PropTypes.object.isRequired,
     registryEntries: PropTypes.object.isRequired,
     registryEntriesStatus: PropTypes.object.isRequired,
     registryEntryParent: PropTypes.object.isRequired,
     selectedRegistryEntryIds: PropTypes.array.isRequired,
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
+        PropTypes.node,
     ]),
     fetchData: PropTypes.func.isRequired,
     addRemoveRegistryEntryId: PropTypes.func.isRequired,
