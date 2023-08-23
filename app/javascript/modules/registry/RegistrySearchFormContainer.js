83fda08d138b55b21323010521a0a3f0e0ec839b
Marc Altmann <marcaltmann@posteo.de>
2023-08-23 17:18:48 +0200

    Refactor RegistrySearchForm

--- app/javascript/modules/registry/RegistrySearchFormContainer.js
+++ app/javascript/modules/registry/RegistrySearchFormContainer.js
@@ -3,20 +3,11 @@ import { bindActionCreators } from 'redux';
 
 import { changeRegistryEntriesViewMode, searchRegistryEntry } from 'modules/search';
 import { hideSidebar } from 'modules/sidebar';
-import { getLocale, getProjectId, getTranslations } from 'modules/archive';
-import { getCurrentProject } from 'modules/data';
 import RegistrySearchForm from './RegistrySearchForm';
 
-const mapStateToProps = (state) => {
-    return {
-        query: state.search.registryEntries.query,
-        translations: getTranslations(state),
-        locale: getLocale(state),
-        isRegistryEntrySearching: state.search.isRegistryEntrySearching,
-        projectId: getProjectId(state),
-        project: getCurrentProject(state),
-    }
-}
+const mapStateToProps = (state) => ({
+    isRegistryEntrySearching: state.search.isRegistryEntrySearching,
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     searchRegistryEntry,
