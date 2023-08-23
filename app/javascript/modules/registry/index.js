4158dfad672632a2d71e89ad3377b366f6991fe1
Marc Altmann <marcaltmann@posteo.de>
2023-08-23 19:15:33 +0200

    INTARCH-2259: Refactor RegistrySearchResult

--- app/javascript/modules/registry/index.js
+++ app/javascript/modules/registry/index.js
@@ -1,5 +1,4 @@
 export { default as RegistryContainer } from './RegistryContainer';
 export { default as RegistryEntryContainer } from './RegistryEntryContainer';
 export { default as RegistrySearchFormContainer } from './RegistrySearchFormContainer';
-export { default as RegistrySearchResultContainer } from './RegistrySearchResultContainer';
 export { default as RefObjectLinkContainer } from './RefObjectLinkContainer';
