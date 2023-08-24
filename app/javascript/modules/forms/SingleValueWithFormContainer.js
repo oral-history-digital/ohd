6589161e72cc330e0e59dfeb190f092434f245fa
cgregor <chrgregor@gmail.com>
2023-08-24 15:14:08 +0200

    refactored SingleValueWithForm to functional component

--- app/javascript/modules/forms/SingleValueWithFormContainer.js
+++ app/javascript/modules/forms/SingleValueWithFormContainer.js
@@ -1,16 +1,13 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getLocale, getProjectId, getEditView, getTranslations } from 'modules/archive';
+import { getEditView, getTranslations } from 'modules/archive';
 import { submitData, getCurrentProject, getCurrentUser } from 'modules/data';
 import { getIsLoggedIn } from 'modules/user';
 import SingleValueWithForm from './SingleValueWithForm';
 
 const mapStateToProps = (state) => {
     return {
-        locale: getLocale(state),
-        projectId: getProjectId(state),
-        project: getCurrentProject(state),
         user: getCurrentUser(state),
         editView: getEditView(state),
         isLoggedIn: getIsLoggedIn(state),
