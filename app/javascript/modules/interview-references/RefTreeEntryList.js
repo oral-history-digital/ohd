9a6649471957c2be1f15c7225686abc8c59e2c5c
Marc Altmann <marcaltmann@posteo.de>
2023-08-24 14:03:37 +0200

    INTARCH-2278: Sort registry entries within interviews

---
+++ app/javascript/modules/interview-references/RefTreeEntryList.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+import { useI18n } from 'modules/i18n';
+import RefTreeEntry from './RefTreeEntry';
+
+export default function RefTreeEntryList({
+    entries,
+}) {
+    const { locale } = useI18n();
+
+    function sortedEntries() {
+        const result = [...entries];
+        result.sort(entryComparator);
+        return result;
+    }
+
+    function entryComparator(entryA, entryB) {
+        const labelA = entryA.desc[locale];
+        const labelB = entryB.desc[locale];
+        return labelA.localeCompare(labelB);
+    }
+
+    return sortedEntries().map((entry, index) => (
+        <RefTreeEntry
+            key={entry.id}
+            entry={entry}
+            index={index}
+        />
+    ));
+}
+
+RefTreeEntryList.propTypes = {
+    entries: PropTypes.array.isRequired,
+};
