0126cffa9656b0a20864f62a5bfd63c872d933e8
cgregor <chrgregor@gmail.com>
2023-09-06 13:19:37 +0200

    uncommented migration parts

--- db/migrate/20230905120506_create_translation_values.rb
+++ db/migrate/20230905120506_create_translation_values.rb
@@ -1,14 +1,14 @@
 class CreateTranslationValues < ActiveRecord::Migration[7.0]
   def change
-    #create_table :translation_values do |t|
-      #t.string :key
+    create_table :translation_values do |t|
+      t.string :key
 
-      #t.timestamps
-    #end
+      t.timestamps
+    end
 
     reversible do |dir|
       dir.up do
-        #TranslationValue.create_translation_table! value: :text
+        TranslationValue.create_translation_table! value: :text
 
         translations_from_yaml = I18n.available_locales.inject({}) do |mem, locale|
           mem[locale] = YAML.load_file(File.join(Rails.root, "config/locales/#{locale}.yml"))[locale.to_s].deep_merge(
