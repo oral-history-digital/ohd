0a5655249dfbecbfb7a98545824d9bbeaf7d8ef1
Marc Altmann <marcaltmann@posteo.de>
2023-09-13 10:14:54 +0200

    Fix deploy config

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -5,7 +5,7 @@ set :stage, :staging
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
-set :branch, :translations_interface
+set :branch, ENV.fetch('BRANCH', 'develop')
 
 #set :default_env, {
 #      'project_name' => fetch(:application)
