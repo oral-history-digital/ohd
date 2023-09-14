213528e862baec662dca0722f26d804ea0d6ec63
cgregor <chrgregor@gmail.com>
2023-09-14 12:47:17 +0200

    comment unnecessary and breaking line

--- app/models/user_registration.rb
+++ app/models/user_registration.rb
@@ -120,7 +120,7 @@ EVAL
 
   def create_account
     self.user_account = UserAccount.where(email: self.email).first_or_initialize
-    self.user_account.login = create_login if self.user_account.login.blank?
+    #self.user_account.login = create_login if self.user_account.login.blank?
     self.user_account.generate_confirmation_token if self.user_account.confirmation_token.blank?
     self.user_account.tos_agreed_at = DateTime.now
     self.user_account.default_locale = I18n.locale
