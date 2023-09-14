58f6823226d22420351cd25f76832d59a58b348c
cgregor <chrgregor@gmail.com>
2023-09-14 12:47:34 +0200

    improve migration

--- db/migrate/20230316142214_rename_user_accounts_to_users.rb
+++ db/migrate/20230316142214_rename_user_accounts_to_users.rb
@@ -1,11 +1,8 @@
 class RenameUserAccountsToUsers < ActiveRecord::Migration[7.0]
   def change
-    #UserRegistration.where(user_account_id: nil).each do |user_registration|
-    UserRegistration.all.each do |user_registration|
-      if user_registration.user_account_id.nil?
-        user_registration.create_account
-        user_registration.save_registration_data_and_user_data_to_user_account
-      end
+    UserRegistration.where(user_account_id: nil).each do |user_registration|
+      user_registration.create_account
+      user_registration.save_registration_data_and_user_data_to_user_account
     end
     drop_table :users
     rename_table :user_accounts, :users
