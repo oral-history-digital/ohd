9aae66a31e7fd01ab71eac159ae153578562af8b
Marc Altmann <marcaltmann@posteo.de>
2023-09-18 16:42:03 +0200

    INTARCH-2299: Only show project-related workbook items

--- app/javascript/modules/workbook/components/WorkbookItemList.js
+++ app/javascript/modules/workbook/components/WorkbookItemList.js
@@ -3,6 +3,7 @@ import { FaPlus, FaMinus } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import { useProject } from 'modules/routes';
 import WorkbookItemContainer from './WorkbookItemContainer';
 
 export default function WorkbookItemList({
@@ -10,9 +11,18 @@ export default function WorkbookItemList({
     title,
 }) {
     const [open, setOpen] = useState(false);
+    const { project, isOhd } = useProject();
 
     function handleClick() {
         setOpen(prev => !prev);
+    }
+
+    function itemsForProject() {
+        if (isOhd) {
+            return contents;
+        } else {
+            return contents.filter((item) => item.project_id === project.id);
+        }
     }
 
     return (
@@ -31,10 +41,10 @@ export default function WorkbookItemList({
             </button>
             <div className={classNames('panel', {'open': open})}>
                 {
-                    contents && contents.map((content, index, array) => (
+                    itemsForProject()?.map((item, index, array) => (
                         <WorkbookItemContainer
-                            key={content.id}
-                            data={content}
+                            key={item.id}
+                            data={item}
                             className={index < array.length - 1 ? 'u-mb' : null}
                         />
                     ))
