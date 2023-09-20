794eff406db08a53bcde5cf54291dcbeb26c60bc
cgregor <chrgregor@gmail.com>
2023-09-20 10:18:39 +0200

    improve replacements in default TextPage templates

--- db/migrate/20230919102858_create_default_archive_texts.rb
+++ db/migrate/20230919102858_create_default_archive_texts.rb
@@ -7,10 +7,28 @@ class CreateDefaultArchiveTexts < ActiveRecord::Migration[7.0]
           code: code,
         )
         I18n.available_locales.each do |locale|
+          html = File.read(File.join(Rails.root, "config/defaults/texts/#{locale}/#{code}.html"))
+          if html.nil?
+            puts "code: #{code}, locale: #{locale}, project: #{project.name(locale)}"
+          end
+          {
+            project_name: project.name(locale),
+            project_manager: project.manager,
+            project_contact_email: project.contact_email,
+            project_leader: project.leader,
+            institution_name: project.institutions.first&.name(locale),
+            institution_website: project.institutions.first&.website,
+            institution_street: project.institutions.first&.street,
+            institution_zip: project.institutions.first&.zip,
+            institution_city: project.institutions.first&.city,
+            institution_country: project.institutions.first&.country,
+            privacy_protection_link: "#{project.domain_with_optional_identifier}/#{locale}/privacy_protection",
+          }.each do |key, value|
+            html.gsub!("%{#{key}}", value || '')
+          end
           text.update(
             locale: locale,
-            text: File.read(File.join(Rails.root, "config/defaults/texts/#{locale}/#{code}.html")).
-            gsub(/\s*project\.(\w+)\s*/) { project.send($1) }
+            text: html,
           )
         end
       end
