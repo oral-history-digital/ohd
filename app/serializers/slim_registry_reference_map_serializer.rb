2745b74587b75eb2ee0398dbef7d7de69d7e8986
Marc Altmann <marcaltmann@posteo.de>
2023-09-20 16:57:21 +0200

    INTARCH-2304: Show pseudonyms in map popups

--- app/serializers/slim_registry_reference_map_serializer.rb
+++ app/serializers/slim_registry_reference_map_serializer.rb
@@ -1,3 +1,19 @@
 class SlimRegistryReferenceMapSerializer < ActiveModel::Serializer
   attributes :id, :registry_reference_type_id, :archive_id, :first_name, :last_name
+
+  def first_name
+    if object.use_pseudonym == 1
+      object.pseudonym_first_name
+    else
+      object.first_name
+    end
+  end
+
+  def last_name
+    if object.use_pseudonym == 1
+      object.pseudonym_last_name
+    else
+      object.last_name
+    end
+  end
 end
