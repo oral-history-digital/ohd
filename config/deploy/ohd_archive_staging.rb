cf6f381d6c9cbf0fd2046915315be3a2b6c5093a
cgregor <chrgregor@gmail.com>
2023-10-16 14:02:53 +0200

    deploy split-languages-branch to ohd-staging

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -5,7 +5,8 @@ set :stage, :staging
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
-set :branch, ENV.fetch('BRANCH', 'develop')
+#set :branch, ENV.fetch('BRANCH', 'develop')
+set :branch, "split-languages"
 
 #set :default_env, {
 #      'project_name' => fetch(:application)
