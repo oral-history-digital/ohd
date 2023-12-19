e6dc61d97f7f7513019bb7de069359ae5574e3d2
cgregor <chrgregor@gmail.com>
2023-12-19 13:48:38 +0100

    fix metadataexport for missing contributors

--- app/lib/metadata_export.rb
+++ app/lib/metadata_export.rb
@@ -45,7 +45,7 @@ class MetadataExport
 
         project.contribution_types.inject(line) do |mem, contribution_type|
           if contribution_type.use_in_export# && contribution_type.code != 'interviewee'
-            contributors = interview.send(contribution_type.code.pluralize)
+            contributors = interview.send(contribution_type.code.pluralize).compact.uniq
             mem << (contributors.count > 0 ? contributors.map{|c| "#{c.last_name}, #{c.first_name}"}.join('#') : nil)
           end
           mem
