c0ad2722c30f2664c358aefc323857832e19353d
cgregor <chrgregor@gmail.com>
2024-02-12 17:03:42 +0100

    rm i18n from ErrorBoundary, rm Container

--- app/javascript/modules/react-toolbox/ErrorBoundary.js
+++ app/javascript/modules/react-toolbox/ErrorBoundary.js
@@ -1,11 +1,46 @@
-import { connect } from 'react-redux';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { getLocale, getTranslations } from 'modules/archive';
-import ErrorBoundaryComponent from './ErrorBoundaryComponent';
+import { t } from 'modules/i18n';
 
-const mapStateToProps = (state) => ({
-    locale: getLocale(state),
-    translations: getTranslations(state),
-});
+export default class ErrorBoundaryComponent extends Component {
+    constructor(props) {
+        super(props);
 
-export default connect(mapStateToProps)(ErrorBoundaryComponent);
+        this.state = { error: null };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { error };
+    }
+
+    render() {
+        const { locale, translations, small } = this.props;
+        const { error } = this.state;
+
+        if (error) {
+            const message = `${t({ locale, translations }, 'error')}: ${error.message}`;
+
+            return (
+                <div className="wrapper-content">
+                    {small ?
+                        <p>{message}</p> :
+                        <h1>{message}</h1>
+                    }
+                    <p></p>
+                </div>
+            );
+        }
+        return this.props.children;
+    }
+}
+
+ErrorBoundaryComponent.propTypes = {
+    locale: PropTypes.string.isRequired,
+    translations: PropTypes.object.isRequired,
+    small: PropTypes.bool,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+};
