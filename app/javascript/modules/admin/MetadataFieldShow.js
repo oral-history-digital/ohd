1c9a6134fb790a85ab62b96e9155834aacafe773
cgregor <chrgregor@gmail.com>
2024-02-15 12:11:41 +0100

    using only translationValue for metadata-fields (might be necessary to change)

--- app/javascript/modules/admin/MetadataFieldShow.js
+++ app/javascript/modules/admin/MetadataFieldShow.js
@@ -1,19 +1,18 @@
 import { useSelector } from 'react-redux';
 
-import { getLocale, getTranslations } from 'modules/archive';
+import { getLocale } from 'modules/archive';
 import { useI18n } from 'modules/i18n';
 
 function MetadataFieldShow({data}) {
     const locale = useSelector(getLocale);
-    const translations = useSelector(getTranslations);
     const { t } = useI18n();
 
     let name;
-    if (translations[`search_facets.${data.name}`]) {
+    //if (translations[`search_facets.${data.name}`]) {
         name = t(`search_facets.${data.name}`);
-    } else {
-        name = data.label && data.label[locale];
-    }
+    //} else {
+        //name = data.label && data.label[locale];
+    //}
 
     return (
         <div className='base-data box'>
