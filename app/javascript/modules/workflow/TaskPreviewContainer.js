c50579382a6c16929951a11d6e693b2eee0ff414
cgregor <chrgregor@gmail.com>
2024-02-15 15:26:44 +0100

    refactored Task-components to functional components

--- app/javascript/modules/workflow/TaskPreviewContainer.js
+++ app/javascript/modules/workflow/TaskPreviewContainer.js
@@ -1,17 +1,12 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { setArchiveId, getLocale, getArchiveId, getProjectId, getTranslations } from 'modules/archive';
-import { getCurrentProject, getCurrentUser } from 'modules/data';
+import { setArchiveId } from 'modules/archive';
+import { getCurrentUser } from 'modules/data';
 import TaskPreview from './TaskPreview';
 
 const mapStateToProps = (state) => {
     return {
-        archiveId: getArchiveId(state),
-        projectId: getProjectId(state),
-        project: getCurrentProject(state),
-        locale: getLocale(state),
-        translations: getTranslations(state),
         user: getCurrentUser(state),
     }
 }
