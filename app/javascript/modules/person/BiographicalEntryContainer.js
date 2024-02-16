38b7fb0f7851b2b8d3a5f7c8c920d3acdf9ca2cc
cgregor <chrgregor@gmail.com>
2024-02-16 10:19:47 +0100

    refactor BiographicalEntry to functional component

--- app/javascript/modules/person/BiographicalEntryContainer.js
+++ app/javascript/modules/person/BiographicalEntryContainer.js
@@ -1,16 +1,10 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { deleteData, getCurrentProject } from 'modules/data';
-import { getLocale, getProjectId, getTranslations } from 'modules/archive';
+import { deleteData } from 'modules/data';
 import BiographicalEntry from './BiographicalEntry';
 
-const mapStateToProps = state => ({
-    locale: getLocale(state),
-    projectId: getProjectId(state),
-    project: getCurrentProject(state),
-    translations: getTranslations(state),
-});
+const mapStateToProps = state => ({ });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     deleteData,
