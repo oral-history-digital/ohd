5a29b6609431fa4b3c38f46f29685e7318cedcf7
cgregor <chrgregor@gmail.com>
2024-02-16 10:49:32 +0100

    refactor MarkTextForm to functional component

--- app/javascript/modules/interview-actions/MarkTextFormContainer.js
+++ app/javascript/modules/interview-actions/MarkTextFormContainer.js
@@ -1,26 +1,19 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { fetchData, submitData, getCurrentInterview, getCurrentProject, getCurrentUser,
-    getMarkTextStatus } from 'modules/data';
-import { getLocale, getProjectId, getArchiveId, getTranslations } from 'modules/archive';
+import { submitData, getCurrentInterview, getMarkTextStatus } from 'modules/data';
+import { getArchiveId } from 'modules/archive';
 import MarkTextForm from './MarkTextForm';
 
 const mapStateToProps = (state) => {
     return {
-        locale: getLocale(state),
-        projectId: getProjectId(state),
-        project: getCurrentProject(state),
         archiveId: getArchiveId(state),
-        translations: getTranslations(state),
-        user: getCurrentUser(state),
         interview: getCurrentInterview(state),
         markTextStatus: getMarkTextStatus(state),
     }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    fetchData,
     submitData,
 }, dispatch);
 
