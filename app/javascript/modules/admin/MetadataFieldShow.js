f85a9a55e840691509a53a4e8435c1a3cd28b748
cgregor <chrgregor@gmail.com>
2024-03-14 13:33:22 +0100

    revert changes on MetadataFieldShow

--- app/javascript/modules/admin/MetadataFieldShow.js
+++ app/javascript/modules/admin/MetadataFieldShow.js
@@ -1,18 +1,19 @@
 import { useSelector } from 'react-redux';
 
-import { getLocale } from 'modules/archive';
+import { getLocale, getTranslations } from 'modules/archive';
 import { useI18n } from 'modules/i18n';
 
 function MetadataFieldShow({data}) {
     const locale = useSelector(getLocale);
+    const translations = useSelector(getTranslations);
     const { t } = useI18n();
 
     let name;
-    //if (translations[`search_facets.${data.name}`]) {
+    if (translations[`search_facets.${data.name}`]) {
         name = t(`search_facets.${data.name}`);
-    //} else {
-        //name = data.label && data.label[locale];
-    //}
+    } else {
+        name = data.label && data.label[locale];
+    }
 
     return (
         <div className='base-data box'>
