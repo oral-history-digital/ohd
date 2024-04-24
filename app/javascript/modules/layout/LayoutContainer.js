af2699e13f862a58ac12468c6fb5825c22effba9
cgregor <chrgregor@gmail.com>
2024-04-24 09:38:05 +0200

    reverting useLoadCompleteProject in Layout

--- app/javascript/modules/layout/LayoutContainer.js
+++ app/javascript/modules/layout/LayoutContainer.js
@@ -2,15 +2,23 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { toggleSidebar, getSidebarVisible } from 'modules/sidebar';
+import {
+    fetchData, getCollectionsStatus, getLanguagesStatus,
+    getProjectsStatus
+} from 'modules/data';
 import { getLoggedInAt } from 'modules/user';
 import Layout from './Layout';
 
 const mapStateToProps = state => ({
+    collectionsStatus: getCollectionsStatus(state),
+    languagesStatus: getLanguagesStatus(state),
     loggedInAt: getLoggedInAt(state),
+    projectsStatus: getProjectsStatus(state),
     sidebarVisible: getSidebarVisible(state),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
+    fetchData,
     toggleSidebar,
 }, dispatch);
 
