06709f375c5f13761b184d2d909be13c0d19f963
cgregor <chrgregor@gmail.com>
2024-04-30 13:40:51 +0200

    fill catalog archive page again

--- app/javascript/modules/data/useLoadCompleteProject.js
+++ app/javascript/modules/data/useLoadCompleteProject.js
@@ -2,21 +2,25 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchData } from './actions';
-import { getProjectsStatus } from './selectors/dataSelectors';
+import { getProjectsStatus, getPublicProjects } from './selectors/dataSelectors';
 import { OHD_DOMAINS } from 'modules/constants';
 
-const useLoadCompleteProject = (projectId) => {
+const useLoadCompleteProject = (pId) => {
 
+    const projects = useSelector(getPublicProjects);
     const projectsStatus = useSelector(getProjectsStatus);  
     const ohdDomain = OHD_DOMAINS[railsMode];
     const ohd = {shortname: 'ohd', archive_domain: ohdDomain};
+    const project = projects.find(p => p.id === pId);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        if (!projectsStatus[projectId]) {
-            fetchData({ locale: 'de', project: ohd}, 'projects', projectId);
+        if (!projectsStatus[pId]) {
+            dispatch(fetchData({ locale: 'de', project: ohd}, 'projects', pId));
         }
-    }, [projectId]);
+    }, [project]);
 
+    return project;
 }
 
 export default useLoadCompleteProject;
