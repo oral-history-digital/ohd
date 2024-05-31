3faff718af8e48a78c3fcb91ed698d6f80c9e355
cgregor <chrgregor@gmail.com>
2024-05-31 16:53:18 +0200

    cleanup

--- app/serializers/interview_serializer.rb
+++ app/serializers/interview_serializer.rb
@@ -14,7 +14,6 @@ class InterviewSerializer < InterviewBaseSerializer
 
   def landing_page_texts
     json = Rails.cache.fetch("#{object.project.shortname}-landing-page-texts-#{object.archive_id}-#{object.project.updated_at}") do
-      interviewee = object.interviewee
       I18n.available_locales.inject({}) do |mem, locale|
         mem[locale] = object.project.landing_page_text(locale) && object.project.landing_page_text(locale).gsub('INTERVIEWEE', object.project.fullname_on_landing_page ? object.short_title(locale) : object.anonymous_title(locale))
         mem
