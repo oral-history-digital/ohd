3e39fda6316806e45af0b57231b3e104bd0d95f6
Marc Altmann <marcaltmann@posteo.de>
2024-06-05 13:35:58 +0200

    INTARCH-2696: Fix opera bug

--- app/javascript/modules/search/components/ResultTable.js
+++ app/javascript/modules/search/components/ResultTable.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 import { useI18n } from 'modules/i18n';
-import { AuthorizedContent } from 'modules/auth';
+import { useAuthorization } from 'modules/auth';
 import { InterviewListRowContainer } from 'modules/interview-preview';
 import { useProject } from 'modules/routes';
 import { useSearchParams } from 'modules/query-string';
@@ -12,14 +12,18 @@ export default function ResultTable({
     const { t, locale } = useI18n();
     const { project } = useProject();
     const { fulltext } = useSearchParams();
+    const { isAuthorized } = useAuthorization();
+
+    function showCheckboxes() {
+        return isAuthorized(interviews?.[0], 'show') &&
+            isAuthorized({ type: 'General' }, 'edit');
+    }
 
     return (
         <table className="Table Table--searchResults">
             <thead className="Table-head">
                 <tr className="Table-row">
-                    <AuthorizedContent object={{type: 'General'}} action="edit">
-                        <th className="Table-header" />
-                    </AuthorizedContent>
+                    {showCheckboxes() && <th className="Table-header" />}
                     <th className="Table-header">
                         {t('interviewee_name')}
                     </th>
