5fd234bd8d1c92c71a670f9208087f4a94850757
cgregor <chrgregor@gmail.com>
2024-06-20 11:12:23 +0200

    confirm dialog on using norm-data

--- db/migrate/20240619122126_add_some_norm_data_translations.rb
+++ db/migrate/20240619122126_add_some_norm_data_translations.rb
@@ -8,5 +8,14 @@ class AddSomeNormDataTranslations < ActiveRecord::Migration[7.0]
     search_api_for.update(value: "Search for: %{descriptor}", locale: :en)
     enter_normdata_manually = TranslationValue.create(key: 'enter_normdata_manually', value: "Manuelle Eingabe", locale: :de)
     enter_normdata_manually.update(value: "Enter manually", locale: :en)
+    update_registry_entry_attributes = TranslationValue.create(key: 'normdata.update_registry_entry_attributes', value: "Sollen Daten überschrieben werden?", locale: :de)
+    update_registry_entry_attributes .update(value: "Overwrite data", locale: :en)
+  end
+
+  def down
+    TranslationValue.where(key: 'enter_descriptor_first').destroy_all
+    TranslationValue.where(key: 'search_api_for').destroy_all
+    TranslationValue.where(key: 'enter_normdata_manually').destroy_all
+    TranslationValue.where(key: 'normdata.update_registry_entry_attributes').destroy_all
   end
 end
