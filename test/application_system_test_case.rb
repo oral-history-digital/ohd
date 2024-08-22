6458b4d944603b89ad686fb12da591adaf76f9be
cgregor <chrgregor@gmail.com>
2024-08-22 11:34:10 +0200

    fixing test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -15,7 +15,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['HEADLESS'] == 'true'
     driven_by :selenium_headless
   else
-    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
   end
 
 end
