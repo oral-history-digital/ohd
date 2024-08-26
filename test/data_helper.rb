af0e6ebcf6319f69fc8a8a8b247bce67b287c1d6
cgregor <chrgregor@gmail.com>
2024-08-26 16:30:32 +0200

    translate metadata_export_spec to metadata_export_test

--- test/data_helper.rb
+++ test/data_helper.rb
@@ -194,6 +194,269 @@ module DataHelper
     Project.create! attribs
   end
 
+  def self.project_with_contribution_types_and_metadata_fields
+    project = test_project(
+      shortname: ('a'..'z').to_a.shuffle[0,4].join,
+    )
+
+    %w(interviewee interviewer translator transcriptor research cinematographer).each do |code|
+      use_in_export = code != 'interviewee'
+      ContributionType.create(project: project, code: code, use_in_export: use_in_export)
+    end
+
+    places = RegistryEntry.create code: 'places', project: project
+
+    {
+      birth_location: 'Geburtsort',
+      interview_location: 'Interviewort'
+    }.each do |code, name|
+      registry_reference_type = RegistryReferenceType.create(
+        registry_entry: places,
+        project: project,
+        name: name,
+        code: code
+      )
+      MetadataField.create(
+        registry_reference_type: registry_reference_type,
+        ref_object_type: code == :birth_location ? 'Person' : 'Interview',
+        project: project,
+        use_in_metadata_import: true,
+        label: name,
+        name: code
+      )
+    end
+    RegistryReferenceType.create(
+      code: "spelling",
+      name: "Bezeichner",
+      project: project
+    )
+    project
+  end
+
+  def self.interview_with_everything(project, archive_id_number)
+    attributes = {
+      archive_id: "#{project.shortname}#{format('%03d', archive_id_number)}",
+      media_type: 'video',
+      observations: '1\nInternational Slave- und Forced Labourers Documentation Project – Internationales Sklaven- und Zwangsarbeiter Befragungsprojekt\nInterview mit Adamez Konstantin Wojtowitsch\nProtokoll\nAudiointerview am 10. September 2005 in Minsk \t\n(Weißrussland/Belarus)\nAdresse: Wohnung von Adamez Konstantin Wojtowitsch',
+      description: 'an einem Sonntag ...',
+      collection: Collection.create!(name: 'Test collection', project: project),
+      properties: {link: 'http://bla.de'},
+      interview_date: '2.3.1978',
+      signature_original: 'karlheinz23',
+    }
+
+    interview = Interview.create!(
+      attributes.merge(
+        project: project,
+        interview_languages: [
+          InterviewLanguage.new(
+            language: Language.create!(code: 'rus'),
+            spec: 'primary'
+          ),
+          InterviewLanguage.new(
+            language: Language.create!(code: 'ger'),
+            spec: 'primary_translation'
+          )
+        ],
+      )
+    )
+
+    person = person_with_biographical_entries
+
+    {interviewer: 'INT', interviewee: 'AB', cinematographer: 'KAM'}.each do |code, speaker_designation|
+      #contribution_type = ContributionType.create!(
+        #code: code,
+        #project: project
+      #)
+      Contribution.create!(
+        interview: interview,
+        speaker_designation: speaker_designation,
+        person: person,
+        contribution_type_id: ContributionType.find_by(code: code, project: project).id
+      )
+    end
+
+    photo = photo_with_translation(interview)
+
+    first_tape = Tape.create(interview: interview, number: 1)
+    second_tape = Tape.create(interview: interview, number: 2)
+
+    first_speaker = interview.contributions.first.person
+    second_speaker = interview.contributions.first(2).last.person
+
+    germany = registry_entry_with_names(project)
+    france = registry_entry_with_names(project, {de: 'Frankreich', ru: 'Фра́нция'})
+    poland = registry_entry_with_names(project, {de: 'Polen', ru: 'По́льша'})
+
+    segment_with_everything(
+      "00:00:02.00",
+      interview,
+      first_tape,
+      first_speaker,
+      [{
+        locale: :ru,
+        text: "Итак, сегодня 10-ое сентября 2005-го года, и мы находимся в гостях у Константина Войтовича Адамца",
+        mainheading: "Вступление",
+        subheading: nil
+      }, {
+        locale: :de,
+        text: "Also gut, heute ist der 10. September 2005, und wir sind bei Konstantin Woitowitsch Adamez",
+        mainheading: "Einleitung",
+        subheading: nil,
+      }],
+      [germany],
+      [
+        {
+          locale: :de,
+          text: "Hauptsitz Berlin Filiale für die Eisenerzgewinnung in Elsass-Lothringen",
+        },
+        {
+          locale: :ru,
+          text: "Главное местонахождение — Берлин Филиал по добыче"
+        }
+      ]
+    )
+    segment_with_everything(
+      "00:02:02.00",
+      interview,
+      second_tape,
+      second_speaker,
+      [{
+        locale: :ru,
+        text: "И, я бы попросил Вас, Константин Войтович, расскажите, пожалуйста, историю Вашей жизни",
+        mainheading: nil,
+        subheading: "жизнь"
+      }, {
+        locale: :de,
+        text: "Und ich würde Sie bitten, Konstantin Woitowitsch, erzählen Sie bitte Ihre Lebensgeschichte",
+        mainheading: nil,
+        subheading: "Leben",
+      }],
+      [france, poland],
+      [
+        {
+          locale: :de,
+          text: "Für die Unterbringung der Ostarbeiter errichtetes Barackenlager"
+        },
+        {
+          locale: :ru,
+          text: "Построенный для размещения восточных рабочих барачный"
+        }
+      ]
+    )
+    interview
+  end
+
+  def self.segment_with_everything(
+    timecode="00:00:02.00",
+    interview=test_interview,
+    tape=test_tape,
+    speaker=nil,
+    translations_attributes=[
+      {
+        locale: :de,
+        text: 'Am Anfang, also das war...',
+        mainheading: 'Anfang',
+        subheading: 'Morgens'
+      },
+      {
+        locale: :en,
+        text: 'In the begining, it was...',
+        mainheading: 'Beginning',
+        subheading: 'In the morning'
+      }
+    ],
+    registry_entries=[],
+    annotations=[
+      {
+        text: 'Für die Unterbringung der Ostarbeiter errichtetes Barackenlager',
+        locale: :de
+      },
+      {
+        text: 'Построенный для размещения восточных рабочих барачный',
+        locale: :ru
+      }]
+  )
+    segment = Segment.create(
+      interview: interview,
+      tape: tape,
+      timecode: timecode,
+      speaking_person: speaker,
+      translations_attributes: translations_attributes,
+      registry_references_attributes: registry_entries.map { |entry| {registry_entry: entry} },
+      annotations: annotations.map { |annotation| Annotation.new(annotation) }
+    )
+  end
+
+  def self.test_tape(interview, number=1)
+    Tape.create(interview: interview, number: number)
+  end
+
+  def self.registry_entry_with_names(project, names={de: 'Deutschland', ru: 'Герма́ния'})
+    registry_entry = RegistryEntry.create(
+      latitude: 52.52,
+      longitude: 13.40,
+      workflow_state: "public",
+      list_priority: 0,
+      project: project,
+      registry_names_attributes: names.map { |locale, name| {
+        locale: locale,
+        descriptor: name,
+        name_position: 1,
+        registry_name_type: project.registry_name_types.first
+      } }
+    )
+  end
+
+  def self.photo_with_translation(interview)
+    photo = Photo.create(
+      interview: interview,
+      workflow_state: "public",
+      public_id: 'cd001_33',
+      photo_file_name: 'cd001_33.jpg',
+      photo_content_type: 'image/jpeg',
+      translations_attributes: [
+        {
+          locale: "de",
+          caption: "Ein Haus am See",
+          date: '1.5.1999',
+          place: 'Berlin',
+          photographer: 'Hubert',
+          license: 'GPL',
+        }
+      ]
+      #file: File.open(Rails.root.join('spec', 'fixtures', 'files', 'photo.jpg')),
+    )
+    photo.photo.attach(io: File.open(Rails.root.join('spec', 'files', 'cd001_33.jpg')), filename: 'cd001_33.jpg', content_type: 'image/jpeg')
+  end
+
+  def self.person_with_biographical_entries
+    entries = [
+      [:de, "15.09.1925: Geburt im Dorf Stasi, Bez. Dikanka, Gebiet Poltawa. Konstantin Wojtowitsch hat vier Geschwister"],
+      [:ru, "\n\nАдамец Константин Войтович родился 15.09.1925 г. в деревне Стаси Диканьского района Полтавской области. У Константина Войтовича было три брата и одна сестр"]
+    ]
+    person = Person.create(
+      first_name: "Константин",
+      last_name: "Адамец",
+      birth_name: "Hans",
+      other_first_names: "Войтович",
+      alias_names: "Адамец Константин Войтович Adamez Konstantin",
+      biographical_entries_attributes: entries.map { |locale, text| {locale: locale, text: text} }
+    )
+    person.update(
+      locale: 'de',
+      first_name: 'Konstantin',
+      last_name: 'Adamez',
+      birth_name: 'Hans',
+      other_first_names: 'Wojtowitsch',
+      alias_names: 'Адамец Константин Войтович Adamez Konstantin',
+      pseudonym_first_name: 'Max',
+      pseudonym_last_name: 'Huber',
+      description: 'Max ist ein freundlicher Mensch',
+    )
+    person
+  end
+
   def self.test_contribution_type(project, attribs = {})
     attribs.reverse_merge!(
       project: project,
