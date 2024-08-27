fad4b799d1e5cc1fdf3e404191b15edc167052ed
cgregor <chrgregor@gmail.com>
2024-08-27 12:45:23 +0200

    fixing institution_project_serializer if no institution

--- app/serializers/institution_project_serializer.rb
+++ app/serializers/institution_project_serializer.rb
@@ -2,11 +2,11 @@ class InstitutionProjectSerializer < ApplicationSerializer
   attributes :id, :name, :shortname, :project_id, :institution_id
 
   def name
-    object.institution.localized_hash(:name)
+    object.institution&.localized_hash(:name)
   end
 
   def shortname
-    object.institution.shortname
+    object.institution&.shortname
   end
 
 end
