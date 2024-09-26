3bc861ef3646424d81ca785d8695b3b4923d24c7
cgregor <chrgregor@gmail.com>
2024-09-26 11:23:58 +0200

    new norm-data-api key

--- app/lib/norm_data_api.rb
+++ app/lib/norm_data_api.rb
@@ -14,8 +14,10 @@ class NormDataApi
     # extended test api:
     #uri = URI.parse("https://c105-230.cloud.gwdg.de/transformation/api/736b3cdd-042a-4409-ac32-eaed223d912e")
 
-    # i19.4.2024 api:
-    uri = URI.parse("https://c105-230.cloud.gwdg.de/transformation_nxt/call/594B006FF481146B0BB734AF10CC5F52")
+    # 19.4.2024 api:
+    #uri = URI.parse("https://c105-230.cloud.gwdg.de/transformation_nxt/call/594B006FF481146B0BB734AF10CC5F52")
+    # 26.9.2024 api:
+    uri = URI.parse("https://c105-230.cloud.gwdg.de/transformation_nxt/call/C1B5C042736BAB4F52CBB9B911573158")
 
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
@@ -24,6 +26,8 @@ class NormDataApi
         placeType: place_type,
         geoFilter: geo_filter,
         displayLang: I18n.available_locales,
+        #from: 30,
+        #size: 10
       }.to_json
       response = http.request request
       response.body
