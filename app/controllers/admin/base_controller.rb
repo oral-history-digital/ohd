93593479f819791577e4291dc7654b04371677f9
cgregor <chrgregor@gmail.com>
2024-11-28 14:05:01 +0100

    allow users with roles with klass 'User' to enter admin controller actions

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -23,7 +23,7 @@ class Admin::BaseController < ApplicationController
       session[:"user.return_to"] = request.request_uri
       flash[:alert] = t(:unauthenticated_search, :scope => 'devise.sessions')
       redirect_to new_user_session_url
-    elsif !current_user.admin? || current_user.permissions.map(&:klass).include?('User')
+    elsif !(current_user.admin? || current_user.permissions.map(&:klass).include?('User'))
       flash[:alert] = "Sie haben keine Administratorenrechte!"
       redirect_to new_user_session_url
     end
