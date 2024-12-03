e089645dbbb5bd607496c2384f32ed350e3eb579
cgregor <chrgregor@gmail.com>
2024-12-03 15:18:22 +0100

    fix metadata-import-template-test

--- test/lib/metadata_import_template_test.rb
+++ test/lib/metadata_import_template_test.rb
@@ -8,6 +8,7 @@ class MetadataImportTemplateTest < ActiveSupport::TestCase
     @project = DataHelper.test_project(shortname: 'mdt')
     @csv = MetadataImportTemplate.new(@project, :de).csv
     expected_headers = [
+      "true",
       "Interview-ID", "Originalsignatur", "Sprache",
       "Zweite Sprache", "Erste Übersetzungssprache", "Sammlung", 
       "Interview-Datum", "Medientyp", "Dauer", "Protokoll", 
@@ -25,6 +26,7 @@ class MetadataImportTemplateTest < ActiveSupport::TestCase
     @project = DataHelper.project_with_contribution_types_and_metadata_fields
     @csv = MetadataImportTemplate.new(@project, :de).csv
     expected_headers = [
+      "true",
       "Interview-ID", "Originalsignatur", "Sprache",
       "Zweite Sprache", "Erste Übersetzungssprache", "Sammlung", 
       "Interview-Datum", "Medientyp", "Dauer", "Protokoll", 
