88655aae631116edf7c533f0cd7d7674f37c54c6
cgregor <chrgregor@gmail.com>
2024-12-10 18:15:10 +0100

    add wrapperComponent for normData

---
+++ app/javascript/modules/registry/NormDatumFormWrapper.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { FaRegFileAlt, FaRegClone } from 'react-icons/fa';
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
+import '@reach/tabs/styles.css';
+
+import { useI18n } from 'modules/i18n';
+import NormDataForDescriptorContainer from './NormDataForDescriptorContainer';
+
+export default function NormDatumFormWrapper({
+    onSubmitCallback,
+    onCancel,
+    descriptor,
+    registryEntryAttributes,
+    setRegistryEntryAttributes,
+    setShowElementsInForm,
+    replaceNestedFormValues,
+    children,
+}) {
+
+    const { t } = useI18n();
+
+    return (
+        <Tabs
+            className="Tabs"
+            keyboardActivation="manual"
+        >
+            <div className="Layout-contentTabs">
+                <TabList className="Tabs-tabList">
+                    <Tab className="Tabs-tab">
+                        <FaRegFileAlt className="Tabs-tabIcon"/>
+                        <span className="Tabs-tabText">
+                            {t('search_in_normdata')}
+                        </span>
+                    </Tab>
+                    <Tab className="Tabs-tab">
+                        <FaRegClone className="Tabs-tabIcon"/>
+                        <span className="Tabs-tabText">
+                            {t('enter_normdata_manually')}
+                        </span>
+                    </Tab>
+                </TabList>
+            </div>
+
+            <div className='wrapper-content'>
+                <TabPanels>
+                    <TabPanel>
+                        { descriptor ?
+                            <>
+                                <p>{t('search_api_for', {descriptor: descriptor})}</p>
+                                <NormDataForDescriptorContainer
+                                    descriptor={descriptor}
+                                    setRegistryEntryAttributes={setRegistryEntryAttributes}
+                                    registryEntryAttributes={registryEntryAttributes}
+                                    onSubmitCallback={onSubmitCallback}
+                                    setShowElementsInForm={setShowElementsInForm}
+                                    replaceNestedFormValues={replaceNestedFormValues}
+                                />
+                            </>: <p>{t('enter_descriptor_first')}</p>
+                        }
+                    </TabPanel>
+                    <TabPanel>
+                        { children }
+                    </TabPanel>
+                </TabPanels>
+            </div>
+        </Tabs>
+    );
+}
