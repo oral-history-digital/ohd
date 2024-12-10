3511fd8b598611ca94f9d9e8fc82e65bed330165
Marc Altmann <marcaltmann@posteo.de>
2024-12-10 17:27:49 +0100

    INTARCH-2937: Show map full names only with access

--- app/serializers/slim_registry_reference_serializer.rb
+++ app/serializers/slim_registry_reference_serializer.rb
@@ -23,6 +23,7 @@ class SlimRegistryReferenceSerializer < ActiveModel::Serializer
   def last_name
     default_locale = @instance_options[:default_locale]
     signed_in = @instance_options[:signed_in]
+    has_permission = @instance_options[:has_permission]
 
     last_names = object.use_pseudonym == 1 ?
       JSON.parse(object.agg_pseudonym_last_names) :
@@ -30,6 +31,6 @@ class SlimRegistryReferenceSerializer < ActiveModel::Serializer
 
     full_last_name = last_names[I18n.locale] || last_names[default_locale] || ''
 
-    signed_in ? full_last_name : "#{full_last_name.slice(0, 1)}."
+    signed_in && has_permission ? full_last_name : "#{full_last_name.slice(0, 1)}."
   end
 end
