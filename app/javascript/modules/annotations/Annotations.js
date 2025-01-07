5f7de1ab655252ed09d5b87b905aedaee32aecc9
cgregor <chrgregor@gmail.com>
2025-01-07 12:21:45 +0100

    change annotations to alpha3

--- app/javascript/modules/annotations/Annotations.js
+++ app/javascript/modules/annotations/Annotations.js
@@ -6,26 +6,24 @@ import { useI18n } from 'modules/i18n';
 import { Modal } from 'modules/ui';
 import AnnotationFormContainer from './AnnotationFormContainer';
 import AnnotationContainer from './AnnotationContainer';
-import { ALPHA3_TO_ALPHA2 } from 'modules/constants';
 
 export default function Annotations({
     segment,
     contentLocale,
 }) {
     const { t } = useI18n();
-    const contentLocaleAlpha2 = ALPHA3_TO_ALPHA2[contentLocale];
 
     return (
         <div>
             {
                 Object.values(segment.annotations)
-                    .filter(annotation => annotation.text.hasOwnProperty(contentLocaleAlpha2))
+                    .filter(annotation => annotation.text.hasOwnProperty(contentLocale))
                     .map(annotation => (
                         <AnnotationContainer
                             annotation={annotation}
                             segment={segment}
                             key={annotation.id}
-                            contentLocale={contentLocaleAlpha2}
+                            contentLocale={contentLocale}
                         />
                     ))
             }
@@ -38,7 +36,7 @@ export default function Annotations({
                         {closeModal => (
                             <AnnotationFormContainer
                                 segment={segment}
-                                contentLocale={contentLocaleAlpha2}
+                                contentLocale={contentLocale}
                                 onSubmit={closeModal}
                                 onCancel={closeModal}
                                 cancelLabel={t('cancel')}
