169ef53b1953b3d24c8dc0bdf6d0210315430a38
cgregor <chrgregor@gmail.com>
2025-01-08 13:00:55 +0100

    extend translations migration for edit-table

--- db/migrate/20241218125424_add_new_translation_values_for_edit_table.rb
+++ db/migrate/20241218125424_add_new_translation_values_for_edit_table.rb
@@ -6,17 +6,21 @@ class AddNewTranslationValuesForEditTable < ActiveRecord::Migration[7.0]
     TranslationValue.where(key: 'edit_column_header.mainheading_orig').update_all(key: 'edit_column_header.mainheading')
     TranslationValue.where(key: 'edit_column_header.subheading_orig').update_all(key: 'edit_column_header.subheading')
 
+    TranslationValue.where(key: "edit_column_header.text_translated").first.translations.each do |t|
+      t.update(value: t.value + ' (%{translation_locale})')
+    end
+    TranslationValue.where(key: "edit_column_header.annotations_translated").first.translations.each do |t|
+      t.update(value: TranslationValue.for("edit_column_header.annotations", t.locale) + ' (%{translation_locale})')
+    end
+
     %w(text_orig annotations).each do |key|
       TranslationValue.where(key: "edit_column_header.#{key}").first.translations.each do |t|
         t.update(value: t.value + ' (%{original_locale})')
       end
     end
 
-    %w(text_translated annotations_translated).each do |key|
-      TranslationValue.where(key: "edit_column_header.#{key}").first.translations.each do |t|
-        t.update(value: t.value + ' (%{translation_locale})')
-      end
-    end
+    TranslationValue.create(key: 'edit_column_header.tape_number', value: 'Band', locale: :de)
+    TranslationValue.create(key: 'edit_column_header.speaker_designation', value: 'Sprecher', locale: :de)
   end
 
   def down
