abfea5d93604ec437cc17d02a2b97b8eefe361a5
cgregor <chrgregor@gmail.com>
2025-01-14 11:00:52 +0100

    write public and subtitle version for empty/deleted text as well

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -99,8 +99,8 @@ class Segment < ApplicationRecord
 
     after_save do
       # run this only after commit of original e.g. 'de' version!
-      if (locale.length == 3 || locale == :'ukr-rus') && text.present?
-      #if text_previously_changed? && (locale.length == 3 || locale == :'ukr-rus') && !text.blank?
+      #if (locale.length == 3 || locale == :'ukr-rus')
+      if text_previously_changed? && (locale.length == 3 || locale == :'ukr-rus')
         segment.write_other_versions(text, locale)
       end
       segment.translations.where(text: nil).where("char_length(locale) = 2").destroy_all # where do these empty translations come from?
