90a9e7fbec67c54add3a816f966149c82c0589bc
cgregor <chrgregor@gmail.com>
2025-02-06 16:23:31 +0100

    deploying edit-table-with-locale-headers to staging

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -5,7 +5,7 @@ set :stage, :staging
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
-set :branch, ENV.fetch('BRANCH', 'alpha3')
+set :branch, ENV.fetch('BRANCH', 'edit-table-with-locale-headers')
 
 set :rbenv_type, :system
 set :rbenv_ruby, '3.0.7'
