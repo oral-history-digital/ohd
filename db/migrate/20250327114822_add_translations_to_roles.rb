d2b280cdf3eeea566162c22029edea633999b768
grgr <chrgregor@gmail.com>
2025-03-27 13:04:26 +0100

    adding translations to roles

---
+++ db/migrate/20250327114822_add_translations_to_roles.rb
@@ -0,0 +1,19 @@
+class AddTranslationsToRoles < ActiveRecord::Migration[7.0]
+  def change
+    reversible do |dir|
+      dir.up do
+        Role.create_translation_table!(
+          {name: :string},
+          {
+            migrate_data: true,
+            remove_source_columns: true
+          }
+        )
+      end
+
+      dir.down do
+        Role.drop_translation_table! migrate_data: true
+      end
+    end
+  end
+end
