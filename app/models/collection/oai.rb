c8a03f37a94785d0464ee074ab5b978ef75931c3
grgr <chrgregor@gmail.com>
2025-04-07 15:04:10 +0200

    oai: collection formats

--- app/models/collection/oai.rb
+++ app/models/collection/oai.rb
@@ -7,4 +7,78 @@ module Collection::Oai
     ]
   end
 
+  def oai_locales
+    %w(de en)
+  end
+
+  def oai_identifier
+    #"oai:#{shortname}"
+    #"oai:#{project.shortname}:#{shortname}"
+    #"oai:#{project.shortname}:collection-#{id}"
+    #"oai:oral-history.digital:#{project.shortname}:collection-#{id}"
+    "oai:oral-history.digital:collection-#{id}"
+  end
+
+  def oai_dc_identifier
+    oai_identifier
+  end
+
+  def oai_url_identifier(locale)
+    "#{project.domain_with_optional_identifier}/#{locale}/catalog/collections/#{id}"
+  end
+
+  def oai_title(locale)
+    name(locale)
+  end
+
+  def oai_contributor(locale)
+    project.institutions.map(&:name).join(", ")
+  end
+
+  def oai_creator(locale)
+    project.institutions.where.not(parent_id: nil).first&.name(locale) ||
+      project.institutions.first&.name(locale)
+  end
+
+  def oai_publisher(locale)
+    project.institutions.where(parent_id: nil).first&.name(locale) ||
+      project.institutions.first&.name(locale)
+  end
+
+  def oai_date
+    created_at.strftime("%d.%m.%Y")
+  end
+
+  def oai_type
+    "audio/video"
+  end
+
+  def type
+    'Collection'
+  end
+
+  def oai_formats
+    [
+      "video/mp4",
+      "audio/mp3"
+    ]
+  end
+
+  def oai_size
+    "#{interviews.count} Interviews"
+  end
+
+  def oai_languages
+    Language.where(id: interviews.map{|i| i.interview_languages.pluck(:language_id)}.flatten.uniq).pluck(:code)
+  end
+
+  def oai_abstract_description(locale)
+    ActionView::Base.full_sanitizer.sanitize(project.introduction(locale))
+  end
+  def oai_media_files_description(locale)
+    TranslationValue.for('media_files', locale)
+  end
+  def oai_transcript_description(locale)
+    TranslationValue.for('transcript', locale)
+  end
 end
--- app/models/collection/oai_dc.rb
+++ app/models/collection/oai_dc.rb
@@ -1,62 +1,104 @@
 module Collection::OaiDc
 
-  def oai_dc_identifier
-    "oai:#{shortname}"
-    #"oai:#{project.shortname}:#{shortname}"
-  end
+  def to_oai_dc
+    xml = Builder::XmlMarkup.new
+    xml.tag!(
+      "oai_dc:dc",
+      'xmlns:oai_dc' => "http://www.openarchives.org/OAI/2.0/oai_dc/",
+      'xmlns:dc' => "http://purl.org/dc/elements/1.1/",
+      'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
+      'xsi:schemaLocation' =>
+      %{http://www.openarchives.org/OAI/2.0/oai_dc/
+          http://www.openarchives.org/OAI/2.0/oai_dc.xsd}
+    ) do
 
-  def oai_dc_creator
-    project.leader
-  end
+      xml.tag!('dc:identifier', oai_identifier)
+      xml.tag!('dc:source', oai_identifier)
 
-  def oai_dc_subject
-    "Interview Sammlung"
-  end
+      oai_locales.each do |locale|
+        xml.tag!('dc:identifier', "xml:lang": locale, identfierType: "URL") do
+          xml.text! oai_url_identifier(locale)
+        end
+      end
 
-  def oai_dc_title
-    name(:de)
-  end
+      oai_locales.each do |locale|
+        xml.tag!('dc:title', "xml:lang": locale) do
+          xml.text! oai_title(locale)
+        end
+      end
 
-  def oai_dc_description
-    "Interview Sammlung"
-  end
+      oai_locales.each do |locale|
+        xml.tag!('dc:creator', "xml:lang": locale) do
+          xml.text! oai_creator(locale)
+        end
+      end
 
-  def oai_dc_publisher
-    "Interview-Archiv \"#{name('de')}\""
-  end
+      oai_locales.each do |locale|
+        xml.tag!('dc:publisher', "xml:lang": locale) do
+          xml.text! oai_publisher(locale)
+        end
+      end
 
-  def oai_dc_contributor
-    "#{institution&.name(:de)} #{[project.cooperation_partner]}"
-  end
+      xml.tag!('dc:contributor', project.manager)
+      oai_locales.each do |locale|
+        xml.tag!('dc:contributor', "xml:lang": locale) do
+          xml.text! oai_contributor(locale)
+        end
+      end
 
-  def oai_dc_date
-  end
+      xml.tag!('dc:date', oai_date)
 
-  def oai_dc_type
-    'Sammlung'
-  end
+      xml.tag!('dc:type', oai_type)
 
-  def type
-    'Collection'
-  end
+      oai_formats.each do |format|
+        xml.tag!('dc:format', format)
+      end
 
-  def oai_dc_format
-  end
+      oai_languages.each do |language|
+        xml.tag!('dc:language', language)
+      end
 
-  def oai_dc_source
-  end
+      xml.tag!('dc:relation', OHD_DOMAIN)
+      xml.tag!('dc:relation', "#{OHD_DOMAIN}/de/catalog/archives/#{project_id}")
+      xml.tag!('dc:relation', project.domain)
+      xml.tag!('dc:relation', "https://portal.oral-history.digital/de/oai_repository?verb=ListRecords&metadataPrefix=oai_dc&set=collection:#{id}")
 
-  def oai_dc_language
-  end
+      xml.tag!('dc:description', oai_size)
+      oai_locales.each do |locale|
+        xml.tag!('dc:description', "xml:lang": locale) do
+          xml.text! oai_abstract_description(locale)
+        end
+        xml.tag!('dc:description', "xml:lang": locale) do
+          xml.text! oai_media_files_description(locale)
+        end
+        xml.tag!('dc:description', "xml:lang": locale) do
+          xml.text! oai_transcript_description(locale)
+        end
+      end
 
-  #def oai_dc_relation
-  #end
+      oai_locales.each do |locale|
+        xml.tag!('dc:rights', "xml:lang": locale) do
+          xml.text! "#{project.domain_with_optional_identifier}/#{locale}/conditions"
+        end
+      end
+      oai_locales.each do |locale|
+        xml.tag!('dc:rights', "xml:lang": locale) do
+          xml.text! "#{OHD_DOMAIN}/#{locale}/conditions"
+        end
+      end
+      oai_locales.each do |locale|
+        xml.tag!('dc:rights', "xml:lang": locale) do
+          xml.text! "#{OHD_DOMAIN}/#{locale}/privacy_protection"
+        end
+      end
+      oai_locales.each do |locale|
+        xml.tag!('dc:rights', "xml:lang": locale) do
+          xml.text! "CC-BY-4.0 #{TranslationValue.for('metadata_licence', locale)}"
+        end
+      end
 
-  #def oai_dc_coverage
-  #end
-
-  def oai_dc_rights
-    "#{project.domain_with_optional_identifier}/#{project.default_locale}/conditions"
+      xml.target!
+    end
   end
 
 end
