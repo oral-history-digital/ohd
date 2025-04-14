1e74b65b72bc65cdf5d93bb8658e276c584b7aad
grgr <chrgregor@gmail.com>
2025-04-14 12:13:23 +0200

    add translations to registry_name_types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_10_080721) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_14_094701) do
   create_table "access_configs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "project_id", null: false
     t.text "organization"
@@ -679,9 +679,18 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_10_080721) do
     t.index ["registry_name_id"], name: "index_registry_name_translations_on_registry_name_id"
   end
 
+  create_table "registry_name_type_translations", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "registry_name_type_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["locale"], name: "index_registry_name_type_translations_on_locale"
+    t.index ["registry_name_type_id"], name: "index_registry_name_type_translations_on_registry_name_type_id"
+  end
+
   create_table "registry_name_types", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "code", limit: 255
-    t.string "name", limit: 255
     t.integer "order_priority"
     t.boolean "allows_multiple"
     t.boolean "mandatory"
