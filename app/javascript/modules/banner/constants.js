023e95f193025b598d49ac6101720d19b53ffcd5
Marc Altmann <marcaltmann@posteo.de>
2025-04-23 16:09:59 +0200

    INTARCH-3080: Implement dynamic banner

---
+++ app/javascript/modules/banner/constants.js
@@ -0,0 +1,3 @@
+const NAME = 'banner';
+
+export { NAME };
---
+++ app/javascript/modules/banner/selectors.js
@@ -0,0 +1,7 @@
+import { NAME } from './constants';
+
+export const getBanner = (state) => {
+    return state[NAME];
+};
+
+export const getBannerActive = state => getBanner(state).active;
