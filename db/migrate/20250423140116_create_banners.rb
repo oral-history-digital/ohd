f8fac8b55aae649619fd074f990f63475c9d8134
Marc Altmann <marcaltmann@posteo.de>
2025-04-24 14:46:27 +0200

    Remove default values

--- db/migrate/20250423140116_create_banners.rb
+++ db/migrate/20250423140116_create_banners.rb
@@ -1,8 +1,8 @@
 class CreateBanners < ActiveRecord::Migration[7.0]
   def change
     create_table :banners do |t|
-      t.text :message_en, null: false, default: ""
-      t.text :message_de, null: false, default: ""
+      t.text :message_en
+      t.text :message_de
       t.boolean :active, null: false, default: false
       t.datetime :start_date, null: false, default: -> { 'CURRENT_TIMESTAMP' }
       t.datetime :end_date, null: false, default: -> { 'DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 10 DAY)' }
