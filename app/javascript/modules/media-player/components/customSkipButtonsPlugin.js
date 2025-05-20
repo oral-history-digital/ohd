742f73e64259630af3399ab9a2efe350ab5ca07c
Jan Kühn <yotka@posteo.org>
2025-05-20 15:32:47 +0000

    feat: add translations for skip buttons in media player controls

--- app/javascript/modules/media-player/components/customSkipButtonsPlugin.js
+++ app/javascript/modules/media-player/components/customSkipButtonsPlugin.js
@@ -5,62 +5,84 @@ import videojs from 'video.js';
 // Import the necessary Video.js components
 const Button = videojs.getComponent('Button');
 
+// Helper to get translations from plugin options
+const getTranslation = (translations, key, defaultValue) => {
+    if (translations && translations[key]) {
+        return translations[key];
+    }
+    return defaultValue;
+};
+
 // Create custom forward button class
 class CustomForwardButton extends Button {
-  constructor(player, options) {
-    super(player, options);
-    this.addClass('vjs-custom-skip-forward');
-    this.controlText('Forward ' + options.step + ' seconds');
-  }
+    constructor(player, options) {
+        super(player, options);
+        this.addClass('vjs-custom-skip-forward');
 
-  handleClick() {
-    const now = this.player_.currentTime();
-    const step = this.options_.step || 5;
-    this.player_.currentTime(now + step);
-  }
-
-  createEl() {
-    const el = super.createEl();
-    
-    // Replace the inner content with our custom React icon
-    const iconContainer = el.querySelector('.vjs-icon-placeholder') || el;
-    if (iconContainer) {
-      createRoot(iconContainer).render(
-        <FaStepForward style={{ fontSize: '.8rem' }} />
-      );
+        const translations = options.translations || {};
+        const stepText = getTranslation(
+            translations,
+            'skipForward',
+            'Forward ' + options.step + ' seconds'
+        );
+        this.controlText(stepText);
     }
-    
-    return el;
-  }
+
+    handleClick() {
+        const now = this.player_.currentTime();
+        const step = this.options_.step || 5;
+        this.player_.currentTime(now + step);
+    }
+
+    createEl() {
+        const el = super.createEl();
+
+        // Replace the inner content with our custom React icon
+        const iconContainer = el.querySelector('.vjs-icon-placeholder') || el;
+        if (iconContainer) {
+            createRoot(iconContainer).render(
+                <FaStepForward style={{ fontSize: '.8rem' }} />
+            );
+        }
+
+        return el;
+    }
 }
 
 // Create custom backward button class
 class CustomBackwardButton extends Button {
-  constructor(player, options) {
-    super(player, options);
-    this.addClass('vjs-custom-skip-backward');
-    this.controlText('Backward ' + options.step + ' seconds');
-  }
+    constructor(player, options) {
+        super(player, options);
+        this.addClass('vjs-custom-skip-backward');
 
-  handleClick() {
-    const now = this.player_.currentTime();
-    const step = this.options_.step || 5;
-    this.player_.currentTime(Math.max(0, now - step));
-  }
-
-  createEl() {
-    const el = super.createEl();
-    
-    // Replace the inner content with our custom React icon
-    const iconContainer = el.querySelector('.vjs-icon-placeholder') || el;
-    if (iconContainer) {
-      createRoot(iconContainer).render(
-        <FaStepBackward style={{ fontSize: '.8rem' }} />
-      );
+        const translations = options.translations || {};
+        const stepText = getTranslation(
+            translations,
+            'skipBack',
+            'Backward ' + options.step + ' seconds'
+        );
+        this.controlText(stepText);
     }
-    
-    return el;
-  }
+
+    handleClick() {
+        const now = this.player_.currentTime();
+        const step = this.options_.step || 5;
+        this.player_.currentTime(Math.max(0, now - step));
+    }
+
+    createEl() {
+        const el = super.createEl();
+
+        // Replace the inner content with our custom React icon
+        const iconContainer = el.querySelector('.vjs-icon-placeholder') || el;
+        if (iconContainer) {
+            createRoot(iconContainer).render(
+                <FaStepBackward style={{ fontSize: '.8rem' }} />
+            );
+        }
+
+        return el;
+    }
 }
 
 // Register the custom components
@@ -68,46 +90,67 @@ videojs.registerComponent('CustomForwardButton', CustomForwardButton);
 videojs.registerComponent('CustomBackwardButton', CustomBackwardButton);
 
 // Plugin function
-function customSkipButtonsPlugin() {
-  const player = this;
-  
-  // Run on ready to make sure the control bar exists
-  player.ready(() => {
-    const controlBar = player.controlBar;
-    
-    // Remove the default skip buttons if they exist
-    const existingForward = controlBar.getChild('skipForward');
-    const existingBackward = controlBar.getChild('skipBackward');
-    
-    if (existingForward) {
-      controlBar.removeChild('skipForward');
-    }
-    
-    if (existingBackward) {
-      controlBar.removeChild('skipBackward');
-    }
-    
-    // Get the step values from options
-    const forwardStep = player.options_.controlBar?.skipButtons?.forward || 5;
-    const backwardStep = player.options_.controlBar?.skipButtons?.backward || 5;
-    
-    // Add our custom buttons in the same positions
-    const playToggleIndex = controlBar.children_.indexOf(controlBar.getChild('playToggle'));
-    
-    if (playToggleIndex !== -1) {
-      controlBar.addChild('CustomForwardButton', {
-        step: forwardStep
-      }, playToggleIndex + 1);
-      
-      controlBar.addChild('CustomBackwardButton', {
-        step: backwardStep
-      }, playToggleIndex);
-    } else {
-      // Fallback if playToggle doesn't exist
-      controlBar.addChild('CustomBackwardButton', { step: backwardStep });
-      controlBar.addChild('CustomForwardButton', { step: forwardStep });
-    }
-  });
+function customSkipButtonsPlugin(options = {}) {
+    const player = this;
+    const translations = options.translations || {};
+
+    // Run on ready to make sure the control bar exists
+    player.ready(() => {
+        const controlBar = player.controlBar;
+
+        // Remove the default skip buttons if they exist
+        const existingForward = controlBar.getChild('skipForward');
+        const existingBackward = controlBar.getChild('skipBackward');
+
+        if (existingForward) {
+            controlBar.removeChild('skipForward');
+        }
+
+        if (existingBackward) {
+            controlBar.removeChild('skipBackward');
+        }
+
+        // Get the step values from options
+        const forwardStep =
+            player.options_.controlBar?.skipButtons?.forward || 5;
+        const backwardStep =
+            player.options_.controlBar?.skipButtons?.backward || 5;
+
+        // Add our custom buttons in the same positions
+        const playToggleIndex = controlBar.children_.indexOf(
+            controlBar.getChild('playToggle')
+        );
+
+        if (playToggleIndex !== -1) {
+            controlBar.addChild(
+                'CustomForwardButton',
+                {
+                    step: forwardStep,
+                    translations: translations,
+                },
+                playToggleIndex + 1
+            );
+
+            controlBar.addChild(
+                'CustomBackwardButton',
+                {
+                    step: backwardStep,
+                    translations: translations,
+                },
+                playToggleIndex
+            );
+        } else {
+            // Fallback if playToggle doesn't exist
+            controlBar.addChild('CustomBackwardButton', {
+                step: backwardStep,
+                translations: translations,
+            });
+            controlBar.addChild('CustomForwardButton', {
+                step: forwardStep,
+                translations: translations,
+            });
+        }
+    });
 }
 
 // Register the plugin
