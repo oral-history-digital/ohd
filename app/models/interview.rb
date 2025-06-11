4ef106df4f4b2a28dbf5699cacec037bd9f3b2b8
grgr <chrgregor@gmail.com>
2025-06-11 14:18:13 +0200

    adding InterviewPermisson-Form, -controller, -policy, -routes

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -302,6 +302,7 @@ class Interview < ApplicationRecord
   handle_asynchronously :solr_index!, queue: 'indexing', priority: 50
 
   scope :shared, -> {where(workflow_state: 'public')}
+  scope :restricted, -> {where(workflow_state: 'restricted')}
   scope :with_media_type, -> {where.not(media_type: nil)}
 
   def project_access
