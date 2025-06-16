b8a94597478792344e761a60a141d5c05a53e365
grgr <chrgregor@gmail.com>
2025-06-16 09:46:29 +0200

    set staging branch to ruby-3.4-rails-8.0.2

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -5,7 +5,7 @@ set :stage, :staging
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
-set :branch, ENV.fetch('BRANCH', 'main')
+set :branch, ENV.fetch('BRANCH', 'ruby-3.4-rails-8.0.2')
 
 set :rbenv_type, :system
 set :rbenv_ruby, '3.0.7'
