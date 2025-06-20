2881b8cadeb4eaac925be0ed9037514dfc0a5ee3
grgr <chrgregor@gmail.com>
2025-06-20 10:41:22 +0200

    user profile with restricted interviews per archive

--- app/serializers/interview_permission_serializer.rb
+++ app/serializers/interview_permission_serializer.rb
@@ -3,11 +3,20 @@ class InterviewPermissionSerializer < ApplicationSerializer
     :id,
     :name,
     :interview_id,
+    :archive_id,
     :user_id,
+    :project_id,
   ]
 
   def name
     object.interview.archive_id
   end
 
+  def archive_id
+    object.interview.archive_id
+  end
+
+  def project_id
+    object.interview.project_id
+  end
 end
