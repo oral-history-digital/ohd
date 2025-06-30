83bc42fe0d9303778df1d5115c2066b5729332e0
grgr <chrgregor@gmail.com>
2025-06-30 11:43:24 +0200

    show interview-permissions-form only if there are restricted interviews

--- app/javascript/modules/users/UserRowInterviewPermissions.js
+++ app/javascript/modules/users/UserRowInterviewPermissions.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { RowActions } from 'modules/tables';
 import InterviewPermissionForm from './InterviewPermissionForm';
 import { useI18n } from 'modules/i18n';
+import useRestrictedInterviews from './useRestrictedInterviews';
 
 export default function UserRowActions({
     row,
@@ -10,12 +11,14 @@ export default function UserRowActions({
 }) {
     const { t } = useI18n();
     const dataPath = getValue();
+    const { interviews } = useRestrictedInterviews();
 
     const none = !row.original.interview_permissions || row.original.interview_permissions.length === 0
     const names = row.original.interview_permissions.map(i => i.name).slice(0, 3).join(', ');
     const hasMore = row.original.interview_permissions.length > 3;
 
     return (
+        interviews?.length === 0 ? null :
         <>
             <span>{!none && `${row.original.interview_permissions.length} ${t('activerecord.models.interview_permission.other')}: `}</span>
             <span>
