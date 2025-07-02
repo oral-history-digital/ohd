834ebe935f0c13e8277ab636ef37d004bfe520c0
Marc Altmann <marcaltmann@posteo.de>
2025-07-02 18:59:42 +0200

    WIP controllers etc.

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -4,4 +4,9 @@ class Pdf < ApplicationRecord
 
   translates :title
   accepts_nested_attributes_for :translations
+
+  def file_path
+    key = file.blob.key
+    File.join("storage", key.first(2), key.first(4).last(2), key)
+  end
 end
