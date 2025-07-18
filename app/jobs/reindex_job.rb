73cf10b4f1b18a4c1a48a7688b1e95f91afe28d0
grgr <chrgregor@gmail.com>
2025-07-18 14:58:50 +0200

    fix reindexing-job

--- app/jobs/reindex_job.rb
+++ app/jobs/reindex_job.rb
@@ -1,9 +1,8 @@
 class ReindexJob < ApplicationJob
   queue_as :default
 
-  def perform(archive_ids)
-    Interview.where(archive_id: archive_ids) 
-    Sunspot.index! objs
+  def perform(conditions = {})
+    Sunspot.index! Interview.where(conditions) 
   end
 
 end
