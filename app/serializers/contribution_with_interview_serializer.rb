b7ec41985ea409ba6c891284f7aadf2beac6edf5
grgr <chrgregor@gmail.com>
2025-08-26 12:26:29 +0200

    failsafe contribution_with_interview_serializer

--- app/serializers/contribution_with_interview_serializer.rb
+++ app/serializers/contribution_with_interview_serializer.rb
@@ -12,18 +12,18 @@ class ContributionWithInterviewSerializer < ApplicationSerializer
     :workflow_state
 
   def contribution_type
-    object.contribution_type && object.contribution_type.code
+    object.contribution_type&.code
   end
 
   def label
-    object.contribution_type.label
+    object.contribution_type&.label
   end
 
   def interview_id
-    object.interview.archive_id
+    object.interview&.archive_id
   end
 
   def interview_title
-    object.interview.anonymous_title
+    object.interview&.anonymous_title
   end
 end
