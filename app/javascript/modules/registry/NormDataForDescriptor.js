fb0422b3d70ded6a623c7346061987331830819d
Jan Kühn <yotka@posteo.org>
2025-10-16 12:01:04 +0000

    chore: remove console.log

--- app/javascript/modules/registry/NormDataForDescriptor.js
+++ app/javascript/modules/registry/NormDataForDescriptor.js
@@ -15,13 +15,11 @@ function NormDataForDescriptor({
     onSubmitCallback,
     setShowElementsInForm,
     setResultsFromNormDataSet,
-    replaceNestedFormValues
+    replaceNestedFormValues,
 }) {
-    const { t, locale } = useI18n();
+    const { t } = useI18n();
     const pathBase = usePathBase();
-    const { project } = useProject();
-    const [filter, setFilter] = useState(null);
-    const [placeTypeFilter, setPlaceTypeFilter] = useState(null);
+    const [setFilter] = useState(null);
     const [showResults, setShowResults] = useState(false);
     const [apiResult, setApiResult] = useState({});
     const [from, setFrom] = useState(0);
@@ -49,143 +47,170 @@ function NormDataForDescriptor({
             optionsScope: 'normdata',
             keepOrder: true,
             handlechangecallback: (name, value) => setFilter(value),
-        }
+        },
     ];
 
     //let formElements = [
-        //{
-            //elementType: 'select',
-            //attribute: 'filter_select',
-            //labelKey: 'normdata.filter_select',
-            //values: ['none', 'geo'],
-            //withEmpty: true,
-            //optionsScope: 'normdata',
-            //handlechangecallback: (name, value) => setFilter(value),
-        //}
+    //{
+    //elementType: 'select',
+    //attribute: 'filter_select',
+    //labelKey: 'normdata.filter_select',
+    //values: ['none', 'geo'],
+    //withEmpty: true,
+    //optionsScope: 'normdata',
+    //handlechangecallback: (name, value) => setFilter(value),
+    //}
     //];
 
     //if (filter === 'geo') {
-        //formElements = formElements.concat([
-            //{
-                //elementType: 'select',
-                //attribute: 'geo_filter',
-                //labelKey: 'normdata.geo_filter',
-                //values: ['de', 'eu'],
-                //withEmpty: true,
-                //optionsScope: 'normdata',
-            //},
-            //{
-                //elementType: 'select',
-                //attribute: 'place_type',
-                //labelKey: 'normdata.place_type',
-                //values: ['location', 'placeOfWorship', 'natural', 'historic', 'tourism'],
-                //withEmpty: true,
-                //optionsScope: 'normdata',
-                //handlechangecallback: (name, value) => setPlaceTypeFilter(value),
-            //},
-        //])
+    //formElements = formElements.concat([
+    //{
+    //elementType: 'select',
+    //attribute: 'geo_filter',
+    //labelKey: 'normdata.geo_filter',
+    //values: ['de', 'eu'],
+    //withEmpty: true,
+    //optionsScope: 'normdata',
+    //},
+    //{
+    //elementType: 'select',
+    //attribute: 'place_type',
+    //labelKey: 'normdata.place_type',
+    //values: ['location', 'placeOfWorship', 'natural', 'historic', 'tourism'],
+    //withEmpty: true,
+    //optionsScope: 'normdata',
+    //handlechangecallback: (name, value) => setPlaceTypeFilter(value),
+    //},
+    //])
     //}
 
     //if (placeTypeFilter === 'location') {
-        //formElements = formElements.concat([
-            //{
-                //elementType: 'select',
-                //attribute: 'place_extended',
-                //values: [
-                    //'continent', 'country', 'state',
-                    //'region', 'province', 'district', 'municipality',
-                    //'city', 'town',
-                    //'borough', 'suburb', 'quarter',
-                    //'village', 'hamlet', 'farm'
-                //],
-                //withEmpty: true,
-                //optionsScope: 'normdata',
-            //},
-        //])
+    //formElements = formElements.concat([
+    //{
+    //elementType: 'select',
+    //attribute: 'place_extended',
+    //values: [
+    //'continent', 'country', 'state',
+    //'region', 'province', 'district', 'municipality',
+    //'city', 'town',
+    //'borough', 'suburb', 'quarter',
+    //'village', 'hamlet', 'farm'
+    //],
+    //withEmpty: true,
+    //optionsScope: 'normdata',
+    //},
+    //])
     //}
 
-    const fetchAPIResults = async(params) => {
-        const urlAndFilters = [`${pathBase}/norm_data_api?expression=${descriptor}`];
-        ['geo_filter', 'place_type', 'place_extended', 'api'].forEach((filter) => {
-            if (params[filter]) {
-                urlAndFilters.push(`${filter}=${params[filter]}`);
+    const fetchAPIResults = async (params) => {
+        const urlAndFilters = [
+            `${pathBase}/norm_data_api?expression=${descriptor}`,
+        ];
+        ['geo_filter', 'place_type', 'place_extended', 'api'].forEach(
+            (filter) => {
+                if (params[filter]) {
+                    urlAndFilters.push(`${filter}=${params[filter]}`);
+                }
             }
-        });
+        );
 
         if (params['from']) {
             urlAndFilters.push(`from=${params['from']}`);
         }
 
         fetch(urlAndFilters.join('&'))
-            .then(res => res.json())
-            .then(json => setApiResult(json));
+            .then((res) => res.json())
+            .then((json) => setApiResult(json));
     };
 
     const displayResults = () => {
         //if (apiResult.success) {
         if (apiResult.response) {
-            console.log(apiResult);
             const items = apiResult.response?.items;
 
             return (
                 <>
-                    {(Array.isArray(items) ? items : [items]).length === 0 ?
-                        <p className='notifications'>
-                           {t('modules.interview_search.no_results')}
-                        </p> :
+                    {(Array.isArray(items) ? items : [items]).length === 0 ? (
+                        <p className="notifications">
+                            {t('modules.interview_search.no_results')}
+                        </p>
+                    ) : (
                         <ul>
-                            { (Array.isArray(items) ? items : [items]).map( result => {
-                                return (
-                                    <li>
-                                        <>
-                                            <UpdateRegistryEntryAttributesModal
-                                                entry={result.Entry}
-                                                registryEntryAttributes={registryEntryAttributes}
-                                                registryNameTypes={registryNameTypes}
-                                                normDataProviders={normDataProviders}
-                                                setRegistryEntryAttributes={setRegistryEntryAttributes}
-                                                setShowElementsInForm={setShowElementsInForm}
-                                                setResultsFromNormDataSet={setResultsFromNormDataSet}
-                                                replaceNestedFormValues={replaceNestedFormValues}
-                                            />
-                                            { result.Entry.Identifier.filter(p => !!p.URL).map( p => {
-                                                return (
-                                                    <a
-                                                        key={p.Value}
-                                                        href={p.URL}
-                                                        target="_blank"
-                                                        rel="noreferrer"
-                                                        className="Link flyout-sub-tabs-content-ico-link"
-                                                        title={t('norm_data.link_hover')}
-                                                    >
-                                                        &nbsp;{p.Provider}&nbsp;
-                                                    </a>
-                                                );
-                                            }) }
-                                        </>
-                                    </li>
-                                )
-                            })}
+                            {(Array.isArray(items) ? items : [items]).map(
+                                (result) => {
+                                    return (
+                                        <li key={result.Entry.ID}>
+                                            <>
+                                                <UpdateRegistryEntryAttributesModal
+                                                    entry={result.Entry}
+                                                    registryEntryAttributes={
+                                                        registryEntryAttributes
+                                                    }
+                                                    registryNameTypes={
+                                                        registryNameTypes
+                                                    }
+                                                    normDataProviders={
+                                                        normDataProviders
+                                                    }
+                                                    setRegistryEntryAttributes={
+                                                        setRegistryEntryAttributes
+                                                    }
+                                                    setShowElementsInForm={
+                                                        setShowElementsInForm
+                                                    }
+                                                    setResultsFromNormDataSet={
+                                                        setResultsFromNormDataSet
+                                                    }
+                                                    replaceNestedFormValues={
+                                                        replaceNestedFormValues
+                                                    }
+                                                />
+                                                {result.Entry.Identifier.filter(
+                                                    (p) => !!p.URL
+                                                ).map((p) => {
+                                                    return (
+                                                        <a
+                                                            key={p.Value}
+                                                            href={p.URL}
+                                                            target="_blank"
+                                                            rel="noreferrer"
+                                                            className="Link flyout-sub-tabs-content-ico-link"
+                                                            title={t(
+                                                                'norm_data.link_hover'
+                                                            )}
+                                                        >
+                                                            &nbsp;{p.Provider}
+                                                            &nbsp;
+                                                        </a>
+                                                    );
+                                                })}
+                                            </>
+                                        </li>
+                                    );
+                                }
+                            )}
                         </ul>
-                    }
-                    { from >= 10 &&
-                            <button
-                                type="button"
-                                className="Icon Icon--secondary"
-                                onClick={() => {
-                                    setFrom(from - 10);
-                                    fetchAPIResults({from: from - 10});
-                            }}>
-                                {t('previous')}
-                            </button>
-                    }
+                    )}
+                    {from >= 10 && (
+                        <button
+                            type="button"
+                            className="Icon Icon--secondary"
+                            onClick={() => {
+                                setFrom(from - 10);
+                                fetchAPIResults({ from: from - 10 });
+                            }}
+                        >
+                            {t('previous')}
+                        </button>
+                    )}
                     <button
                         type="button"
                         className="Icon Icon--primary u-ml-tiny"
                         onClick={() => {
                             setFrom(from + 10);
-                            fetchAPIResults({from: from + 10});
-                    }}>
+                            fetchAPIResults({ from: from + 10 });
+                        }}
+                    >
                         {t('next')}
                     </button>
                     <button
@@ -195,30 +220,29 @@ function NormDataForDescriptor({
                             setShowResults(false);
                             setApiResult({});
                             setFrom(0);
-                    }}>
+                        }}
+                    >
                         {t('normdata.reset')}
                     </button>
                 </>
             );
         } else if (apiResult.error) {
-            return (<p className='notifications'>
-                {t('error')}
-            </p>);
+            return <p className="notifications">{t('error')}</p>;
         } else {
-            return (<Spinner />);
+            return <Spinner />;
         }
-    }
+    };
 
-    return ( showResults ?
-        displayResults() :
+    return showResults ? (
+        displayResults()
+    ) : (
         <Form
-            scope='normdata'
+            scope="normdata"
             formClasses={'nested-form default'}
             onSubmit={(params) => {
                 !showResults && fetchAPIResults(params.normdata);
                 setShowResults(!showResults);
             }}
-            elements={[]}
             elements={formElements}
             helpTextCode="normdata_form"
             submitText={t('search')}
@@ -227,4 +251,3 @@ function NormDataForDescriptor({
 }
 
 export default NormDataForDescriptor;
-
