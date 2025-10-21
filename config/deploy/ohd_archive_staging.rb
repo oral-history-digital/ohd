ed3a85588cc4101ff9f2962aab0dc24af5a5c921
grgr <chrgregor@gmail.com>
2025-10-21 18:43:16 +0200

    set 2fa-branch to be deployed on staging

--- config/deploy/ohd_archive_staging.rb
+++ config/deploy/ohd_archive_staging.rb
@@ -5,7 +5,7 @@ set :stage, :staging
 set :deploy_to, "/data/applications/#{fetch :application}"
 set :bundle_path, "/data/bundle/01"
 set :project_yml, "ohd_archive_staging.yml"
-set :branch, ENV.fetch('BRANCH', 'delete-transcript-by-language')
+set :branch, ENV.fetch('BRANCH', '2fa')
 
 set :rbenv_type, :system
 set :rbenv_ruby, '3.3.4'
