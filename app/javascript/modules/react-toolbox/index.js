ef41147505f3f861af21a936305b88202943cd56
grgr <chrgregor@gmail.com>
2025-11-07 17:02:48 +0100

    cleanup

--- app/javascript/modules/react-toolbox/index.js
+++ app/javascript/modules/react-toolbox/index.js
@@ -1,5 +1,4 @@
 export { default as ErrorBoundary } from './ErrorBoundary';
 
 export { usePrevious } from './usePrevious';
-export { default as useQuery } from './useQuery';
 export { default as useCopyState } from './useCopyState';
