e4cfaf29cc8a59d5eac5b48ee45812448be828e9
Jan Kühn <yotka@posteo.org>
2025-11-11 07:51:09 +0000

    fix: re-add missing constants

--- app/javascript/modules/media-player/constants/index.js
+++ app/javascript/modules/media-player/constants/index.js
@@ -28,5 +28,8 @@ export const MEDIA_PLAYER_HEIGHT_MOBILE = 20 * ONE_REM;
 // Buffer space between top of active segment and Video/Menu
 export const SPACE_BEFORE_ACTIVE_ELEMENT = 8 * CSS_BASE_UNIT;
 
+export const DEFAULT_VIDEO_RESOLUTION = '480p';
+export const DEFAULT_AUDIO_RESOLUTION = '192k';
+
 // Re-export translations constants
 export * from './translations';
