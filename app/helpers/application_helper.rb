ff6b584d1f8b16e42fa2f2080faddc85d0d23937
grgr <chrgregor@gmail.com>
2025-11-18 11:31:57 +0100

    moving translation helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,8 +27,4 @@ module ApplicationHelper
   def is_ohd?
     current_project && current_project.shortname == 'ohd'
   end
-
-  def tv(key)
-    TranslationValue.for(key, I18n.locale)
-  end
 end
--- app/helpers/translation_values_helper.rb
+++ app/helpers/translation_values_helper.rb
@@ -1,2 +1,5 @@
 module TranslationValuesHelper
+  def tv(key)
+    TranslationValue.for(key, I18n.locale)
+  end
 end
