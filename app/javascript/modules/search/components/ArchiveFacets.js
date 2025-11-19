2ca577c70311c7be4801dcd8296cd9ad0cbc905a
Jan Kühn <yotka@posteo.org>
2025-11-19 10:07:58 +0000

    fix: Error: can't access property "find", i is undefined

--- app/javascript/modules/search/components/ArchiveFacets.js
+++ app/javascript/modules/search/components/ArchiveFacets.js
@@ -1,38 +1,35 @@
-import classNames from 'classnames';
-
 import { useAuthorization } from 'modules/auth';
-import { useI18n } from 'modules/i18n';
-import { Spinner, SearchSpinnerOverlay } from 'modules/spinners';
 import { useEventTypes } from 'modules/event-types';
-import FacetDropdown from './FacetDropdown';
-import Facet from './Facet';
-import BirthYearFacet from './BirthYearFacet';
-import RangeFacet from './RangeFacet';
+import { useI18n } from 'modules/i18n';
+import { SearchSpinnerOverlay, Spinner } from 'modules/spinners';
 import useFacets from '../useFacets';
+import BirthYearFacet from './BirthYearFacet';
+import Facet from './Facet';
+import FacetDropdown from './FacetDropdown';
+import RangeFacet from './RangeFacet';
 
 export default function ArchiveFacets() {
     const { locale } = useI18n();
     const { isAuthorized } = useAuthorization();
     const { facets, isLoading } = useFacets();
-    const { isLoading: eventTypesAreLoading, data: eventTypes } = useEventTypes();
+    const { isLoading: eventTypesAreLoading, data: eventTypes } =
+        useEventTypes();
 
     if (!facets || eventTypesAreLoading) {
         return <Spinner withPadding />;
     }
 
-    const adminFacets = [
-        'tasks_user_ids',
-        'tasks_supervisor_ids',
-    ];
+    const adminFacets = ['tasks_user_ids', 'tasks_supervisor_ids'];
 
     return (
         <SearchSpinnerOverlay loading={isLoading}>
-            {Object.keys(facets).map(facetName => {
+            {Object.keys(facets).map((facetName) => {
                 const facetData = facets[facetName];
 
                 if (facetName === 'year_of_birth') {
-                    const years = Object.keys(facetData.subfacets)
-                        .map(year => Number.parseInt(year));
+                    const years = Object.keys(facetData.subfacets).map((year) =>
+                        Number.parseInt(year)
+                    );
 
                     return (
                         <FacetDropdown
@@ -47,8 +44,9 @@ export default function ArchiveFacets() {
                         </FacetDropdown>
                     );
                 } else if (facetData.type === 'EventType') {
-                    const eventType = eventTypes.find(et =>
-                        et.code === facetData.name)
+                    const eventType = eventTypes?.find(
+                        (et) => et.code === facetData.name
+                    );
 
                     const values = Object.keys(facetData.subfacets);
                     if (!Array.isArray(values) || values.length === 0) {
@@ -78,7 +76,9 @@ export default function ArchiveFacets() {
                     );
                 } else {
                     const isAdminFacet = adminFacets.includes(facetName);
-                    const isVisible = !isAdminFacet || isAuthorized({ type: 'General' }, 'edit');
+                    const isVisible =
+                        !isAdminFacet ||
+                        isAuthorized({ type: 'General' }, 'edit');
 
                     if (!isVisible) {
                         return null;
@@ -91,10 +91,7 @@ export default function ArchiveFacets() {
                             label={facetData.name[locale]}
                             admin={isAdminFacet}
                         >
-                            <Facet
-                                facet={facetName}
-                                data={facetData}
-                            />
+                            <Facet facet={facetName} data={facetData} />
                         </FacetDropdown>
                     );
                 }
