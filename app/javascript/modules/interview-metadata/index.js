66b6d80ec7fe4b748b78e418115e458027af66d5
Marc Altmann <marcaltmann@posteo.de>
2025-11-19 17:47:08 +0100

    INTARCH-3244: Fix catalog links

--- app/javascript/modules/interview-metadata/index.js
+++ app/javascript/modules/interview-metadata/index.js
@@ -3,5 +3,4 @@ export { default as InterviewContributorsContainer } from './InterviewContributo
 export { default as InterviewTextMaterialsContainer } from './InterviewTextMaterialsContainer';
 export { default as ContributionFormContainer } from './ContributionFormContainer';
 export { default as CitationInfoContainer } from './CitationInfoContainer';
-export { default as CollectionLink } from './CollectionLink';
-export { default as FacetLink } from './FacetLink';
+export { default as CatalogLink } from './CatalogLink';
