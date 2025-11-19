1e7a39edd563e35a7419c54eb4aa4f17dca3f3cf
Jan Kühn <yotka@posteo.org>
2025-11-19 07:58:53 +0000

    feat: replace spinner image with ColorRing component for improved loading UX

--- app/javascript/modules/spinners/components/Spinner.js
+++ app/javascript/modules/spinners/components/Spinner.js
@@ -1,7 +1,6 @@
+import { useTheme } from 'modules/layout/ThemeProvider';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-
-import spinnerSrc from './large_spinner.gif';
+import { ColorRing } from 'react-loader-spinner';
 
 export default function Spinner({
     small = false,
@@ -9,16 +8,30 @@ export default function Spinner({
     className,
     style,
 }) {
+    const { secondaryColor } = useTheme();
     return (
-        <img
-            src={spinnerSrc}
-            className={classNames('Spinner', className, {
-                'Spinner--small': small,
-                'Spinner--withPadding': withPadding,
-            })}
-            style={style}
-            alt=""
-        />
+        <>
+            <ColorRing
+                visible={true}
+                height={small ? 32 : 96}
+                width={small ? 32 : 96}
+                ariaLabel="color-ring-loading"
+                wrapperStyle={style}
+                wrapperClass={`color-ring-wrapper ${className} ${
+                    withPadding ? 'color-ring-wrapper--withPadding' : ''
+                } ${small ? 'color-ring-wrapper--small' : ''}`}
+                // Some versions of the loader ignore `color` and render the default
+                // 5-color gradient. Provide a 5-item `colors` array where every
+                // entry is the same color to guarantee a solid appearance.
+                colors={[
+                    secondaryColor,
+                    secondaryColor,
+                    secondaryColor,
+                    secondaryColor,
+                    secondaryColor,
+                ]}
+            />
+        </>
     );
 }
 
