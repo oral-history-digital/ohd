4e22262aad16d48aa3240ecb2e71c8dab5c36bfd
grgr <chrgregor@gmail.com>
2025-12-15 12:12:05 +0100

    rm turbo

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -1,9 +1,7 @@
 import { Application } from '@hotwired/stimulus';
-import '@hotwired/turbo-rails';
 import BackupCodeController from 'controllers/backup_code_controller';
 import OtpController from 'controllers/otp_controller';
 
 const application = Application.start();
 application.register('otp', OtpController);
 application.register('backup-code', BackupCodeController);
--1;
