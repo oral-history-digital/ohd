a6c62aaa4fd3410207df9c0030107cc65dfd8848
grgr <chrgregor@gmail.com>
2025-12-17 14:43:37 +0100

    cleanup

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -1,7 +1,5 @@
 import { Application } from '@hotwired/stimulus';
-import BackupCodeController from 'controllers/backup_code_controller';
 import OtpController from 'controllers/otp_controller';
 
 const application = Application.start();
 application.register('otp', OtpController);
-application.register('backup-code', BackupCodeController);
