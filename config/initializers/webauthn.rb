d0e4f20a84a32803ccd1ebd60ed384d495fea8c8
grgr <chrgregor@gmail.com>
2026-01-07 13:51:15 +0100

    cleanup

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,14 +1,4 @@
 WebAuthn.configure do |config|
-  #config.origin = OHD_DOMAIN
-  #config.origin = ->(request) do
-    #if Rails.env.development?
-      #"#{request.scheme}://#{request.host}:#{request.port}"
-    #else
-      #"https://#{request.host}"
-    #end
-  #end
-
-  config.origin = "http://localhost:3000"
-
+  config.origin = OHD_DOMAIN
   config.rp_name = "OHD"
 end
