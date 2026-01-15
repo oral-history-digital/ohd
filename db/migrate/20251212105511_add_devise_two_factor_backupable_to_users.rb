d40f0d794f06762e6fcd71403c7ea5dcd9cc283f
grgr <chrgregor@gmail.com>
2026-01-15 16:50:44 +0100

    do not apply two_factor_backupable migration

--- db/migrate/20251212105511_add_devise_two_factor_backupable_to_users.rb
+++ db/migrate/20251212105511_add_devise_two_factor_backupable_to_users.rb
@@ -1,5 +1,5 @@
 class AddDeviseTwoFactorBackupableToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :otp_backup_codes, :text
+    #add_column :users, :otp_backup_codes, :text
   end
 end
