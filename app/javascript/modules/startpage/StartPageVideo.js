e4af9e73510b4129c72525576d8ed5621d8ce016
Jan Kühn <yotka@posteo.org>
2026-01-21 10:28:07 +0100

    fix: correct display of StartPageVideo (#64)
    PR #43 changes to interview multimedia player affected the display of the videoplayer in StartPageVideo. This separates both and fixes the style issues.

--- app/javascript/modules/startpage/StartPageVideo.js
+++ app/javascript/modules/startpage/StartPageVideo.js
@@ -5,12 +5,15 @@ export default function StartPageVideo() {
     const locale = useSelector(getLocale);
 
     return (
-        <div className="MediaElement MediaElement--poster">
+        <div className="StartPageVideo MediaElement--poster">
             <video
                 poster="https://medien.cedis.fu-berlin.de/eog/interviews/mog/home/still-home-video.jpg"
                 controls
                 src={`https://medien.cedis.fu-berlin.de/eog/interviews/mog/home/mog_home_movie_${locale}_1803.mp4`}
-            />
+            >
+                {/* Empty track element to prevent warnings. Add captions here if available */}
+                <track kind="captions" />{' '}
+            </video>
         </div>
     );
 }
