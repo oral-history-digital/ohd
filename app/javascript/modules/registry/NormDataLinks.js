14d1d7432d94d38a5f1f30421cbf797784777c4d
grgr <chrgregor@gmail.com>
2026-01-21 15:58:36 +0100

    safe normdata links wo provider

--- app/javascript/modules/registry/NormDataLinks.js
+++ app/javascript/modules/registry/NormDataLinks.js
@@ -4,18 +4,20 @@ import PropTypes from 'prop-types';
 export default function NormDataLinks({ registryEntry }) {
     const { t } = useI18n();
 
-    return registryEntry.norm_data.map((normDatum) => (
-        <a
-            key={normDatum.nid}
-            href={`${normDatum.norm_data_provider.url_without_id}${normDatum.nid}`}
-            target="_blank"
-            rel="noreferrer"
-            className="Link flyout-sub-tabs-content-ico-link"
-            title={t('norm_data.link_hover')}
-        >
-            &nbsp;{normDatum.norm_data_provider.name}&nbsp;
-        </a>
-    ));
+    return registryEntry.norm_data
+        .filter((n) => n.nid && n.norm_data_provider)
+        .map((normDatum) => (
+            <a
+                key={normDatum.nid}
+                href={`${normDatum.norm_data_provider.url_without_id}${normDatum.nid}`}
+                target="_blank"
+                rel="noreferrer"
+                className="Link flyout-sub-tabs-content-ico-link"
+                title={t('norm_data.link_hover')}
+            >
+                &nbsp;{normDatum.norm_data_provider.name}&nbsp;
+            </a>
+        ));
 }
 
 NormDataLinks.propTypes = {
