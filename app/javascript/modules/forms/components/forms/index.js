24d904425ae62fed0714600d7017f76c426a749f
Jan Kühn <yotka@posteo.org>
2026-01-29 14:33:16 +0000

    refactor: remove redux container pattern in Form module

--- app/javascript/modules/forms/components/forms/index.js
+++ app/javascript/modules/forms/components/forms/index.js
@@ -1,8 +1,5 @@
 export { default as DeleteItemForm } from './DeleteItemForm';
 export { default as SingleTextInputForm } from './SingleTextInputForm';
-export { default as SingleTextInputFormContainer } from './SingleTextInputFormContainer';
 export { default as SingleValueWithForm } from './SingleValueWithForm';
-export { default as SingleValueWithFormContainer } from './SingleValueWithFormContainer';
 export { default as StatusForm } from './StatusForm';
 export { default as SubmitOnBlurForm } from './SubmitOnBlurForm';
-export { default as SubmitOnBlurFormComponent } from './SubmitOnBlurFormComponent';
