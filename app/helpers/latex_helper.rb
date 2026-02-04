4ba5607faa5e93c9e3cf161972b0f2f9577eb1c2
grgr <chrgregor@gmail.com>
2026-02-04 16:13:56 +0100

    cleanup

--- app/helpers/latex_helper.rb
+++ app/helpers/latex_helper.rb
@@ -1,26 +1,4 @@
 module LatexHelper
-  def latex_multiscript(text)
-    text = latex_escape(CGI.unescapeHTML(text))
-    case text
-    when /\p{Hebrew}/ then "\\texthebrew{#{text}}"
-    when /\p{Arabic}/ then "\\textarabic{#{text}}"
-    when /\p{Tamil}/  then "\\texttamil{#{text}}"
-    else text
-    end
-    text.html_safe
-  end
-
-  def latex_multiscript_with_speaker(speaker, text, rtl)
-    if speaker.present?
-      if rtl
-        "#{latex_multiscript(text)} :\\textit{#{speaker}}"
-      else
-        "\\textit{#{speaker}}: #{latex_multiscript(text)}"
-      end
-    else
-      latex_multiscript(text)
-    end
-  end
 
   def latex_speaker(segment, speaker_id, rtl, header_locale)
     if speaker_id != segment.speaker_id && segment.speaking_person.present?
@@ -40,4 +18,5 @@ module LatexHelper
 
     return speaker_id, rtl_speaker, ltr_speaker
   end
+
 end
