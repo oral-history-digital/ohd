9d7bd33a85598c87de9e94d8e6cd76f4aaf2f843
Jan Kühn <yotka@posteo.org>
2026-02-11 09:37:43 +0000

    refactor: move EditProject forms from Redux container pattern to hooks

--- app/javascript/modules/admin/EditProjectInfo.js
+++ app/javascript/modules/admin/EditProjectInfo.js
@@ -1,9 +1,11 @@
 import AuthShowContainer from 'modules/auth/AuthShowContainer';
+import { submitData } from 'modules/data';
 import { useI18n } from 'modules/i18n';
 import { useProject } from 'modules/routes';
 import { Helmet } from 'react-helmet';
+import { useDispatch } from 'react-redux';
 
-import EditProjectInfoAttributesContainer from './EditProjectInfoAttributesContainer';
+import EditData from './EditData';
 import EditViewOrRedirect from './EditViewOrRedirect';
 import ExternalLinksContainer from './ExternalLinksContainer';
 import InstitutionProjectsContainer from './InstitutionProjectsContainer';
@@ -11,6 +13,56 @@ import InstitutionProjectsContainer from './InstitutionProjectsContainer';
 export default function EditProjectInfo() {
     const { t } = useI18n();
     const { project } = useProject();
+    const dispatch = useDispatch();
+
+    function submitHandler(props, params, opts, callback) {
+        dispatch(submitData(props, params, opts, callback));
+    }
+
+    const formElements = [
+        {
+            attribute: 'name',
+            multiLocale: true,
+        },
+        {
+            attribute: 'introduction',
+            elementType: 'richTextEditor',
+            multiLocale: true,
+        },
+        {
+            attribute: 'more_text',
+            elementType: 'richTextEditor',
+            multiLocale: true,
+        },
+        {
+            attribute: 'landing_page_text',
+            elementType: 'richTextEditor',
+            multiLocale: true,
+            help: 'activerecord.attributes.project.landing_page_edit_help',
+        },
+        {
+            attribute: 'restricted_landing_page_text',
+            elementType: 'richTextEditor',
+            multiLocale: true,
+            help: 'activerecord.attributes.project.restricted_landing_page_edit_help',
+        },
+        {
+            attribute: 'cooperation_partner',
+        },
+        {
+            attribute: 'leader',
+        },
+        {
+            attribute: 'manager',
+        },
+        {
+            attribute: 'pseudo_funder_names',
+        },
+        {
+            attribute: 'media_missing_text',
+            multiLocale: true,
+        },
+    ];
 
     return (
         <EditViewOrRedirect>
@@ -22,7 +74,13 @@ export default function EditProjectInfo() {
                     <h1 className="registry-entries-title">
                         {t(`edit.project.info`)}
                     </h1>
-                    <EditProjectInfoAttributesContainer data={project} />
+                    <EditData
+                        data={project}
+                        scope="project"
+                        helpTextCode="archive_info_form"
+                        formElements={formElements}
+                        submitData={submitHandler}
+                    />
                     <h2 className="registry-entries-title">
                         {t(`edit.external_link.admin`)}
                     </h2>
