0135ec71164824fd708a536bbb5258a67a70821e
grgr <chrgregor@gmail.com>
2026-02-18 15:48:55 +0100

    comment MFA on registration

--- test/system/webauthn_test.rb
+++ test/system/webauthn_test.rb
@@ -5,24 +5,24 @@ class WebauthnTest < WebauthnSystemTestCase
   EMAIL = 'john@example.com'
   PASSWORD = 'Password123!'
 
-  test "register as a new user with Webauthn MFA" do
-    add_virtual_authenticator
-    email = 'hubert@example.com'
-    fill_registration_form(
-      first_name: 'Hubert',
-      last_name: 'Rossi',
-      email: email,
-      passkey_required: true
-    )
-    confirm_registration_email
+  #test "register as a new user with Webauthn MFA" do
+    #add_virtual_authenticator
+    #email = 'hubert@example.com'
+    #fill_registration_form(
+      #first_name: 'Hubert',
+      #last_name: 'Rossi',
+      #email: email,
+      #passkey_required: true
+    #)
+    #confirm_registration_email
 
-    user = User.find_by(email: email)
-    assert_not_nil user
-    assert user.passkey_required_for_login
-    assert user.changed_to_passkey_at + 10.minutes > Time.current
-    register_passkey
-    assert user.webauthn_credentials.count == 1
-  end
+    #user = User.find_by(email: email)
+    #assert_not_nil user
+    #assert user.passkey_required_for_login
+    #assert user.changed_to_passkey_at + 10.minutes > Time.current
+    #register_passkey
+    #assert user.webauthn_credentials.count == 1
+  #end
 
   test "passkey registration and login for a registered user" do
     add_virtual_authenticator
