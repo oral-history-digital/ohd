5ace320f6d11f6734256652061ea6d11cf342276
grgr <chrgregor@gmail.com>
2026-02-26 10:11:11 +0100

    verify devise mappings set in two_factor email

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -64,6 +64,7 @@ class CustomDeviseMailer < Devise::Mailer
   end
 
   def two_factor_authentication_code(user, code)
+    Devise.mappings[:user]
     @code = code
     @valid_for = User::EMAIL_OTP_VALID_FOR / 60
     devise_mail(user, :two_factor_authentication_code)
