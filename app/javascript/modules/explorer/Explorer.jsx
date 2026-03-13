eb6bb34c729bb6b35efe916602474d26f0c38740
Jan Kühn <yotka@posteo.org>
2026-03-13 11:04:24 +0000

    feat: add title to Explorer component with corresponding styles

--- app/javascript/modules/explorer/Explorer.jsx
+++ app/javascript/modules/explorer/Explorer.jsx
@@ -2,6 +2,7 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@reach/tabs';
 import '@reach/tabs/styles.css';
 import { useI18n } from 'modules/i18n';
 import { useShowSidebarOnMount } from 'modules/sidebar';
+import { Helmet } from 'react-helmet';
 import { FaArchive, FaUniversity } from 'react-icons/fa';
 import { useSearchParams } from 'react-router-dom';
 
@@ -44,57 +45,65 @@ export function Explorer() {
         );
 
     return (
-        <div className="Explorer">
-            <Tabs
-                className="Explorer-tabs"
-                index={tabIndex}
-                onChange={handleTabChange}
-                keyboardActivation="manual"
-            >
-                <TabList className="Explorer-tabList">
-                    <Tab className="Explorer-tab">
-                        <FaArchive className="Explorer-tabIcon" />
-                        <span className="Explorer-tabText">
-                            {t('explorer.tab.archives_and_collections')}
-                        </span>
-                    </Tab>
-                    <Tab className="Explorer-tab">
-                        <FaUniversity className="Explorer-tabIcon" />
-                        <span className="Explorer-tabText">
-                            {t('explorer.tab.institutions')}
-                        </span>
-                    </Tab>
-                </TabList>
+        <>
+            <Helmet>
+                <title>{t('modules.catalog.title')}</title>
+            </Helmet>
+            <div className="Explorer">
+                <h1 className="Explorer--title">
+                    {t('modules.catalog.title')}
+                </h1>
+                <Tabs
+                    className="Explorer-tabs"
+                    index={tabIndex}
+                    onChange={handleTabChange}
+                    keyboardActivation="manual"
+                >
+                    <TabList className="Explorer-tabList">
+                        <Tab className="Explorer-tab">
+                            <FaArchive className="Explorer-tabIcon" />
+                            <span className="Explorer-tabText">
+                                {t('explorer.tab.archives_and_collections')}
+                            </span>
+                        </Tab>
+                        <Tab className="Explorer-tab">
+                            <FaUniversity className="Explorer-tabIcon" />
+                            <span className="Explorer-tabText">
+                                {t('explorer.tab.institutions')}
+                            </span>
+                        </Tab>
+                    </TabList>
 
-                <TabPanels className="Explorer-tabPanels">
-                    <TabPanel className="Explorer-tabPanel">
-                        {tabIndex === 0 && (
-                            <ArchivesList
-                                query={query}
-                                interviewMin={interviewMin}
-                                interviewMax={interviewMax}
-                                collectionMin={collectionMin}
-                                collectionMax={collectionMax}
-                                yearMin={yearMin}
-                                yearMax={yearMax}
-                                institutionIds={institutionIds}
-                            />
-                        )}
-                    </TabPanel>
-                    <TabPanel className="Explorer-tabPanel">
-                        {tabIndex === 1 && (
-                            <InstitutionsList
-                                query={query}
-                                interviewMin={interviewMin}
-                                interviewMax={interviewMax}
-                                instArchiveMin={instArchiveMin}
-                                instArchiveMax={instArchiveMax}
-                            />
-                        )}
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-        </div>
+                    <TabPanels className="Explorer-tabPanels">
+                        <TabPanel className="Explorer-tabPanel">
+                            {tabIndex === 0 && (
+                                <ArchivesList
+                                    query={query}
+                                    interviewMin={interviewMin}
+                                    interviewMax={interviewMax}
+                                    collectionMin={collectionMin}
+                                    collectionMax={collectionMax}
+                                    yearMin={yearMin}
+                                    yearMax={yearMax}
+                                    institutionIds={institutionIds}
+                                />
+                            )}
+                        </TabPanel>
+                        <TabPanel className="Explorer-tabPanel">
+                            {tabIndex === 1 && (
+                                <InstitutionsList
+                                    query={query}
+                                    interviewMin={interviewMin}
+                                    interviewMax={interviewMax}
+                                    instArchiveMin={instArchiveMin}
+                                    instArchiveMax={instArchiveMax}
+                                />
+                            )}
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </div>
+        </>
     );
 }
 
