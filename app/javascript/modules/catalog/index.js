543f77e3ea8a68798fd4a668247f674faa455744
Jan Kühn <yotka@posteo.org>
2026-05-06 06:38:28 +0000

    chore: remove legacy catalog
    Catalog was replaced by the explorer module

--- app/javascript/modules/catalog/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MainCatalogPage } from './MainCatalogPage';
-export { default as InstitutionCatalogPage } from './InstitutionCatalogPage';
-export { default as ArchiveCatalogPage } from './ArchiveCatalogPage';
-export { default as CollectionCatalogPage } from './CollectionCatalogPage';
--- app/javascript/modules/catalog/rowComparator.js
+++
@@ -1,3 +0,0 @@
-export default function rowComparator(a, b) {
-    return a.name.localeCompare(b.name);
-}
