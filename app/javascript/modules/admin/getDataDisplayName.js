fd3140c734d73b770c3bb10de224b0b35291e6f0
grgr <chrgregor@gmail.com>
2026-06-01 12:15:36 +0200

    displayname for personal data types fixed

--- app/javascript/modules/admin/getDataDisplayName.js
+++ app/javascript/modules/admin/getDataDisplayName.js
@@ -3,6 +3,17 @@ export default function getDataDisplayName(data, locale) {
         return data.title;
     }
 
+    if (data.name_type === 'Personal' && data.first_name && data.last_name) {
+        const firstName = data.first_name.hasOwnProperty(locale)
+            ? data.first_name[locale]
+            : data.first_name;
+        const lastName = data.last_name.hasOwnProperty(locale)
+            ? data.last_name[locale]
+            : data.last_name;
+
+        return `${firstName} ${lastName}`;
+    }
+
     if (data.name) {
         return data.name.hasOwnProperty(locale) ? data.name[locale] : data.name;
     }
