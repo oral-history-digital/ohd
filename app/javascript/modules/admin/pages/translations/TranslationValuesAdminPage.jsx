da3770d6844ff7decd14b33d105d9a3f17460afe
Jan Kühn <yotka@posteo.org>
2026-09-03 14:29:11 +0000

    refactor: organize translation values admin page

--- app/javascript/modules/admin/pages/translations/TranslationValuesAdminPage.jsx
+++ app/javascript/modules/admin/pages/translations/TranslationValuesAdminPage.jsx
@@ -10,7 +10,7 @@ import {
 import { getTranslationValuesQuery, setQueryParams } from 'modules/search';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { PaginatedAdminRecordList } from './components';
+import { PaginatedAdminRecordList } from '../../components';
 
 export default function TranslationValuesAdminPage() {
     const dispatch = useDispatch();
