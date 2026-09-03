b81b5df118d926681d63ceef5a5e39296ee16648
Jan Kühn <yotka@posteo.org>
2026-09-03 11:23:56 +0000

    refactor: migrate project configuration search forms to Redux hooks

---
+++ app/javascript/modules/admin/RoleSearchForm.jsx
@@ -0,0 +1,19 @@
+import { getRolesQuery } from 'modules/search';
+
+import DataSearchForm from './DataSearchForm';
+import { useAdminSearch } from './hooks';
+
+export default function RoleSearchForm() {
+    const searchProps = useAdminSearch(getRolesQuery);
+
+    return (
+        <DataSearchForm
+            {...searchProps}
+            scope="role"
+            searchableAttributes={[
+                { attributeName: 'name' },
+                { attributeName: 'desc' },
+            ]}
+        />
+    );
+}
