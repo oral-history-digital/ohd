88279e70beea95878d444f3ef53c480f665d31b9
Jan Kühn <yotka@posteo.org>
2026-09-04 07:52:47 +0000

    refactor: organize project admin relation components

--- app/javascript/modules/admin/pages/projects/CooperationPartners.jsx
+++ app/javascript/modules/admin/pages/projects/CooperationPartners.jsx
@@ -1,23 +1,27 @@
 import { getCurrentProject } from 'modules/data';
 import { useSelector } from 'react-redux';
 
-import AffiliateForm from './AffiliateForm';
-import AffiliateShow from './AffiliateShow';
-import DataList from './DataList';
-import { useAdminDataActions } from './hooks';
+import AffiliateForm from '../../AffiliateForm';
+import AffiliateShow from '../../AffiliateShow';
+import DataList from '../../DataList';
+import { useAdminDataActions } from '../../hooks';
 
-export default function Managers() {
+export default function CooperationPartners() {
     const project = useSelector(getCurrentProject);
     const { fetchData, deleteData, submitData } = useAdminDataActions();
+
     return (
         <DataList
             editView
-            data={project.managers}
+            data={project.cooperation_partners}
             outerScope="project"
             outerScopeId={project.id}
-            scope="manager"
+            scope="cooperation_partner"
             detailsAttributes={['name', 'first_name', 'last_name']}
-            initialFormValues={{ project_id: project.id, type: 'Manager' }}
+            initialFormValues={{
+                project_id: project.id,
+                type: 'CooperationPartner',
+            }}
             form={AffiliateForm}
             showComponent={AffiliateShow}
             fetchData={fetchData}
